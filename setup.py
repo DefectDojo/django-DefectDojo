b1a33cbc30dd76b9836df6f56a5be4e38b66356e
jay7958 <jay.paz@rackspace.com>
2015-03-25 21:25:08 -0500

    added django-tastypie-swagger to setup.py the right way, with dependency link.

--- setup.py
+++ setup.py
@@ -7,18 +7,21 @@ setup(
     version='1.0',
     author='Greg Anderson',
     description="Tool for managing vulnerability engagements",
+    dependency_links=[
+        "https://github.com/concentricsky/django-tastypie-swagger/tarball/v0.1.3#egg=django-tastypie-swagger"
+    ],
     install_requires=[
         'Django>=1.7',
         'MySQL-python==1.2.5',
         'Pillow==2.3.0',
         'django-secure==1.0',
         'django-tastypie==0.12.1',
+        'django-tastypie-swagger',
         'gunicorn==19.1.1',
         'python-nmap==0.3.4',
         'pytz==2013.9',
         'requests==2.2.1',
         'wsgiref==0.1.2',
-        '-e git+git@github.com:concentricsky/django-tastypie-swagger.git@v0.1.3#egg=django-tastypie-swagger',
         'django-filter',
         'supervisor',
         'humanize',
