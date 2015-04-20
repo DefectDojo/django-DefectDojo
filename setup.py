72064755ce2314205af101dd50adda290902f6d2
jay7958 <jay.paz@rackspace.com>
2015-04-20 13:11:07 -0500

    pdf report optimizations and limits

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     install_requires=[
         'Django==1.7.7',
         'MySQL-python==1.2.3',
-        'Pillow==2.3.0',
+        'Pillow>=2.3.0',
         'django-secure==1.0',
         'django-tastypie==0.12.1',
         'django-tastypie-swagger',
@@ -28,7 +28,7 @@ setup(
         'django-bower',
         'django-easy-pdf',
         'xhtml2pdf>=0.0.6',
-        'reportlab>=2.7,<3'],
+        'reportlab'],
     # packages=['DefectDojo'],
     url='https://github.com/rackerlabs/django-DefectDojo'
 
