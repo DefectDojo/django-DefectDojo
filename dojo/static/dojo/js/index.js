d21db120e2707b98ac4378e34de2a62029f7bd2f
jay7958 <jay.paz@rackspace.com>
2015-06-12 23:22:37 -0500

    Add endpoint fixes: port number was truncated, multiple endpoints were not correctly added to related form.

--- dojo/static/dojo/js/index.js
+++ dojo/static/dojo/js/index.js
@@ -38,6 +38,35 @@ $(function () {
 
 });
 
+function dismissAddAnotherPopupDojo(win, newId, newRepr) {
+    // newId and newRepr are expected to have previously been escaped by
+    // django.utils.html.escape.
+    newId = html_unescape(newId);
+    newRepr = html_unescape(newRepr);
+    var name = windowname_to_id(win.name);
+    var elem = document.getElementById(name);
+    var o;
+    if (elem) {
+        var elemName = elem.nodeName.toUpperCase();
+        if (elemName == 'SELECT') {
+            o = new Option(newRepr, newId);
+            elem.options[elem.options.length] = o;
+            o.selected = true;
+        } else if (elemName == 'INPUT') {
+            if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
+                elem.value += ',' + newId;
+            } else {
+                elem.value = newId;
+            }
+        }
+    } else {
+        var toId = name + "_to";
+        o = new Option(newRepr, newId);
+        SelectBox.add_to_cache(toId, o);
+        SelectBox.redisplay(toId);
+    }
+}
+
 function punchcard(element, data, ticks) {
     var d1 = data;
     var options = {
