64b02084353cde2f99f0a5467e65423fa335d8ad
jay7958 <jay.paz@rackspace.com>
2015-07-27 15:57:11 -0500

    updated version number to 1.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='DefectDojo',
-    version='1.0.2',
+    version='1.0.3',
     author='Greg Anderson',
     description="Tool for managing vulnerability engagements",
     install_requires=[
