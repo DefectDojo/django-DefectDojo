9ce6dd41c6e9fb1f31c9104b12dc141aaa921d0e
jay7958 <jay.paz@rackspace.com>
2015-11-04 14:31:32 -0600

    forcing Pillow version to 2.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         'Django==1.8.4',
         'MySQL-python',
-        'Pillow>=2.3.0',
+        'Pillow==2.3.0',
         'django-secure>1.0',
         'django-tastypie>=0.12.2',
         'django-tastypie-swagger',
