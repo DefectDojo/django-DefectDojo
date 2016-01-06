572ff05eef51a3e89973fc99b3fcb387822f7384
Aaron Weaver <aaron.weaver2@gmail.com>
2016-01-06 09:06:52 -0500

    Spelling error fix for engagement

--- dojo/api.py
+++ dojo/api.py
@@ -436,7 +436,7 @@ class FindingResource(BaseModelResource):
     def dehydrate(self, bundle):
         engagement = Engagement.objects.select_related('product'). \
             filter(test__finding__id=bundle.obj.id)
-        bundle.data['engagemet'] = "/api/v1/engagements/%s/" % engagement[0].id
+        bundle.data['engagement'] = "/api/v1/engagements/%s/" % engagement[0].id
         bundle.data['product'] = \
             "/api/v1/products/%s/" % engagement[0].product.id
         return bundle
@@ -470,7 +470,7 @@ class StubFindingResource(BaseModelResource):
     def dehydrate(self, bundle):
         engagement = Engagement.objects.select_related('product'). \
             filter(test__stub_finding__id=bundle.obj.id)
-        bundle.data['engagemet'] = "/api/v1/engagements/%s/" % engagement[0].id
+        bundle.data['engagement'] = "/api/v1/engagements/%s/" % engagement[0].id
         bundle.data['product'] = \
             "/api/v1/products/%s/" % engagement[0].product.id
         return bundle
