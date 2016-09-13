d0986e89302cddae792682d048623bee82f1f6c0
devGregA <danderso815@gmail.com>
2016-09-13 14:01:08 -0500

    removed deleted packages

--- tests/check_status_ui.py
+++ tests/check_status_ui.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from seleniumrequests import Firefox
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
