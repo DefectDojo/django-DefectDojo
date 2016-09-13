8b12907c5623871fa85ad7c84d316151e52ab437
devGregA <danderso815@gmail.com>
2016-09-13 13:27:43 -0500

    expanded ui status checks

--- tests/check_status_ui.py
+++ tests/check_status_ui.py
@@ -39,27 +39,15 @@ class Login(unittest.TestCase):
         url = self.base_url+ "product"
         r = driver.request('GET', url)
         self.assertEqual(r.status_code, 200)
-    """
+
     def test_finding_status(self):
-        api_key = self.get_api_key()
-        api_url = self.base_url+ "api/v1/findings"
-        user = os.environ['DOJO_ADMIN_USER']
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'ApiKey %s:%s' % (user, api_key)}
-        r = requests.get(api_url, headers=headers, verify=False)
-        self.assertEqual(r.status_code, 200)
-
-    def test_t_status(self):
-        api_key = self.get_api_key()
-        api_url = self.base_url+ "api/v1/tests"
-        user = os.environ['DOJO_ADMIN_USER']
-        headers = {'content-type': 'application/json',
-                   'Authorization': 'ApiKey %s:%s' % (user, api_key)}
-        r = requests.get(api_url, headers=headers, verify=False)
+        driver = self.login_page()
+        url = self.base_url+ "finding/open"
+        r = driver.request('GET', url)
         self.assertEqual(r.status_code, 200)
 
 
-    """
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
