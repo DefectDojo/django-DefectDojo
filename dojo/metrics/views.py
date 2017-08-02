1cc257987d48733951645926de61b7ea04028743
Jay Paz <jay.paz@gmail.com>
2017-08-02 10:18:13 -0500

    fixes #331and other metrics quirtks and errors

--- dojo/metrics/views.py
+++ dojo/metrics/views.py
@@ -306,8 +306,7 @@ def metrics(request, mtype):
         'name': page_name,
         'start_date': start_date,
         'end_date': end_date,
-        'findings': findings.qs,
-        'find_object': findings,
+        'findings': findings,
         'opened_per_month': monthly_counts['opened_per_period'],
         'active_per_month': monthly_counts['active_per_period'],
         'opened_per_week': weekly_counts['opened_per_period'],
@@ -366,6 +365,7 @@ def simple_metrics(request):
                                        false_p=False,
                                        duplicate=False,
                                        out_of_scope=False,
+                                       date=now,
                                        ).distinct()
 
         for f in total.all():
