742fbe3d1bdd98c77d43c90a1252e43b5c4fe215
devGregA <greg.anderson@owasp.org>
2017-12-23 16:46:28 -0600

    Improves static scanner finding model, dedupe, for: retire.js, OWASP DC, checkmarx TBD: veracode VCG

--- dojo/utils.py
+++ dojo/utils.py
@@ -49,9 +49,12 @@ def sync_false_history(new_finding, *args, **kwargs):
 
 def sync_dedupe(new_finding, *args, **kwargs):
     eng_findings_cwe = Finding.objects.filter(test__engagement__product=new_finding.test.engagement.product,
-                                              cwe=new_finding.cwe).exclude(id=new_finding.id).exclude(cwe=None)
+                                              cwe=new_finding.cwe, static_finding=new_finding.static_finding,
+                                              dynamic_finding=new_finding.dynamic_finding).exclude(id=new_finding.id).exclude(cwe=None)
     eng_findings_title = Finding.objects.filter(test__engagement__product=new_finding.test.engagement.product,
-                                                title=new_finding.title).exclude(id=new_finding.id)
+                                                title=new_finding.title,
+                                                static_finding=new_finding.static_finding,
+                                                dynamic_finding=new_finding.dynamic_finding).exclude(id=new_finding.id)
     total_findings = eng_findings_cwe | eng_findings_title
     total_findings = total_findings.order_by('date')
     for find in total_findings:
@@ -64,6 +67,11 @@ def sync_dedupe(new_finding, *args, **kwargs):
                 find.duplicate_list.add(new_finding)
                 super(Finding, new_finding).save(*args, **kwargs)
                 break
+        elif find.line == new_finding.line and find.file_path  == new_finding.file_path and new_finding.static_finding and len(new_finding.file_path) > 0:
+            new_finding.duplicate = True
+            new_finding.duplicate_finding = find
+            find.duplicate_list.add(new_finding)
+            super(Finding, new_finding).save(*args, **kwargs)
         elif find.get_hash_code() == new_finding.get_hash_code():
                 new_finding.duplicate = True
                 new_finding.duplicate_finding = find
