6111b4d6fd17cbf28f36f38e15460cccef216287
Jeckelmann Manuel <Manuel.Jeckelmann@swisscom.com>
2018-02-27 10:15:25 +0100

    Added unittests to travis declaration

---
+++ scripts/travis-unit-test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Run available unittests with a simple setup
+
+pip install virtualenv
+virtualenv ~/dojo-venv
+
+cp ./dojo/settings/settings.dist.py ./dojo/settings/settings.py
+
+source ~/dojo-venv/bin/activate
+
+pip install -r requirements.txt
+
+DJANGO_SETTINGS_MODULE=dojo.settings.unittest python manage.py makemigrations
+DJANGO_SETTINGS_MODULE=dojo.settings.unittest python manage.py migrate
+DJANGO_SETTINGS_MODULE=dojo.settings.unittest python manage.py test dojo.unittests
+
+deactivate
+
+rm ./dojo/settings/settings.py
