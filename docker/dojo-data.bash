0fafb757699a6849badbaf6e5311c7ed2d1af422
Jeckelmann Manuel <Manuel.Jeckelmann@swisscom.com>
2018-03-04 17:48:57 +0100

    Applied common ENV and shared-functions to docker startup and data loading scripts

--- docker/dojo-data.bash
+++ docker/dojo-data.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
 #Exports and loads sample data for dojo
 
+# Set the paths
+source ./entrypoint_scripts/common/dojo-build-env.sh
+
 if [ $# > 1 ]
 then
     if [[ "$1" = "load" ]]
     then
-      python /django-DefectDojo/manage.py loaddata /django-DefectDojo/docker/sample_data/initial_dojo_data.json
+      python $DOJO_ROOT_DIR/manage.py loaddata $DOJO_DOCKER_DIR/sample_data/initial_dojo_data.json
       echo "Data imported from: sample_data/initial_dojo_data.json"
     elif [[ "$1" = "export" ]]; then
-      python /django-DefectDojo/manage.py dumpdata --exclude auth.user > /django-DefectDojo/docker/sample_data/initial_dojo_data.json
+      python $DOJO_ROOT_DIR/manage.py dumpdata --exclude auth.user > $DOJO_DOCKER_DIR/sample_data/initial_dojo_data.json
       echo "Data exported to: sample_data/initial_dojo_data.json"
     fi
 else
