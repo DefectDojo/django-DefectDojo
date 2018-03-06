9f2e2722390f2cf13b5462623dc16ee0dc04b543
Jeckelmann Manuel <Manuel.Jeckelmann@swisscom.com>
2018-03-06 14:00:37 +0100

    Silence debugging curl

--- scripts/travis-integration-test.sh
+++ scripts/travis-integration-test.sh
@@ -26,7 +26,7 @@ sudo ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
 docker ps -a
 docker logs $CONTAINER_NAME
 echo "Checking to see if dojo is running"
-curl http://127.0.0.1:8000/login?next=/ || docker exec $CONTAINER_NAME bash -c "cat /opt/django-DefectDojo/dojo.log" && exit 1
+curl -s http://127.0.0.1:8000/login?next=/ || docker exec $CONTAINER_NAME bash -c "cat /opt/django-DefectDojo/dojo.log" && exit 1
 
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
