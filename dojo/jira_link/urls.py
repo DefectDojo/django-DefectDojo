c1dd6714725fe712647d11b040d3a52dfc01b983
devGregA <greg.anderson@owasp.org>
2018-03-11 14:21:55 -0500

    Adds delete for JIRA Configurations

--- dojo/jira_link/urls.py
+++ dojo/jira_link/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     url(r'^jira/add', views.new_jira, name='add_jira'),
     url(r'^jira/(?P<jid>\d+)/edit$', views.edit_jira,
         name='edit_jira'),
+    url(r'^jira/(?P<tid>\d+)/delete$', views.delete_jira,
+        name='delete_jira'),
     url(r'^jira$', views.jira, name='jira'), ]
