c8f7ab19962a6077f9640e15c23c991528876042
Aaron Weaver <aweaver@MacBook-Pro.localdomain>
2018-03-12 14:37:00 -0400

    Bumping version and adding install notes.

--- dojo/__init__.py
+++ dojo/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app  # noqa
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 __url__ = 'https://github.com/OWASP/django-DefectDojo'
 __docs__ = 'http://defectdojo.readthedocs.io/'
 __demo__ = 'http://defectdojo.pythonanywhere.com/'
