cace9cc1c5dc3d42c7589a0429e3bacf6af16532
yaro24 <robin.hoerner@gmail.com>
2018-03-23 08:31:59 +0100

    fix user so celery will start up properly

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,8 @@
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 service mysql start
-su - dojo 
+
 cd /opt/django-DefectDojo
-celery -A dojo worker -l info --concurrency 3 >> worker.log 2>&1 &
-celery beat -A dojo -l info  >> beat.log 2>&1 &
-python manage.py runserver 0.0.0.0:8000 >> dojo.log 2>&1	
+su - dojo -c "cd /opt/django-DefectDojo && celery -A dojo worker -l info --concurrency 3 >> /opt/django-DefectDojo/worker.log 2>&1 &" \
+&& su - dojo -c "cd /opt/django-DefectDojo && celery beat -A dojo -l info  >> /opt/django-DefectDojo/beat.log 2>&1 &" \
+&& su - dojo -c "cd /opt/django-DefectDojo && python manage.py runserver 0.0.0.0:8000 >> /opt/django-DefectDojo/dojo.log 2>&1"
