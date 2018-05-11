1163e06e35044f3161bd181b289a489ddf37f018
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2018-05-11 09:39:55 -0400

    PEP8

--- dojo/tools/contrast/parser.py
+++ dojo/tools/contrast/parser.py
@@ -3,11 +3,7 @@ __author__ = 'aaronweaver'
 import pandas as pd
 import hashlib
 from dojo.models import Finding, Endpoint
-from dateutil.parser import parse
-import re
-from urlparse import urlparse
-import sys
-import math
+
 
 class ContrastCSVParser(object):
 
@@ -22,7 +18,7 @@ class ContrastCSVParser(object):
         df = pd.read_csv(filename, header=0)
 
         for i, row in df.iterrows():
-            #Vulnerability Name,Vulnerability ID,Category,Rule Name,Severity,Status,Number of Events,First Seen,Last Seen,Application Name,Application ID,Application Code,CWE ID,Request Method,Request Port,Request Protocol,Request Version,Request URI,Request Qs,Request Body
+            # Vulnerability Name,Vulnerability ID,Category,Rule Name,Severity,Status,Number of Events,First Seen,Last Seen,Application Name,Application ID,Application Code,CWE ID,Request Method,Request Port,Request Protocol,Request Version,Request URI,Request Qs,Request Body
             cwe = self.format_cwe(df.ix[i, 'CWE ID'])
             title = df.ix[i, 'Rule Name']
             category = df.ix[i, 'Category']
@@ -34,32 +30,34 @@ class ContrastCSVParser(object):
             impact = "N/A"
             references = "N/A"
 
-            dupe_key = category + str(cwe) + title
+            dupe_key = hashlib.md5(category + '|' + str(cwe) + '|' + title + '|').hexdigest()
 
             if dupe_key in dupes:
                 finding = dupes[dupe_key]
                 if finding.description:
-                    finding.description = finding.description + "\nVulnerability ID: " + df.ix[i, 'Vulnerability ID'] + "\n" + df.ix[i, 'Vulnerability Name'] + "\n"
+                    finding.description = finding.description + "\nVulnerability ID: " + \
+                        df.ix[i, 'Vulnerability ID'] + "\n" + \
+                        df.ix[i, 'Vulnerability Name'] + "\n"
                 self.process_endpoints(finding, df, i)
                 dupes[dupe_key] = finding
             else:
                 dupes[dupe_key] = True
 
                 finding = Finding(title=title,
-                               cwe=int(cwe),
-                               test=test,
-                               active=False,
-                               verified=False,
-                               description=description,
-                               severity=severity,
-                               numerical_severity=Finding.get_numerical_severity(severity),
-                               mitigation=mitigation,
-                               impact=impact,
-                               references=references,
-                               url='N/A',
-                               dynamic_finding=True)
+                                  cwe=int(cwe),
+                                  test=test,
+                                  active=False,
+                                  verified=False,
+                                  description=description,
+                                  severity=severity,
+                                  numerical_severity=Finding.get_numerical_severity(
+                                      severity),
+                                  mitigation=mitigation,
+                                  impact=impact,
+                                  references=references,
+                                  url='N/A',
+                                  dynamic_finding=True)
 
-                key = hashlib.md5(finding.url + '|' + finding.severity + '|' + finding.title + '|' + finding.description).hexdigest()
                 dupes[dupe_key] = finding
                 self.process_endpoints(finding, df, i)
 
@@ -68,19 +66,22 @@ class ContrastCSVParser(object):
     def format_description(self, df, i):
         description = "Request URI: " + str(df.ix[i, 'Request URI']) + "\n"
         description = "Rule Name: " + df.ix[i, 'Rule Name'] + "\n"
-        description = "Vulnerability ID: " + df.ix[i, 'Vulnerability ID'] + "\n"
-        description = description + df.ix[i, 'Vulnerability Name']  + "\n\n"
-        if pd.isnull(df.ix[i, 'Request Qs']) == False:
-            description = description + "Request QueryString: " + str(df.ix[i, 'Request Qs']) + "\n"
+        description = "Vulnerability ID: " + \
+            df.ix[i, 'Vulnerability ID'] + "\n"
+        description = description + df.ix[i, 'Vulnerability Name'] + "\n\n"
+        if pd.isnull(df.ix[i, 'Request Qs']) is False:
+            description = description + "Request QueryString: " + \
+                str(df.ix[i, 'Request Qs']) + "\n"
         if pd.isnull(df.ix[i, 'Request Body']):
-            description = description + "Request Body: " + str(df.ix[i, 'Request Body'])  + "\n"
+            description = description + "Request Body: " + \
+                str(df.ix[i, 'Request Body']) + "\n"
         return description
 
     def format_cwe(self, url):
-        #Get the last path
+        # Get the last path
         filename = url.rsplit('/', 1)[1]
 
-        #Split out the . to get the CWE id
+        # Split out the . to get the CWE id
         filename = filename.split('.')[0]
 
         return filename
@@ -92,7 +93,7 @@ class ContrastCSVParser(object):
         fragment = ""
         path = df.ix[i, 'Request URI']
 
-        if pd.isnull(path) == False:
+        if pd.isnull(path) is False:
             try:
                 dupe_endpoint = Endpoint.objects.get(protocol="protocol",
                                                      host=host,
@@ -100,7 +101,7 @@ class ContrastCSVParser(object):
                                                      fragment=fragment,
                                                      path=path,
                                                      product=finding.test.engagement.product)
-            except:
+            except Endpoint.DoesNotExist:
                 dupe_endpoint = None
 
             if not dupe_endpoint:
