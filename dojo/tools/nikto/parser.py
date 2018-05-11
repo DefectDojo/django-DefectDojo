c8d7808efc08881dea59e775cff8defd7e36d417
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2018-05-11 09:39:55 -0400

    PEP8

--- dojo/tools/nikto/parser.py
+++ dojo/tools/nikto/parser.py
@@ -2,11 +2,11 @@ __author__ = 'aaronweaver'
 
 import re
 from defusedxml import ElementTree as ET
-import html2text
 import hashlib
 from urlparse import urlparse
 
 from dojo.models import Finding, Endpoint
+
 
 class NiktoXMLParser(object):
 
@@ -23,32 +23,34 @@ class NiktoXMLParser(object):
         scan = root.find('scandetails')
 
         for item in scan.findall('item'):
-            #Title
+            # Title
             titleText = None
             description = item.find("description").text
-            #Cut the title down to the first sentence
-            sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', description)
+            # Cut the title down to the first sentence
+            sentences = re.split(
+                r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', description)
             if len(sentences) > 0:
                 titleText = sentences[0][:900]
             else:
                 titleText = description[:900]
 
-            #Url
+            # Url
             ip = item.find("iplink").text
-            #Remove the port numbers for 80/443
-            ip = ip.replace(":80","")
-            ip = ip.replace(":443","")
+            # Remove the port numbers for 80/443
+            ip = ip.replace(":80", "")
+            ip = ip.replace(":443", "")
 
-            #Severity
-            severity = "Info" #Nikto doesn't assign severity, default to Info
+            # Severity
+            severity = "Info"  # Nikto doesn't assign severity, default to Info
 
-            #Description
-            description = "\nHost: " + ip + "\n" + item.find("description").text
+            # Description
+            description = "\nHost: " + ip + "\n" + \
+                item.find("description").text
             mitigation = "N/A"
             impact = "N/A"
             references = "N/A"
 
-            dupe_key = description
+            dupe_key = hashlib.md5(description).hexdigest()
 
             if dupe_key in dupes:
                 finding = dupes[dupe_key]
@@ -60,19 +62,19 @@ class NiktoXMLParser(object):
                 dupes[dupe_key] = True
 
                 finding = Finding(title=titleText,
-                               test=test,
-                               active=False,
-                               verified=False,
-                               description=description,
-                               severity=severity,
-                               numerical_severity=Finding.get_numerical_severity(severity),
-                               mitigation=mitigation,
-                               impact=impact,
-                               references=references,
-                               url='N/A',
-                               dynamic_finding=True)
+                                  test=test,
+                                  active=False,
+                                  verified=False,
+                                  description=description,
+                                  severity=severity,
+                                  numerical_severity=Finding.get_numerical_severity(
+                                      severity),
+                                  mitigation=mitigation,
+                                  impact=impact,
+                                  references=references,
+                                  url='N/A',
+                                  dynamic_finding=True)
 
-                key = hashlib.md5(finding.description).hexdigest()
                 dupes[dupe_key] = finding
                 self.process_endpoints(finding, ip)
 
@@ -89,8 +91,8 @@ class NiktoXMLParser(object):
             path = url.path
 
         rhost = re.search(
-                "(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))[\:]*([0-9]+)*([/]*($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+)).*?$",
-                host)
+            "(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))[\:]*([0-9]+)*([/]*($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+)).*?$",
+            host)
         protocol = rhost.group(1)
         host = rhost.group(4)
 
@@ -101,7 +103,7 @@ class NiktoXMLParser(object):
                                                  fragment=fragment,
                                                  path=path,
                                                  product=finding.test.engagement.product)
-        except:
+        except Endpoint.DoesNotExist:
             dupe_endpoint = None
 
         if not dupe_endpoint:
--- dojo/tools/trufflehog/parser.py
+++ dojo/tools/trufflehog/parser.py
@@ -1,8 +1,7 @@
 import json
-import io
 import hashlib
-from dojo.models import Finding, Endpoint
-from django.utils.encoding import smart_text, force_str
+from dojo.models import Finding
+
 
 class TruffleHogJSONParser(object):
     def __init__(self, filename, test):
@@ -36,9 +35,9 @@ class TruffleHogJSONParser(object):
 
             strings_found = ""
             for string in json_data["stringsFound"]:
-                    strings_found += string + "\n"
+                strings_found += string + "\n"
 
-            dupe_key = file + reason
+            dupe_key = hashlib.md5(file + reason).hexdigest()
             description += "\n**Strings Found:**\n" + strings_found + "\n"
 
             if dupe_key in self.dupes:
@@ -49,22 +48,21 @@ class TruffleHogJSONParser(object):
                 self.dupes[dupe_key] = True
 
                 finding = Finding(title=titleText,
-                               test=test,
-                               cwe=798,
-                               active=False,
-                               verified=False,
-                               description=description,
-                               severity=severity,
-                               numerical_severity=Finding.get_numerical_severity(severity),
-                               mitigation="Secrets and passwords should be stored in a secure vault and/or secure storage.",
-                               impact="This weakness can lead to the exposure of resources or functionality to unintended actors, possibly providing attackers with sensitive information or even execute arbitrary code.",
-                               references="N/A",
-                               file_path=file,
-                               url='N/A',
-                               dynamic_finding=False,
-                               static_finding=True)
+                                  test=test,
+                                  cwe=798,
+                                  active=False,
+                                  verified=False,
+                                  description=description,
+                                  severity=severity,
+                                  numerical_severity=Finding.get_numerical_severity(severity),
+                                  mitigation="Secrets and passwords should be stored in a secure vault and/or secure storage.",
+                                  impact="This weakness can lead to the exposure of resources or functionality to unintended actors, possibly providing attackers with sensitive information or even execute arbitrary code.",
+                                  references="N/A",
+                                  file_path=file,
+                                  url='N/A',
+                                  dynamic_finding=False,
+                                  static_finding=True)
 
-                key = hashlib.md5(file).hexdigest()
                 self.dupes[dupe_key] = finding
 
         self.items = self.dupes.values()
@@ -72,7 +70,7 @@ class TruffleHogJSONParser(object):
     def parse_json(self, json_output):
         try:
             json_data = json.loads(json_output)
-        except:
+        except ValueError:
             raise Exception("Invalid format")
 
         return json_data
