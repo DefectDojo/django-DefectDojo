a9cd26b8a1aeb8e00feafc8707a3e42c0cb9892c
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2018-05-21 21:45:10 -0400

    Fixtures for setup.bash

--- entrypoint_scripts/common/dojo-shared-resources.sh
+++ entrypoint_scripts/common/dojo-shared-resources.sh
@@ -489,6 +489,12 @@ function install_app(){
     python manage.py loaddata test_type
     python manage.py loaddata development_environment
     python manage.py loaddata system_settings
+    python manage.py loaddata benchmark_category
+    python manage.py loaddata benchmark_requirement
+    python manage.py loaddata benchmark_type
+    python manage.py loaddata language_type
+    python manage.py loaddata objects_review
+
     python manage.py installwatson
     python manage.py buildwatson
 
