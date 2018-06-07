0623f4aadfb139aad441b1570feb7db018f0f8d6
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2018-06-07 19:45:18 -0400

    updated urls

--- dojo/urls.py
+++ dojo/urls.py
@@ -158,7 +158,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
