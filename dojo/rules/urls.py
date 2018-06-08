30acc12726231fcb51858d1e5db8e0216fdcb28f
devGregA <greg.anderson@owasp.org>
2018-06-08 11:28:48 -1000

    Added rules urls and html

--- dojo/rules/urls.py
+++ dojo/rules/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import url
 import views
 
 urlpatterns = [
-    url(r'^webhook', views.webhook, name='web_hook'),
-    url(r'^jira/add', views.new_jira, name='add_jira'),
-    url(r'^jira/(?P<jid>\d+)/edit$', views.edit_jira,
-        name='edit_jira'),
-    url(r'^jira/(?P<tid>\d+)/delete$', views.delete_jira,
-        name='delete_jira'),
-    url(r'^jira$', views.jira, name='jira'), ]
+    url(r'^rules', views.rules, name='Rules Framework'),
+    url(r'^rules/add', views.new_jira, name='Add Rule'),
+    url(r'^rule/(?P<pid>\d+)/edit$', views.edit_rule,
+        name='Edit Rule'),
+    url(r'^rule/(?P<tid>\d+)/delete$', views.delete_rule,
+        name='Delete Rule'), ]
