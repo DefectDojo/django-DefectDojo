caf086c9aa5609f5299f9b98d64b3ab725faa36f
Eric Kelson <ekelson@gmail.com>
2018-06-19 14:45:48 -0400

    fixed pep8 formatting, removed a few print statements

--- dojo/tools/trustwave_csv/parser.py
+++ dojo/tools/trustwave_csv/parser.py
@@ -1,5 +1,5 @@
-# Sorry for the lazyness but I just update the column name fields
-# Didn't change the class names, only the main one..
+# Based on CSV, but rewrote because
+# values in different columns required concatinaton
 
 import StringIO
 import csv
@@ -9,26 +9,27 @@ import re
 from urlparse import urlparse
 import socket
 
-MAPPINGS={"title":"Vulnerability Name",
-          'description':'Description',
-          'protocol':'Port',
-          'references':'Evidence',
-          'mitigation':'Remediation',
-          'cwe':'CVE',
-          'fqdn':'Domain',
-          'severity':'Severity',
-          'ip':'IP'
-          }
+MAPPINGS = {"title": "Vulnerability Name",
+            'description': 'Description',
+            'protocol': 'Port',
+            'references': 'Evidence',
+            'mitigation': 'Remediation',
+            'cwe': 'CVE',
+            'fqdn': 'Domain',
+            'severity': 'Severity',
+            'ip': 'IP'
+            }
+
 
 class Urlfilter():
 
     def __init__(self):
-        self.host=''
+        self.host = ''
         self.path = ''
         self.query = ''
         self.fragment = ''
-        self.url=''
-        self.validip=False
+        self.url = ''
+        self.validip = False
 
     def is_valid_ipv4_address(self, address):
         valid = True
@@ -51,7 +52,7 @@ class Urlfilter():
         ParseResult(scheme='http', netloc='www.cwi.nl:80', path='/%7Eguido/Python.html',
                     params='', query='', fragment='')
         """
-        if self.is_valid_ipv4_address(url) == False:
+        if self.is_valid_ipv4_address(url) is False:
             rhost = re.search(
                 "(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))[\:]*([0-9]+)*([/]*($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+)).*?$",
                 url)
@@ -62,24 +63,31 @@ class Urlfilter():
                 self.fragment = o.fragment
                 self.validip = False
 
-        #URL is an IP so save as an IP endpoint
-        elif self.is_valid_ipv4_address(url) == True:
-            self.host=url
-            self.path = None 
+        # URL is an IP so save as an IP endpoint
+        elif self.is_valid_ipv4_address(url) is True:
+            self.host = url
+            self.path = None
             self.query = None
             self.fragment = None
             self.validip = True
-            
+
+
 class Severityfilter():
     def __init__(self):
-        self.severity_mapping = {'I':'Info', 'L':'Low', 'M':'Medium', 'H':'High', 'C':'Critical'}
-        self.severity=None
-        
+        self.severity_mapping = {'I': 'Info',
+                                 'L': 'Low',
+                                 'M': 'Medium',
+                                 'H': 'High',
+                                 'C': 'Critical'
+                                 }
+        self.severity = None
+
     def eval_column(self, column_value):
         if column_value in self.severity_mapping.keys():
-            self.severity=self.severity_mapping[column_value]
+            self.severity = self.severity_mapping[column_value]
         else:
             self.severity = 'Info'
+
 
 class TrustwaveUploadCsvParser(object):
 
@@ -92,44 +100,39 @@ class TrustwaveUploadCsvParser(object):
             return
 
         content = filename.read()
-
         reader = csv.DictReader(StringIO.StringIO(content), delimiter=',', quotechar='"')
-        
-        csvarray=[]
+        csvarray = []
 
         for row in reader:
             csvarray.append(row)
-            
-        
-            
-        for row in csvarray: 
+
+        for row in csvarray:
             finding = Finding(test=test)
-            findingdict={}
-            endpointdict={}
-            referencesarray=[]
-            
-            for field,column_name in MAPPINGS.items():
+            findingdict = {}
+            endpointdict = {}
+            referencesarray = []
+
+            for field, column_name in MAPPINGS.items():
                 if column_name == 'IP':
-                    urlfilter=Urlfilter()
+                    urlfilter = Urlfilter()
                     urlfilter.eval_column(row[column_name])
-                    endpointdict['host']=urlfilter.host
-                    endpointdict['path']= urlfilter.path
-                    endpointdict['query']= urlfilter.query
-                    endpointdict['fragment']=urlfilter.fragment
-                    findingdict['url']=urlfilter.url
+                    endpointdict['host'] = urlfilter.host
+                    endpointdict['path'] = urlfilter.path
+                    endpointdict['query'] = urlfilter.query
+                    endpointdict['fragment'] = urlfilter.fragment
+                    findingdict['url'] = urlfilter.url
                 elif column_name == 'Severity':
                     severityfilter = Severityfilter()
                     severityfilter.eval_column(row[column_name])
-                    findingdict['severity']=severityfilter.severity
+                    findingdict['severity'] = severityfilter.severity
                 elif column_name == 'Port':
-                    endpointdict[field]=row[column_name]
-                elif column_name in ['Evidence','CVE']:
+                    endpointdict[field] = row[column_name]
+                elif column_name in ['Evidence', 'CVE']:
                     referencesarray.append(row[column_name])
                 else:
                     if column_name in row.keys():
-                        findingdict[field]=row[column_name]
-            
-            print row['IP']
+                        findingdict[field] = row[column_name]
+
             try:
                 dupe_endpoint = Endpoint.objects.get(protocol=endpointdict['protocol'],
                                                      host=endpointdict['host'],
@@ -156,7 +159,6 @@ class TrustwaveUploadCsvParser(object):
                 endpoints = [endpoint, dupe_endpoint]
 
             finding.unsaved_endpoints = endpoints
-        
             finding.title = findingdict['title']
             finding.description = findingdict['description']
             finding.references = "\n".join(referencesarray)
@@ -164,26 +166,19 @@ class TrustwaveUploadCsvParser(object):
             finding.fqdn = findingdict['fqdn']
             finding.severity = findingdict['severity']
             finding.url = findingdict['url']
-            
-            print finding       
-          
 
-            if finding is not None: 
+            if finding is not None:
                 if finding.url is None:
                     finding.url = ""
                 if finding.title is None:
                     finding.title = ""
                 if finding.description is None:
                     finding.description = ""
-    
+
                 key = hashlib.md5(finding.url + '|' + finding.severity + '|' + finding.title + '|' + finding.description).hexdigest()
-    
+
                 if key not in self.dupes:
                     self.dupes[key] = finding
 
-    
-
-        
-        print self.dupes.values()
         self.items = self.dupes.values()
-       
+
