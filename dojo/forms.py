772c76b45e082006299a3525b4e9d2fadae7c724
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2018-07-12 15:11:09 -0400

    UI Refreshment

--- dojo/forms.py
+++ dojo/forms.py
@@ -1364,7 +1364,7 @@ class ReportOptionsForm(forms.Form):
     include_finding_images = forms.ChoiceField(choices=yes_no, label="Finding Images")
     include_executive_summary = forms.ChoiceField(choices=yes_no, label="Executive Summary")
     include_table_of_contents = forms.ChoiceField(choices=yes_no, label="Table of Contents")
-    report_type = forms.ChoiceField(choices=(('AsciiDoc', 'AsciiDoc'), ('HTML', 'HTML'), ('PDF', 'PDF')))
+    report_type = forms.ChoiceField(choices=(('HTML', 'HTML'), ('AsciiDoc', 'AsciiDoc'), ('PDF', 'PDF')))
 
 
 class CustomReportOptionsForm(forms.Form):
