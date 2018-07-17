19636bd7738f52602af2c51869328f53c7db7593
Raghu <raghu@rockmail.co>
2018-07-17 21:53:11 +0800

    fixed typo

--- dojo/forms.py
+++ dojo/forms.py
@@ -246,7 +246,7 @@ class ImportScanForm(forms.Form):
                          ("Generic Findings Import", "Generic Findings Import"),
                          ("SKF Scan", "SKF Scan"),
                          ("Bandit Scan", "Bandit Scan"),
-                         ("SSL Labs Scan", "SSL Labs Scan"),q
+                         ("SSL Labs Scan", "SSL Labs Scan"),
                          ("GoAST Scanner", "GoAST Scanner"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
