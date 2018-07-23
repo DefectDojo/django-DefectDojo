3ccba1cfe0da847c63d30a7ff8a0631b6ddb2b14
Ryan Frizzell <ryan.frizzell@homeadvisor.com>
2018-07-22 19:22:15 -0600

    save

--- setup_docker_production.bash
+++ setup_docker_production.bash
@@ -1,16 +1,4 @@
 #!/bin/bash
-# setup.bash helps you installing DefectDojo in your current environment
-#
-# setup.bash covers the following cases (and types of environments):
-# - Fresh installs on non-transient environments (physical or virtual OS)
-# - Fresh installs on transient environment blueprints (containerized environments)
-# - Fresh installs on transient VMs (Vagrant and the like)
-# - Updates on non-transient environments
-#
-# Not (yet) addressed:
-# - Updates for transient environments
-#
-
 echo
 echo "Welcome to DefectDojo! This is a quick script to get you up and running."
 echo
@@ -19,21 +7,31 @@ echo
 source entrypoint_scripts/common/dojo-shared-resources.sh
 source .env_dojo
 verify_cwd
+echo "Flushdb is ${FLUSHDB}"
+if [[ "${FLUSHDB}" =~ ^[yY]$ ]]; then
+  # Create the application DB or recreate it, if it's already present
+  # we're opionated here and we only mysql
+  ensure_mysql_application_db
+  
+  # Adjust the settings.py file
+  # we skip this in favor of envfile format
+  # prepare_settings_file
+  pushd /django-DefectDojo/dojo/settings
+  python template.py
+  popd
+  
+  # Ensure, we're running on a supported python version
+  verify_python_version
+  
+  # Install the actual application
+  install_app
+fi
 
-# Create the application DB or recreate it, if it's already present
-# we're opionated here and we only mysql
-ensure_mysql_application_db
-ensure_mysql_application_db
-
-# Adjust the settings.py file
-# we skip this in favor of envfile format
-# prepare_settings_file
-pushd /django-DefectDojo/dojo/settings
-python template.py
-popd
-
-# Ensure, we're running on a supported python version
-verify_python_version
-
-# Install the actual application
-install_app
+# most times we just start
+export C_FORCE_ROOT=True
+source .env_dojo
+echo "doing stuff" 
+wait-for-it/wait-for-it.sh -h mysqldb -p 3306 -t 30
+celery -A dojo worker -l info --concurrency 3 &
+celery beat -A dojo -l info &
+uwsgi --socket :8000 --wsgi-file wsgi.py --workers 7
--- start_docker_production.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash 
-celery -A dojo worker -l info --concurrency 3
-celery beat -A dojo -l info
-uwsgi --socket :8001 --wsgi-file wsgi.py --workers 7
