fbb0dbae615651935c749dda592fd0843f5108b2
greg <greg@terminal.local>
2018-09-20 21:23:08 -0400

    bug fixes APIv2

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -144,9 +144,9 @@ MIDDLEWARE_CLASSES = (
     'dojo.middleware.TimezoneMiddleware'
 )
 
-#AUTHENTICATION_BACKENDS = [
-#'axes.backends.AxesModelBackend',
-#]
+##AUTHENTICATION_BACKENDS = [
+##'axes.backends.AxesModelBackend',
+##]
 
 ROOT_URLCONF = 'dojo.urls'
 LOGIN_URL = '/login'
@@ -208,7 +208,7 @@ INSTALLED_APPS = (
     'rest_framework.authtoken',
     'rest_framework_swagger',
     'dbbackup',
-#    'axes'
+    # 'axes'
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
