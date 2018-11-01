DefectDojo/django-DefectDojo

commit dd7987ae2fad782538919742b5ada23e64fa92aa

Prowler and Scout2, MobSF for iOS

dojo/models.py
@@ -1198,6 +1198,7 @@ class Finding(models.Model):
         if self.pk is None:
             from dojo.utils import apply_cwe_to_template
             self = apply_cwe_to_template(self)
+        else:
             # Only compute hash code for new findings.
             self.hash_code = self.compute_hash_code()
         super(Finding, self).save(*args, **kwargs)

dojo/engagement/views.py
@@ -513,10 +513,7 @@ def import_scan_results(request, eid=None, pid=None):
                     new_f.cred_id = cred_user.cred_id
                     new_f.save()
 
-            try:
-                parser = import_parser_factory(file, t)
-            except ValueError:
-                raise Http404()
+            parser = import_parser_factory(file, t)
 
             try:
                 for item in parser.items:

dojo/forms.py
@@ -254,7 +254,9 @@ class ImportScanForm(forms.Form):
                          ("MobSF Scan", "MobSF Scan"),
                          ("Trufflehog Scan", "Trufflehog Scan"),
                          ("Nikto Scan", "Nikto Scan"),
-                         ("Clair Scan", "Clair Scan"))
+                         ("Clair Scan", "Clair Scan"),
+                         ("AWS Scout2 Scan", "AWS Scout2 Scan"),
+                         ("AWS Prowler Scan", "AWS Prowler Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
 
@@ -276,7 +278,7 @@ class ImportScanForm(forms.Form):
                            help_text="Add tags that help describe this scan.  "
                                      "Choose from the list or add new tags.  Press TAB key to add.")
     file = forms.FileField(widget=forms.widgets.FileInput(
-        attrs={"accept": ".xml, .csv, .nessus, .json, .html"}),
+        attrs={"accept": ".xml, .csv, .nessus, .json, .html, .js"}),
         label="Choose report file",
         required=True)
 

dojo/templates/dojo/dashboard-metrics.html
@@ -170,9 +170,9 @@
     <script src="{% static "flot/jquery.flot.resize.js" %}"></script>
     <script src="{% static "flot-axis/jquery.flot.axislabels.js" %}"></script>
     {% if punchcard %}
-        <script src="{% static "JUMFlot/JUMFlot.min.js" %}"></script>
-        <script src="{% static "JUMFlot/jquery.flot.mouse.js" %}"></script>
-        <script src="{% static "JUMFlot/jquery.flot.bubbles.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/JUMFlot.min.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/jquery.flot.mouse.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/jquery.flot.bubbles.js" %}"></script>
     {% endif %}
     <script>
         var highest_count = {{ highest_count }};
@@ -422,7 +422,7 @@
                 var ticks = []
                 {% for t in top_ten_products %}
                     ttp = {{forloop.counter}};
-                    ticks.push([ttp, "{{t.name|safe}}"]);
+                    ticks.push([ttp, "{{t.name}}"]);
                     critical.push([ttp, {{t.critical|default_if_none:0}}]);
                     high.push([ttp, {{t.high|default_if_none:0}}]);
                     medium.push([ttp, {{t.medium|default_if_none:0}}]);

dojo/templates/dojo/dashboard.html
@@ -181,9 +181,9 @@
     <script src="{% static "flot/jquery.flot.stack.js" %}"></script>
     <script src="{% static "flot/jquery.flot.resize.js" %}"></script>
     {% if punchcard %}
-        <script src="{% static "JUMFlot/JUMFlot.min.js" %}"></script>
-        <script src="{% static "JUMFlot/jquery.flot.mouse.js" %}"></script>
-        <script src="{% static "JUMFlot/jquery.flot.bubbles.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/JUMFlot.min.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/jquery.flot.mouse.js" %}"></script>
+        <script src="{% static "JUMFlot/javascripts/jquery.flot.bubbles.js" %}"></script>
     {% endif %}
     <script>
         var highest_count = {{ highest_count }};

dojo/templates/dojo/import_scan_results.html
@@ -26,9 +26,11 @@
 
 	<p>DefectDojo accepts:</p>
     <ul>
-	<li><b>Arachni Scanner</b> - Arachni JSON report format.</li>
+  <li><b>AWS Scout2 Scanner</b> - JS file in scout2-report/inc-awsconfig/aws_config.js.</li>
+  <li><b>AWS Prowler Scanner</b> - Prowler file can be imported as a CSV file (-M csv).</li>
+  <li><b>Arachni Scanner</b> - Arachni JSON report format.</li>
 	<li><b>AppSpider (Rapid7)</b> - Use the VulnerabilitiesSummary.xml file found in the zipped report download.</li>
-	<li><b>Bandit</b> - JSON report format</li>
+  <li><b>Bandit</b> - JSON report format</li>
 	<li><b>Burp XML</b> - When the Burp report is generated, the recommended option is Base64 encoding both the request and
 	    response fields. These fields will be processed and made available in the 'Finding View' page.</li>
 	<li><b>Clair Scan</b> - Import JSON reports of Docker image vulnerabilities.</li>
@@ -38,7 +40,7 @@
 	<li><b>Generic Findings Import</b> - Import Generic findings in CSV format.</li>
 	<li><b>Gosec Scanner </b> - Import Gosec Scanner findings in JSON format.</li>
   <li><b>MobSF Scanner </b> - Export a JSON file using the API, api/v1/report_json.</li>
-	<li><b>Nessus (Tenable)</b> - Reports can be imported in the CSV, and .nessus (XML) report formats.</li>
+	<li><b>Nessus (Tenable)</b> - Reports can be imported as CSV or .nessus (XML) report formats.</li>
 	<li><b>Nexpose XML 2.0 (Rapid7)</b> - Use the full XML export template from Nexpose.</li>
 	<li><b>Nikto</b> - XML output</li>
 	<li><b>Nmap</b> - XML output (use -oX)</li>

dojo/templates/dojo/view_finding.html
@@ -386,7 +386,7 @@
                         class="glyphicon glyphicon-chevron-up"></i></a></span></h4>
             </div>
             <div id="vuln_desc" class="panel-body finding-description collapse in">
-                <pre id="description_text">{{ finding.description|markdown_render }}</pre>
+              {{ finding.description|markdown_render }}
             </div>
         </div>
 

dojo/templates/dojo/view_test.html
@@ -125,7 +125,7 @@
                       class="glyphicon glyphicon-chevron-up"></i></a></span></h4>
           </div>
           <div id="vuln_desc" class="panel-body finding-description collapse in">
-              <pre id="description_text">{{ test.description|markdown_render }}</pre>
+              {{ test.description|markdown_render }}
           </div>
       </div>
       </div>

commit 4142aa0227cf576468d1272ad61fe846f6202370

Fix hash code computation for Findings
dd7987ae introduced a regression where we stopped computing hash codes
for new Findings.

dojo/models.py
@@ -1201,7 +1201,6 @@ class Finding(models.Model):
         if self.pk is None:
             from dojo.utils import apply_cwe_to_template
             self = apply_cwe_to_template(self)
-        else:
             # Only compute hash code for new findings.
             self.hash_code = self.compute_hash_code()
         super(Finding, self).save(*args, **kwargs)