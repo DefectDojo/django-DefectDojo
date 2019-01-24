50272a2a661838bc1814e0c4af4243d471654d1e
JamesCullum <JamesCullum@users.noreply.github.com>
2019-01-24 15:46:57 +0100

    Fix for broken encryption due to change of database key name

--- dojo/tool_config/views.py
+++ dojo/tool_config/views.py
@@ -53,8 +53,11 @@ def edit_tool_config(request, ttid):
     tool_config = Tool_Configuration.objects.get(pk=ttid)
     if request.method == 'POST':
         tform = ToolConfigForm(request.POST, instance=tool_config)
+        
         if tform.is_valid():
+            print("PW", tform.cleaned_data['password'])
             form_copy = tform.save(commit=False)
+            print("PW2", tform.cleaned_data['password'])
             form_copy.password = dojo_crypto_encrypt(tform.cleaned_data['password'])
             print "######"
             print tform.cleaned_data['ssh']
