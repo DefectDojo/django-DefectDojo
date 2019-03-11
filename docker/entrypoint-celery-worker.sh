7fff0a58a348dc35336823117eb722ad099f9aad
Hendrik M Halkow <hendrik@halkow.com>
2019-03-11 12:25:11 +0100

    Celery beat schedule path fixed.

--- docker/entrypoint-celery-worker.sh
+++ docker/entrypoint-celery-worker.sh
@@ -2,6 +2,14 @@
 
 umask 0002
 
+echo -n "Waiting for database to be reachable "
+until echo "select 1;" | python manage.py dbshell > /dev/null
+do
+  echo -n "."
+  sleep 1
+done
+echo
+
 C_FORCE_ROOT=true exec celery \
   --app=dojo \
   worker \
