10dedca5ee0af1890c143a4c049fc3f78eed7121
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2019-03-14 13:55:49 -0400

    build check

--- travis/script.sh
+++ travis/script.sh
@@ -108,7 +108,7 @@ echo "Running test=$TEST"
           echo "Running Flake8 tests on dev branch aka pull requests"
           # We need to checkout dev for flake8-diff to work properly
           git checkout dev
-          pip install pep8 flake8 flake8-diff
+          sudo pip install pep8 flake8 flake8-diff
           flake8-diff
       else
           echo "true"
