ee8bdcc29361770ac95d328cf53ac8152d914f03
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2019-03-19 12:17:24 -0400

    travis

--- travis/after-success-script.sh
+++ travis/after-success-script.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 TRAVIS_TAG="dev"
+ORG="defectdojo"
 
 if { [ "${TRAVIS_TAG}" != "" ] || [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; } && [ "${DOCKER_USER}" != "" ] && [  "${DOCKER_PASS}" != "" ]; then
   DOCKER_IMAGES=(django nginx)
   for docker_image in "${DOCKER_IMAGES[@]}"
   do
+      # Cron pushed into branch-weekly-dev-image, triggered weekly
       if [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
         TRAVIS_TAG=`date +%Y-%m-%d`
+        REPO="${ORG}/weekly-${TRAVIS_BRANCH}-${ORG}-${docker_image}"
+      elif [ ${TRAVIS_BRANCH} == "master" ]; then
+        REPO="${ORG}/${ORG}-${docker_image}"
+      else
+        REPO="${ORG}/${TRAVIS_BRANCH}-${ORG}-${docker_image}"
       fi
-      REPO="defectdojo/defectdojo-${docker_image}"
-      docker tag ${REPO} "defectdojo/${TRAVIS_BRANCH}-defectdojo-${docker_image}":$TRAVIS_TAG
+
+      CONTAINER="${ORG}/${ORG}-${docker_image}"
+
+      docker tag $CONTAINER $REPO:$TRAVIS_TAG
       docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
-      docker push "defectdojo/${TRAVIS_BRANCH}-defectdojo-${docker_image}"
+      docker push $REPO
   done
 fi
