4b404212bd0c590c3140d9f21ec138247c4435d1
Aaron Weaver <aaronweaver@users.noreply.github.com>
2019-03-20 13:54:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     install_requires=[
         'celery==4.1.1',
         'defusedxml',
-        'Django==1.11.19',
+        'Django==1.11.20',
         'django-auditlog',
         'django-custom-field',
         'django-filter==1.0.4',
