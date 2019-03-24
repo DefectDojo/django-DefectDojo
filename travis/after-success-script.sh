2386f35d798338b644bb3b731f90d5a67ad92985
aaron.weaver2@gmail.com <aaronweaver@users.noreply.github.com>
2019-03-24 16:52:17 -0400

    deployment

--- travis/after-success-script.sh
+++ travis/after-success-script.sh
@@ -5,8 +5,11 @@ ORG="defectdojo"
 deploy_demo() {
   if [ "$HEROKU_API_KEY" != "" ]; then
     # Deploy
+    docker tag "defectdojo/defectdojo-django" "registry.heroku.com/defectdojo-dev/web"
+    docker login -u "$HEROKU_EMAIL" -p "$HEROKU_TOKEN" registry.heroku.com
     docker push registry.heroku.com/defectdojo-dev/web
     heroku container:release web -a defectdojo-dev
+    heroku run bash /opt/heroku-DefectDojo/scripts/migrate.bash -a defectdojo-dev
   fi
 }
 
