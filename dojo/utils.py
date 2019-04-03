DefectDojo/django-DefectDojo

commit e4cbdfab1a3d034777a38396d663efb2cee1c964

linting

dojo/utils.py
@@ -72,36 +72,36 @@ def is_deduplication_on_engamgent(new_finding, to_duplicate_finding):
 
 
 def sync_dedupe(new_finding, *args, **kwargs):
-        if new_finding.test.engagement.deduplication_on_engagement:
-            eng_findings_cwe = Finding.objects.filter(
-                test__engagement=new_finding.test.engagement,
-                cwe=new_finding.cwe,
-                static_finding=new_finding.static_finding,
-                dynamic_finding=new_finding.dynamic_finding,
-                date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
-                cwe=0).exclude(duplicate=True)
-            eng_findings_title = Finding.objects.filter(
-                test__engagement=new_finding.test.engagement,
-                title=new_finding.title,
-                static_finding=new_finding.static_finding,
-                dynamic_finding=new_finding.dynamic_finding,
-                date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
-                duplicate=True)
-        else:
-            eng_findings_cwe = Finding.objects.filter(
-                test__engagement__product=new_finding.test.engagement.product,
-                cwe=new_finding.cwe,
-                static_finding=new_finding.static_finding,
-                dynamic_finding=new_finding.dynamic_finding,
-                date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
-                cwe=0).exclude(duplicate=True)
-            eng_findings_title = Finding.objects.filter(
-                test__engagement__product=new_finding.test.engagement.product,
-                title=new_finding.title,
-                static_finding=new_finding.static_finding,
-                dynamic_finding=new_finding.dynamic_finding,
-                date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
-                duplicate=True)
+    if new_finding.test.engagement.deduplication_on_engagement:
+        eng_findings_cwe = Finding.objects.filter(
+            test__engagement=new_finding.test.engagement,
+            cwe=new_finding.cwe,
+            static_finding=new_finding.static_finding,
+            dynamic_finding=new_finding.dynamic_finding,
+            date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
+            cwe=0).exclude(duplicate=True)
+        eng_findings_title = Finding.objects.filter(
+            test__engagement=new_finding.test.engagement,
+            title=new_finding.title,
+            static_finding=new_finding.static_finding,
+            dynamic_finding=new_finding.dynamic_finding,
+            date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
+            duplicate=True)
+    else:
+        eng_findings_cwe = Finding.objects.filter(
+            test__engagement__product=new_finding.test.engagement.product,
+            cwe=new_finding.cwe,
+            static_finding=new_finding.static_finding,
+            dynamic_finding=new_finding.dynamic_finding,
+            date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
+            cwe=0).exclude(duplicate=True)
+        eng_findings_title = Finding.objects.filter(
+            test__engagement__product=new_finding.test.engagement.product,
+            title=new_finding.title,
+            static_finding=new_finding.static_finding,
+            dynamic_finding=new_finding.dynamic_finding,
+            date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
+            duplicate=True)
 
         total_findings = eng_findings_cwe | eng_findings_title
         # total_findings = total_findings.order_by('date')

dojo/settings/settings.dist.py
@@ -65,6 +65,7 @@ env = environ.Env(
     DD_CREDENTIAL_AES_256_KEY=(str, '.'),
 )
 
+
 def generate_url(scheme, double_slashes, user, password, host, port, path):
     result_list = []
     result_list.append(scheme)
@@ -85,6 +86,7 @@ def generate_url(scheme, double_slashes, user, password, host, port, path):
         result_list.append('/')
     result_list.append(path)
     return ''.join(result_list)
+
 
 # Read .env file as default or from the command line, DD_ENV_PATH
 if os.path.isfile(root('dojo/settings/.env.prod')) or 'DD_ENV_PATH' in os.environ:

dojo/tools/burp/parser.py
@@ -229,7 +229,7 @@ def get_item(item_node, test):
         else:
             endpoints = [endpoint, dupe_endpoint]
 
-    if len(endpoints) is 0:
+    if len(endpoints) == 0:
         endpoints = [endpoint]
 
     text_maker = html2text.HTML2Text()

dojo/tools/dependencycheck/parser.py
@@ -31,10 +31,9 @@ class DependencyCheckParser(object):
             m = re.match(r"^(CWE-)?(\d+)", cwe_field)
             if m:
                 cwe = int(m.group(2))
-                
         cvssv2_node = vulnerability.find(self.namespace + 'cvssV2')
         if cvssv2_node is not None:
-            severity = self.get_field_value(cvssv2_node,'severity').lower().capitalize()
+            severity = self.get_field_value(cvssv2_node, 'severity').lower().capitalize()
         else:
             severity = self.get_field_value(vulnerability, 'severity').lower().capitalize()
 

dojo/tools/nexpose/parser.py
@@ -116,7 +116,7 @@ class NexposeFullXmlParser(object):
             last = ""
 
             for attr in node.attrib:
-                if last is not "":
+                if last != "":
                     if node.get(attr) != node.get(last):
                         ret += str(node.get(attr)) + " "
                 last = attr

dojo/tools/nmap/parser.py
@@ -9,7 +9,6 @@ class NmapXMLParser(object):
     def __init__(self, file, test):
         self.dupes = dict()
         self.items = ()
-        
         if file is None:
             return
 

commit 19d972f0f9dd7ca779b99a5cc1ed3ecf2f250585

fix deduplication_on_engagement regression caused by optimistic linting
Regression was caused by https://github.com/DefectDojo/django-DefectDojo/commit/e4cbdfab1a3d034777a38396d663efb2cee1c964#diff-63c97e945dc0af3507d89798a8841819

dojo/utils.py
@@ -29,6 +29,9 @@ from dojo.models import Finding, Engagement, Finding_Template, Product, JIRA_PKe
 from asteval import Interpreter
 from requests.auth import HTTPBasicAuth
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 """
 Helper functions for DefectDojo
@@ -67,11 +70,12 @@ def sync_false_history(new_finding, *args, **kwargs):
         super(Finding, new_finding).save(*args, **kwargs)
 
 
-def is_deduplication_on_engamgent(new_finding, to_duplicate_finding):
+def is_deduplication_on_engagement_mismatch(new_finding, to_duplicate_finding):
     return not new_finding.test.engagement.deduplication_on_engagement and to_duplicate_finding.test.engagement.deduplication_on_engagement
 
 
 def sync_dedupe(new_finding, *args, **kwargs):
+    logger.debug('sync_dedupe for: ' + str(new_finding.id) + ":" + str(new_finding.title))
     if new_finding.test.engagement.deduplication_on_engagement:
         eng_findings_cwe = Finding.objects.filter(
             test__engagement=new_finding.test.engagement,
@@ -103,26 +107,17 @@ def sync_dedupe(new_finding, *args, **kwargs):
             date__lte=new_finding.date).exclude(id=new_finding.id).exclude(
             duplicate=True)
 
-        total_findings = eng_findings_cwe | eng_findings_title
-        # total_findings = total_findings.order_by('date')
+    total_findings = eng_findings_cwe | eng_findings_title
+    # total_findings = total_findings.order_by('date')
 
-        for find in total_findings:
-            if is_deduplication_on_engamgent(new_finding, find):
-                continue
-            if find.endpoints.count() != 0 and new_finding.endpoints.count() != 0:
-                list1 = new_finding.endpoints.all()
-                list2 = find.endpoints.all()
-                if all(x in list1 for x in list2):
-                    new_finding.duplicate = True
-                    new_finding.active = False
-                    new_finding.verified = False
-                    new_finding.duplicate_finding = find
-                    find.duplicate_list.add(new_finding)
-                    find.found_by.add(new_finding.test.test_type)
-                    super(Finding, new_finding).save(*args, **kwargs)
-                    break
-            elif find.line == new_finding.line and find.file_path == new_finding.file_path and new_finding.static_finding and len(
-                    new_finding.file_path) > 0:
+    for find in total_findings:
+        if is_deduplication_on_engagement_mismatch(new_finding, find):
+            logger.debug('deduplication_on_engagement_mismatch, skipping dedupe.')
+            continue
+        if find.endpoints.count() != 0 and new_finding.endpoints.count() != 0:
+            list1 = new_finding.endpoints.all()
+            list2 = find.endpoints.all()
+            if all(x in list1 for x in list2):
                 new_finding.duplicate = True
                 new_finding.active = False
                 new_finding.verified = False
@@ -130,14 +125,24 @@ def sync_dedupe(new_finding, *args, **kwargs):
                 find.duplicate_list.add(new_finding)
                 find.found_by.add(new_finding.test.test_type)
                 super(Finding, new_finding).save(*args, **kwargs)
-            elif find.hash_code == new_finding.hash_code:
-                new_finding.duplicate = True
-                new_finding.active = False
-                new_finding.verified = False
-                new_finding.duplicate_finding = find
-                find.duplicate_list.add(new_finding)
-                find.found_by.add(new_finding.test.test_type)
-                super(Finding, new_finding).save(*args, **kwargs)
+                break
+        elif find.line == new_finding.line and find.file_path == new_finding.file_path and new_finding.static_finding and len(
+                new_finding.file_path) > 0:
+            new_finding.duplicate = True
+            new_finding.active = False
+            new_finding.verified = False
+            new_finding.duplicate_finding = find
+            find.duplicate_list.add(new_finding)
+            find.found_by.add(new_finding.test.test_type)
+            super(Finding, new_finding).save(*args, **kwargs)
+        elif find.hash_code == new_finding.hash_code:
+            new_finding.duplicate = True
+            new_finding.active = False
+            new_finding.verified = False
+            new_finding.duplicate_finding = find
+            find.duplicate_list.add(new_finding)
+            find.found_by.add(new_finding.test.test_type)
+            super(Finding, new_finding).save(*args, **kwargs)
 
 
 def sync_rules(new_finding, *args, **kwargs):