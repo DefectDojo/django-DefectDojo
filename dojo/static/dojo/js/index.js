22951a5739f06b7b2996ec4240906a3ae53ed564
propersam <propersam2012@gmail.com>
2019-04-23 16:44:00 +0100

    Added Password visibility toggle button to Login page

--- dojo/static/dojo/js/index.js
+++ dojo/static/dojo/js/index.js
@@ -229,3 +229,12 @@ function punchcard(element, data, ticks) {
 
 }
 
+function togglePassVisibility() {
+    var x = document.getElementById("id_password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+} 
+
