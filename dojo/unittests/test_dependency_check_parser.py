eea6785725e0c0ba0d8df5786788b9cf0812f80c
propersam <propersam2012@gmail.com>
2019-04-27 18:17:53 +0100

    make restored file flake 8 compatible

--- dojo/unittests/test_dependency_check_parser.py
+++ dojo/unittests/test_dependency_check_parser.py
@@ -383,3 +383,4 @@ class TestDependencyCheckParser(TestCase):
                 'Description of a bad vulnerability.',
                 finding.description)
         self.assertEqual(expected_references, finding.references)
+        
