016582d90f0dbadc8c0c8e0bfba15bba6c927749
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-05-09 19:21:59 +0530

    fix indent

--- dojo/forms.py
+++ dojo/forms.py
@@ -290,7 +290,7 @@ class ImportScanForm(forms.Form):
                          ("DawnScanner Scan", "DawnScanner Scan"),
                          ("Anchore Engine Scan", "Anchore Engine Scan"),
                          ("Bundler-Audit Scan", "Bundler-Audit Scan"),
-                         ("Twistlock Image Scan", "Twistlock Image Scan"))
+                         ("Twistlock Image Scan", "Twistlock Image Scan"),
                          ("Kiuwan Scan", "Kiuwan Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
