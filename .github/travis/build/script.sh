d5e9a4b3fcc74fdb006e871bcc7596e310f94126
Piotr PAWLICKI <piotrek@seovya.net>
2019-05-13 04:57:22 +0200

    Add build_containers to build/script.sh

--- .github/travis/build/script.sh
+++ .github/travis/build/script.sh
@@ -6,3 +6,21 @@ source ../common-functions.bash
 # -e: exit as soon as one command returns a non-zero exit code
 # -v: print all lines before executing them, to help identify which step failed
 set -ev
+
+function build_containers {
+  # Build Docker images
+  travis_fold start docker_image_build
+  DOCKER_IMAGES=(django nginx)
+  for docker_image in "${DOCKER_IMAGES[@]}"
+  do
+    docker build \
+      --tag "defectdojo/defectdojo-${docker_image}:{TRAVIS_BUILD_NUMBER}" \
+      --file "Dockerfile.${docker_image}" \
+      .
+    $? || error_and_exit "cannot build '${docker_image}' image"
+  done
+  travis_fold end docker_image_build
+}
+
+
+build_containers
