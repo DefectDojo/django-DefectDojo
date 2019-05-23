232ef3c05079dbf985ba8b2efae1477389100276
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-05-23 20:35:33 +0530

    fix flake8

--- tests/Engagement_unit_test.py
+++ tests/Engagement_unit_test.py
@@ -77,7 +77,7 @@ class EngagementTest(unittest.TestCase):
         driver.find_element_by_id('id_active').get_attribute('checked')
         driver.find_element_by_id('id_verified').get_attribute('checked')
         Select(driver.find_element_by_id("id_scan_type")).select_by_visible_text("Nmap Scan")
-        driver.find_element_by_id('id_file').send_keys(os.getcwd().replace('/tests','') + "/dojo/unittests/scans/nmap_sample/nmap_multiple_port.xml")
+        driver.find_element_by_id('id_file').send_keys(os.getcwd().replace('/tests', '') + "/dojo/unittests/scans/nmap_sample/nmap_multiple_port.xml")
         driver.find_element_by_css_selector("input.btn.btn-primary").click()
         EngagementTXT = driver.find_element_by_tag_name("BODY").text
         self.assertTrue(re.search(r'Nmap Scan processed, a total of 13 findings were processed', EngagementTXT))
