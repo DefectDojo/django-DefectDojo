cca945447a097bb1920153b4b7170a82f6fa59a2
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-05-23 20:35:07 +0530

    add engagement unit tests

---
+++ tests/Engagement_unit_test.py
@@ -0,0 +1,141 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import unittest
+import re
+import sys
+import os
+
+
+class EngagementTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome('chromedriver')
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def login_page(self):
+        driver = self.driver
+        driver.get(self.base_url + "login")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys(os.environ['DD_ADMIN_USER'])
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys(os.environ['DD_ADMIN_PASSWORD'])
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        return driver
+
+    def create_new_product_to_test_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_id("dropdownMenu1").click()
+        driver.find_element_by_link_text("Add Product").click()
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys("Engagement Test")
+        driver.find_element_by_id("id_name").send_keys("\tthis is engagement test.")
+        Select(driver.find_element_by_id("id_prod_type")).select_by_visible_text("Research and Development")
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Product added successfully', EngagementTXT))
+
+    def test_add_new_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_class_name("pull-left").click()
+        driver.find_element_by_link_text("Add New Engagement").click()
+        driver.find_element_by_id("id_name").send_keys("test engagement")
+        driver.find_element_by_id("id_name").send_keys("\tthis is engagement test.")
+        driver.find_element_by_id("id_test_strategy").clear()
+        driver.find_element_by_id('id_test_strategy').send_keys("http://localhost:5000")
+        Select(driver.find_element_by_id("id_status")).select_by_visible_text("In Progress")
+        driver.find_element_by_css_selector("input[value='Done']").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Engagement added successfully.', EngagementTXT))
+
+    def test_edit_created_new_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_class_name("pull-left").click()
+        driver.find_element_by_link_text("View Engagements").click()
+        driver.find_element_by_link_text("test engagement").click()
+        driver.find_element_by_id("dropdownMenu1").click()
+        driver.find_element_by_link_text("Edit Engagement").click()
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys("edited test engagement")
+        Select(driver.find_element_by_id("id_status")).select_by_visible_text("In Progress")
+        driver.find_element_by_css_selector("input[value='Done']").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Engagement updated successfully.', EngagementTXT))
+
+    def test_engagement_import_scan_result(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_class_name("pull-left").click()
+        driver.find_element_by_link_text('View Engagements').click()
+        driver.find_element_by_link_text("edited test engagement").click()
+        driver.find_elements_by_id("dropdownMenu1")[1].click()
+        driver.find_element_by_link_text('Import Scan Results').click()
+        driver.find_element_by_id('id_active').get_attribute('checked')
+        driver.find_element_by_id('id_verified').get_attribute('checked')
+        Select(driver.find_element_by_id("id_scan_type")).select_by_visible_text("Nmap Scan")
+        driver.find_element_by_id('id_file').send_keys(os.getcwd().replace('/tests','') + "/dojo/unittests/scans/nmap_sample/nmap_multiple_port.xml")
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Nmap Scan processed, a total of 13 findings were processed', EngagementTXT))
+
+    def test_close_new_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_class_name("pull-left").click()
+        driver.find_element_by_link_text("View Engagements").click()
+        driver.find_element_by_link_text("edited test engagement").click()
+        driver.find_element_by_id("dropdownMenu1").click()
+        driver.find_element_by_link_text("Close Engagement").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Engagement closed successfully.', EngagementTXT))
+
+    def test_delete_new_closed_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_class_name("pull-left").click()
+        driver.find_element_by_link_text('View Engagements').click()
+        driver.find_element_by_link_text("edited test engagement").click()
+        driver.find_element_by_id("dropdownMenu1").click()
+        driver.find_element_by_link_text('Delete Engagement').click()
+        driver.find_element_by_name('delete_name').click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Engagement and relationships removed.', EngagementTXT))
+
+    def test_new_ci_cd_engagement(self):
+        driver = self.login_page()
+        driver.get(self.base_url + "product")
+        driver.find_element_by_link_text('Engagement Test').click()
+        driver.find_element_by_xpath("//a[@class='dropdown-toggle active']//span[@class='hidden-xs']").click()
+        driver.find_element_by_link_text('Add New CI/CD Engagement').click()
+        driver.find_element_by_id("id_name").send_keys("test new ci/cd engagement")
+        driver.find_element_by_id("id_name").send_keys("\ttest new ci/cd engagement")
+        driver.find_element_by_id('id_deduplication_on_engagement').get_attribute('checked')
+        driver.find_element_by_css_selector("input[value='Done']").click()
+        EngagementTXT = driver.find_element_by_tag_name("BODY").text
+        self.assertTrue(re.search(r'Engagement added successfully.', EngagementTXT))
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(EngagementTest('create_new_product_to_test_engagement'))
+    suite.addTest(EngagementTest('test_add_new_engagement'))
+    suite.addTest(EngagementTest('test_edit_created_new_engagement'))
+    suite.addTest(EngagementTest('test_engagement_import_scan_result'))
+    suite.addTest(EngagementTest('test_close_new_engagement'))
+    suite.addTest(EngagementTest('test_delete_new_closed_engagement'))
+    suite.addTest(EngagementTest('test_new_ci_cd_engagement'))
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(descriptions=True, failfast=True)
+    ret = not runner.run(suite()).wasSuccessful()
+    sys.exit(ret)
