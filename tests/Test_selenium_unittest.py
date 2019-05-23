73925a81be2a683f6a126555d3de57bf318c0965
propersam <propersam2012@gmail.com>
2019-05-23 02:28:47 +0100

    Add Test Create testsuite

---
+++ tests/Test_selenium_unittest.py
@@ -0,0 +1,178 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import unittest
+import re
+import sys
+import os
+
+
+class TestUnitTest(unittest.TestCase):
+    def setUp(self):
+        # Initialize the driver
+        # When used with Travis, chromdriver is stored in the same
+        # directory as the unit tests
+        self.driver = webdriver.Chrome('chromedriver')
+        # Allow a little time for the driver to initialize
+        self.driver.implicitly_wait(30)
+        # Set the base address of the dojo
+        self.base_url = "http://localhost:8000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def login_page(self):
+        # Make a member reference to the driver
+        driver = self.driver
+        # Navigate to the login page
+        driver.get(self.base_url + "login")
+        # Good practice to clear the entry before typing
+        driver.find_element_by_id("id_username").clear()
+        # These credentials will be used by Travis when testing new PRs
+        # They will not work when testing on your own build
+        # Be sure to change them before submitting a PR
+        driver.find_element_by_id("id_username").send_keys(os.environ['DD_ADMIN_USER'])
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys(os.environ['DD_ADMIN_PASSWORD'])
+        # "Click" the but the login button
+        driver.find_element_by_css_selector("button.btn.btn-success").click()
+        return driver
+
+    # first thing first. We create product, just to make sure there is atleast 1 product available
+    # to assign engagements and tests to when creating or editing tests.
+    def test_create_product(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.login_page()
+        # Navigate to the product page
+        driver.get(self.base_url + "product")
+        # "Click" the dropdown button to see options
+        driver.find_element_by_id("dropdownMenu1").click()
+        # "Click" the add prodcut button
+        driver.find_element_by_link_text("Add Product").click()
+        # Fill in th product name
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys("QA Test")
+        # Tab into the description area to fill some text
+        # Couldnt find a way to get into the box with selenium
+        driver.find_element_by_id("id_name").send_keys("\tThis is just a test. Be very afraid.")
+        # Select an option in the poroduct type
+        Select(driver.find_element_by_id("id_prod_type")).select_by_visible_text("Research and Development")
+        # "Click" the submit button to complete the transaction
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        # Query the site to determine if the product has been added
+        productTxt = driver.find_element_by_tag_name("BODY").text
+        # Assert ot the query to dtermine status of failure
+        # Also confirm success even if Product is returned as already exists for test sake
+        self.assertTrue(re.search(r'Product added successfully', productTxt) or
+            re.search(r'Product with this Name already exists.', productTxt))
+
+    def test_create_test(self):
+        # To create test for a product
+        # You must have an engagement and then tests are packed in engagements
+        # Login to the site.
+        # Username and password will be gotten from environ
+        driver = self.login_page()
+        # Navigate to the Product page to select the product we created earlier
+        driver.get(self.base_url + "product")
+        # "Click" the dropdown option
+        driver.find_element_by_class_name("pull-left").click()
+        # 'click' the Add New Engagement option
+        driver.find_element_by_link_text("Add New Engagement").click()
+        # Keep a good practice of clearing field before entering value
+        # fill up at least all required input field options.
+        # fields: 'Name', 'Description', 'Target Start', 'Target End', 'Testing Lead' and 'Status'
+        # engagement name
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys("Beta Test")
+        # engagement description
+        # Tab into the description area to fill some text
+        # Couldnt find a way to get into the box with selenium
+        driver.find_element_by_id("id_name").send_keys("\tRunning Test on product before approving and push to production.")
+        # engagement target start and target end already have defaults
+        # we can safely skip
+        # Testing Lead: This can be the logged in user
+        Select(driver.find_element_by_id("id_lead")).select_by_visible_text(os.environ['DOJO_ADMIN_USER'])
+        # engagement status
+        Select(driver.find_element_by_id("id_status")).select_by_visible_text("In Progress")
+        # "Click" the 'Add Test' button to Add Test to engagement
+        driver.find_element_by_name("_Add Tests").click()
+        # Fill at least required fields needed to create Test
+        # Test title
+        driver.find_element_by_id("id_title").clear() # clear field before inserting anything
+        driver.find_element_by_id("id_title").send_keys("Quick Security Testing")
+        # Select Test type
+        Select(driver.find_element_by_id("id_test_type")).select_by_visible_text("Manual Code Review")
+        # skip Target start and Target end leaving their default values
+        # Select Testing Environment
+        Select(driver.find_element_by_id("id_environment")).select_by_visible_text("Development")
+        # submit
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        # Query the site to determine if the Test has been added
+        productTxt = driver.find_element_by_tag_name("BODY").text
+        # Assert on the query to determine success or failure
+        self.assertTrue(re.search(r'Test added successfully', productTxt))
+
+    def test_edit_endpoint(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.login_page()
+        # Navigate to the endpoint page
+        driver.get(self.base_url + "endpoint")
+        # Select one of the previously created endpoint to edit
+        driver.find_element_by_link_text("moving.com.rnd").click()
+         # "Click" the dropdown button to see options
+        driver.find_element_by_id("dropdownMenu1").click()
+         # "Click" the Edit Endpoint
+        driver.find_element_by_link_text("Edit Endpoint").click()
+        # Clear the old endpoint host name
+        driver.find_element_by_id("id_host").clear()
+        # Fill in the endpoint host name
+        driver.find_element_by_id("id_host").send_keys("https://moving.com/")
+        # Fill in port for endpoint
+        driver.find_element_by_id("id_port").clear()
+        driver.find_element_by_id("id_port").send_keys("8080")
+        # "Click" the submit button to complete the transaction
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        # Query the site to determine if the product has been added
+        productTxt = driver.find_element_by_tag_name("BODY").text
+        # Assert ot the query to dtermine status of failure
+        self.assertTrue(re.search(r'Endpoint updated successfully', productTxt))
+
+    def test_delete_endpoint(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.login_page()
+        # Navigate to the endpoint page
+        driver.get(self.base_url + "endpoint")
+        # Select one of the previously created endpoint to delete
+        driver.find_element_by_link_text("123.22.43.101").click()
+        # "Click" the dropdown button to see options
+        driver.find_element_by_id("dropdownMenu1").click()
+         # "Click" the Delete Endpoint
+        driver.find_element_by_link_text("Delete Endpoint").click()
+        # "Click" the delete button to complete the transaction
+        driver.find_element_by_css_selector("button.btn.btn-danger").click()
+        # Query the site to determine if the product has been added
+        productTxt = driver.find_element_by_tag_name("BODY").text
+        # Assert ot the query to dtermine status of failure
+        self.assertTrue(re.search(r'Endpoint and relationships removed.', productTxt))
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+
+def suite():
+    suite = unittest.TestSuite()
+    # Add each test the the suite to be run
+    # success and failure is output by the test
+    suite.addTest(TestUnitTest('test_create_product'))
+    suite.addTest(TestUnitTest('test_create_test'))
+    # suite.addTest(TestUnitTest('test_edit_endpoint'))
+    # suite.addTest(TestUnitTest('test_delete_endpoint'))
+    return suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(descriptions=True, failfast=True)
+    ret = not runner.run(suite()).wasSuccessful()
+    sys.exit(ret)
