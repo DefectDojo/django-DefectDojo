8f6a6f16c5619e80716a8bcaca65629500f336bb
propersam <propersam2012@gmail.com>
2019-05-23 01:24:08 +0100

    Endpoint selenium unittest completed

--- tests/Endpoint_selenium_unittest.py
+++ tests/Endpoint_selenium_unittest.py
@@ -92,39 +92,46 @@ class EndpointTest(unittest.TestCase):
         # Login to the site. Password will have to be modified
         # to match an admin password in your own container
         driver = self.login_page()
-        # Navigate to the product page
-        driver.get(self.base_url + "product")
-        # "Click" the dropdown option
-        driver.find_element_by_class_name("pull-left").click()
-        # "Click" the edit option
-        driver.find_element_by_link_text("Edit").click()
-        # Clear the old product name
-        driver.find_element_by_id("id_name").clear()
-        # Fill in the product name
-        driver.find_element_by_id("id_name").send_keys("EDITED QA Test")
+        # Navigate to the endpoint page
+        driver.get(self.base_url + "endpoint")
+        # Select one of the previously created endpoint to edit
+        driver.find_element_by_link_text("moving.com.rnd").click()
+         # "Click" the dropdown button to see options
+        driver.find_element_by_id("dropdownMenu1").click()
+         # "Click" the Edit Endpoint
+        driver.find_element_by_link_text("Edit Endpoint").click()
+        # Clear the old endpoint host name
+        driver.find_element_by_id("id_host").clear()
+        # Fill in the endpoint host name
+        driver.find_element_by_id("id_host").send_keys("https://moving.com/")
+        # Fill in port for endpoint
+        driver.find_element_by_id("id_port").clear()
+        driver.find_element_by_id("id_port").send_keys("8080")
         # "Click" the submit button to complete the transaction
         driver.find_element_by_css_selector("input.btn.btn-primary").click()
         # Query the site to determine if the product has been added
         productTxt = driver.find_element_by_tag_name("BODY").text
         # Assert ot the query to dtermine status of failure
-        self.assertTrue(re.search(r'Product updated successfully', productTxt))
+        self.assertTrue(re.search(r'Endpoint updated successfully', productTxt))
 
     def test_delete_endpoint(self):
         # Login to the site. Password will have to be modified
         # to match an admin password in your own container
         driver = self.login_page()
-        # Navigate to the product page
-        driver.get(self.base_url + "product")
-        # "Click" the dropdown option
-        driver.find_element_by_class_name("pull-left").click()
-        # "Click" the edit option
-        driver.find_element_by_link_text("Delete").click()
+        # Navigate to the endpoint page
+        driver.get(self.base_url + "endpoint")
+        # Select one of the previously created endpoint to delete
+        driver.find_element_by_link_text("123.22.43.101").click()
+        # "Click" the dropdown button to see options
+        driver.find_element_by_id("dropdownMenu1").click()
+         # "Click" the Delete Endpoint
+        driver.find_element_by_link_text("Delete Endpoint").click()
         # "Click" the delete button to complete the transaction
         driver.find_element_by_css_selector("button.btn.btn-danger").click()
         # Query the site to determine if the product has been added
         productTxt = driver.find_element_by_tag_name("BODY").text
         # Assert ot the query to dtermine status of failure
-        self.assertTrue(re.search(r'Product and relationships removed.', productTxt))
+        self.assertTrue(re.search(r'Endpoint and relationships removed.', productTxt))
 
     def tearDown(self):
         self.driver.quit()
@@ -137,8 +144,8 @@ def suite():
     # success and failure is output by the test
     suite.addTest(EndpointTest('test_create_product'))
     suite.addTest(EndpointTest('test_create_endpoint'))
-    # suite.addTest(EndpointTest('test_edit_endpoint'))
-    # suite.addTest(EndpointTest('test_delete_endpoint'))
+    suite.addTest(EndpointTest('test_edit_endpoint'))
+    suite.addTest(EndpointTest('test_delete_endpoint'))
     return suite
 
 
