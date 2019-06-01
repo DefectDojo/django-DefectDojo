1a2cc27a78d92fb8ced4741528a7cae102d21f46
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-06-01 11:07:02 +0530

    add cobalt importer

---
+++ dojo/tools/cobalt/parser.py
@@ -0,0 +1,47 @@
+import StringIO
+import csv
+import hashlib
+from dojo.models import Finding
+
+__author__ = 'dr3dd589'
+
+
+class CobaltCSVParser(object):
+    def __init__(self, filename, test):
+        self.dupes = dict()
+        self.items = ()
+
+        if filename is None:
+            self.items = ()
+            return
+
+        content = filename.read()
+        reader = csv.DictReader(StringIO.StringIO(content), delimiter=',', quotechar='"')
+        csvarray = []
+
+        for row in reader:
+            csvarray.append(row)
+
+        for row in csvarray:
+            finding = Finding(test=test)
+            finding.title  = row['Title']
+            finding.description = "**Type** : " + row['Type'] + "\n\n" + \
+                                "**Description** : " + row['Description'] + "\n"
+            finding.mitigation = row['SuggestedFix'] 
+            finding.references = row['ResearcherUrl']
+            finding.steps_to_reproduce = row['StepsToReproduce']
+            finding.severity_justification = row['CriticalityJustification']
+            finding.severity = "Medium"
+            
+            if finding is not None:
+                if finding.title is None:
+                    finding.title = ""
+                if finding.description is None:
+                    finding.description = ""
+
+                key = hashlib.md5(finding.title + '|' + finding.description).hexdigest()
+
+                if key not in self.dupes:
+                    self.dupes[key] = finding
+
+        self.items = self.dupes.values()
