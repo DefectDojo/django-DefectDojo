9128bac94a395326f4345d59f4429a13da75f8ef
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-06-01 11:08:55 +0530

    add cobalt importer

--- dojo/forms.py
+++ dojo/forms.py
@@ -297,7 +297,8 @@ class ImportScanForm(forms.Form):
                          ("Openscap Vulnerability Scan", "Openscap Vulnerability Scan"),
                          ("Wapiti Scan", "Wapiti Scan"),
                          ("Immuniweb Scan", "Immuniweb Scan"),
-                         ("Sonatype Application Scan", "Sonatype Application Scan"))
+                         ("Sonatype Application Scan", "Sonatype Application Scan"),
+                         ("Cobalt.io Scan", "Cobalt.io Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
     scan_date = forms.DateTimeField(
