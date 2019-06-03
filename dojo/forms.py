20628b52607889c65aebf1a95fb4762b3858cf8e
propersam <propersam2012@gmail.com>
2019-06-03 16:16:23 +0100

    fix flake8 error from origin/dev in forms.py file

--- dojo/forms.py
+++ dojo/forms.py
@@ -294,7 +294,7 @@ class ImportScanForm(forms.Form):
                          ("Twistlock Image Scan", "Twistlock Image Scan"),
                          ("Kiuwan Scan", "Kiuwan Scan"),
                          ("Blackduck Hub Scan", "Blackduck Hub Scan"),
-                         ("Openscap Vulnerability Scan", "Openscap Vulnerability Scan"))
+                         ("Openscap Vulnerability Scan", "Openscap Vulnerability Scan"),
                          ("Immuniweb Scan", "Immuniweb Scan"),
                          ("Sonatype Application Scan", "Sonatype Application Scan"))
 
@@ -336,6 +336,7 @@ class ImportScanForm(forms.Form):
     def get_scan_type(self):
         TGT_scan = self.cleaned_data['scan_type']
         return TGT_scan
+
 
 class ReImportScanForm(forms.Form):
     scan_date = forms.DateTimeField(
@@ -484,7 +485,7 @@ class ScanSettingsForm(forms.ModelForm):
         help_text=addHelpTxt,
         validators=[
             validators.RegexValidator(
-                regex='^\s*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+,*\s*)+\s*$',
+                regex=r'^\s*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+,*\s*)+\s*$',
                 message=msg,
                 code='invalid_address')])
     options = (('Weekly', 'Weekly'), ('Monthly', 'Monthly'),
