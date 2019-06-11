ee0270f83607cc8ecad9da99eead5fd96e787a28
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-06-11 20:35:33 +0530

    add whitesource importer

--- dojo/forms.py
+++ dojo/forms.py
@@ -298,7 +298,8 @@ class ImportScanForm(forms.Form):
                          ("Wapiti Scan", "Wapiti Scan"),
                          ("Immuniweb Scan", "Immuniweb Scan"),
                          ("Sonatype Application Scan", "Sonatype Application Scan"),
-                         ("Cobalt.io Scan", "Cobalt.io Scan"))
+                         ("Cobalt.io Scan", "Cobalt.io Scan"),
+                         ("Whitesource Scan", "Whitesource Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
     scan_date = forms.DateTimeField(
