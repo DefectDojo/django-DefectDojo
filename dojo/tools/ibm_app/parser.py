3ae3025c8fcc36ac72b615528e2d79e1be622c76
propersam <propersam2012@gmail.com>
2019-06-11 22:53:44 +0100

    Done with IBM App Scan Importer

--- dojo/tools/ibm_app/parser.py
+++ dojo/tools/ibm_app/parser.py
@@ -1,36 +1,165 @@
 from xml.dom import NamespaceErr
 import hashlib
-import re
 from urlparse import urlparse
 from dojo.models import Endpoint, Finding
 from defusedxml import ElementTree
 
-__author__= 'properam'
+__author__ = 'propersam'
 
 
 class IbmAppScanXMLParser(object):
     def __init__(self, file, test):
         self.items = ()
+
         if file is None:
             return
 
-        IbmAppScanTree = ElementTree.parse(file)
-        root = IbmAppScanTree.getroot()
+        ibm_scan_tree = ElementTree.parse(file)
+        self.root = ibm_scan_tree.getroot()
+
         # validate XML file
-        if 'Vulnerabilities' not in root.tag:
+        if 'xml-report' not in self.root.tag:
             raise NamespaceErr("This does not look like a valid expected Ibm App Scan XML file.")
 
+        self.issue_list = []
+        # self.hosts = self.fetch_host_details()
+        self.issue_types = self.fetch_issue_types()
         self.dupes = dict()
+        # Now time to loop through individual issues and perform necessary actions
+        for issue in self.root.iter("issue-group"):
+            for item in issue.iter("item"):
+                impact = "N/A"
 
-        for vulnerability in root.iter("Vulnerability"):
-            """
-                The Tags available in XML File are:
-                ID, Name, Date, Status,
-                Type, CWE_ID, CVE_ID, CVSSv3,
-                Risk, URL, Description, PoC
-            """
-            url = vulnerability.find("URL").text
-            parseUrl = urlparse(url)
-            print(parseUrl)
+                ref_link = ""
+                if item.find("issue-type/ref") is not None:
+                    recommendation_data = ""
+                    issue_data = self.issue_types[item.find("issue-type/ref").text]
 
-        return
+                    name = issue_data['name']
+                    # advisory = issue_data['advisory']
+
+                    cve = ""
+                    if "cve" in issue_data:
+                        cve = issue_data['cve']
+
+                    url = self.get_url(item.find('url/ref').text)
+                    host = urlparse(url).netloc
+                    path = urlparse(url).path
+                    scheme = urlparse(url).scheme
+                    if scheme == "https":
+                        port = '443'
+                    else:
+                        port = '80'
+                    query = urlparse(url).query
+
+                    severity = item.find('severity').text
+                    issue_description = self.fetch_advisory_group(issue_data['advisory'])
+
+                    for fix_recommendation_group in self.root.iter("fix-recommendation-group"):
+                        for recommendation in fix_recommendation_group.iter("item"):
+                            if recommendation.attrib['id'] == issue_data["fix-recommendation"]:
+                                for data in recommendation.find("general/fixRecommendation/text").text:
+                                    recommendation_data += data  # some texts are being repeated
+
+                                if recommendation.find('general/fixRecommendation/link') is not None:
+                                    ref_link = recommendation.find('general/fixRecommendation/link').text
+
+                    # Now time to start assigning issues to findings and endpoints
+                    dupe_key = hashlib.md5(issue_description + name + severity).hexdigest()
+                    # check if finding is a duplicate
+                    if dupe_key in self.dupes:
+                        finding = self.dupes[dupe_key]  # fetch finding
+                        if issue_description is not None:
+                            finding.description += issue_description
+                    else:  # finding is not a duplicate
+                        # create finding
+                        finding = Finding(title=name,
+                                          test=test, active=False,
+                                          verified=False, cve=cve,
+                                          description=issue_description,
+                                          severity=severity,
+                                          numerical_severity=Finding.get_numerical_severity(
+                                              severity
+                                          ),
+                                          mitigation=recommendation_data,
+                                          impact=impact,
+                                          references=ref_link,
+                                          dynamic_finding=True)
+
+                        finding.unsaved_endpoints = list()
+                        self.dupes[dupe_key] = finding
+
+                        finding.unsaved_endpoints.append(Endpoint(
+                            host=host, port=port,
+                            path=path,
+                            protocol=scheme,
+                            query=query
+                        ))
+
+            self.items = self.dupes.values()
+
+
+
+
+
+
+
+
+
+
+
+    # # Loop through xml and fetch hosts
+    # def fetch_host_details(self):
+    #     hosts = []
+    #     hosts_dict = {}
+    #     for scan in self.root.iter("scan-configuration"):
+    #         for host in scan.iter("scanned-hosts"):
+    #             for item in host.iter("item"):
+    #                 hosts_dict['hostname'] = item.find('host').text
+    #                 hosts_dict['os'] = item.find('operating-system').text
+    #                 hosts_dict['port'] = item.find('port').text
+    #
+    #                 # The schema/protocol from the AppScanner Can either be
+    #                 # 'http' or 'https'
+    #                 if item.find('port').text == '443':
+    #                     hosts_dict['scheme'] = 'https'
+    #                 else:
+    #                     hosts_dict['scheme'] = 'http'
+    #
+    #                 hosts.append(hosts_dict)
+    #                 hosts_dict = {}
+    #     return hosts
+
+    # Loop through file and fetch all issue-types found
+    def fetch_issue_types(self):
+        issues = {}
+        for issue_type in self.root.iter("issue-type-group"):
+            for item in issue_type.iter("item"):
+                issues[item.attrib['id']] = {
+                    'name': item.find("name").text,
+                    'advisory': item.find("advisory/ref").text,
+                    'fix-recommendation': item.find("fix-recommendation/ref").text
+                }
+
+                if "cve" in issue_type:
+                    issues[issue_type.attrib['id']] = {'cve': issue_type.find("cve").text}
+
+        return issues
+
+    # this fetches Issue description
+    def fetch_advisory_group(self, advisory):
+        """
+        Function that parse advisory-group in order to get the item's description
+        """
+        for advisory_group in self.root.iter("advisory-group"):
+            for item in advisory_group.iter("item"):
+                if item.attrib['id'] == advisory:
+                    return item.find('advisory/testTechnicalDescription/text').text
+
+    def get_url(self, ref):
+        for url_group in self.root.iter('url-group'):
+            for item in url_group.iter('item'):
+                if item.attrib['id'] == ref:
+                    return item.find('name').text
+
+
