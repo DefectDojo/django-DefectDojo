21630ca522316134f12dabe03a07df1048bc530d
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-06-14 15:04:13 -0500

    Add private notes to finings and prevent showing on reports

--- dojo/forms.py
+++ dojo/forms.py
@@ -1212,7 +1212,7 @@ class NoteForm(forms.ModelForm):
 
     class Meta:
         model = Notes
-        fields = ['entry']
+        fields = ['entry', 'private']
 
 
 class CloseFindingForm(forms.ModelForm):
