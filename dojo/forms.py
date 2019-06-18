1fe349000fb00b8532096d4017f29e7821634984
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-06-18 15:50:53 -0500

    Fix accesslint and merge confilicts

--- dojo/forms.py
+++ dojo/forms.py
@@ -301,7 +301,8 @@ class ImportScanForm(forms.Form):
                          ("Cobalt.io Scan", "Cobalt.io Scan"),
                          ("Mozilla Observatory Scan", "Mozilla Observatory Scan"),
                          ("Whitesource Scan", "Whitesource Scan"),
-                         ("Contrast Scan", "Contrast Scan"))
+                         ("Contrast Scan", "Contrast Scan"),
+                         ("Microfocus Webinspect Scan", "Microfocus Webinspect Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
     scan_date = forms.DateTimeField(
