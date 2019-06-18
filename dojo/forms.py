fdb0ad20ed6027e31c7aa59b16fd5b9699a30a19
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-06-18 19:48:08 +0530

    fix flake8

--- dojo/forms.py
+++ dojo/forms.py
@@ -299,7 +299,7 @@ class ImportScanForm(forms.Form):
                          ("Immuniweb Scan", "Immuniweb Scan"),
                          ("Sonatype Application Scan", "Sonatype Application Scan"),
                          ("Cobalt.io Scan", "Cobalt.io Scan"),
-                         ("Contrast Scan","Contrast Scan"),
+                         ("Contrast Scan", "Contrast Scan"),
                          ("Mozilla Observatory Scan", "Mozilla Observatory Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
