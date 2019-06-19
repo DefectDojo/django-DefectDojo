ecf0269ddacf12087ed82b905c716bc6bfe16564
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-06-19 09:51:17 -0500

    Fix flake8 code errors and hopefully build errors

--- travis/script.sh
+++ travis/script.sh
@@ -177,7 +177,7 @@ echo "Running test ${TEST}"
           echo "Running Flake8 tests on dev branch aka pull requests"
           # We need to checkout dev for flake8-diff to work properly
           git checkout dev
-          sudo pip install pep8 flake8 flake8-diff
+          sudo pip3 install pep8 flake8 flake8-diff
           flake8-diff
       else
           echo "Skipping because not on dev branch"
