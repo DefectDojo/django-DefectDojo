e0e6994aec69c09d04eef8cc3195bfb80cd7b91e
devEricA <eric.anderson413@gmail.com>
2019-06-28 14:23:09 -0500

    Updated Fixes for #1322 and #1323

--- dojo/engagement/views.py
+++ dojo/engagement/views.py
@@ -545,11 +545,18 @@ def import_scan_results(request, eid=None, pid=None):
                     if hasattr(item, 'unsaved_req_resp') and len(
                             item.unsaved_req_resp) > 0:
                         for req_resp in item.unsaved_req_resp:
-                            burp_rr = BurpRawRequestResponse(
-                                finding=item,
-                                burpRequestBase64=req_resp["req"],
-                                burpResponseBase64=req_resp["resp"],
-                            )
+                            if form.get_scan_type() == "Arachni Scan":
+                                burp_rr = BurpRawRequestResponse(
+                                    finding=item,
+                                    burpRequestBase64=req_resp["req"],
+                                    burpResponseBase64=req_resp["resp"],
+                                )
+                            else:
+                                burp_rr = BurpRawRequestResponse(
+                                    finding=item,
+                                    burpRequestBase64=req_resp["req"].encode("utf-8"),
+                                    burpResponseBase64=req_resp["resp"].encode("utf-8"),
+                                )
                             burp_rr.clean()
                             burp_rr.save()
 
