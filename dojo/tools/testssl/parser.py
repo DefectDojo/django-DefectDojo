5bc877d761180b1e8be05f885cd15a47308c7b1b
dr3dd589 <skumar1@mt.iitr.ac.in>
2019-07-18 19:50:18 +0530

    add testssl done

--- dojo/tools/testssl/parser.py
+++ dojo/tools/testssl/parser.py
@@ -1,9 +1,13 @@
 import io
 import csv
 import hashlib
-from dojo.models import Finding
+import re
+from urllib.parse import urlparse
+from dojo.models import Finding, Endpoint
 
 __author__ = 'dr3dd589'
+
+SEV = ['INFO', 'LOW', 'HIGH', 'WARN']
 
 
 class TestsslCSVParser(object):
@@ -25,18 +29,100 @@ class TestsslCSVParser(object):
             csvarray.append(row)
 
         for row in csvarray:
-            if 'OK' not in row['severity']:
-                finding = Finding(test=test)
-                finding.title = row['Title'] if row['Title'][0] != "'" else row['Title'][1:]
-                if finding is not None:
-                    if finding.title is None:
-                        finding.title = ""
-                    if finding.description is None:
-                        finding.description = ""
+            if row['severity'] in SEV:
+                url = row['fqdn/ip'].split('/')[0]
+                port = row['port']
+                title = row['id']
+                severity = row['severity'].lower().capitalize()
+                if severity == 'Warn':
+                    severity = 'Info'
+                cves = row['cve'].split(' ')
+                description = "**finding** : " + row['finding'] + "\n\n"
+                if len(cves) > 1:
+                    cve_desc = ""
+                    cve = cves[0]
+                    for cve_ in cves:
+                        cve_desc += '[{0}](https://cve.mitre.org/cgi-bin/cvename.cgi?name={0})'.format(cve_) + ", "
 
-                    key = hashlib.md5((finding.title + '|' + finding.description).encode("utf-8")).hexdigest()
+                    description += "**Releted CVE's** : " + cve_desc[:-2]
+                else:
+                    try:
+                        cve = cves[0]
+                    except:
+                        cve = None
+                try:
+                    cwe = re.findall(r'\d+', row['cwe'])[0]
+                except:
+                    cwe = None
+                if title and description is not None:
+                    dupe_key = hashlib.md5(str(description + title).encode('utf-8')).hexdigest()
+                    if dupe_key in self.dupes:
+                        finding = self.dupes[dupe_key]
+                        if finding.references:
+                            finding.references = finding.references
+                        self.process_endpoints(finding, url)
+                        self.dupes[dupe_key] = finding
+                    else:
+                        self.dupes[dupe_key] = True
 
-                    if key not in self.dupes:
-                        self.dupes[key] = finding
+                        finding = Finding(
+                            title=title,
+                            test=test,
+                            active=False,
+                            verified=False,
+                            description=description,
+                            severity=severity,
+                            cve=cve,
+                            cwe=cwe,
+                            numerical_severity=Finding.get_numerical_severity(severity))
+                        finding.unsaved_endpoints = list()
+                        self.dupes[dupe_key] = finding
+                        self.process_endpoints(finding, url)
+                self.items = self.dupes.values()
 
-            self.items = list(self.dupes.values())
+    def process_endpoints(self, finding, host):
+        protocol = "http"
+        query = ""
+        fragment = ""
+        path = ""
+        url = urlparse(host)
+
+        if url:
+            path = url.path
+            if path == host:
+                path = ""
+
+        rhost = re.search(
+            r"(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))[\:]*([0-9]+)*([/]*($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+)).*?$",
+            host)
+        try:
+            protocol = rhost.group(1)
+            host = rhost.group(4)
+        except:
+            pass
+        try:
+            dupe_endpoint = Endpoint.objects.get(protocol=protocol,
+                                                 host=host,
+                                                 query=query,
+                                                 fragment=fragment,
+                                                 path=path
+                                                 )
+        except Endpoint.DoesNotExist:
+            dupe_endpoint = None
+
+        if not dupe_endpoint:
+            endpoint = Endpoint(protocol=protocol,
+                                host=host,
+                                query=query,
+                                fragment=fragment,
+                                path=path
+                                )
+        else:
+            endpoint = dupe_endpoint
+
+        if not dupe_endpoint:
+            endpoints = [endpoint]
+        else:
+            endpoints = [endpoint, dupe_endpoint]
+
+        finding.unsaved_endpoints = finding.unsaved_endpoints + endpoints
