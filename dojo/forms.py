80aacdb5ef36520bc10d4a367c8412f3726f4f0d
William Billaud 10062072 <10062072@gemalto.com>
2019-07-29 10:31:30 -0400

    compliance to flake 8

--- dojo/forms.py
+++ dojo/forms.py
@@ -319,7 +319,7 @@ class ImportScanForm(forms.Form):
                          ("JFrog Xray Scan", "JFrog Xray Scan"),
                          ("Sslyze Scan", "Sslyze Scan"),
                          ("Testssl Scan", "Testssl Scan"),
-                         ("Hadolint Dockerfile check","Hadolint Dockerfile check"))
+                         ("Hadolint Dockerfile check", "Hadolint Dockerfile check"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
     scan_date = forms.DateTimeField(
