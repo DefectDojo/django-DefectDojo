46c36cad7d8844ad240274d51e5801a902b55ace
TROVATELLI Pascal <pascal.trovatelli@soprasteria.com>
2019-08-06 11:43:28 +0200

    optimize rules iteration; fix rule not found; add sonar rule in references; add unit tests

--- dojo/tools/sonarqube/parser.py
+++ dojo/tools/sonarqube/parser.py
@@ -1,6 +1,10 @@
 from lxml import etree
 from dojo.models import Finding
 from django.utils.html import strip_tags
+import logging
+import re
+
+logger = logging.getLogger(__name__)
 
 
 class SonarQubeHtmlParser(object):
@@ -23,6 +27,15 @@ class SonarQubeHtmlParser(object):
             # Second is "Known Security Rules"
             vulnerabilities_table = list(detailTbody[0].iter("tr"))
             rules_table = list(detailTbody[1].xpath("tr"))
+
+            # iterate over the rules once to get the information we need
+            rulesDic = dict()
+            for rule in rules_table:
+                rule_properties = list(rule.iter("td"))
+                rule_name = list(rule_properties[0].iter("a"))[0].text
+                rule_details = list(rule_properties[1].iter("details"))[0]
+                rulesDic[rule_name] = rule_details
+
             for vuln in vulnerabilities_table:
                 vuln_properties = list(vuln.iter("td"))
                 vuln_rule_name = list(vuln_properties[0].iter("a"))[0].text
@@ -32,18 +45,16 @@ class SonarQubeHtmlParser(object):
                 vuln_title = vuln_properties[4].text
                 vuln_mitigation = vuln_properties[5].text
                 if vuln_title is None or vuln_mitigation is None:
-                    raise Exception('Parser ValueError')
-
-                vuln_details = self.get_rule_details(vuln_rule_name, rules_table)
-                if vuln_details is not None:
+                    raise Exception("Parser ValueError: can't find a title or a mitigation for vulnerability of name " + vuln_rule_name)
+                try:
+                    vuln_details = rulesDic[vuln_rule_name]
                     vuln_description = self.get_description(vuln_details)
-                    vuln_references = self.get_references(vuln_details)
+                    vuln_references = self.get_references(vuln_rule_name, vuln_details)
                     vuln_cwe = self.get_cwe(vuln_references)
-                else:
+                except KeyError:
                     vuln_description = "No description provided"
                     vuln_references = ""
                     vuln_cwe = 0
-
                 find = Finding(title=vuln_title,
                                cwe=int(vuln_cwe),
                                description=vuln_description,
@@ -79,16 +90,6 @@ class SonarQubeHtmlParser(object):
         else:
             return "Info"
 
-    def get_rule_details(self, vuln_rule_name, rules_table):
-        if vuln_rule_name is not None:
-            for rule in rules_table:
-                    rule_properties = list(rule.iter("td"))
-                    rule_name = list(rule_properties[0].iter("a"))[0].text
-                    if rule_name == vuln_rule_name:
-                        rule_details = list(rule_properties[1].iter("details"))[0]
-                        return rule_details
-        return None
-
     def get_description(self, vuln_details):
         rule_description = etree.tostring(vuln_details, pretty_print=True).decode('utf-8', errors='replace')
         rule_description = rule_description.split("<h2>See", 1)[0]
@@ -97,18 +98,16 @@ class SonarQubeHtmlParser(object):
         rule_description = strip_tags(rule_description).strip()
         return rule_description
 
-    def get_references(self, vuln_details):
-        rule_references = ""
+    def get_references(self, rule_name, vuln_details):
+        rule_references = rule_name
         for a in vuln_details.iter("a"):
-            rule_references += a.text + "\n"
+            rule_references += "\n" + a.text
         return rule_references
 
     def get_cwe(self, vuln_references):
-        if "CWE-" in vuln_references:
-            cwe = vuln_references.split("CWE-", 1)
-            if ":" in cwe[1]:
-                cwe = cwe[1].split(":", 1)
-            elif "\n" in cwe[1]:
-                cwe = cwe[1].split("\n", 1)
-            return cwe[0]
-        return 0
+        # Match only the first CWE!
+        cweSearch = re.search("CWE-([0-9]*)", vuln_references, re.IGNORECASE)
+        if cweSearch:
+            return cweSearch.group(1)
+        else:
+            return 0
