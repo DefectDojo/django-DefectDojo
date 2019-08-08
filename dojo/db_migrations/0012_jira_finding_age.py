a6aded06e5b916c3df63604f2ea2a423b7ebe0dd
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2019-08-08 11:40:13 -0500

    Update 0012_jira_finding_age.py

--- dojo/db_migrations/0012_jira_finding_age.py
+++ dojo/db_migrations/0012_jira_finding_age.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0011_finding_template_activity.py'),
+        ('dojo', '0011_finding_template_activity'),
     ]
 
     operations = [
