c7943de0d5897f0dd377dbfe56a181e04bef537a
Fred Blaise <fred.blaise@protonmail.com>
2019-09-09 14:57:22 +0200

    v1 aqua parser
    Signed-off-by: Fred Blaise <fred.blaise@protonmail.com>

--- dojo/forms.py
+++ dojo/forms.py
@@ -319,7 +319,8 @@ class ImportScanForm(forms.Form):
                          ("JFrogXray Scan", "JFrog Xray Scan"),
                          ("Sslyze Scan", "Sslyze Scan"),
                          ("Testssl Scan", "Testssl Scan"),
-                         ("Hadolint Dockerfile check", "Hadolint Dockerfile check"))
+                         ("Hadolint Dockerfile check", "Hadolint Dockerfile check"),
+                         ("Aqua Scan", "Aqua Scan"))
 
     SORTED_SCAN_TYPE_CHOICES = sorted(SCAN_TYPE_CHOICES, key=lambda x: x[1])
     scan_date = forms.DateTimeField(
