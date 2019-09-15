1c59f6522072a3a1846a9e8124a359d0651b00d7
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2019-09-15 12:31:27 -0500

    Change dependency as well

--- dojo/db_migrations/0019_system_settings_allow_anonymous_survey_repsonse.py
+++ dojo/db_migrations/0019_system_settings_allow_anonymous_survey_repsonse.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0014_jira_conf_resolution_mappings'),
+        ('dojo', '0018_sonarqube_api_integration'),
     ]
 
     operations = [
