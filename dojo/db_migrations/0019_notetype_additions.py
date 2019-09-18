f1a8db43baf61f88bb3d1a8d44404acca2b0e6dc
piyarathnalakmali <piyarathnalakmali@gmail.com>
2019-09-18 16:05:42 +0530

    Fixed the conflicts in db migrations files

--- dojo/db_migrations/0019_notetype_additions.py
+++ dojo/db_migrations/0019_notetype_additions.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0014_jira_conf_resolution_mappings'),
+        ('dojo', '0018_sonarqube_api_integration'),
     ]
 
     operations = [
