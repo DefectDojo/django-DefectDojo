42de3320935714d01e908cd134e8059b51f946fa
Pascal Trovatelli <ptrovatelli@gmail.com>
2019-09-21 17:23:49 +0200

    fix db_migrations

--- dojo/db_migrations/0019_SAST_track_unique_vulnerabilities.py
+++ dojo/db_migrations/0019_SAST_track_unique_vulnerabilities.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0018_sonarqube_api_integration.py'),
+        ('dojo', '0018_sonarqube_api_integration'),
     ]
 
     operations = [
