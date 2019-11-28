dd105fb4fc6031c65729b6b9c8ffd5173d359d11
Jan Kuehl <58301902+jankuehl@users.noreply.github.com>
2019-11-28 22:00:55 +0100

    adapt to flake8 style guide

--- dojo/tools/xanitizer/parser.py
+++ dojo/tools/xanitizer/parser.py
@@ -9,12 +9,12 @@ class XanitizerXMLParser(object):
 
     def __init__(self, filename, test):
         self.items = []
-        
+
         if filename is None:
             return
-        
-        root = self.parse_xml(filename);
-        
+
+        root = self.parse_xml(filename)
+
         if root is not None:
             self.items = self.get_findings(root, test)
 
@@ -23,7 +23,7 @@ class XanitizerXMLParser(object):
             tree = ET.parse(filename)
         except SyntaxError as se:
             raise se
-        
+
         root = tree.getroot()
         if 'XanitizerFindingsList' not in root.tag:
             raise ValueError("'{}' is not a valid Xanitizer findings list report XML file.".format(filename))
@@ -32,25 +32,24 @@ class XanitizerXMLParser(object):
 
     def get_findings(self, root, test):
         items = list()
-        
+
         globalDate = root.get('timeStamp', default=None)
         if globalDate is not None:
             # only date no time
             globalDate = globalDate[:10]
-        
+
         for finding in root.findall('finding'):
-            
             line = finding.find('line').text
             if line and int(line) <= 0:
                 line = None
-                
+
             date = globalDate
             if finding.find('date') is not None:
                 # only date no time
                 date = finding.find('date').text[:10]
-            
+
             description = self.generate_description(finding)
-            
+
             dojofinding = Finding(
                 test=test,
                 title=self.generate_title(finding, line),
@@ -63,16 +62,15 @@ class XanitizerXMLParser(object):
                 date=date,
                 active=False,
                 verified=False,
-                static_finding=True
-                )
-            
+                static_finding=True)
+
             items.append(dojofinding)
-        
-        return items;
-    
+
+        return items
+
     def generate_title(self, finding, line):
         title = finding.find('problemType').text
-        
+
         pckg = finding.find('package')
         cl = finding.find('class')
         file = finding.find('file')
@@ -86,12 +84,12 @@ class XanitizerXMLParser(object):
                 title = '{} ({}:{})'.format(title, file.text, line)
             else:
                 title = '{} ({})'.format(title, file.text)
-        
+
         return title
-    
+
     def generate_description(self, finding):
         description = '**Description:**\n{}'.format(finding.find('description').text)
-        
+
         if finding.find('startNode') is not None:
             startnode = finding.find('startNode')
             endnode = finding.find('endNode')
@@ -112,20 +110,20 @@ class XanitizerXMLParser(object):
             else:
                 description = '{}\n**Finding at:** {}'.format(description, location)
             description = self.add_code(node, True, description)
-        
+
         return description
-    
+
     def add_code(self, node, showline, description):
         codelines = node.findall('code')
-        
+
         if codelines is None or len(codelines) == 0:
             return description
-        
+
         if showline or len(codelines) == 1:
             for code in codelines:
                 if code.get('finding') == 'true':
                     description = '{}\n**Finding Line:** {}'.format(description, code.text)
-        
+
         if len(codelines) > 1:
             description = '{}\n**Code Excerpt:** '.format(description)
             for code in codelines:
@@ -133,48 +131,48 @@ class XanitizerXMLParser(object):
                     description = '{}\n{}: {}'.format(description, code.get('lineNo'), code.text)
                 else:
                     description = '{}\n{}: '.format(description, code.get('lineNo'))
-            
+
         return description
-    
+
     def generate_file_path(self, finding):
         file_path = None
-        
+
         if finding.find('endNode') is not None and finding.find('endNode').get('relativePath'):
             return finding.find('endNode').get('relativePath')
         elif finding.find('node') is not None and finding.find('node').get('relativePath'):
             return finding.find('node').get('relativePath')
-        
+
         pckg = finding.find('package')
         file = finding.find('file')
         if pckg is not None:
             return '{}/{}'.format(pckg.text.replace('.', '/'), file.text)
-        
+
         return file.text
-    
+
     def resolve_cwe(self, finding):
         if finding.find('cweNumber') is not None:
             cwe = finding.find('cweNumber').text
             if len(cwe) > 4 and cwe[:4] == 'CWE-':
                 # remove leading 'CWE-' and ',' '.'
                 return cwe[4:].replace(",", "").replace(".", "")
-        
+
         return None
-    
+
     def find_cve(self, description):
         # copy from models.py
         match = re.search('CVE-\d{4}-\d{4,7}', description)
-        
+
         if match:
             return match.group()
-        
+
         return None
 
     def resolve_severity(self, finding):
         if finding.find('rating') is None or not finding.find('rating').text:
             return 'Info'
-        
+
         rating = float(finding.find('rating').text)
-        
+
         if rating == 0:
             return 'Info'
         if rating < 4:
@@ -183,6 +181,5 @@ class XanitizerXMLParser(object):
             return 'Medium'
         if rating < 9:
             return 'High'
-        
+
         return 'Critical'
-    
