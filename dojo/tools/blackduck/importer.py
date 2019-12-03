a5de1670e1f93223bc961f5a985ffdab281f346e
Anthony Pipia <apipia@starbucks.com>
2019-12-03 11:53:50 -0800

    Updated importer to work with zip file properly, placed limits on location and cve

--- dojo/tools/blackduck/importer.py
+++ dojo/tools/blackduck/importer.py
@@ -1,11 +1,13 @@
+
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from pathlib import Path
-from typing import Iterable, Union
+from typing import Iterable
 from dojo.tools.blackduck.model import BlackduckFinding
 import csv
 import io
 import zipfile
+import re
 
 
 class Importer(ABC):
@@ -20,59 +22,112 @@ class BlackduckImporter(Importer):
         if not issubclass(type(report), Path):
             report = Path(report.temporary_file_path())
 
+        try:
+            if zipfile.is_zipfile(str(report)):
+                return self._process_zipfile(report)
+            else:
+                return self._process_csvfile(report)
+        except Exception as e:
+            print("Error processing file: {}".format(e))
+
+    def _process_csvfile(self, report):
+        """
+        If passed in a regular security.csv, process it.
+        No file information then.
+        """
+        security_issues = dict()
+        try:
+            with open(str(report), 'r') as f:
+                security_issues = self.__partition_by_project_id(f)
+
+        except Exception as e:
+            print("Could not process csv file: {}".format(e))
+
+        project_ids = set(security_issues.keys())
+        return self._process_project_findings(project_ids, security_issues, None)
+
+    def _process_zipfile(self, report):
+        """
+        Will take a zip file, look for security.csv and files.csv and union them on project id.
+        This allows to have the file component for a vulnerability.
+        """
         files = dict()
         security_issues = dict()
         try:
-            if zipfile.is_zipfile(str(report)):
-                with zipfile.ZipFile(str(report)) as zip:
-                    for file_name in zip.namelist():
-                        if file_name.endswith('files.csv'):
-                            with io.TextIOWrapper(zip.open(file_name), newline='') as f:
-                                files = self.__partition_by_project_id(f)
-                        elif file_name.endswith('security.csv'):
-                            with io.TextIOWrapper(zip.open(file_name), newline='') as f:
-                                security_issues = self.__partition_by_project_id(f)
-            else:
-                print("Not a zip file?")
-                raise ValueError
+            with zipfile.ZipFile(str(report)) as zip:
+                for full_file_name in zip.namelist():
+                    file_name = full_file_name.split("/")[-1]
+                    if 'source' in file_name:
+                        with io.TextIOWrapper(zip.open(full_file_name)) as f:
+                            files = self.__partition_by_project_id(f)
+                    elif 'security' in file_name:
+                        with io.TextIOWrapper(zip.open(full_file_name)) as f:
+                            security_issues = self.__partition_by_project_id(f)
+
         except Exception as e:
             print("Could not process zip file: {}".format(e))
 
         project_ids = set(files.keys()) & set(security_issues.keys())
+        return self._process_project_findings(project_ids, security_issues, files)
+
+    def _process_project_findings(self, project_ids, security_issues, files=None):
+        """
+        Process findings per projects and return a BlackduckFinding object per the model
+        """
         for project_id in project_ids:
             locations = set()
-            for file_entry in files[project_id]:
-                path = file_entry[8]
-                archive_context = file_entry[9]
-                if archive_context:
-                    locations.add("{}{}".format(archive_context, path[1:]))
-                else:
-                    locations.add(path)
+            if files is not None:
+                for file_entry in files[project_id]:
+                    file_entry_dict = dict(file_entry)
+                    path = file_entry_dict.get('Path')
+                    archive_context = file_entry_dict.get('Archive context')
+                    if archive_context:
+                        full_path = "{}{}".format(archive_context, path[1:])
+                    else:
+                        full_path = path
+
+                    # 4000 character limit on this field
+                    total_len = len(full_path)
+                    for location in list(locations):
+                        # + 2 for the ", " that will be added.
+                        total_len += (len(location) + 2)
+                    if total_len < 4000:
+                        locations.add(full_path)
+                    else:
+                        break
+
             for issue in security_issues[project_id]:
+                security_issue_dict = dict(issue)
+                cve = self.get_cve(security_issue_dict.get("Vulnerability id")).upper()
+                location = ", ".join(locations)
+
                 yield BlackduckFinding(
-                    issue[8],  # vuln ID
-                    issue[9],  # description
-                    issue[21],  # security_risk
-                    issue[14],  # impact
-                    issue[15],  # vulnerability source
-                    issue[20],  # url
-                    issue[6],  # channel version origin id
-                    issue[10],  # published date
-                    issue[11],  # updated on
-                    issue[12],  # base score
-                    issue[13],  # exploitability
-                    issue[16],  # remediation status
-                    issue[17],  # remediation target date
-                    issue[18],  # remediation actual date
-                    issue[19],  # remediation comment
-                    ', '.join(locations)
+                    cve,
+                    security_issue_dict.get('Description'),
+                    security_issue_dict.get('Security Risk'),
+                    security_issue_dict.get('Impact'),
+                    security_issue_dict.get('Vulnerability source'),
+                    security_issue_dict.get('URL'),
+                    security_issue_dict.get('Channel version origin id'),
+                    security_issue_dict.get('Component origin id'),
+                    security_issue_dict.get('Published on'),
+                    security_issue_dict.get('Updated on'),
+                    security_issue_dict.get('Base score'),
+                    security_issue_dict.get('Exploitability'),
+                    security_issue_dict.get('Remediation status'),
+                    security_issue_dict.get('Remediation target date'),
+                    security_issue_dict.get('Remediation actual date'),
+                    security_issue_dict.get('Remediation comment'),
+                    location
                 )
 
-    # return type elided due to higher kinded types bug in Python 3.5
-    def __partition_by_project_id(self, csv_file: Union[Path, zipfile.ZipFile]):
-        records = csv.reader(csv_file)
-        next(csv_file)
+    def __partition_by_project_id(self, csv_file):
+        records = csv.DictReader(csv_file)
         findings = defaultdict(set)
         for record in records:
-            findings[record[0]].add(tuple(record))
+            findings[record.get('Component id')].add(frozenset(record.items()))
         return findings
+
+    def get_cve(self, vuln_id):
+        cve_search = re.search("CVE-\d{4}-\d{4,7}", vuln_id, re.IGNORECASE)
+        return cve_search.group(0) if cve_search else vuln_id
