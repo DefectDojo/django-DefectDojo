96828138ea9252bc4e4589c7543081b7a478eac0
Fred Blaise <fred.blaise@protonmail.com>
2019-12-13 14:52:54 +0100

    regex pattern changes

--- dojo/models.py
+++ dojo/models.py
@@ -1229,8 +1229,9 @@ class Finding(models.Model):
     title = models.TextField(max_length=1000)
     date = models.DateField(default=get_current_date)
     cwe = models.IntegerField(default=0, null=True, blank=True)
-    cve_regex = RegexValidator(regex=r'^[A-Z]{1,10}-\d{4}-\d{4,12}$',
-                                 message="Vulnerability ID must be entered in the format: 'ABC-9999-9999'. ")
+    regex_pattern = re.compile(r'^[A-Z]{1,10}-\d{4}-\d{4,12}$', re.UNICODE)
+    cve_regex = RegexValidator(regex_pattern,
+                                 message="Vulnerability ID must comply with regex {}".format(regex_pattern.pattern))
     cve = models.TextField(validators=[cve_regex], max_length=28, null=True)
     url = models.TextField(null=True, blank=True, editable=False)
     severity = models.CharField(max_length=200, help_text="The severity level of this flaw (Critical, High, Medium, Low, Informational)")
@@ -1730,9 +1731,10 @@ class Stub_Finding(models.Model):
 class Finding_Template(models.Model):
     title = models.TextField(max_length=1000)
     cwe = models.IntegerField(default=None, null=True, blank=True)
-    cve_regex = RegexValidator(regex=r'^[A-Z]{1,10}-\d{4}-\d{4,12}$',
-                                 message="Vulnerability ID must be entered in the format: 'ABC-9999-9999'. ")
-    cve = models.TextField(validators=[cve_regex], max_length=28, null=True)
+    regex_pattern = re.compile(r'^[A-Z]{1,10}-\d{4}-\d{4,12}$', re.UNICODE)
+    cve_regex = RegexValidator(regex_pattern,
+                                 message="Vulnerability ID must comply with regex {}".format(regex_pattern.pattern))
+    cve = models.CharField(validators=[cve_regex], max_length=28, null=True)
     severity = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     mitigation = models.TextField(null=True, blank=True)
