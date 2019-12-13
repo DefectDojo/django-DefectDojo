fb7484d1211da30a5a705ef8a416dc773e7ad01a
Anthony Pipia <apipia@starbucks.com>
2019-12-13 14:39:04 -0800

    resolved error in test file with wrong blackduck parser

--- dojo/unittests/test_blackduck_v2_zip_parser.py
+++ dojo/unittests/test_blackduck_v2_zip_parser.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from dojo.tools.blackduck.parser import BlackduckHubCSVParser
+from dojo.tools.blackduck_v2.parser import BlackduckHubParser
 from dojo.models import Test
 from pathlib import Path
 
@@ -7,5 +7,5 @@ from pathlib import Path
 class TestBlackduckHubV2Parser(TestCase):
     def test_blackduck_enhanced_zip_upload(self):
         testfile = Path("dojo/unittests/scans/blackduck_V2/blackduck_hub_v2.zip")
-        parser = BlackduckHubCSVParser(testfile, Test())
+        parser = BlackduckHubParser(testfile, Test())
         self.assertEqual(12, len(parser.items))
