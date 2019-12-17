d85fb9fab8873a4d3c99fc7ad4b49bf1a98c8539
Anthony Pipia <apipia@starbucks.com>
2019-12-17 09:35:12 -0800

    fortify parser updated

--- dojo/tools/fortify/parser.py
+++ dojo/tools/fortify/parser.py
@@ -12,8 +12,8 @@ class FortifyXMLParser(object):
     language_list = []
 
     def __init__(self, filename, test):
-        Fortifyscan = ElementTree.parse(filename)
-        root = Fortifyscan.getroot()
+        fortify_scan = ElementTree.parse(filename)
+        root = fortify_scan.getroot()
 
         # Get Date
         date_string = root.getchildren()[5].getchildren()[1].getchildren()[2].text
@@ -23,100 +23,61 @@ class FortifyXMLParser(object):
 
         # Get Language
         lang_string = root[8][4][2].text
-        lang_need_string = re.findall("^.*com.fortify.sca.Phase0HigherOrder.Languages.*$", lang_string, re.MULTILINE)
+        lang_need_string = re.findall("^.*com.fortify.sca.Phase0HigherOrder.Languages.*$",
+                                      lang_string, re.MULTILINE)
         lang_my_string = lang_need_string[0]
         language = lang_my_string.split('=')[1]
         if language not in self.language_list:
             self.language_list.append(language)
 
-        # Get Finding Details
-        dupes = dict()
-
+        # Get all issues
+        issues = []
         for ReportSection in root.findall('ReportSection'):
             if ReportSection.findtext('Title') == "Results Outline":
-                kingdom = ''
-                category = ''
-                mitigation = 'N/A'
-                impact = 'N/A'
-                references = ''
-                findingdetail = ''
-                title = ''
-                filename = ''
-                filepath = ''
-                linestart = ''
-                dupe_key = ''
-                filename = ''
-                linestart = ''
+                for issue in ReportSection.iter("Issue"):
+                    issues.append(issue)
 
-            for GroupingSection in ReportSection.iter('GroupingSection'):
-                for groupTitle in GroupingSection.iter('groupTitle'):
-                    grouptitle = groupTitle.text
-                cwe_id = grouptitle.split(' ')
-                if len(cwe_id) > 2:
-                    if cwe_id[2].isdigit():
-                        cwe_id = cwe_id[2]
-                    elif "," in cwe_id[2]:
-                        cwe_id = cwe_id[:1]
-                    else:
-                        cwe_id = 0
-                else:
-                    cwe_id = 0
+        # All issues obtained, create a map for reference
+        issue_map = {}
+        for issue in issues:
+            details = {}
+            details["Category"] = issue.find("Category").text
+            details["Folder"] = issue.find("Folder").text
+            details["Kingdom"] = issue.find("Kingdom").text
+            details["Abstract"] = issue.find("Abstract").text
+            details["Friority"] = issue.find("Friority").text
+            details["FileName"] = issue.find("Primary").find("FileName").text
+            details["FilePath"] = issue.find("Primary").find("FilePath").text
+            details["LineStart"] = issue.find("Primary").find("LineStart").text
+            details["Snippet"] = issue.find("Primary").find("Snippet").text
+            issue_map.update({issue.attrib['iid']: details})
+        # map created
 
-                for Friority in GroupingSection.iter('Friority'):
-                    sev = Friority.text
+        self.items = []
+        for issue_key, issue in issue_map.items():
+            self.items.append(Finding(
+                title=self.format_title(issue["Category"], issue["FileName"], issue["LineStart"]),
+                severity=issue["Friority"],
+                numerical_severity=Finding.get_numerical_severity(issue["Friority"]),
+                file_path=issue['FilePath'],
+                line_number=int(issue['LineStart']),
+                line=int(issue['LineStart']),
+                static_finding=True,
+                active=False,
+                verified=False,
+                test=test,
+                date=find_date,
+                description=self.format_description(issue["Abstract"],
+                                                    issue["FileName"],
+                                                    issue["Snippet"]),
+                unique_id_from_tool=issue_key
+            ))
 
-                for Category in GroupingSection.iter('Category'):
-                    category = Category.text
+    def format_title(self, category, filename, line_no):
+        return "{} - {}: {}".format(category, filename, line_no)
 
-                for Kingdom in GroupingSection.iter('Kingdom'):
-                    kingdom = Kingdom.text
+    def format_description(self, abstract, filename, snippet):
+        desc = "###Abstract:\n{}\n###Snippet:\n**File: {}**\n```\n{}\n```\n".format(
+                abstract, filename, snippet)
+        return desc
 
-                for LineStart in GroupingSection.iter('LineStart'):
-                    linestart = LineStart.text
-                    if linestart is not None:
-                        findingdetail += "**Line Start:**" + linestart + '\n'
-
-                for Snippet in GroupingSection.iter('Snippet'):
-                    snippet = Snippet.text
-                    if snippet is not None:
-                        findingdetail += "\n**Code:**\n'''\n" + snippet + "\n\n"
-
-                    for FileName in GroupingSection.iter('FileName'):
-                        filename = FileName.text
-                        if filename is not None:
-                            findingdetail += "**FileName:**" + filename + '\n'
-                    for FilePath in GroupingSection.iter('FilePath'):
-                        filepath = FilePath.text
-                        if filepath is not None:
-                            findingdetail += "**Filepath:**" + filepath + '\n'
-
-                    title = category + " " + kingdom
-                    if not isinstance(cwe_id, int):
-                        dupe_key = (title + sev + cwe_id)
-                    else:
-                        dupe_key = (title + sev + str(cwe_id))
-
-                    if dupe_key in dupes:
-                        find = dupes[dupe_key]
-                    else:
-                        dupes[dupe_key] = True
-                        find = Finding(title=title,
-                                        cwe=cwe_id,
-                                        test=test,
-                                        active=False,
-                                        verified=False,
-                                        description=findingdetail,
-                                        severity=sev,
-                                        numerical_severity=Finding.get_numerical_severity(sev),
-                                        mitigation=mitigation,
-                                        impact=impact,
-                                        references=references,
-                                        file_path=filepath,
-                                        line=linestart,
-                                        url='N/A',
-                                        date=find_date,
-                                        static_finding=True)
-                    dupes[dupe_key] = find
-                    findingdetail = ''
-
-        self.items = list(dupes.values())
