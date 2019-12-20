16e1817b89e53fed06a6c5dc4db66e7a3655f97c
Kirill Gotsman <kgotsman@cloudbees.com>
2019-12-20 15:13:23 +0200

    Add unittests add-hackeroneparcer-359

--- dojo/unittests/test_h1_parser.py
+++ dojo/unittests/test_h1_parser.py
@@ -5,16 +5,16 @@ from dojo.models import Test
 
 class TestHackerOneParser(TestCase):
 
-    def test_parse_without_file_has_no_findings(self):
+    def test_parse_without_file_has_no_finding(self):
         parser = HackerOneJSONParser(None, Test())
         self.assertEqual(0, len(parser.items))
 
-    def test_parse_file_with_no_vuln_has_no_findings(self):
+    def test_parse_file_with_no_vuln_has_no_finding(self):
         testfile = open("dojo/unittests/scans/h1/data_empty.json")
         parser = HackerOneJSONParser(testfile, Test())
         self.assertEqual(0, len(parser.items))
 
-    def test_parse_file_with_one_vuln_has_one_findings(self):
+    def test_parse_file_with_one_vuln_has_one_finding(self):
         testfile = open("dojo/unittests/scans/h1/data_one.json")
         parser = HackerOneJSONParser(testfile, Test())
         self.assertEqual(1, len(parser.items))
