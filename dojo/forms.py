6fece136b52351f597d7a7675bb16ca96ea83449
Anthony Pipia <apipia@starbucks.com>
2020-01-02 08:47:23 -0800

    added entry to fixture test_type.json, updated forms.py with new parser

--- dojo/forms.py
+++ dojo/forms.py
@@ -343,6 +343,7 @@ class ImportScanForm(forms.Form):
                          ("Twistlock Image Scan", "Twistlock Image Scan"),
                          ("Kiuwan Scan", "Kiuwan Scan"),
                          ("Blackduck Hub Scan", "Blackduck Hub Scan"),
+                         ("Blackduck Hub Scan V2", "Blackduck Hub Scan V2"),
                          ("Openscap Vulnerability Scan", "Openscap Vulnerability Scan"),
                          ("Wapiti Scan", "Wapiti Scan"),
                          ("Immuniweb Scan", "Immuniweb Scan"),
