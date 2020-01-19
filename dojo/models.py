541e56e8d90600e8dfce233d9a70884367728281
Valentijn Scholten <valentijnscholten@gmail.com>
2020-01-19 15:10:15 +0100

    optimize: add select_related to reduce amount of queries needed

--- dojo/models.py
+++ dojo/models.py
@@ -2056,7 +2056,7 @@ class JIRA_Details_Cache(models.Model):
 
 class JIRA_PKey(models.Model):
     project_key = models.CharField(max_length=200, blank=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='jira_project_key_product')
     conf = models.ForeignKey(JIRA_Conf, verbose_name="JIRA Configuration",
                              null=True, blank=True, on_delete=models.CASCADE, related_name='jira_conf')
     component = models.CharField(max_length=200, blank=True)
