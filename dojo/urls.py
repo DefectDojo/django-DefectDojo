94498c20c4e19c400502d8c0da169797015f8ee6
Valentijn Scholten <valentijnscholten@gmail.com>
2020-01-19 12:23:19 +0100

    debug: add silk profiler

--- dojo/urls.py
+++ dojo/urls.py
@@ -196,3 +196,7 @@ if hasattr(settings, 'DJANGO_ADMIN_ENABLED'):
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
+
+import debug_toolbar
+urlpatterns += [url(r"^__debug__/", include(debug_toolbar.urls))]
