08fdedfee0e290a31cc1693cb07168201bf86e00
Valentijn Scholten <valentijnscholten@gmail.com>
2020-01-23 23:34:56 +0100

    dedupe: fix logging statement

--- dojo/tasks.py
+++ dojo/tasks.py
@@ -292,9 +292,8 @@ def async_update_findings_from_source_issues(*args, **kwargs):
 @app.task(bind=True)
 def async_dupe_delete(*args, **kwargs):
     logger.info("delete excess duplicates")
-    deduplicationLogger("delete excess duplicates")
-
-    system_settings = System_Settings.objects.get()
+    deduplicationLogger.info("delete excess duplicates")
+    system_settings = get_system_settings()
     if system_settings.delete_dupulicates:
         dupe_max = system_settings.max_dupes
         findings = Finding.objects \
