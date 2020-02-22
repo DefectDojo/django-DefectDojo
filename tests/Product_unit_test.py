fdf8287da72d8caf654e97ad7030694ac3106de7
Valentijn Scholten <valentijnscholten@gmail.com>
2020-02-22 19:26:05 +0100

    wild attempt at printing debug info

--- tests/Product_unit_test.py
+++ tests/Product_unit_test.py
@@ -80,14 +80,12 @@ class ProductTest(unittest.TestCase):
         # Select an option in the poroduct type
         # some wild guess to print some debug info
         print('\nDEBUGGING TEST PROD CREATION FAILURE\n')
-        print(driver.page_source)
+        # print(driver.page_source)
         print('\nDEBUGGING TEST PROD CREATION FAILURE2\n')
         print(driver.find_element_by_id("id_prod_type").get_attribute("innerHTML"))
-        print(driver.find_element_by_id("id_prod_type").get_attribute("outerHTML"))
         print('\nDEBUGGING TEST PROD CREATION FAILURE3\n')
         print('\nDEBUGGING TEST PROD CREATION FAILURE END\n')
-        print(driver.find_element_by_id("base-content").get_attribute("innerHTML"))
-        print(driver.find_element_by_id("base-content").get_attribute("outerHTML"))
+        # print(driver.find_element_by_id("base-content").get_attribute("innerHTML"))
         Select(driver.find_element_by_id("id_prod_type")).select_by_visible_text("Research and Development")
         # "Click" the submit button to complete the transaction
         driver.find_element_by_css_selector("input.btn.btn-primary").click()
