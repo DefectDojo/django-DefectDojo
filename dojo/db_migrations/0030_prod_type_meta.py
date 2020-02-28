8738785876245ad2dc851d17b429c39bac5cc083
Valentijn Scholten <valentijnscholten@gmail.com>
2020-02-28 12:10:44 +0100

    fix migrations

--- dojo/db_migrations/0030_prod_type_meta.py
+++ dojo/db_migrations/0030_prod_type_meta.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0028_finding_indices'),
+        ('dojo', '0029_cve_regex'),
     ]
 
     operations = [
