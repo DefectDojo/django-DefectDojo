33b274d9ed2d553dc6fa7db687ca1c3b47d523ad
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2020-03-02 12:18:54 -0600

    Complete parser

--- dojo/tools/burp_enterprise/parser.py
+++ dojo/tools/burp_enterprise/parser.py
@@ -1,7 +1,9 @@
 from lxml import etree
-from dojo.models import Finding
+from dojo.models import Endpoint, Finding
 import logging
 import re
+from urllib.parse import urlparse
+
 
 logger = logging.getLogger(__name__)
 
@@ -30,42 +32,39 @@ class BurpEnterpriseHtmlParser(object):
             self.items = dict()
 
     def get_content(self, container):
-        # print('\ncontent')
         s = ''
-        for elem in container.iterchildren():
-            # print(elem.tag, ' : ', elem.text, '\n')
-            if elem.text is not None and elem.text.strip() != '':
-                if elem.tag == 'a':
-                    s += '(' + elem.text + ')[' + elem.attrib['href'] + ']' + '\n'
-                elif elem.tag == 'p':
-                    s += elem.text + '\n'
-                elif elem.tag == 'li':
-                    s += '* '
-                    if elem.text is not None:
+        if container.tag == 'div' and container.text is not None and not container.text.isspace() and len(container.text) > 0:
+            s += ''.join(container.itertext()).strip().replace('Snip', '<-------------- Snip -------------->').replace('\t', '')
+        else:
+            for elem in container.iterchildren():
+                # print(elem.tag, ' : ', elem.text, '\n')
+                if elem.text is not None and elem.text.strip() != '':
+                    if elem.tag == 'a':
+                        s += '(' + elem.text + ')[' + elem.attrib['href'] + ']' + '\n'
+                    elif elem.tag == 'p':
                         s += elem.text + '\n'
-                elif elem.text.isspace():
-                    s += list(elem.itertext())[0]
-                elif elem.tag == 'div' or elem.tag == 'span':
-                    s += elem.text.strip() + '\n'
+                    elif elem.tag == 'li':
+                        s += '* '
+                        if elem.text is not None:
+                            s += elem.text + '\n'
+                    elif elem.text.isspace():
+                        s += list(elem.itertext())[0]
+                    elif elem.tag == 'div' or elem.tag == 'span':
+                        s += elem.text.strip() + '\n'
+                    else:
+                        continue
                 else:
-                    continue
-            else:
-                return s + self.get_content(elem)
-
-        if s == '' and container.tag == 'div' and container.text is not None and len(container.text) > 0:
-            if container.text[0] == '<':
-                s += etree.tostring(container, encoding='unicode') + '\n'
-            else:
-                s += container.text.strip() + '\n'
-
-        print('Returning :: ', s)
+                    s += self.get_content(elem)
         return s
 
     def get_items(self, tree):
         # Check that there is at least one vulnerability (the vulnerabilities table is absent when no vuln are found)
-        items = dict()
+        items = list()
+        endpoint_text = severities = tree.xpath("/html/body/div/div[contains(@class, 'section')]/h1")
+        url = endpoint_text[0].text[16:]
+        print(url)
+
         severities = tree.xpath("/html/body/div/div[contains(@class, 'section')]/table[contains(@class, 'issue-table')]/tbody")
-        
         sev_table = list(severities[0].iter("tr"))
         # for sev in sev_table:
         for item in range(0, len(sev_table), 2):
@@ -79,62 +78,50 @@ class BurpEnterpriseHtmlParser(object):
             vuln['Mitigation'] = ''
             vuln['References'] = ''
             vuln['CWE'] = ''
-            items[title] = vuln
+            items.append(vuln)
 
         vulns = tree.xpath("/html/body/div/div[contains(@class, 'section details')]/div[contains(@class, 'issue-container')]")
         if(len(vulns) > 0):
+            dict_index = 0
+            description = ['Issue detail:', 'Issue description', 'Request:', 'Response:']
             for issue in vulns:
                 elems = list(issue.iterchildren())
-                title = elems[1].text.strip()
-                items[title]['Endpoint'] = elems[2].text.strip()
-                description = ['Issue detail:', 'Issue description', 'Request:', 'Response:']
+                print('dict_index :: ', dict_index)
+                vuln = items[dict_index]
+                vuln['Endpoint'] = url + elems[2].text.strip()
 
                 for index in range(3, len(elems), 2):
                     primary, secondary = elems[index].text.strip(), elems[index + 1]
-
-                    # print('pri')
-                    # print(index, ' : tag  :: ', elems[index].tag)
-                    # print(index, ' : text :: ', elems[index].text, '\n')
-                    # if elems[index].text is not None and elems[index].text.strip() == '':
-                    #     print(self.get_content(elems[index]))
-                    # print('sec')
-                    # print(index + 1, ' : tag  :: ', elems[index + 1].tag)
-                    # print(index + 1, ' : text :: ', elems[index + 1].text, '\n')
-                    # if elems[index + 1].text is not None and elems[index + 1].text.strip() == '':
-                    #     print(self.get_content(elems[index + 1]))
-
                     # Description
                     if primary in description:
-                        print('\"', primary, '\"')
-                        print('sec')
-                        print(index + 1, ' : tag  :: ', elems[index + 1].tag)
-                        print(index + 1, ' : text :: ', elems[index + 1].text, '\n')
-                        if elems[index + 1].text is not None and elems[index + 1].text.strip() == '':
-                            print(self.get_content(elems[index + 1]))
-
-                        print('************************************************')
-                        s = str(items[title]['Description']) + str(self.get_content(secondary))
-                        items[title]['Description'] = s
+                        s = self.get_content(secondary)
+                        if primary == 'Request:' or primary == 'Response:':
+                            s = '\n\n' + primary + '\n' + s
+                        vuln['Description'] = vuln['Description'] + s
                     # Impact
                     if primary == 'Issue background':
-                        items[title]['Impact'] = items[title]['Impact'] + self.get_content(secondary)
+                        vuln['Impact'] = vuln['Impact'] + self.get_content(secondary)
                     # Mitigation
                     if primary == 'Issue remediation':
-                        items[title]['Mitigation'] = items[title]['Mitigation'] + self.get_content(secondary)
+                        vuln['Mitigation'] = vuln['Mitigation'] + self.get_content(secondary)
                     # References
                     if primary == 'References':
-                        items[title]['References'] += items[title]['References'] + self.get_content(secondary)
+                        vuln['References'] = vuln['References'] + self.get_content(secondary)
                     # CWE
                     if primary == 'Vulnerability classifications':
-                        items[title]['CWE'] += items[title]['CWE'] + self.get_content(secondary)
+                        s = self.get_content(secondary)
+                        if len(vuln['CWE']) < 1:
+                            vuln['CWE'] += vuln['CWE'] + s
+                        vuln['References'] = vuln['References'] + s
+
+                dict_index += 1
 
             print('Printing vulns\n\n')
-            for k, v in items.items():
+            for v in items:
                 for key, value in v.items():
                     print(key, ' :: ', value)
                 print()
 
-            raise Exception("Stop")
             self.create_findings(items)
             findings = list(self.dupes.values())
         else:
@@ -150,10 +137,10 @@ class BurpEnterpriseHtmlParser(object):
             return 0
 
     def create_findings(self, items):
-        for title, details in items.items():
+        for details in items:
             cwe = self.get_cwe(details.get('CWE'))
-            aggregateKeys = "{}{}{}".format(title, details.get('Description'), cwe)
-            find = Finding(title=title,
+            aggregateKeys = "{}{}{}".format(details.get('Title'), details.get('Description'), cwe)
+            find = Finding(title=details.get('Title'),
                            description=details.get('Description'),
                            test=self.test,
                            severity=details.get('Severity'),
@@ -171,6 +158,27 @@ class BurpEnterpriseHtmlParser(object):
                            static_finding=False,
                            dynamic_finding=True,
                            nb_occurences=1)
+
+            url = details.get('Endpoint')
+            parsedUrl = urlparse(url)
+            protocol = parsedUrl.scheme
+            query = parsedUrl.query
+            fragment = parsedUrl.fragment
+            path = parsedUrl.path
+            port = ""  # Set port to empty string by default
+            # Split the returned network address into host and
+            try:  # If there is port number attached to host address
+                host, port = parsedUrl.netloc.split(':')
+            except:  # there's no port attached to address
+                host = parsedUrl.netloc
+
+            find.unsaved_endpoints = list()
             self.dupes[aggregateKeys] = find
+
+            find.unsaved_endpoints.append(Endpoint(
+                    host=host, port=port,
+                    path=path,
+                    protocol=protocol,
+                    query=query, fragment=fragment))
+
         return list(self.dupes.values())
-            
