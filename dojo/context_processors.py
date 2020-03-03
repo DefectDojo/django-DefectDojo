d3d64c9537bab5c52940be915a6ccd0a600271d7
bgoareguer <43874676+bgoareguer@users.noreply.github.com>
2020-03-03 10:21:16 +0100

    Implement Gitlab OAuth2 authentication. Fix #1966 (#1967)

--- dojo/context_processors.py
+++ dojo/context_processors.py
@@ -6,6 +6,7 @@ def globalize_oauth_vars(request):
     # return the value you want as a dictionnary. you may add multiple values in there.
     return {'GOOGLE_ENABLED': settings.GOOGLE_OAUTH_ENABLED,
             'OKTA_ENABLED': settings.OKTA_OAUTH_ENABLED,
+            'GITLAB_ENABLED': settings.GITLAB_OAUTH2_ENABLED,
             'AZUREAD_TENANT_OAUTH2_ENABLED': settings.AZUREAD_TENANT_OAUTH2_ENABLED}
 
 
