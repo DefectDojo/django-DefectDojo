04191121f86f46c6b4653c71b22795d6a62b1f01
Fred Blaise <fred.blaise@protonmail.com>
2020-03-13 14:22:47 +0100

    Aligning accepted file formats for re-import

--- dojo/forms.py
+++ dojo/forms.py
@@ -438,7 +438,7 @@ class ReImportScanForm(forms.Form):
                            help_text="Add tags that help describe this scan.  "
                                      "Choose from the list or add new tags.  Press TAB key to add.")
     file = forms.FileField(widget=forms.widgets.FileInput(
-        attrs={"accept": ".xml, .csv, .nessus, .json, .html"}),
+        attrs={"accept": ".xml, .csv, .nessus, .json, .html, .js, .zip, .xlsx"}),
         label="Choose report file",
         required=False)
 
