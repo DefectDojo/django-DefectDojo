816b092af219a9a065cb112ae070529824e00042
mestrade <matthieu.estrade@gmail.com>
2020-03-27 18:57:38 +0100

    Fix db migration issue

--- dojo/db_migrations/0034_add_github_support.py
+++ dojo/db_migrations/0034_add_github_support.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0031_finding_component'),
+        ('dojo', '0033_remove_finding_duplicate_list'),
     ]
 
     operations = [
