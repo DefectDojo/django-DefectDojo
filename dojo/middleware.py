2ceac46d3387444b6b61f9b52ab872052d56b8eb
Felix Wiedemann <felix@kann.it>
2020-03-27 11:03:57 +0100

    Remove timezone middleware
    Django should support timezones out of the box with `USE_TZ = True`, so
    let's remove this.

--- dojo/middleware.py
+++ dojo/middleware.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from django.utils import timezone
 from django.utils.http import urlquote
-from dojo.utils import get_system_setting
 from re import compile
 
 
@@ -42,18 +40,4 @@ class LoginRequiredMiddleware:
                 else:
                     fullURL = "%s?next=%s" % (settings.LOGIN_URL, urlquote(request.get_full_path()))
                 return HttpResponseRedirect(fullURL)
-        return response
-
-
-class TimezoneMiddleware:
-    """
-    Middleware that checks the configured timezone to use in each request
-    """
-
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        timezone.activate(get_system_setting('time_zone'))
         return response
