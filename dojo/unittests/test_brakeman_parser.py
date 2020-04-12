8adff7ce2522ee503b6859acd6fe11e26e899898
Steeve Barbeau <steeve.barbeau@gmail.com>
2020-04-12 03:26:11 +0000

    fixing file paths

--- dojo/unittests/test_brakeman_parser.py
+++ dojo/unittests/test_brakeman_parser.py
@@ -10,18 +10,18 @@ class TestBrakemanScanParser(TestCase):
         self.assertEqual(0, len(parser.items))
 
     def test_parse_file_no_finding(self):
-        testfile = open("dojo/unittests/scans/brakeman/no_vuln.json")
+        testfile = open("dojo/unittests/scans/brakeman/no_finding.json")
         parser = BrakemanScanParser(testfile, Test())
         self.assertEqual(0, len(parser.items))
 
     def test_parse_file_has_two_findings(self):
-        testfile = open("dojo/unittests/scans/brakeman/many_vulns.json")
+        testfile = open("dojo/unittests/scans/brakeman/two_findings.json")
         parser = BrakemanScanParser(testfile, Test())
         testfile.close()
         self.assertEqual(2, len(parser.items))
 
     def test_parse_file_has_many_findings(self):
-        testfile = open("dojo/unittests/scans/brakeman/many_vulns.json")
+        testfile = open("dojo/unittests/scans/brakeman/many_findings.json")
         parser = BrakemanScanParser(testfile, Test())
         testfile.close()
         self.assertEqual(18, len(parser.items))
