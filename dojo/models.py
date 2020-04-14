c76e006b1f639745f91d8c7c6e0b16dfa1920968
Anthony Pipia <apipia@starbucks.com>
2020-04-14 09:12:09 -0700

    using jira_conf_new() function for finding rather than db query

--- dojo/models.py
+++ dojo/models.py
@@ -1766,11 +1766,7 @@ class Finding(models.Model):
         # what.
         if not push_to_jira:
             # only if there is a JIRA configuration
-            try:
-                push_to_jira = JIRA_PKey.objects.get(
-                    product=self.test.engagement.product).push_all_issues
-            except JIRA_PKey.DoesNotExist:
-                logger.info("There is no JIRA configuration, cannot push all issues!")
+            push_to_jira = self.jira_conf_new() and self.jira_conf_new().push_all_issues
 
         if self.pk is None:
             # We enter here during the first call from serializers.py
