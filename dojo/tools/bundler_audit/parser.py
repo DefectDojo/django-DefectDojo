47bac4bceb2765dbd592c87c88ac9121bc1c1c0b
Steeve Barbeau <steeve.barbeau@gmail.com>
2020-04-26 01:41:08 +0000

    fixing bug

--- dojo/tools/bundler_audit/parser.py
+++ dojo/tools/bundler_audit/parser.py
@@ -7,7 +7,8 @@ from dojo.models import Finding
 
 class BundlerAuditParser(object):
     def __init__(self, filename, test):
-        lines = filename.read()
+        lines = filename.read().decode('utf8')
+        print(repr(lines))
         dupes = dict()
         find_date = datetime.now()
         warnings = lines.split('\n\n')
@@ -45,7 +46,7 @@ class BundlerAuditParser(object):
             mitigation = advisory_solution
             references = advisory_url
             fingerprint = "bundler-audit" + gem_name + gem_version + advisory_cve + sev
-            dupe_key = hashlib.md5(fingerprint).hexdigest()
+            dupe_key = hashlib.md5(fingerprint.encode("utf-8")).hexdigest()
             if dupe_key in dupes:
                 find = dupes[dupe_key]
             else:
