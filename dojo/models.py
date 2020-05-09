e0a5f125292c7cabf0989dea1a9112d31eb8aa82
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-09 20:49:54 +0200

    Revert "also add index for hash_code"
    This reverts commit 80f4e4742e7864f2f23c19356df89cd55c6504cd.

--- dojo/models.py
+++ dojo/models.py
@@ -1433,7 +1433,6 @@ class Finding(models.Model):
             models.Index(fields=['numerical_severity']),
             models.Index(fields=['date']),
             models.Index(fields=['title']),
-            models.Index(fields=['hash_code']),
         ]
 
     @classmethod
