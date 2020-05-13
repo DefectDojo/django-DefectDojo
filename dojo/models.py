d599d0add538e8ae2e1ae6ebb93394873d57e0c3
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-13 23:11:58 +0200

    fix alerts for product_added, and more

--- dojo/models.py
+++ dojo/models.py
@@ -2926,6 +2926,7 @@ admin.site.register(Cred_Mapping)
 admin.site.register(System_Settings, System_SettingsAdmin)
 admin.site.register(CWE)
 admin.site.register(Regulation)
+admin.site.register(Notifications)
 
 # Watson
 watson.register(Product)
