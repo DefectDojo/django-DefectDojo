88e2de07c21b6934f72d04a5ea4cb96f7048f59a
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-18 09:11:34 +0200

    fix migration

--- dojo/db_migrations/0040_finding_cwe_index.py
+++ dojo/db_migrations/0040_finding_cwe_index.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0040_finding_cwe_index'),
+        ('dojo', '0039_test_version'),
     ]
 
     operations = [
