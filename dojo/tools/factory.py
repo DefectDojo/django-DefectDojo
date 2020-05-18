e013e695206320d395bd414a02091e8ef0f95339
Javed <tjaved@c02v25e0hv2q.agoda.local>
2020-05-18 18:22:47 +0700

    github vulnerability parser added

--- dojo/tools/factory.py
+++ dojo/tools/factory.py
@@ -77,6 +77,7 @@ from dojo.tools.burp_enterprise.parser import BurpEnterpriseHtmlParser
 from dojo.tools.anchore_enterprise.parser import AnchoreEnterprisePolicyCheckParser
 from dojo.tools.gitleaks.parser import GitleaksJSONParser
 from dojo.tools.harbor_vulnerability.parser import HarborVulnerabilityParser
+from dojo.tools.github_vulnerability.parser import GithubVulnerabilityParser
 from dojo.tools.choctaw_hog.parser import ChoctawhogParser
 from dojo.tools.gitlab_sast.parser import GitlabSastReportParser
 
@@ -254,6 +255,8 @@ def import_parser_factory(file, test, active, verified, scan_type=None):
         parser = GitleaksJSONParser(file, test)
     elif scan_type == 'Harbor Vulnerability Scan':
         parser = HarborVulnerabilityParser(file, test)
+    elif scan_type == 'Github Vulnerability Scan':
+        parser = GithubVulnerabilityParser(file, test)
     elif scan_type == 'Choctaw Hog Scan':
         parser = ChoctawhogParser(file, test)
     elif scan_type == 'GitLab SAST Report':
