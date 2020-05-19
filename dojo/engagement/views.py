b88dcac69129e2ce7e7761847aa8e1f0d3289245
valentijnscholten <valentijnscholten@gmail.com>
2020-05-19 09:58:22 +0200

    make notifications work with users created without usercontactinfo (#2373)
    * make notifications work without users created outside DD

--- dojo/engagement/views.py
+++ dojo/engagement/views.py
@@ -691,7 +691,8 @@ def import_scan_results(request, eid=None, pid=None):
                     extra_tags='alert-success')
 
                 create_notification(
-                    event='results_added',
+                    initiator=request.user,
+                    event='scan_added',
                     title=str(finding_count) + " findings for " + engagement.product.name,
                     finding_count=finding_count,
                     test=t,
