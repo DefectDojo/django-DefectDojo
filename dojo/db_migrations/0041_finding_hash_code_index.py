0de5447ab51e0d5ba099763258219e298f9d512a
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-23 20:13:49 +0200

    rebase migration

--- dojo/db_migrations/0041_finding_hash_code_index.py
+++ dojo/db_migrations/0041_finding_hash_code_index.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0040_finding_cwe_index'),
+        ('dojo', '0041_engagement_survey_import'),
     ]
 
     operations = [
