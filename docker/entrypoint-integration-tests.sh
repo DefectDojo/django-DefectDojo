45fb879e08963a00005100c71d00bf05c01b4822
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-24 09:28:33 +0200

    fix merge artifact

--- docker/entrypoint-integration-tests.sh
+++ docker/entrypoint-integration-tests.sh
@@ -12,7 +12,6 @@ if [ "$CR" != 200 ]; then
 fi
 
 # Run available unittests with a simple setup
-echo "Running Product type integration tests"
 # All available Integrationtest Scripts are activated below
 # If successsful, A successs message is printed and the script continues
 # If any script is unsuccesssful a failure message is printed and the test script
