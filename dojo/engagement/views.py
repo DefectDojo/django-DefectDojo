565bac3981d70ff80b8685e248e569ab70263cfd
Valentijn Scholten <valentijnscholten@gmail.com>
2020-05-24 11:48:49 +0200

    fix merge artifact

--- dojo/engagement/views.py
+++ dojo/engagement/views.py
@@ -686,7 +686,7 @@ def import_scan_results(request, eid=None, pid=None):
                     extra_tags='alert-success')
 
                 create_notification(
-                    event='results_added',
+                    event='scan_added',
                     initiator=request.user,
                     title=str(finding_count) + " findings for " + engagement.product.name,
                     finding_count=finding_count,
