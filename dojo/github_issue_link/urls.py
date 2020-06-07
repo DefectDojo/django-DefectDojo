cfa20845938a13d1d9b7c8b6051d43ca2e71f4e6
Fred Blaise <madchap@users.noreply.github.com>
2020-06-07 20:11:00 +0200

    JIRA fixes (#2462)
    * Explicit cast to avoid possible TypeError on bulk edits
    * only display bug icon for issue tracker when conf is actually present
    * actually log things, small rework
    * wild guess at fixing jira webhook
    * re-adding jira alerts
    Signed-off-by: Fred Blaise <fred.blaise@protonmail.com>

--- dojo/github_issue_link/urls.py
+++ dojo/github_issue_link/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^webhook', views.webhook, name='web_hook'),
+    url(r'^github-webhook', views.webhook, name='github_web_hook'),
     url(r'^github/add', views.new_github, name='add_github'),
     url(r'^github/(?P<tid>\d+)/delete$', views.delete_github,
         name='delete_github'),
