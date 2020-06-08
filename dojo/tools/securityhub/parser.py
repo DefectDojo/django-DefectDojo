d3cb73a117e0bf1df6f33355aa73818a4e4cacd8
Al S <xee5ch.gh.al@il5.in>
2020-06-08 00:29:17 -0400

    Set up skeleton of parser class.

---
+++ dojo/tools/securityhub/parser.py
@@ -0,0 +1,12 @@
+import json
+import re
+from json.decoder import JSONDecodeError
+from datetime import datetime
+from dojo.models import Finding
+
+logger = logging.getLogger(__name__)
+
+class AwsSecurityFindingFormatParser:
+
+    def __init__(self, filename, test):
+        pass
