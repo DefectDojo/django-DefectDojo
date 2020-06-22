d6753c55283fb5e5e0c900b5ef1c9ac7614645b3
Al S <xee5ch.gh.al@il5.in>
2020-06-21 23:15:35 -0400

    Relocate parser directory.

--- dojo/tools/factory.py
+++ dojo/tools/factory.py
@@ -82,7 +82,7 @@ from dojo.tools.choctaw_hog.parser import ChoctawhogParser
 from dojo.tools.gitlab_sast.parser import GitlabSastReportParser
 from dojo.tools.yarn_audit.parser import YarnAuditParser
 from dojo.tools.bugcrowd.parser import BugCrowdCSVParser
-from dojo.tools.securityhub.parser import AwsSecurityFindingFormatParser
+from dojo.tools.awssecurityhub.parser import AwsSecurityFindingFormatParser
 
 
 __author__ = 'Jay Paz'
