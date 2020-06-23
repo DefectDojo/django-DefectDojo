dc76eeaa66a74de47bd1a0cbf20bf6d06fd7762b
Ederson Brilhante <ederson.brilhante@grandparade.co.uk>
2020-06-23 17:51:37 +0200

    feature: add tool CCVS Report

--- dojo/tools/factory.py
+++ dojo/tools/factory.py
@@ -82,6 +82,7 @@ from dojo.tools.choctaw_hog.parser import ChoctawhogParser
 from dojo.tools.gitlab_sast.parser import GitlabSastReportParser
 from dojo.tools.yarn_audit.parser import YarnAuditParser
 from dojo.tools.bugcrowd.parser import BugCrowdCSVParser
+from dojo.tools.ccvs.parser import CCVSReportParser
 
 
 __author__ = 'Jay Paz'
@@ -265,6 +266,8 @@ def import_parser_factory(file, test, active, verified, scan_type=None):
         parser = YarnAuditParser(file, test)
     elif scan_type == 'BugCrowd Scan':
         parser = BugCrowdCSVParser(file, test)
+    elif scan_type == 'CCVS Report':
+        parser = CCVSReportParser(file, test)
     else:
         raise ValueError('Unknown Test Type')
 
