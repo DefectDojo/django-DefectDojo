b31af021272d6420249cccf9b069c98d9c685a1e
Kareem Khan <kareem.khan@digitalasset.com>
2020-06-25 14:02:17 -0400

    Fixed query for jira config.

--- dojo/finding/views.py
+++ dojo/finding/views.py
@@ -382,10 +382,13 @@ def close_finding(request, fid):
                 finding.last_reviewed = finding.mitigated
                 finding.last_reviewed_by = request.user
                 finding.endpoints.clear()
-                push_all_issues_enabled = finding.test.engagement.product.jira_pkey_set.first().push_all_issues
-                if push_all_issues_enabled and JIRA_Issue.objects.filter(finding=finding).exists():
-                    finding.save(push_to_jira=True)
-                else:
+                try:
+                    jpkey = JIRA_PKey.objects.get(product=finding.test.engagement.product)
+                    if jpkey.push_all_issues and JIRA_Issue.objects.filter(finding=finding).exists():
+                        finding.save(push_to_jira=True)
+                    else:
+                        finding.save()
+                except JIRA_PKey.DoesNotExist:
                     finding.save()
 
                 messages.add_message(
@@ -500,11 +503,14 @@ def reopen_finding(request, fid):
     finding.mitigated_by = request.user
     finding.last_reviewed = finding.mitigated
     finding.last_reviewed_by = request.user
-    push_all_issues_enabled = finding.test.engagement.product.jira_pkey_set.first().push_all_issues
-    if push_all_issues_enabled and JIRA_Issue.objects.filter(finding=finding).exists():
-        finding.save(push_to_jira=True)
-    else:
-        finding.save()
+    try:
+       jpkey = JIRA_PKey.objects.get(product=finding.test.engagement.product)
+       if jpkey.push_all_issues and JIRA_Issue.objects.filter(finding=finding).exists():
+           finding.save(push_to_jira=True)
+       else:
+           finding.save()
+   except JIRA_PKey.DoesNotExist:
+       finding.save()
 
     reopen_external_issue_task.delay(finding, 're-opened by defectdojo', 'github')
 
