9aa270e810552faefd1252e0fcf78522655efcc4
valentijnscholten <valentijnscholten@gmail.com>
2020-07-06 09:13:40 +0200

    JIRA/github improvements and fixes July 2020 (#2630)
    bugfixes:
    
    bulk edit with push to github: create github issue instead of JIRA issue
    improvements:
    
    set duedate field based on SLA targets when creating JIRA issue and if SLA is enabled in DD
    display jira issue + url in edit_finding
    display github issue + url in edit_finding (fixes #2607)
    simplify displaying of jira_issue and github_issue in view_finding
    send full url to DD finding to JIRA / GitHub instead of only finding id
    send labels to JIRA in the same call as creating/updating an issue
    maintenance
    
    if a user has set block_execution set to True in their profile, the JIRA/GitHub actions will now run in uwsgi and not in celery. (similar to what happens with dedupe, notifications etc)
    simplify code around accessing jira/github issue and configuration in view/edit finding.

--- dojo/finding/views.py
+++ dojo/finding/views.py
@@ -40,7 +40,7 @@ from dojo.models import Finding, Notes, NoteHistory, Note_Type, \
 from dojo.utils import get_page_items, add_breadcrumb, FileIterWrapper, process_notifications, \
     add_comment, jira_get_resolution_id, jira_change_resolution_id, get_jira_connection, \
     get_system_setting, apply_cwe_to_template, Product_Tab, calculate_grade, log_jira_alert, \
-    redirect_to_return_url_or_else, get_return_url
+    redirect_to_return_url_or_else, get_return_url, add_issue, update_issue, add_external_issue, update_external_issue
 from dojo.notifications.helper import create_notification
 
 from dojo.tasks import add_issue_task, update_issue_task, update_external_issue_task, add_comment_task, \
@@ -235,28 +235,6 @@ def view_finding(request, fid):
     except Finding_Template.DoesNotExist:
         pass
 
-    try:
-        jissue = JIRA_Issue.objects.get(finding=finding)
-    except:
-        jissue = None
-        pass
-    try:
-        jpkey = JIRA_PKey.objects.get(product=finding.test.engagement.product)
-        jconf = jpkey.conf
-    except:
-        jconf = None
-        pass
-    try:
-        gissue = GITHUB_Issue.objects.get(finding=finding)
-    except:
-        gissue = None
-        pass
-    try:
-        github_pkey = GITHUB_PKey.objects.get(product=finding.test.engagement.product)
-        gconf = github_pkey.git_conf
-    except:
-        gconf = None
-        pass
     dojo_user = get_object_or_404(Dojo_User, id=user.id)
     if user.is_staff or user in finding.test.engagement.product.authorized_users.all(
     ):
@@ -287,7 +265,7 @@ def view_finding(request, fid):
             finding.last_reviewed = new_note.date
             finding.last_reviewed_by = user
             finding.save()
-            if jissue is not None:
+            if finding.has_jira_issue():
                 add_comment_task(finding, new_note)
             if note_type_activation:
                 form = FindingNoteForm(available_note_types=available_note_types)
@@ -326,10 +304,6 @@ def view_finding(request, fid):
             'product_tab': product_tab,
             'finding': finding,
             'burp_request': burp_request,
-            'jissue': jissue,
-            'jconf': jconf,
-            'gissue': gissue,
-            'gconf': gconf,
             'cred_finding': cred_finding,
             'creds': creds,
             'cred_engagement': cred_engagement,
@@ -597,17 +571,7 @@ def edit_finding(request, fid):
         push_all_issues_enabled = finding.test.engagement.product.jira_pkey_set.first().push_all_issues
         jform = JIRAFindingForm(enabled=push_all_issues_enabled, prefix='jiraform')
 
-    try:
-        jissue = JIRA_Issue.objects.get(finding=finding)
-        jira_link_exists = True
-    except:
-        jira_link_exists = False
-
-    try:
-        gissue = GITHUB_Issue.objects.get(finding=finding)
-        github_enabled = True
-    except:
-        github_enabled = False
+    github_enabled = finding.has_github_issue()
 
     gform = None
     if get_system_setting('enable_github'):
@@ -699,13 +663,15 @@ def edit_finding(request, fid):
                     request.POST, prefix='githubform', enabled=github_enabled)
                 if gform.is_valid():
                     if GITHUB_Issue.objects.filter(finding=new_finding).exists():
-                        update_external_issue_task.delay(
-                            new_finding, old_status,
-                            'github')
+                        if Dojo_User.wants_block_execution(request.user):
+                            update_external_issue(new_finding, old_status, 'github')
+                        else:
+                            update_external_issue_task.delay(new_finding, old_status, 'github')
                     else:
-                        add_external_issue_task.delay(
-                            new_finding,
-                            'github')
+                        if Dojo_User.wants_block_execution(request.user):
+                            add_external_issue(new_finding, 'github')
+                        else:
+                            add_external_issue_task.delay(new_finding, 'github')
 
             new_finding.save(push_to_jira=push_to_jira)
 
@@ -1201,8 +1167,10 @@ def promote_to_finding(request, fid):
                     enabled=GITHUB_PKey.objects.get(
                         product=test.engagement.product).push_all_issues)
                 if gform.is_valid():
-                    add_external_issue_task.delay(
-                        new_finding, 'github')
+                    if Dojo_User.wants_block_execution(request.user):
+                        add_external_issue(new_finding, 'github')
+                    else:
+                        add_external_issue_task.delay(new_finding, 'github')
 
             messages.add_message(
                 request,
@@ -1754,9 +1722,15 @@ def finding_bulk_update_all(request, pid=None):
                         old_status = finding.status()
                         if form.cleaned_data['push_to_github']:
                             if GITHUB_Issue.objects.filter(finding=finding).exists():
-                                update_issue_task.delay(finding, old_status, True)
+                                if Dojo_User.wants_block_execution(request.user):
+                                    update_external_issue(finding, old_status, 'github')
+                                else:
+                                    update_external_issue_task.delay(finding, old_status, 'github')
                             else:
-                                add_external_issue_task.delay(finding, 'github')
+                                if Dojo_User.wants_block_execution(request.user):
+                                    add_external_issue(finding, 'github')
+                                else:
+                                    add_external_issue_task.delay(finding, 'github')
 
                 if form.cleaned_data['tags']:
                     for finding in finds:
@@ -1787,9 +1761,15 @@ def finding_bulk_update_all(request, pid=None):
                         #     product=finding.test.engagement.product).push_all_issues
                         if form.cleaned_data['push_to_jira'] or push_anyway:
                             if JIRA_Issue.objects.filter(finding=finding).exists():
-                                update_issue_task.delay(finding, True)
+                                if request.user.usercontactinfo.block_execution:
+                                    update_issue(finding, True)
+                                else:
+                                    update_issue_task.delay(finding, True)
                             else:
-                                add_issue_task.delay(finding, True)
+                                if request.user.usercontactinfo.block_execution:
+                                    add_issue(finding, True)
+                                else:
+                                    add_issue_task.delay(finding, True)
 
                 messages.add_message(request,
                                      messages.SUCCESS,
