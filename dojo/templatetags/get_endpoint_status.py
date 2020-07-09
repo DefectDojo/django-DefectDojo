fba5ce671dca3b52e1a16481d8c2e26787acdd40
Cody Maffucci <66391448+cody-m-tibco@users.noreply.github.com>
2020-07-08 20:21:00 -0500

    Partial endpoint remediation (#2632)
    * Partial endpoint remediation
    
    * AccessLint Appeasement
    
    * Sneaky Flake8
    
    * Remediated -> Mitgated + endpoint snippet
    
    * Add permissions to status update
    
    * Add re-import/close_finding support + remove edpoint_status from form
    
    * Add labels for AccessLint
    
    * Fix botched conflict
    
    Co-authored-by: Cody Maffucci <cmaffucc@cmaffucc-MBP.local>

--- dojo/templatetags/get_endpoint_status.py
+++ dojo/templatetags/get_endpoint_status.py
@@ -1,12 +1,53 @@
 from django import template
+from dojo.models import Endpoint_Status
 register = template.Library()
 
 
 @register.filter(name='get_vulnerable_endpoints')
-def get_vulnerable_endpoints(endpoints):
-    return endpoints.filter(remediated=False)
+def get_vulnerable_endpoints(finding):
+    status_list = finding.endpoint_status.all().filter(mitigated=False)
+    return [status.endpoint for status in status_list]
 
 
-@register.filter(name='get_remediated_endpoints')
-def get_remediated_endpoints(endpoints):
-    return endpoints.filter(remediated=True)
+@register.filter(name='get_mitigated_endpoints')
+def get_mitigated_endpoints(finding):
+    status_list = finding.endpoint_status.all().filter(mitigated=True)
+    return [status.endpoint for status in status_list]
+
+
+@register.filter
+def endpoint_display_status(endpoint, finding):
+    status = Endpoint_Status.objects.get(endpoint=endpoint, finding=finding)
+    if status.false_positive:
+        return "False Positive"
+    if status.risk_accepted:
+        return "Risk Accepted"
+    if status.out_of_scope:
+        return "Out of Scope"
+    if status.mitigated:
+        return "Mitigated"
+    return "Active"
+
+
+@register.filter
+def endpoint_update_time(endpoint, finding):
+    status = Endpoint_Status.objects.get(endpoint=endpoint, finding=finding)
+    return status.last_modified
+
+
+@register.filter
+def endpoint_date(endpoint, finding):
+    status = Endpoint_Status.objects.get(endpoint=endpoint, finding=finding)
+    return status.date
+
+
+@register.filter
+def endpoint_mitigator(endpoint, finding):
+    status = Endpoint_Status.objects.get(endpoint=endpoint, finding=finding)
+    return status.mitigated_by
+
+
+@register.filter
+def endpoint_mitigated_time(endpoint, finding):
+    status = Endpoint_Status.objects.get(endpoint=endpoint, finding=finding)
+    return status.mitigated_time
