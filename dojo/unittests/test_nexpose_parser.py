c40c2d5157bae9f626d5c1d5e820c380493c962b
Sunat.P <31818562+SunatP@users.noreply.github.com>
2020-07-13 15:36:44 +0700

    Update test_nexpose_parser.py

--- dojo/unittests/test_nexpose_parser.py
+++ dojo/unittests/test_nexpose_parser.py
@@ -7,14 +7,14 @@ class TestNexposeParser(TestCase):
     def test_nexpose_parser_has_no_finding(self):
         testfile = open("dojo/unittests/scans/nexpose/no_vuln.xml")
         parser = NexposeFullXmlParser(testfile, Test())
-        self.assertEqual(0, len(self.parser.items))
+        self.assertEqual(0, len(parser.items))
 
     def test_nexpose_parser_has_one_finding(self):
         testfile = open("dojo/unittests/scans/nexpose/one_vuln.xml")
         parser = NexposeFullXmlParser(testfile, Test())
-        self.assertEqual(1, len(self.parser.items))
+        self.assertEqual(1, len(parser.items))
 
     def test_nexpose_parser_has_many_finding(self):
         testfile = open("dojo/unittests/scans/nexpose/many_vulns.xml")
         parser = NexposeFullXmlParser(testfile, Test())
-        self.assertEqual(157, len(self.parser.items))
+        self.assertEqual(157, len(parser.items))
