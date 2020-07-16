0cae94df3290da0eb875ec75565177d2a7e27f87
Valentijn Scholten <valentijnscholten@gmail.com>
2020-07-16 17:50:07 +0200

    k8s: check login page after deployment

--- travis/script.sh
+++ travis/script.sh
@@ -148,6 +148,7 @@ if [ -z "${TEST}" ]; then
 
   echo "Testing DefectDojo Service"
   kubectl port-forward --namespace=default service/defectdojo-django 8080:80
+  kubectl describe --namespace=default service/defectdojo-django
 
   echo '::1       defectdojo.default.minikube.local' | sudo tee -a /etc/hosts
   echo '127.0.0.1 defectdojo.default.minikube.local' | sudo tee -a /etc/hosts
