0c3f33509f9b6f5721d70e807dda4323d3041f93
Fred Blaise <fred.blaise@protonmail.com>
2020-07-20 19:36:12 +0200

    comment

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -381,7 +381,7 @@ SLA_NOTIFY_ACTIVE = False  # this will include 'verified' findings as well.
 SLA_NOTIFY_ACTIVE_VERIFIED_ONLY = True
 SLA_NOTIFY_WITH_JIRA_ONLY = False  # this will include 'verified' findings as well.
 SLA_NOTIFY_PRE_BREACH = 3  # in days, notification until date of breach
-SLA_NOTIFY_POST_BREACH = 7  # in days, skip over these old findings you don't care about anymore.
+SLA_NOTIFY_POST_BREACH = 7  # in days, skip notifications for findings that go past
 
 LOGIN_EXEMPT_URLS = (
     r'^%sstatic/' % URL_PREFIX,
