20510f1d2352b3cd9db53f0af26cbb241ec83b15
Al S <xee5ch.gh.al@il5.in>
2020-07-23 23:52:11 -0400

    Update test info.

--- dojo/unittests/test_aws_security_hub.py
+++ dojo/unittests/test_aws_security_hub.py
@@ -1,7 +1,7 @@
 import os.path
 
 from django.test import TestCase
-from dojo.tools.
+from dojo.tools.securityhub.parser import AwsSecurityFindingFormatParser
 from dojo.models import Test
 
 
@@ -11,14 +11,24 @@ def sample_path(file_name):
 
 class AwsSecurityFindingFormatParser(TestCase):
 
-    def setUp(self):
-        self.dojo_test = Test()
-
     def test_no_findings(self):
-        pass
+        parser = AwsSecurityFindingFormatParser(None, Test())
+        self.assertEqual(0, len(parser.items))
 
     def test_one_finding(self):
-        pass
+        with open(sample_path('one_finding.json')) as test_file:
+            parser = AwsSecurityFindingFormatParser(test_file, Test())
+
+        self.assertEqual(1, len(parser.items))
 
     def test_many_findings(self):
-        pass
+        with open(sample_path('many_findings.json')) as test_file:
+            parser = AwsSecurityFindingFormatParser(test_file, Test())
+
+        self.assertEqual(3, len(parser.items))
+
+    def test_repeated_findings(self):
+        with open(sample_path('repeated_findings.json')) as test_file:
+            parser = AwsSecurityFindingFormatParser(test_file, Test())
+
+        self.assertEqual(1, len(parser.items))
