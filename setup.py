d8fb1cd38e981a2e24465dcc579409607274e137
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2020-07-27 13:20:04 -0500

    Update master version (#2731)
    * Update dev version to 1.8.0-dev
    
    * Update to version 1.7.1
    
    Co-authored-by: Maffooch <codymaffucci@Codys-MacBook-Pro.local>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='DefectDojo',
-    version='1.7.0',
+    version='1.7.1',
     author='Greg Anderson',
     description="Tool for managing vulnerability engagements",
     install_requires=[
