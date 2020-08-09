622af27714a05393c449b765f18c42c4fce58ff5
Al S <xee5ch.gh.al@il5.in>
2020-08-09 19:23:57 -0400

    String up ingestion and migration PK.

--- dojo/tools/semgrep/parser.py
+++ dojo/tools/semgrep/parser.py
@@ -6,10 +6,9 @@ from .models import SemgrepJSONResult
 class SemgrepJSONParser(object):
     def __init__(self, filehandle, test):
         tree = self.parse_json(filehandle)
-        self.dupes = dict()
 
         if tree:
-            self.items = list()
+            self.items = get_items(tree, test)
         else:
             self.items = list()
 
@@ -27,35 +26,31 @@ class SemgrepJSONParser(object):
         return tree
 
     def get_items(self, tree, test):
-        items = {}
+        dupes = dict()
         results = tree.get('results')
 
         if not results:
             return list()
 
         for finding in results():
-            item = get_item(finding)
-
-
-def get_item(finding, test):
-    f = SemgrepJSONResult(finding)
-
-    return Finding(
-        static_finding = True
-        title = None
-        severity = f.severity
-        description = f.message
-        mitigation = f.fix
-        references = f.references
-        file_path = f.path
-        line = f.start + f.end
-        cve = None
-        cwe = f.cwe
-        active = True
-        verified = False
-        false_p = False
-        duplicate = False
-        out_of_scope = False
-        impact = None
-        test = self.test
-    )
+            f = SemgrepJSONResult(finding)
+            dupes[f.dedupe_key] = Finding(
+                title = f.check_id
+                severity = f.severity
+                description = f.message
+                mitigation = f.fix
+                references = f.references
+                file_path = f.path
+                line = ' '.join([f.start, f.end])
+                cve = None
+                cwe = f.cwe
+                active = True
+                verified = False
+                false_p = False
+                duplicate = False
+                out_of_scope = False
+                impact = 'No impact provided'
+                static_finding = True
+                test = self.test
+            )
+            return list(dupes.values())
