3f80134c7bd41773cf6309168ae0059b0baae4bc
Sebastien gioria <seb@gioria.org>
2020-08-14 15:57:05 +0200

    First working version, with some refactoring/rework. Need more work because some values are 'static' for testing

--- dojo/tools/semgrep/models.py
+++ dojo/tools/semgrep/models.py
@@ -1,38 +1,47 @@
 from dojo.tools.semgrep.helpers import (create_dedupe_key,
-                     format_code,
-                     format_linenums,
-                     format_message,
-                     format_metavarss)
+                                        format_code,
+                                        format_linenums,
+                                        format_message,
+                                        format_metavars)
 
 class SemgrepJSONResult:
 
-    def __init__(self, check_id='', path='', start={}, end={}, extra={}):
-        self.check_id = check_id
+    def __init__(self,  path='', start={}, end={}, extra={}):
         self.path = path
         self.start = format_linenums(start)
         self.end = format_linenums(end)
-        self.dedupe_key = create_dedupe_key(
-            self.check_id,
-            self.path,
-            self.start,
-            self.end
-        )
+
+        self.severity = "Info"
+        self.cwe=42
+        self.message="Detected by semgrep rule"
+        self.fix="None"
+        self.lines="None"
+        self.references="Detected by semgrep rule"
+        self.test="SemGrep rule"
 
         if not extra:
             return
 
         metadata, metavars = extra.get('metadata'), extra.get('metavars')
-
         self.fix = format_code(extra.get('fix'))
         self.lines = format_code(extra.get('lines'))
         self.message = format_message(extra.get('message'))
-        self.severity = extra.get('severity')
 
         if not metadata:
             return
 
         self.cwe = metadata.get('cwe')
         self.owasp = metadata.get('owasp')
+
+        # Convert Semgrep severity to defectDojo Severity
+        semSeverity = metadata.get('severity')
+
+        if semSeverity=="WARNING":
+            self.severity="Low"
+
+        if semSeverity=="ERROR":
+            self.severity="High"
+
         self.references = format_message(metadata.get('message'))
         self.source_rule_url = metadata.get('source-rule-url')
 
