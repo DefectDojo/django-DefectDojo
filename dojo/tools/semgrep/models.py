05b18c7691d4a9d9bc13bc904cba46c29566a53e
Sebastien gioria <seb@gioria.org>
2020-08-17 11:53:41 +0200

    First total working version. Rewrite of some call to the parser, add parameters (from a forget of last commit). Tested with the examples upload by @cristiano-corrado on #2720 and no 500 error detected. Enjoy !

--- dojo/tools/semgrep/models.py
+++ dojo/tools/semgrep/models.py
@@ -1,24 +1,33 @@
-from dojo.tools.semgrep.helpers import (format_code,
+from dojo.tools.semgrep.helpers import (create_dedupe_key,
+                                        format_code,
                                         format_linenums,
                                         format_message,
                                         format_metavars)
 
-
 class SemgrepJSONResult:
-    def __init__(self, path='', start={}, end={}, extra={}):
-        self.path = path
-        self.start = format_linenums(start)
-        self.end = format_linenums(end)
 
+    def __init__(self, extra=None, path='', start=None, end=None):
+        self.path = path
+
+        self.start = 0
+        self.end = 0
         self.severity = "Info"
-        self.cwe = 42
+        self.title = "SemGrep detection rule"
         self.message = "Detected by semgrep rule"
         self.fix = "None"
         self.lines = "None"
         self.references = "Detected by semgrep rule"
         self.test = "SemGrep rule"
+        self.description = "Detected by semgrep rules"
+        self.cwe = 0
 
-        if not extra:
+
+        if start is not None:
+            self.start = start['line']
+        if end is not None:
+            self.end = end['line']
+
+        if extra is None:
             return
 
         metadata, metavars = extra.get('metadata'), extra.get('metavars')
@@ -29,7 +38,9 @@ class SemgrepJSONResult:
         if not metadata:
             return
 
-        self.cwe = metadata.get('cwe')
+        #parse CWE
+        self.title = metadata.get("cwe").partition(':')[2]
+        self.cwe = metadata.get("cwe").partition(':')[0].partition('-')[2]
         self.owasp = metadata.get('owasp')
 
         # Convert Semgrep severity to defectDojo Severity
