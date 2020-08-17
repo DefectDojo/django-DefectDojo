5bd5a1ad9c4588ee7435e4590c6c0f559b43b262
Sebastien gioria <seb@gioria.org>
2020-08-17 12:42:36 +0200

    Make travis happy

--- dojo/tools/semgrep/models.py
+++ dojo/tools/semgrep/models.py
@@ -1,8 +1,4 @@
-from dojo.tools.semgrep.helpers import (create_dedupe_key,
-                                        format_code,
-                                        format_linenums,
-                                        format_message,
-                                        format_metavars)
+
 
 class SemgrepJSONResult:
 
@@ -21,7 +17,6 @@ class SemgrepJSONResult:
         self.description = "Detected by semgrep rules"
         self.cwe = 0
 
-
         if start is not None:
             self.start = start['line']
         if end is not None:
@@ -31,14 +26,14 @@ class SemgrepJSONResult:
             return
 
         metadata, metavars = extra.get('metadata'), extra.get('metavars')
-        self.fix = format_code(extra.get('fix'))
-        self.lines = format_code(extra.get('lines'))
-        self.message = format_message(extra.get('message'))
+        self.fix = extra.get('fix')
+        self.lines = extra.get('lines')
+        self.message = extra.get('message')
 
         if not metadata:
             return
 
-        #parse CWE
+        # parse CWE
         self.title = metadata.get("cwe").partition(':')[2]
         self.cwe = metadata.get("cwe").partition(':')[0].partition('-')[2]
         self.owasp = metadata.get('owasp')
@@ -52,10 +47,10 @@ class SemgrepJSONResult:
         if semSeverity == "ERROR":
             self.severity = "High"
 
-        self.references = format_message(metadata.get('message'))
+        self.references = metadata.get('message')
         self.source_rule_url = metadata.get('source-rule-url')
 
         if not metavars:
             return
 
-        self.metavars = format_metavars(extra.get('metavars'))
+        self.metavars = extra.get('metavars')
