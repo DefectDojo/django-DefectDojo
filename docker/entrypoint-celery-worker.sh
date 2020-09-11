6a961c40fbccf29d63f8d8bac83d6e0b80f0d5fd
Fred Blaise <fred.blaise@protonmail.com>
2020-09-11 09:01:10 +0200

    conflict
    Signed-off-by: Fred Blaise <fred.blaise@protonmail.com>

--- docker/entrypoint-celery-worker.sh
+++ docker/entrypoint-celery-worker.sh
@@ -15,9 +15,8 @@ if [ "${DD_CELERY_WORKER_POOL_TYPE}" = "prefork" ]; then
     --prefetch-multiplier=${DD_CELERY_WORKER_PREFETCH_MULTIPLIER}"
 fi
 
-C_FORCE_ROOT=true exec celery \
+exec celery worker \
   --app=dojo \
-  worker \
   --loglevel="${DD_CELERY_LOG_LEVEL}" \
   --pool="${DD_CELERY_WORKER_POOL_TYPE}" \
   --concurrency=${DD_CELERY_WORKER_CONCURRENCY:-1} \
