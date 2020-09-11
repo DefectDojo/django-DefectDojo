14f2c98a08180d4e3e6af3f5d28b8ffe4ec73db2
Fred Blaise <fred.blaise@protonmail.com>
2020-09-11 08:59:04 +0200

    missing backslash

--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -7,6 +7,6 @@ exec uwsgi \
   --protocol uwsgi \
   --enable-threads \
   --processes ${DD_UWSGI_NUM_OF_PROCESSES:-2} \
-  --threads ${DD_UWSGI_NUM_OF_THREADS:-2} 
+  --threads ${DD_UWSGI_NUM_OF_THREADS:-2} \
   --wsgi dojo.wsgi:application \
   --buffer-size="${DD_UWSGI_BUFFER_SIZE:-4096}"
