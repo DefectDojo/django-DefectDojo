2271790d0e27046875cd97a571ec82fd8c4c2a9d
Reinier Vegter <info@reiniervegter.dev>
2020-09-25 15:13:02 +0200

    Veracode: default dedup strategy DEDUPE_ALGO_UNIQUE_ID_FROM_TOOL_OR_HASH_CODE

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -768,7 +768,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'PHP Symfony Security Check': DEDUPE_ALGO_HASH_CODE,
     'Clair Scan': DEDUPE_ALGO_HASH_CODE,
     'Clair Klar Scan': DEDUPE_ALGO_HASH_CODE,
-    'Veracode Scan': DEDUPE_ALGO_UNIQUE_ID_FROM_TOOL,
+    'Veracode Scan': DEDUPE_ALGO_UNIQUE_ID_FROM_TOOL_OR_HASH_CODE,
     # for backwards compatibility because someone decided to rename this scanner:
     'Symfony Security Check': DEDUPE_ALGO_HASH_CODE,
     'DSOP Scan': DEDUPE_ALGO_HASH_CODE,
