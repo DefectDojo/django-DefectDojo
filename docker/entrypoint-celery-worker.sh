2ac7e0e44860a7c2e18fbf21d0007e1713806bd2
Fred Blaise <madchap@users.noreply.github.com>
2020-09-25 17:43:24 +0200

    docker-compose and helm: Add variables for celeryworker prefork and uwsgi thread/procs control (#2813)

--- docker/entrypoint-celery-worker.sh
+++ docker/entrypoint-celery-worker.sh
@@ -11,8 +11,14 @@ do
 done
 echo
 
+if [ "${DD_CELERY_WORKER_POOL_TYPE}" = "prefork" ]; then
+  EXTRA_PARAMS="--autoscale=${DD_CELERY_WORKER_AUTOSCALE_MAX},${DD_CELERY_WORKER_AUTOSCALE_MIN}
+    --prefetch-multiplier=${DD_CELERY_WORKER_PREFETCH_MULTIPLIER}"
+fi
+
 exec celery worker \
-    --app=dojo \
-    --loglevel="${DD_CELERY_LOG_LEVEL}" \
-    --pool=solo \
-    --concurrency=1
+  --app=dojo \
+  --loglevel="${DD_CELERY_LOG_LEVEL}" \
+  --pool="${DD_CELERY_WORKER_POOL_TYPE}" \
+  --concurrency=${DD_CELERY_WORKER_CONCURRENCY:-1} \
+  ${EXTRA_PARAMS}
