a4f2c2878273fb8c2e5d1ec06892d889a9445d52
Fred Blaise <madchap@users.noreply.github.com>
2020-09-25 17:43:24 +0200

    docker-compose and helm: Add variables for celeryworker prefork and uwsgi thread/procs control (#2813)

--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -6,7 +6,7 @@ exec uwsgi \
   "--${DD_UWSGI_MODE}" "${DD_UWSGI_ENDPOINT}" \
   --protocol uwsgi \
   --enable-threads \
-  --processes 2 \
-  --threads 2 \
+  --processes ${DD_UWSGI_NUM_OF_PROCESSES:-2} \
+  --threads ${DD_UWSGI_NUM_OF_THREADS:-2} \
   --wsgi dojo.wsgi:application \
   --buffer-size="${DD_UWSGI_BUFFER_SIZE:-4096}"
