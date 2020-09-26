e1a57ef1a5874f200adc0bb26429ed93774916f4
Valentijn Scholten <valentijnscholten@gmail.com>
2020-09-26 17:04:25 +0200

    github actions: test

--- flake8_errors_test.py
+++
@@ -1,3 +0,0 @@
-print("hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo hallo")
-
-print("hoi")
