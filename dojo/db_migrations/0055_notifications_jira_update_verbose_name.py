b4dae8e12710a3b2afcf1812de77ebb97198d9c9
Valentijn Scholten <valentijnscholten@gmail.com>
2020-09-27 15:55:11 +0200

    fix migrations from some past shananigans

--- dojo/db_migrations/0055_notifications_jira_update_verbose_name.py
+++ dojo/db_migrations/0055_notifications_jira_update_verbose_name.py
@@ -1,5 +1,6 @@
-from django.db import migrations
+from django.db import migrations, models
 import multiselectfield.db.fields
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -14,4 +15,20 @@ class Migration(migrations.Migration):
             name='jira_update',
             field=multiselectfield.db.fields.MultiSelectField(blank=True, choices=[('slack', 'slack'), ('hipchat', 'hipchat'), ('mail', 'mail'), ('alert', 'alert')], default='alert', help_text='JIRA sync happens in the background, errors will be shown as notifications/alerts so make sure to subscribe', max_length=24, verbose_name='JIRA problems'),
         ),
+        migrations.AlterField(
+            model_name='alerts',
+            name='title',
+            field=models.CharField(default='', max_length=200),
+        ),
+        migrations.AlterField(
+            model_name='jira_pkey',
+            name='conf',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='dojo.JIRA_Conf', verbose_name='JIRA Configuration'),
+        ),
+        migrations.AlterField(
+            model_name='jira_pkey',
+            name='product',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='dojo.Product'),
+        ),
+
     ]
