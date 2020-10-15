3c66357d37477961b093ae3c19c255d1af638e1d
Ricardo Meulendijks <ricardo.meulendijks@isaac.nl>
2020-10-15 10:48:25 +0200

    removed distinct from component views.py

--- dojo/components/views.py
+++ dojo/components/views.py
@@ -8,7 +8,7 @@ from dojo.filters import ComponentFilter
 def components(request):
     add_breadcrumb(title='Components', top_level=True, request=request)
     component_query = Finding.objects.filter().values("component_name", "component_version")
-    component_query = component_query.order_by('component_name', 'component_version').distinct()
+    component_query = component_query.order_by('component_name', 'component_version')
     component_query = component_query.annotate(total=Count('id')).order_by('component_name', 'component_version')
     component_query = component_query.annotate(active=Count('id', filter=Q(active=True)))
     component_query = component_query.annotate(duplicate=(Count('id', filter=Q(duplicate=True))))
