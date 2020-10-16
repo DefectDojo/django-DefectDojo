2c8f2d8725238839e67280f007d7556f60a25b32
Cody Maffucci <66391448+cody-m-tibco@users.noreply.github.com>
2020-10-16 10:41:24 -0500

    Make Product Type reports more descriptive (#2783)
    * Make Product Type reports more descriptive
    
    * AccessLint Appeasements
    
    * Update display_tags.py
    
    Co-authored-by: Cody Maffucci <cmaffucc@cmaffucc-MBP.local>
    Co-authored-by: Cody Maffucci <46459665+Maffooch@users.noreply.github.com>

--- dojo/templatetags/display_tags.py
+++ dojo/templatetags/display_tags.py
@@ -863,6 +863,10 @@ def finding_related_action_title(related_action):
     return finding_related_action_title_dict.get(related_action, '')
 
 
+def product_findings(product):
+    return Finding.objects.filter(test__engagement__product=product)
+
+
 @register.filter
 def class_name(value):
     return value.__class__.__name__
