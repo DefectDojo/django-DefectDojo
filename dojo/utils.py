46912e3d87388292f5cf5e9fd25773b1006830dc
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2020-10-19 14:08:18 -0500

    Add ssl verification variable to JIRA integration (#3016)
    Co-authored-by: Maffooch <codymaffucci@Codys-MacBook-Pro.local>

--- dojo/utils.py
+++ dojo/utils.py
@@ -1222,7 +1222,8 @@ def get_jira_connection(finding):
         if jira_conf is not None:
             jira = JIRA(
                 server=jira_conf.url,
-                basic_auth=(jira_conf.username, jira_conf.password))
+                basic_auth=(jira_conf.username, jira_conf.password),
+                verify=settings.JIRA_SSL_VERIFY)
     except JIRA_PKey.DoesNotExist:
         pass
     return jira
@@ -1370,7 +1371,8 @@ def add_jira_issue(find, push_to_jira):
                 JIRAError.log_to_tempfile = False
                 jira = JIRA(
                     server=jira_conf.url,
-                    basic_auth=(jira_conf.username, jira_conf.password))
+                    basic_auth=(jira_conf.username, jira_conf.password),
+                    verify=settings.JIRA_SSL_VERIFY)
 
                 meta = None
 
@@ -1514,7 +1516,8 @@ def update_jira_issue(find, push_to_jira):
             JIRAError.log_to_tempfile = False
             jira = JIRA(
                 server=jira_conf.url,
-                basic_auth=(jira_conf.username, jira_conf.password))
+                basic_auth=(jira_conf.username, jira_conf.password),
+                verify=settings.JIRA_SSL_VERIFY)
             issue = jira.issue(j_issue.jira_id)
 
             meta = None
@@ -1627,7 +1630,8 @@ def update_epic(eng, push_to_jira):
         try:
             jira = JIRA(
                 server=jira_conf.url,
-                basic_auth=(jira_conf.username, jira_conf.password))
+                basic_auth=(jira_conf.username, jira_conf.password),
+                verify=settings.JIRA_SSL_VERIFY)
             j_issue = JIRA_Issue.objects.get(engagement=eng)
             issue = jira.issue(j_issue.jira_id)
             issue.update(summary=eng.name, description=eng.name)
@@ -1657,7 +1661,8 @@ def add_epic(eng, push_to_jira):
         try:
             jira = JIRA(
                 server=jira_conf.url,
-                basic_auth=(jira_conf.username, jira_conf.password))
+                basic_auth=(jira_conf.username, jira_conf.password),
+                verify=settings.JIRA_SSL_VERIFY)
             new_issue = jira.create_issue(fields=issue_dict)
             j_issue = JIRA_Issue(
                 jira_id=new_issue.id,
@@ -1681,7 +1686,8 @@ def jira_get_issue(jpkey, issue_key):
     try:
         jira = JIRA(
             server=jira_conf.url,
-            basic_auth=(jira_conf.username, jira_conf.password))
+            basic_auth=(jira_conf.username, jira_conf.password),
+            verify=settings.JIRA_SSL_VERIFY)
         issue = jira.issue(issue_key)
         # print(vars(issue))
         return issue
@@ -1706,7 +1712,8 @@ def add_comment(find, note, force_push=False):
                 try:
                     jira = JIRA(
                         server=jira_conf.url,
-                        basic_auth=(jira_conf.username, jira_conf.password))
+                        basic_auth=(jira_conf.username, jira_conf.password),
+                        verify=settings.JIRA_SSL_VERIFY)
                     j_issue = JIRA_Issue.objects.get(finding=find)
                     jira.add_comment(
                         j_issue.jira_id,
@@ -1722,7 +1729,8 @@ def add_simple_jira_comment(jira_conf, jira_issue, comment):
     try:
         jira = JIRA(
             server=jira_conf.url,
-            basic_auth=(jira_conf.username, jira_conf.password)
+            basic_auth=(jira_conf.username, jira_conf.password),
+            verify=settings.JIRA_SSL_VERIFY
         )
         jira.add_comment(
             jira_issue.jira_id, comment
