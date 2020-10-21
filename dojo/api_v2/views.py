570e8e6b6d832948b6b5afc0bef61bc650d245aa
Romain Aviolat <r.aviolat@gmail.com>
2020-10-21 20:52:12 +0200

    apiv2: add doc for finding's related_fields

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -7,7 +7,9 @@ from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
 from django_filters.rest_framework import DjangoFilterBackend
+from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema, no_body
+from drf_yasg import openapi
 import base64
 from dojo.engagement.services import close_engagement, reopen_engagement
 from dojo.models import Product, Product_Type, Engagement, Test, Test_Type, Finding, \
@@ -228,6 +230,20 @@ class FindingTemplatesViewSet(mixins.ListModelMixin,
     #         return Finding_Template.objects.all()
 
 
+def _finding_related_fields_decorator():
+    return swagger_auto_schema(
+        responses={status.HTTP_200_OK: serializers.FindingSerializer},
+        manual_parameters=[
+            openapi.Parameter(
+                name="related_fields",
+                in_=openapi.IN_QUERY,
+                description="Expand finding external relations (engagement, environment, product, product_type, test, test_type)",
+                type=openapi.TYPE_BOOLEAN)
+        ])
+
+
+@method_decorator(name="list", decorator=_finding_related_fields_decorator())
+@method_decorator(name="retrieve", decorator=_finding_related_fields_decorator())
 class FindingViewSet(mixins.ListModelMixin,
                      mixins.RetrieveModelMixin,
                      mixins.UpdateModelMixin,
