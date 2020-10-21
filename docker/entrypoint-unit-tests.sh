5f83b18779ea5eb56c532a4ab695662da42f8fea
Valentijn Scholten <valentijnscholten@gmail.com>
2020-10-21 18:12:57 +0200

    github actions: add unit tests

--- docker/entrypoint-unit-tests.sh
+++ docker/entrypoint-unit-tests.sh
@@ -10,6 +10,12 @@ cd /app
 # Unset the database URL so that we can force the DD_TEST_DATABASE_NAME (see django "DATABASES" configuration in settings.dist.py)
 unset DD_DATABASE_URL
 
+TARGET_SETTINGS_FILE=dojo/settings/settings.py
+if [ ! -f ${TARGET_SETTINGS_FILE} ]; then
+  echo "Creating settings.py"
+  cp dojo/settings/settings.dist.py dojo/settings/settings.py
+fi
+
 python3 manage.py makemigrations --no-input --check --dry-run --verbosity 3 || {
     cat <<-EOF
 
