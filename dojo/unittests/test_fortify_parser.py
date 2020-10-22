fbaa0b31291a95154e8c40439732ece599fbeb86
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2020-10-22 13:24:52 -0500

    Add verbose importing flow to Fortify scans (#3055)
    * Add flow to collect findings from "Count by Category" section
    
    * Add unit tests
    
    Co-authored-by: Maffooch <codymaffucci@Codys-MacBook-Pro.local>

--- dojo/unittests/test_fortify_parser.py
+++ dojo/unittests/test_fortify_parser.py
@@ -14,3 +14,8 @@ class TestFortifyParser(TestCase):
         testfile = "dojo/unittests/scans/fortify/fortify_few_findings.xml"
         parser = FortifyXMLParser(testfile, Test())
         self.assertEqual(2, len(parser.items))
+
+    def test_fortify_few_findings_count_chart(self):
+        testfile = "dojo/unittests/scans/fortify/fortify_few_findings_count_chart.xml"
+        parser = FortifyXMLParser(testfile, Test())
+        self.assertEqual(3, len(parser.items))
