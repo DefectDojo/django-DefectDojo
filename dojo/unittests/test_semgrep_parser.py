13ae1b60b5ba9657d21a5f03406ee79dc375d6ee
Sebastien gioria <SPoint42@users.noreply.github.com>
2020-11-02 13:35:03 +0100

    Update test_semgrep_parser.py

--- dojo/unittests/test_semgrep_parser.py
+++ dojo/unittests/test_semgrep_parser.py
@@ -25,4 +25,4 @@ class TestSemGrepJSONParser(TestCase):
         testfile = open("dojo/unittests/scans/semgrep/repeated_findings.json")
         parser = SemgrepJSONParser(testfile, Test())
         testfile.close()
-        self.assertEqual(2, len(parser.items)                  
+        self.assertEqual(2, len(parser.items))
