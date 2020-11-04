9d51829ced4ae65306b43c5e9af439810a76693f
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2020-11-04 12:10:56 -0600

    Add missing Q import

--- dojo/reports/views.py
+++ dojo/reports/views.py
@@ -14,6 +14,7 @@ from django.http import Http404, HttpResponseRedirect, HttpResponseForbidden, Js
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.db.models import Q
 
 from dojo.celery import app
 from dojo.endpoint.views import get_endpoint_ids
