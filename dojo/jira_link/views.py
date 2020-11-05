b0bd35600740f45c598f3926622877759466f699
Valentijn Scholten <valentijnscholten@gmail.com>
2020-11-05 19:33:35 +0100

    jira setup: log exceptions instead of swallowing

--- dojo/jira_link/views.py
+++ dojo/jira_link/views.py
@@ -131,7 +131,8 @@ def express_new_jira(request):
                     jira = JIRA(server=jira_server,
                         basic_auth=(jira_username, jira_password),
                         options={"verify": settings.JIRA_SSL_VERIFY})
-                except Exception:
+                except Exception as e:
+                    logger.exception(e)
                     messages.add_message(request,
                                      messages.ERROR,
                                      'Unable to authenticate. Please check the URL, username, and password.',
@@ -228,7 +229,8 @@ def new_jira(request):
                                     url=request.build_absolute_uri(reverse('jira')),
                                     )
                 return HttpResponseRedirect(reverse('jira', ))
-            except Exception:
+            except Exception as e:
+                logger.exception(e)
                 messages.add_message(request,
                                      messages.ERROR,
                                      'Unable to authenticate. Please check the URL, username, and password.',
@@ -279,7 +281,7 @@ def edit_jira(request, jid):
                                     )
                 return HttpResponseRedirect(reverse('jira', ))
             except Exception as e:
-                logger.error(e)
+                logger.exception(e)
                 messages.add_message(request,
                                      messages.ERROR,
                                      'Unable to authenticate. Please check the URL, username, and password.',
