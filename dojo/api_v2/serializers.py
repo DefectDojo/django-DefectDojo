f47e10eec3c6eec3b6a387fd0495e6c802bfe753
Valentijn Scholten <valentijnscholten@gmail.com>
2020-11-13 14:04:01 +0100

    advisory: do not return jira password in API v1 response

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -401,6 +401,11 @@ class ToolConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tool_Configuration
         fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'ssh': {'write_only': True},
+            'api_key': {'write_only': True},
+        }
 
 
 class ToolProductSettingsSerializer(serializers.ModelSerializer):
