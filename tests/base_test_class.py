eddd52efcc76c7271ae5e75a6a64f775fd806c4e
Maffooch <codymaffucci@Codys-MacBook-Pro.local>
2020-11-15 21:35:50 -0600

    Finalize unit test

--- tests/base_test_class.py
+++ tests/base_test_class.py
@@ -238,8 +238,12 @@ class BaseTestCase(unittest.TestCase):
             examples:
             http://localhost:8080/static/dojo/img/zoom-in.cur - Failed to load resource: the server responded with a status of 404 (Not Found)
             http://localhost:8080/media/CACHE/images/finding_images/1bf9c0b1-5ed1-4b4e-9551-bcbfd198b90a/7d8d9af058566b8f2fe6548d96c63237.jpg - Failed to load resource: the server responded with a status of 404 (Not Found)
+            
+            The addition of the trigger exception is due to the Report Builder tests. All of the moving objects are from javascrip
+            Tooltips are attached to each object and operate fine at human speeds. Selenium moves too fast for tooltips to be
+            cleaned up, edited, and displayed, so the issue is only present in the test
             """
-            accepted_javascript_messages = r'((zoom\-in\.cur.*)|(images\/finding_images\/.*))404\ \(Not\ Found\)'
+            accepted_javascript_messages = r'((zoom\-in\.cur.*)|(images\/finding_images\/.*))404\ \(Not\ Found\)|Cannot read property \'trigger\' of null'
             # accepted_javascript_messages = r'((zoom\-in\.cur.*)|(images\/finding_images\/.*))404\ \(Not\ Found\)|(bootstrap\-chosen\.css\.map)'
 
             if (entry['level'] == 'SEVERE'):
