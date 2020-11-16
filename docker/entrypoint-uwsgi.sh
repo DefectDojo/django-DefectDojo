be547a6e2752706db07e7c68b8f6cf32f6ba3cf3
valentijnscholten <valentijnscholten@gmail.com>
2020-11-16 21:51:56 +0100

    uwsgi: increase default buffer-size (#3269)
    * increase default uwsgi buffer-size from 4k to 8k
    
    * Update entrypoint-uwsgi-dev.sh
    
    * increase default uwsgi buffer-size from 4k to 8k

--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -36,6 +36,6 @@ exec uwsgi \
   --processes ${DD_UWSGI_NUM_OF_PROCESSES:-2} \
   --threads ${DD_UWSGI_NUM_OF_THREADS:-2} \
   --wsgi dojo.wsgi:application \
-  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-4096}" \
+  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-8192}" \
   # HTTP endpoint is enabled for Kubernetes liveness checks. It should not be exposed as a serivce.
   --http 0.0.0.0:8081 --http-to ${DD_UWSGI_ENDPOINT}
