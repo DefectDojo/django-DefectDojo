8c1856dbdb244de6c5d1eef2a9a041df5ae3e2e5
Ricardo Meulendijks <ricardo.meulendijks@isaac.nl>
2020-11-19 15:58:08 +0100

    changed component view orderBy total desc

--- dojo/components/views.py
+++ dojo/components/views.py
@@ -12,6 +12,7 @@ def components(request):
     component_query = component_query.annotate(total=Count('id')).order_by('component_name', 'component_version')
     component_query = component_query.annotate(active=Count('id', filter=Q(active=True)))
     component_query = component_query.annotate(duplicate=(Count('id', filter=Q(duplicate=True))))
+    component_query = component_query.order_by('-total')  # Default sort by total descending
 
     comp_filter = ComponentFilter(request.GET, queryset=component_query)
     result = get_page_items(request, comp_filter.qs, 25)
