a5b57da58651179871b590540e619a71f691063b
Valentijn Scholten <valentijnscholten@gmail.com>
2020-12-06 12:45:05 +0100

    tagulous: cleanup, fix finding template forms

--- dojo/object/views.py
+++ dojo/object/views.py
@@ -73,10 +73,7 @@ def edit_object(request, pid, ttid):
                                  extra_tags='alert-success')
             return HttpResponseRedirect(reverse('view_objects', args=(pid,)))
     else:
-        # TODO TAGS
         tform = ObjectSettingsForm(instance=object)
-        #                             initial={'tags': get_tag_list(Tag.objects.get_for_object(object))})
-        pass
 
     # tform.initial['tags'] = [tag.name for tag in object.tags.all()]
     product_tab = Product_Tab(pid, title="Edit Tracked Files", tab="settings")
