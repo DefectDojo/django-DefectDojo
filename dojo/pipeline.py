3038d04fdf287bad5b7c9f69f806bcbe1d548b85
christophe226 <61734671+christophe226@users.noreply.github.com>
2020-12-08 16:57:33 +0100

    Update pipeline.py

--- dojo/pipeline.py
+++ dojo/pipeline.py
@@ -67,7 +67,7 @@ def modify_permissions(backend, uid, user=None, social=None, *args, **kwargs):
 def update_product_access(backend, uid, user=None, social=None, *args, **kwargs):
     if settings.GITLAB_PROJECT_AUTO_IMPORT is True:
         # Get all product names
-		user_product_names = [prod.name for prod in Product.objects.filter(authorized_users__in=[user])]
+        user_product_names = [prod.name for prod in Product.objects.filter(authorized_users__in=[user])]
         # Get Gitlab access token
         soc = user.social_auth.get()
         token = soc.extra_data['access_token']
