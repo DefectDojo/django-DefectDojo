ee9cb5100af59c3f76c6030c46e04c8fcdaeab30
RomainJufer <romain.jufer@gmail.com>
2020-12-21 11:56:16 +0100

    Add support for prefetch mixin and composable swagger schema

---
+++ dojo/api_v2/prefetch/__init__.py
@@ -0,0 +1,2 @@
+from .mixins import PrefetchListMixin, PrefetchRetrieveMixin
+from .schema import get_prefetch_schema
---
+++ dojo/api_v2/schema/__init__.py
@@ -0,0 +1,2 @@
+from .extra_schema import IdentitySchema, ExtraParameters, ExtraResponseField, ComposableSchema
+from .utils import LazySchemaRef, try_apply, resolve_lazy_ref
