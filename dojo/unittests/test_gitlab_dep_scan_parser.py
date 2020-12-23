8483d276a95b9f39f311bf21b59bcb2fa6b9b8c2
Guilherme Macedo <guilherme.macedo@personio.de>
2020-12-23 15:21:14 +0100

    Fix wrong name of test files

--- dojo/unittests/test_gitlab_dep_scan_parser.py
+++ dojo/unittests/test_gitlab_dep_scan_parser.py
@@ -10,16 +10,16 @@ class TestGitlabDepScanReportParser(TestCase):
         self.assertEqual(0, len(parser.items))
 
     def test_parse_file_with_no_vuln_has_no_findings(self):
-        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-0-vuln.json")
+        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-report-0-vuln.json")
         parser = GitlabDepScanReportParser(testfile, Test())
         self.assertEqual(0, len(parser.items))
 
     def test_parse_file_with_one_vuln_has_one_finding(self):
-        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-1-vuln.json")
+        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-report-1-vuln.json")
         parser = GitlabDepScanReportParser(testfile, Test())
         self.assertEqual(1, len(parser.items))
 
     def test_parse_file_with_multiple_vuln_has_multiple_findings(self):
-        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-many-vuln.json")
+        testfile = open("dojo/unittests/scans/gitlab_dep_scan/gl-dependency-scanning-report-many-vuln.json")
         parser = GitlabDepScanReportParser(testfile, Test())
         self.assertTrue(len(parser.items) > 2)
