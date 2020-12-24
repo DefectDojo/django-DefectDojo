30e1177a835f8a6463b66bb511a07661ea29531a
Damien Carol <damien.carol@gmail.com>
2020-12-24 12:33:47 +0100

    Fix formatting

--- dojo/tools/burp_suite_pro/parser.py
+++ dojo/tools/burp_suite_pro/parser.py
@@ -15,6 +15,7 @@ DESCRIPTION_TEMPLATE = """**{title}**
 
 class BurpSuiteProParser(object):
     """"""
+
     def __init__(self, file, test):
         self.items = []
 
@@ -25,7 +26,7 @@ class BurpSuiteProParser(object):
         test.title = f"Burp Suite Pro scan"
 
         # for each issue found
-        for issue_event in tree.get('issue_events', list()):
+        for issue_event in tree.get("issue_events", list()):
             if "issue_found" == issue_event.get("type"):
                 verified = False
                 false_p = False
@@ -33,35 +34,40 @@ class BurpSuiteProParser(object):
                 out_of_scope = False
                 impact = None
                 mitigated = None
-                title = issue_event['issue']['name']
-                severity = issue_event['issue']['severity'].title()
+                title = issue_event["issue"]["name"]
+                severity = issue_event["issue"]["severity"].title()
                 description_formated = DESCRIPTION_TEMPLATE.format(
                     title=title,
-                    serial_number=issue_event['issue']['serial_number'],
-                    type_index=issue_event['issue']['type_index'],
-                    confidence=issue_event['issue']['confidence'],
-                    description_text=issue_event['issue']['description']
+                    serial_number=issue_event["issue"]["serial_number"],
+                    type_index=issue_event["issue"]["type_index"],
+                    confidence=issue_event["issue"]["confidence"],
+                    description_text=issue_event["issue"]["description"],
                 )
 
-                finding = Finding(title=title,
-                                    test=test,
-                                    severity=severity,
-                                    numerical_severity=Finding.get_numerical_severity(severity),
-                                    description=description_formated,
-                                    mitigation="No mitigation provided",
-                                    references="No references provided",
-                                    cve=None,  # for now CVE are not managed or it's not very clear how in the spec
-                                    cwe=0,
-                                    active=True,
-                                    verified=verified,
-                                    false_p=false_p,
-                                    duplicate=duplicate,
-                                    out_of_scope=out_of_scope,
-                                    mitigated=mitigated,
-                                    impact="No impact provided",
-                                    static_finding=False,  # by definition
-                                    dynamic_finding=True,  # by definition
-                                    unique_id_from_tool=str(issue_event['issue']['serial_number']),  # the serial number is a good candidate for this attribute
-                                    vuln_id_from_tool=str(issue_event['issue']['type_index'])  # the type index is a good candidate for this attribute
-                                )
+                finding = Finding(
+                    title=title,
+                    test=test,
+                    severity=severity,
+                    numerical_severity=Finding.get_numerical_severity(severity),
+                    description=description_formated,
+                    mitigation="No mitigation provided",
+                    references="No references provided",
+                    cve=None,  # for now CVE are not managed or it's not very clear how in the spec
+                    cwe=0,
+                    active=True,
+                    verified=verified,
+                    false_p=false_p,
+                    duplicate=duplicate,
+                    out_of_scope=out_of_scope,
+                    mitigated=mitigated,
+                    impact="No impact provided",
+                    static_finding=False,  # by definition
+                    dynamic_finding=True,  # by definition
+                    unique_id_from_tool=str(
+                        issue_event["issue"]["serial_number"]
+                    ),  # the serial number is a good candidate for this attribute
+                    vuln_id_from_tool=str(
+                        issue_event["issue"]["type_index"]
+                    ),  # the type index is a good candidate for this attribute
+                )
                 self.items.append(finding)
