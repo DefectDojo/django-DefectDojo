141528ebfc2ba14281ba3e98fd3d233db8a7c6e7
Valentijn Scholten <valentijnscholten@gmail.com>
2021-01-10 21:44:09 +0100

    jira: fix error messages from celery tasks

--- dojo/jira_link/helper.py
+++ dojo/jira_link/helper.py
@@ -282,7 +282,7 @@ def get_jira_connection_raw(jira_server, jira_username, jira_password):
         logger.exception(re)
         log_jira_generic_alert('Unknown JIRA Connection Error', re)
 
-        add_error_message_to_response('Unable to authenticate. Please check the URL, username, password, captcha challenge, Network connection. Details in alert on top right. ' + e.text)
+        add_error_message_to_response('Unable to authenticate. Please check the URL, username, password, captcha challenge, Network connection. Details in alert on top right. ' + re.text)
         raise re
 
     # except RequestException as re:
