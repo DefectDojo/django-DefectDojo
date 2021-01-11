76355ad2ab6e2f00fa42133e684f44dfcfc80684
Damien Carol <damien.carol@gmail.com>
2021-01-11 15:36:55 +0100

    Fix unit tests

--- dojo/unittests/test_jfrogxray_parser.py
+++ dojo/unittests/test_jfrogxray_parser.py
@@ -34,7 +34,7 @@ class TestJfrogXrayJSONParser(TestCase):
         self.assertEquals('pip', item.component_name)
         self.assertEquals('9.0.1', item.component_version)
         item = parser.items[1]
-        self.assertEquals('linux', item.component_name)
+        self.assertEquals('ubuntu:bionic:linux', item.component_name)
         self.assertEquals('4.15.0-88.88', item.component_version)
         self.assertEquals('CVE-2020-14386', item.cve)
         self.assertEquals(787, item.cwe)
