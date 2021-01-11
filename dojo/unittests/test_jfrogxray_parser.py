dc180267d466592b5c07c3ced8f538067db1fc7f
Damien Carol <damien.carol@gmail.com>
2021-01-11 13:54:03 +0100

    Add more unit tests and reduce test data

--- dojo/unittests/test_jfrogxray_parser.py
+++ dojo/unittests/test_jfrogxray_parser.py
@@ -29,9 +29,13 @@ class TestJfrogXrayJSONParser(TestCase):
         testfile = open("dojo/unittests/scans/jfrogxray/many_vulns2.json")
         parser = XrayJSONParser(testfile, Test())
         testfile.close()
-        self.assertEqual(357, len(parser.items))
-        item = parser.items[36]
-        self.assertEquals('requests', item.component_name)
-        self.assertEquals('2.18.4', item.component_version)
-        self.assertEquals('CVE-2018-18074', item.cve)
-        self.assertEquals(522, item.cwe)
+        self.assertEqual(2, len(parser.items))
+        item = parser.items[0]
+        self.assertEquals('pip', item.component_name)
+        self.assertEquals('9.0.1', item.component_version)
+        item = parser.items[1]
+        self.assertEquals('linux', item.component_name)
+        self.assertEquals('4.15.0-88.88', item.component_version)
+        self.assertEquals('CVE-2020-14386', item.cve)
+        self.assertEquals(787, item.cwe)
+        self.assertEquals("AV:L/AC:L/PR:L/UI:N/S:U/C:H/I:H/A:H", item.cvssv3)
