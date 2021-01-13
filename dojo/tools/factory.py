0bd5f8cf51f1c85a038b43bec5fc19a139f40a77
mohcer <msayyed456@gmail.com>
2021-01-13 18:27:25 +0800

    Add Scantist Vulnerability Scan Parser (#3610)
    * renovate: allow automatic creation of PRs
    
    * updating to add scanting parser
    
    * updating to make review changes and run test cases
    
    * updating to make review changes and run test cases
    
    Co-authored-by: valentijnscholten <valentijnscholten@gmail.com>

--- dojo/tools/factory.py
+++ dojo/tools/factory.py
@@ -95,6 +95,7 @@ from dojo.tools.checkov.parser import CheckovParser
 from dojo.tools.kubebench.parser import KubeBenchParser
 from dojo.tools.ort.parser import OrtParser
 from dojo.tools.sarif.parser import SarifParser
+from dojo.tools.scantist.parser import ScantistJSONParser
 
 
 __author__ = 'Jay Paz'
@@ -304,6 +305,8 @@ def import_parser_factory(file, test, active, verified, scan_type=None):
         parser = OrtParser(file, test)
     elif scan_type == 'SARIF':
         parser = SarifParser(file, test)
+    elif scan_type == 'Scantist Scan':
+        parser = ScantistJSONParser(file, test)
     else:
         raise ValueError('Unknown Test Type')
 
