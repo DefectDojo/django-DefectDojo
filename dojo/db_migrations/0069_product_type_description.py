513895887b720a62aa7b29787ce87421b3453d67
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-01-20 19:05:54 +0100

    typo in db migration

--- dojo/db_migrations/0069_product_type_description.py
+++ dojo/db_migrations/0069_product_type_description.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dojo', '0068_file_upload.py'),
+        ('dojo', '0068_file_upload'),
     ]
 
     operations = [
