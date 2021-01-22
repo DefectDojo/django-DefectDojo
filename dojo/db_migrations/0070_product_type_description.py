3370f6ae664b4c360d6222923b14d729b6fbf14d
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-01-22 19:27:34 +0100

    adapt to new db migration

--- dojo/db_migrations/0070_product_type_description.py
+++ dojo/db_migrations/0070_product_type_description.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dojo', '0068_file_upload'),
+        ('dojo', '0069_risk_acceptance'),
     ]
 
     operations = [
