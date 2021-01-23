15261aba493dcf555fea7d46ab3ab5218309ca56
damiencarol <damien.carol@gmail.com>
2021-01-23 13:57:55 +0100

    Add more unit tests for ZAP and YARN

--- dojo/unittests/test_zap.py
+++ dojo/unittests/test_zap.py
@@ -5,21 +5,55 @@ from dojo.models import Test, Engagement, Product
 
 class TestZAPXML(TestCase):
 
-    def setUp(self):
-        self.test = Test()
-        self.test.engagement = Engagement()
-        self.test.engagement.product = Product()
+    def get_test(self):
+        test = Test()
+        test.engagement = Engagement()
+        test.engagement.product = Product()
+        return test
 
     def test_parse_without_file_has_no_findings(self):
-        parser = ZapXmlParser(None, self.test)
-        self.assertEqual(0, len(parser.items))
+        parser = ZapXmlParser()
+        findings = parser.get_findings(None, self.get_test())
+        self.assertEqual(0, len(findings))
 
     def test_parse_no_findings(self):
         testfile = open("dojo/unittests/scans/zap/empty_2.9.0.xml")
-        parser = ZapXmlParser(testfile, self.test)
-        self.assertEqual(0, len(parser.items))
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertEqual(0, len(findings))
 
     def test_parse_some_findings(self):
         testfile = open("dojo/unittests/scans/zap/some_2.9.0.xml")
-        parser = ZapXmlParser(testfile, self.test)
-        self.assertEqual(7, len(parser.items))
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertEqual(7, len(findings))
+
+    def test_parse_some_findings_0(self):
+        testfile = open("dojo/unittests/scans/zap/0_zap_sample.xml")
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertIsInstance(list, findings)
+
+    def test_parse_some_findings_1(self):
+        testfile = open("dojo/unittests/scans/zap/1_zap_sample_0_and_new_absent.xml")
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertIsInstance(list, findings)
+
+    def test_parse_some_findings_2(self):
+        testfile = open("dojo/unittests/scans/zap/2_zap_sample_0_and_new_endpoint.xml")
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertIsInstance(list, findings)
+
+    def test_parse_some_findings_3(self):
+        testfile = open("dojo/unittests/scans/zap/3_zap_sampl_0_and_different_severities.xml")
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertIsInstance(list, findings)
+
+    def test_parse_some_findings_5(self):
+        testfile = open("dojo/unittests/scans/zap/5_zap_sample_one.xml")
+        parser = ZapXmlParser()
+        findings = parser.get_findings(testfile, self.get_test())
+        self.assertIsInstance(list, findings)
