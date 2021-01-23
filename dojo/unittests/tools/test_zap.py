8c4550dce3fa2fc83dff07ebca0e2ad764730d11
damiencarol <damien.carol@gmail.com>
2021-01-23 19:18:10 +0100

    fix 2 unit tests

--- dojo/unittests/tools/test_zap.py
+++ dojo/unittests/tools/test_zap.py
@@ -32,28 +32,28 @@ class TestZAPXML(TestCase):
         testfile = open("dojo/unittests/scans/zap/0_zap_sample.xml")
         parser = ZapXmlParser()
         findings = parser.get_findings(testfile, self.get_test())
-        self.assertIsInstance(list, findings)
+        self.assertIsInstance(findings, list)
 
     def test_parse_some_findings_1(self):
         testfile = open("dojo/unittests/scans/zap/1_zap_sample_0_and_new_absent.xml")
         parser = ZapXmlParser()
         findings = parser.get_findings(testfile, self.get_test())
-        self.assertIsInstance(list, findings)
+        self.assertIsInstance(findings, list)
 
     def test_parse_some_findings_2(self):
         testfile = open("dojo/unittests/scans/zap/2_zap_sample_0_and_new_endpoint.xml")
         parser = ZapXmlParser()
         findings = parser.get_findings(testfile, self.get_test())
-        self.assertIsInstance(list, findings)
+        self.assertIsInstance(findings, list)
 
     def test_parse_some_findings_3(self):
         testfile = open("dojo/unittests/scans/zap/3_zap_sampl_0_and_different_severities.xml")
         parser = ZapXmlParser()
         findings = parser.get_findings(testfile, self.get_test())
-        self.assertIsInstance(list, findings)
+        self.assertIsInstance(findings, list)
 
     def test_parse_some_findings_5(self):
         testfile = open("dojo/unittests/scans/zap/5_zap_sample_one.xml")
         parser = ZapXmlParser()
         findings = parser.get_findings(testfile, self.get_test())
-        self.assertIsInstance(list, findings)
+        self.assertIsInstance(findings, list)
