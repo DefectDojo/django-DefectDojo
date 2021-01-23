3e0284daa0b9babcc9512b2aba19cac061608f17
damiencarol <damien.carol@gmail.com>
2021-01-23 15:45:32 +0100

    Enable openvas parser

---
+++ dojo/unittests/tools/test_openvas_csv_parser.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from dojo.tools.openvas_csv.parser import OpenVASUploadCsvParser
+from dojo.models import Test
+
+
+class TestOpenVASUploadCsvParser(TestCase):
+
+    def test_openvas_csv_parser_without_file_has_no_findings(self):
+        parser = OpenVASUploadCsvParser()
+        findings = parser.get_findings(None, Test())
+        self.assertEqual(0, len(findings))
