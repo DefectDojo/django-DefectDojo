f3ffc063419f8902811f33998630c965077e6340
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-01-26 22:10:19 +0100

    updated db migration

--- dojo/db_migrations/0071_product_type_enhancement.py
+++ dojo/db_migrations/0071_product_type_enhancement.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dojo', '0069_risk_acceptance'),
+        ('dojo', '0070_increase_alert_field_size'),
     ]
 
     operations = [
