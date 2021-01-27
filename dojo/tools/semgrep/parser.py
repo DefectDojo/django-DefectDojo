673f4d98e0580a4d9707534581e7a74dea7ac94a
damiencarol <damien.carol@gmail.com>
2021-01-27 21:43:54 +0100

    Fix semgrep parser

--- dojo/tools/semgrep/parser.py
+++ dojo/tools/semgrep/parser.py
@@ -7,44 +7,25 @@ from dojo.tools.semgrep.models import SemgrepJSONResult
 class SemgrepJSONParser(object):
 
     def get_findings(self, filehandle, test):
-        tree = self.parse_json(filehandle)
-
-        self.items = []
-        if tree:
-            results = tree.get('results')
-
-            if not results:
-                return
-
-            for item in results:
-                title = item['check_id']
-                semgrep_result = SemgrepJSONResult(item['extra'], item['path'], item['start'], item['end'])
-
-                findingItem = Finding(
-                    title=semgrep_result.title,
-                    severity=semgrep_result.severity,
-                    numerical_severity=Finding.get_numerical_severity(semgrep_result.severity),
-                    description=semgrep_result.message,
-                    mitigation='N/A',
-                    file_path=item['path'],
-                    cwe=semgrep_result.cwe,
-                    line=semgrep_result.start,
-                    url='N/A',
-                    impact='N/A',
-                    static_finding=True,
-                    test=test
-                )
-                self.items.append(findingItem)
-
-    def parse_json(self, filehandle):
-        try:
-            data = filehandle.read()
-        except:
-            return None
-
-        try:
-            tree = json.loads(data)
-        except:
-            raise Exception("Invalid format")
-
-        return tree
+        tree = json.load(filehandle)
+        items = []
+        results = tree.get('results')
+        for item in results:
+            title = item['check_id']
+            semgrep_result = SemgrepJSONResult(item['extra'], item['path'], item['start'], item['end'])
+            findingItem = Finding(
+                title=semgrep_result.title,
+                severity=semgrep_result.severity,
+                numerical_severity=Finding.get_numerical_severity(semgrep_result.severity),
+                description=semgrep_result.message,
+                mitigation='N/A',
+                file_path=item['path'],
+                cwe=semgrep_result.cwe,
+                line=semgrep_result.start,
+                url='N/A',
+                impact='N/A',
+                static_finding=True,
+                test=test
+            )
+            items.append(findingItem)
+        return items
