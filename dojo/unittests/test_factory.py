c8cd3fbe4a656c7dc4d08cec1d22c3ee1d9e4389
damiencarol <damien.carol@gmail.com>
2021-01-27 19:34:22 +0100

    Add more unit tests

--- dojo/unittests/test_factory.py
+++ dojo/unittests/test_factory.py
@@ -5,8 +5,16 @@ from dojo.models import Test
 
 class TestFactory(TestCase):
 
-    def test_parse_file_with_one_finding(self):
+    def test_acunetix_one_finding(self):
         testfile = open('dojo/unittests/scans/acunetix/one_finding.xml')
         parser = import_parser_factory(testfile, Test(), False, False, 'Acunetix Scan')
         findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        self.assertEqual(1, len(findings))
+
+    def test_anchore_one_finding(self):
+        testfile = open("dojo/unittests/scans/anchore/one_vuln.json")
+        parser = import_parser_factory(testfile, Test(), False, False, 'Anchore Engine Scan')
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
         self.assertEqual(1, len(findings))
