fb21c8c3d4a0e72eb5342edbb93b816b4605b5a9
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-01-31 19:27:37 +0100

    fix for the fix

--- dojo/forms.py
+++ dojo/forms.py
@@ -35,6 +35,7 @@ from tagulous.forms import TagField
 import logging
 from crum import get_current_user
 from dojo.utils import get_system_setting
+from django.conf import settings
 from dojo.authorization.roles_permissions import Permissions, Roles
 from dojo.product_type.queries import get_authorized_product_types
 
