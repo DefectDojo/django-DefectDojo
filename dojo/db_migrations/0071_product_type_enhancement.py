0f6e83e21ec6b12bd05770c22ea0700430e3a756
Valentijn Scholten <valentijnscholten@gmail.com>
2021-02-03 09:54:47 +0100

    findings: add composite index

--- dojo/db_migrations/0071_product_type_enhancement.py
+++ dojo/db_migrations/0071_product_type_enhancement.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dojo', '0070_increase_alert_field_size'),
+        ('dojo', '0071_product_type_enhancement'),
     ]
 
     operations = [
