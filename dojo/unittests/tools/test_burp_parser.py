f9f65e5bf4afa92c116c258c49bba96e2df4d28e
damiencarol <damien.carol@gmail.com>
2021-02-04 22:34:17 +0100

    Fix Burp blank response bug #3795

--- dojo/unittests/tools/test_burp_parser.py
+++ dojo/unittests/tools/test_burp_parser.py
@@ -17,6 +17,8 @@ class TestBurpParser(TestCase):
             findings = parser.get_findings(test_file, Test())
 
             self.assertEqual(1, len(findings))
+            self.assertEqual("1049088", findings[0].vuln_id_from_tool)
+            self.assertEqual(3, len(findings[0].unsaved_endpoints))
 
     def test_burp_with_multiple_vulns_has_multiple_findings(self):
         with open(sample_path("seven_findings.xml")) as test_file:
@@ -24,3 +26,27 @@ class TestBurpParser(TestCase):
             findings = parser.get_findings(test_file, Test())
 
             self.assertEqual(7, len(findings))
+
+    def test_burp_with_one_vuln_with_blank_response(self):
+        with open(sample_path("one_finding_with_blank_response.xml")) as test_file:
+            parser = BurpXmlParser()
+            findings = parser.get_findings(test_file, Test())
+
+            self.assertEqual(1, len(findings))
+
+            self.assertEqual("7121655797013284864", findings[0].unique_id_from_tool)
+            self.assertEqual("1049088", findings[0].vuln_id_from_tool)
+            self.assertEqual("SQL injection", findings[0].title)
+            self.assertEqual(1, len(findings[0].unsaved_endpoints))
+
+    def test_burp_with_one_vuln_with_cwe(self):
+        with open(sample_path("one_finding_with_cwe.xml")) as test_file:
+            parser = BurpXmlParser()
+            findings = parser.get_findings(test_file, Test())
+
+            self.assertEqual(1, len(findings))
+
+            self.assertEqual("456437653765735", findings[0].unique_id_from_tool)
+            self.assertEqual("7340288", findings[0].vuln_id_from_tool)
+            self.assertEqual("Cacheable HTTPS response", findings[0].title)
+            self.assertEqual(524, findings[0].cwe)
