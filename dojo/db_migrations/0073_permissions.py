177adf5b7cc4720b2aec4b029869f6282c560e38
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-02-05 22:14:52 +0100

    rename db migration after merge

--- dojo/db_migrations/0073_permissions.py
+++ dojo/db_migrations/0073_permissions.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('dojo', '0071_product_type_enhancement'),
+        ('dojo', '0072_composite_index'),
     ]
 
     operations = [
