242deb456599c4f6fba17b9b2636b513f1fc83c5
Udit Mishra <uditmishra128@gmail.com>
2021-02-06 17:23:08 +0530

    Create a new react app ,login and resistor page

---
+++ my-app/src/components/Login.component.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Log in</h3>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-dark btn-lg btn-block">
+          Sign in
+        </button>
+        <p className="forgot-password text-right">
+          {" "}
+          Forgot <a href="www.google.com">password?</a>
+        </p>
+      </form>
+    );
+  }
+}
---
+++ my-app/src/components/Signup.component.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+  render() {
+    return (
+      <form>
+        <h3>Register</h3>
+
+        <div className="form-group">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-dark btn-lg btn-block">
+          Register
+        </button>
+        <p className="forgot-password text-right">
+          Already registered <a href="www.google.com">log in?</a>
+        </p>
+      </form>
+    );
+  }
+}
