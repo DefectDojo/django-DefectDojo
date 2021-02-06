4a05b8867c88131ec44e24b78294736abb4824e9
Daniel Naab <dan@crushingpennies.com>
2021-02-06 12:01:01 -0600

    Metrics query optimizations (#3730)
    * Metrics: fix bug, undefined object "finding"
    
    * Remove unused variables in Metrics views
    
    * Move "in-period" queries into separate function
    
    * Move accepted_in_period_details into separate function
    
    * Metrics: Move closed_in_period_details and closed_in_period_counts into separate function
    
    * Flake8 cleanup
    
    * Remove unnecessary COUNT query. Because the queryset is iterated over, we can use the original query and check the list length.
    
    * Save a few queries by tweaking select_related and prefetch_related
    
    * Add test type filter to metric finding filter
    
    * Django admin: Use raw ID fields for ManyToManyFields on Finding table, to reduce the number of database queries.
    
    * Undo pylinting

--- dojo/filters.py
+++ dojo/filters.py
@@ -1412,6 +1412,10 @@ class MetricsFindingFilter(FilterSet):
     start_date = DateFilter(field_name='date', label='Start Date', lookup_expr=('gt'))
     end_date = DateFilter(field_name='date', label='End Date', lookup_expr=('lt'))
     date = MetricsDateRangeFilter()
+    test__test_type = ModelMultipleChoiceFilter(
+        queryset=Test_Type.objects.all().order_by('name'),
+        label="Test Type"
+    )
     test__engagement__product__prod_type = ModelMultipleChoiceFilter(
         queryset=Product_Type.objects.all().order_by('name'),
         label="Product Type")
