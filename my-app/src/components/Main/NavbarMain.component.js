7428bab403e5266e15f52bbf72907a28ed84c6b8
Udit Mishra <uditmishra128@gmail.com>
2021-02-08 23:48:57 +0530

    add dropdown in Dashboard

--- my-app/src/components/Main/NavbarMain.component.js
+++ my-app/src/components/Main/NavbarMain.component.js
@@ -1,43 +1,63 @@
 import React, { Component } from "react";
 import "./NavbarMain.css";
+
 import {
   Navbar,
-  Container,
-  NavbarBrand,
+  NavDropdown,
   Nav,
+  Button,
   Form,
   FormControl,
-  Button,
-} from "react-bootstrap/";
+} from "react-bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 export default class Navbars extends Component {
   render() {
     return (
-      <div>
-        <Navbar bg="light" expand="lg">
-          <Container>
-            <NavbarBrand>
+      <div className="Navbars">
+        <Navbar bg="light" variant="light">
+          <Nav className="mr-auto">
+            <Navbar.Brand href="#home">
               <img
                 src="https://demo.defectdojo.org/static/dojo/img/logo.png"
                 alt="DefectDojo-logo"
                 width="240px"
               />
-            </NavbarBrand>
-            <Navbar.Collapse id="responsive-navbar-nav"></Navbar.Collapse>
-            <Form inline>
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-              <Button type="submit">
-                <SearchIcon />
-              </Button>
-            </Form>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          </Container>
+            </Navbar.Brand>
+          </Nav>
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              label="Search"
+              aria-label="Search"
+              name="query"
+            />
+            <Button
+              variant="dark"
+              id="submit_Search"
+              aria-label="Serach"
+              size="sm"
+            >
+              <SearchIcon />
+            </Button>
+          </Form>
+
+          <NavDropdown title={<NotificationsIcon />} id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
         </Navbar>
       </div>
     );
