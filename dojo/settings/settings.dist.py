29b04da9b91386b4cfa5e21563cca09c38ef9170
Sebastien gioria <s.gioria@lectra.com>
2021-02-19 16:20:32 +0100

    fake line and path to test dedup

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -795,6 +795,8 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Acunetix Scan': ['title', 'description'],
     'Trivy Scan': ['title', 'severity', 'cve', 'cwe'],
     'Snyk Scan': ['vuln_id_from_tool', 'file_path', 'component_name', 'component_version'],
+    'SpotBugs Scan': ['cwe', 'severity', 'file_path'],
+
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -814,6 +816,7 @@ HASHCODE_ALLOWS_NULL_CWE = {
     'DSOP Scan': True,
     'Acunetix Scan': True,
     'Trivy Scan': True,
+    'SpotBugs Scan ': False,
 }
 
 # List of fields that are known to be usable in hash_code computation)
