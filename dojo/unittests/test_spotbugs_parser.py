94163abc03983af24baf73fb4929b105107cf5de
Sebastien gioria <s.gioria@lectra.com>
2021-02-22 13:24:51 +0100

    fake line and path to test dedup

--- dojo/unittests/test_spotbugs_parser.py
+++ dojo/unittests/test_spotbugs_parser.py
@@ -9,11 +9,6 @@ class TestSpotbugsXMLParser(TestCase):
         parser = SpotbugsXMLParser(None, Test())
         self.assertEqual(0, len(parser.items))
 
-    def test_parse_one_finding(self):
-        testfile = open("dojo/unittests/scans/spotbugs/one_finding.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
-        self.assertEqual(1, len(parser.items))
 
     def test_parse_many_finding(self):
         testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
