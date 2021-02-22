34ee26ccb8d5d185615479c2453a5953b684139e
Sebastien gioria <s.gioria@lectra.com>
2021-02-22 13:36:59 +0100

    make Flake8 Happy

--- dojo/unittests/test_spotbugs_parser.py
+++ dojo/unittests/test_spotbugs_parser.py
@@ -9,11 +9,9 @@ class TestSpotbugsXMLParser(TestCase):
         parser = SpotbugsXMLParser(None, Test())
         self.assertEqual(0, len(parser.items))
 
-
     def test_parse_many_finding(self):
         testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
         parser = SpotbugsXMLParser(testfile, Test())
         testfile.close()
         self.assertEqual(3, len(parser.items))
-
 
