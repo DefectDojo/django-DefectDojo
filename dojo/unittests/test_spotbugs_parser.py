e3b53a4eed0b59bc71365dfe80a0d5d58a92c9a6
Sebastien gioria <SPoint42@users.noreply.github.com>
2021-03-02 14:53:14 +0100

    Update dojo/unittests/test_spotbugs_parser.py
    Co-authored-by: Damien Carol <damiencarol@voodoo.io>

--- dojo/unittests/test_spotbugs_parser.py
+++ dojo/unittests/test_spotbugs_parser.py
@@ -22,7 +22,7 @@ class TestSpotbugsParser(TestCase):
         self.assertEqual(95, test_finding.sast_source_line)
 
     def test_find_sast_source_path(self):
-        parser = SpotbugsXMLParser()
+        parser = SpotbugsParser()
         parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml", Test())
         test_finding = parser.items[0]
         self.assertEqual("securitytest/command/IdentityFunctionCommandInjection.kt", test_finding.sast_source_file_path)
