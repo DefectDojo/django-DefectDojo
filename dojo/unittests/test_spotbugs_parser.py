52772aa42f9818e8f52aa82729a10e2a673a53a4
Sebastien gioria <s.gioria@lectra.com>
2021-03-02 08:39:48 +0100

    chore: rewrite tests to pass on latest dev version

--- dojo/unittests/test_spotbugs_parser.py
+++ dojo/unittests/test_spotbugs_parser.py
@@ -6,41 +6,35 @@ from dojo.models import Test
 class TestSpotbugsParser(TestCase):
 
     def test_no_findings(self):
-        testfile = open("dojo/unittests/scans/spotbugs/no_finding.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/no_finding.xml",Test())
         self.assertEqual(0, len(parser.items))
 
     def test_parse_many_finding(self):
-        testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml",Test())
         self.assertEqual(81, len(parser.items))
 
     def test_find_sast_source_line(self):
-        testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml",Test())
         test_finding = parser.items[0]
         self.assertEqual(95, test_finding.sast_source_line)
 
     def test_find_sast_source_path(self):
-        testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml",Test())
         test_finding = parser.items[0]
         self.assertEqual("securitytest/command/IdentityFunctionCommandInjection.kt", test_finding.sast_source_file_path)
 
     def test_find_source_line(self):
-        testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml",Test())
         test_finding = parser.items[0]
         self.assertEqual(95, test_finding.line)
 
     def test_find_file_path(self):
-        testfile = open("dojo/unittests/scans/spotbugs/many_findings.xml")
-        parser = SpotbugsXMLParser(testfile, Test())
-        testfile.close()
+        parser = SpotbugsXMLParser()
+        parser.get_findings("dojo/unittests/scans/spotbugs/many_findings.xml",Test())
         test_finding = parser.items[0]
         self.assertEqual("securitytest/command/IdentityFunctionCommandInjection.kt", test_finding.file_path)
