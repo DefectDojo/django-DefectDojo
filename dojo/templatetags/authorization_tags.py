620230627253f371398e30085e9457cee75fa975
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-03-09 10:27:15 +0100

    Authorization V2: authorization for products (#3926)

--- dojo/templatetags/authorization_tags.py
+++ dojo/templatetags/authorization_tags.py
@@ -1,7 +1,7 @@
 from django import template
 from django.conf import settings
 from crum import get_current_user
-from dojo.authorization.roles_permissions import Roles
+from dojo.authorization.roles_permissions import Roles, Permissions
 from dojo.authorization.authorization import user_has_permission
 
 register = template.Library()
@@ -24,7 +24,13 @@ def feature_new_authorization_or_user_is_staff(user):
 
 @register.filter
 def has_object_permission(obj, permission):
+
     if settings.FEATURE_NEW_AUTHORIZATION:
-        return user_has_permission(get_current_user(), obj, permission)
+        return user_has_permission(get_current_user(), obj, Permissions[permission])
     else:
-        return get_current_user().is_staff
+        return False
+
+
+@register.filter
+def product_type_name(product_type):
+    return product_type.name
