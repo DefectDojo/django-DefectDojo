551cf9bd1b612f872e8c12a7edae32377b40b096
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-03-16 10:11:27 +0100

    Add: db extra field and migration

--- dojo/models.py
+++ dojo/models.py
@@ -838,6 +838,8 @@ class Tool_Configuration(models.Model):
                                                 'Username/Password'),
                                                ('SSH', 'SSH')),
                                            null=True, blank=True)
+    extras = models.CharField(max_length=50, null=True, blank=True, help_text="Additional definitions that will be "
+                                                                              "consumed by scanner")
     username = models.CharField(max_length=200, null=True, blank=True)
     password = models.CharField(max_length=600, null=True, blank=True)
     auth_title = models.CharField(max_length=200, null=True, blank=True,
