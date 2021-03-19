34d1d23ec958d4137ece28ec2796f27f4182da1e
Sever <dubravko.sever@gmail.com>
2021-03-19 08:14:18 +0100

    SonarQube api filters (#4068)
    * Add: db extra field and migration
    
    * Add: Check for extra filters in sonar
    
    * Fix: flake
    
    * Add: filter documentation
    
    * Increasing Field size to 255
    
    * Fix: model
    
    Co-authored-by: Dubravko Sever <dubravko.sever@pan-net.eu>

--- dojo/models.py
+++ dojo/models.py
@@ -836,6 +836,8 @@ class Tool_Configuration(models.Model):
                                                 'Username/Password'),
                                                ('SSH', 'SSH')),
                                            null=True, blank=True)
+    extras = models.CharField(max_length=255, null=True, blank=True, help_text="Additional definitions that will be "
+                                                                              "consumed by scanner")
     username = models.CharField(max_length=200, null=True, blank=True)
     password = models.CharField(max_length=600, null=True, blank=True)
     auth_title = models.CharField(max_length=200, null=True, blank=True,
