cedd61291f92fdbd52c0ea545295ab573487c3d1
Damien Carol <damien.carol@gmail.com>
2021-03-22 10:54:17 +0100

    Nikto parser: add support for JSON format (#4097)
    * Nikto parser: add support for JSON format
    
    * Add comments and remove more useless attributes
    
    * Add more documentation
    
    * Update import.md
    
    * Update parser.py
    
    * Update parser.py

--- dojo/tools/nikto/parser.py
+++ dojo/tools/nikto/parser.py
@@ -1,9 +1,9 @@
-__author__ = 'aaronweaver'
 
 import hashlib
 import logging
 import re
-from urllib.parse import urlparse
+import hyperlink
+import json
 
 from defusedxml import ElementTree as ET
 
@@ -13,6 +13,15 @@ logger = logging.getLogger(__name__)
 
 
 class NiktoParser(object):
+    """Nikto web server scanner - https://cirt.net/Nikto2
+
+    The current parser support 3 sources:
+     - XML output (old)
+     - new XML output (with nxvmlversion=\"1.2\" type)
+     - JSON output
+
+    See: https://github.com/sullo/nikto
+    """
 
     def get_scan_types(self):
         return ["Nikto Scan"]
@@ -21,12 +30,68 @@ class NiktoParser(object):
         return scan_type  # no custom label for now
 
     def get_description_for_scan_types(self, scan_type):
-        return "XML output"
+        return "XML output (old and new nxvmlversion=\"1.2\" type) or JSON output"
 
     def get_findings(self, filename, test):
+        if filename.name.lower().endswith('.xml'):
+            return self.process_xml(filename, test)
+        elif filename.name.lower().endswith('.json'):
+            return self.process_json(filename, test)
+        else:
+            raise ValueError('Unknown File Format')
+
+    def process_json(self, file, test):
+        data = json.load(file)
+
+        dupes = dict()
+        host = data.get('host')
+        port = data.get('port')
+        if port is not None:
+            port = int(port)
+        for vulnerability in data.get('vulnerabilities', []):
+            finding = Finding(
+                title=vulnerability.get('msg'),
+                severity="Info",  # Nikto doesn't assign severity, default to Info
+                description="\n".join([
+                    f"**id:** `{vulnerability.get('id')}`",
+                    f"**msg:** `{vulnerability.get('msg')}`",
+                    f"**HTTP Method:** `{vulnerability.get('method')}`",
+                    f"**OSVDB:** `{vulnerability.get('OSVDB')}`",
+                ]),
+                vuln_id_from_tool=vulnerability.get('id'),
+                nb_occurences=1,
+            )
+            # manage if we have an ID from OSVDB
+            if "OSVDB" in vulnerability and "0" != vulnerability.get('OSVDB'):
+                finding.unique_id_from_tool = "OSVDB-" + vulnerability.get('OSVDB')
+                finding.description += "\n*This finding is marked as medium as there is a link to OSVDB*"
+                finding.severity = "Medium"
+            # build the endpoint
+            endpoint = Endpoint(
+                host=host,
+                port=port,
+                path=vulnerability.get('url'),
+            )
+            finding.unsaved_endpoints = [endpoint]
+
+            # internal de-duplication
+            dupe_key = finding.severity + finding.title
+            if dupe_key in dupes:
+                find = dupes[dupe_key]
+                find.description += "\n-----\n" + finding.description
+                find.unsaved_endpoints.append(endpoint)
+                find.unique_id_from_tool = None  # as it is an aggregated finding we erase ids
+                find.vuln_id_from_tool = None  # as it is an aggregated finding we erase ids
+                find.nb_occurences += 1
+            else:
+                dupes[dupe_key] = finding
+
+        return list(dupes.values())
+
+    def process_xml(self, file, test):
         dupes = dict()
 
-        tree = ET.parse(filename)
+        tree = ET.parse(file)
         root = tree.getroot()
         scan = root.find('scandetails')
 
@@ -37,7 +102,8 @@ class NiktoParser(object):
             # This find statement below is to support new file format while not breaking older Nikto scan files versions.
             for scan in root.findall('./niktoscan/scandetails'):
                 self.process_scandetail(scan, test, dupes)
-        return dupes
+
+        return list(dupes.values())
 
     def process_scandetail(self, scan, test, dupes):
         for item in scan.findall('item'):
@@ -62,86 +128,41 @@ class NiktoParser(object):
             severity = "Info"  # Nikto doesn't assign severity, default to Info
 
             # Description
-            description = "\n \n".join((("Host: " + ip),
-                                    ("Description: " + item.find("description").text),
-                                    ("HTTP Method: " + item.attrib["method"]),
-                                    ))
-            mitigation = "N/A"
-            impact = "N/A"
-            references = "N/A"
+            description = "\n".join([
+                    f"**Host:** `{ip}`",
+                    f"**Description:** `{item.find('description').text}`",
+                    f"**HTTP Method:** `{item.attrib['method']}`",
+            ])
 
-            dupe_key = hashlib.md5(description.encode("utf-8")).hexdigest()
+            url = hyperlink.parse(ip)
+            endpoint = Endpoint(
+                protocol=url.scheme,
+                host=url.host,
+                port=url.port,
+                path="/".join(url.path),
+            )
+
+            dupe_key = hashlib.sha256(description.encode("utf-8")).hexdigest()
 
             if dupe_key in dupes:
                 finding = dupes[dupe_key]
                 if finding.description:
                     finding.description = finding.description + "\nHost:" + ip + "\n" + description
-                self.process_endpoints(finding, ip)
-                dupes[dupe_key] = finding
+                finding.unsaved_endpoints.append(endpoint)
+                finding.nb_occurences += 1
+
             else:
-                dupes[dupe_key] = True
-
                 finding = Finding(title=titleText,
-                                  test=test,
-                                  active=False,
-                                  verified=False,
-                                  description=description,
-                                  severity=severity,
-                                  numerical_severity=Finding.get_numerical_severity(
-                                      severity),
-                                  mitigation=mitigation,
-                                  impact=impact,
-                                  references=references,
-                                  url='N/A',
-                                  dynamic_finding=True)
+                                    test=test,
+                                    active=False,
+                                    verified=False,
+                                    description=description,
+                                    severity=severity,
+                                    numerical_severity=Finding.get_numerical_severity(
+                                        severity),
+                                    dynamic_finding=True,
+                                    nb_occurences=1,
+                                  )
+                finding.unsaved_endpoints = [endpoint]
 
                 dupes[dupe_key] = finding
-                self.process_endpoints(finding, ip)
-
-        self.items = list(dupes.values())
-
-    def process_endpoints(self, finding, host):
-        protocol = "http"
-        query = ""
-        fragment = ""
-        path = ""
-        url = urlparse(host)
-
-        if url:
-            path = url.path
-
-        try:
-            rhost = re.search(
-                r"(http|https|ftp)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&amp;%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))[\:]*([0-9]+)*([/]*($|[a-zA-Z0-9\.\,\?\'\\\+&amp;%\$#\=~_\-]+)).*?$",
-                host)
-            protocol = rhost.group(1)
-            host = rhost.group(4)
-        except:
-            logger.exception("Could not apply regex to endpoint")
-
-        try:
-            dupe_endpoint = Endpoint.objects.get(protocol="protocol",
-                                                 host=host,
-                                                 query=query,
-                                                 fragment=fragment,
-                                                 path=path,
-                                                 product=finding.test.engagement.product)
-        except Endpoint.DoesNotExist:
-            dupe_endpoint = None
-
-        if not dupe_endpoint:
-            endpoint = Endpoint(protocol=protocol,
-                                host=host,
-                                query=query,
-                                fragment=fragment,
-                                path=path,
-                                product=finding.test.engagement.product)
-        else:
-            endpoint = dupe_endpoint
-
-        if not dupe_endpoint:
-            endpoints = [endpoint]
-        else:
-            endpoints = [endpoint, dupe_endpoint]
-
-        finding.unsaved_endpoints = finding.unsaved_endpoints + endpoints
