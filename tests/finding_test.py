458928e83204d4c7189f912d0141c5af1aa3adf2
Pascal Trovatelli <34663482+ptrovatelli@users.noreply.github.com>
2021-03-24 08:45:29 +0100

    fix flake8

--- tests/finding_test.py
+++ tests/finding_test.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 
 import unittest
