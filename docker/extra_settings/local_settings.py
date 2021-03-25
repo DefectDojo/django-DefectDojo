8c07d8ea289225e2d8cae4e37154b17086acbdab
Fred Blaise <madchap@users.noreply.github.com>
2021-03-25 10:16:49 +0100

    Update local_settings.py

--- docker/extra_settings/local_settings.py
+++ docker/extra_settings/local_settings.py
@@ -12,7 +12,7 @@ CELERY_BEAT_SCHEDULE['auto-delete-engagements'] = {
 
 CELERY_BEAT_SCHEDULE['jira_status_reconciliation'] = { 
     'task': 'dojo.tasks.jira_status_reconciliation_task',
-    'schedule': 'schedule': timedelta(hours=24),
+    'schedule': timedelta(hours=24),
     'kwargs': {'mode': 'import_status_from_jira', 'dryrun': False, 'daysback': 2}
 }
 
