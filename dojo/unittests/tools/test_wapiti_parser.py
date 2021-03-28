b952d03b4ee1aaa6a0b8a9f04dd2386969647b02
damiencarol <damien.carol@gmail.com>
2021-03-28 19:57:45 +0200

    Fix unit tests for severity

--- dojo/unittests/tools/test_wapiti_parser.py
+++ dojo/unittests/tools/test_wapiti_parser.py
@@ -13,15 +13,15 @@ class TestWapitiParser(TestCase):
         self.assertEqual(3, len(findings))
         finding = findings[0]
         self.assertEqual("Content Security Policy Configuration: CSP is not set", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
         self.assertEqual("Content Security Policy (CSP) is an added layer of security that helps to detect and mitigate certain types of attacks, including Cross Site Scripting (XSS) and data injection attacks.", finding.description)
         self.assertEqual("Configuring Content Security Policy involves adding the Content-Security-Policy HTTP header to a web page and giving it values to control what resources the user agent is allowed to load for that page.", finding.mitigation)
         finding = findings[1]
         self.assertEqual("HTTP Secure Headers: X-XSS-Protection is not set", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
         finding = findings[2]
         self.assertEqual("HTTP Secure Headers: Strict-Transport-Security is not set", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
 
     def test_parse_file_demo(self):
         """"""
@@ -31,7 +31,7 @@ class TestWapitiParser(TestCase):
         self.assertEqual(3, len(findings))
         finding = findings[2]
         self.assertEqual("Secure Flag cookie: Secure flag is not set in the cookie : csrftoken", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
 
     def test_parse_file_example(self):
         """"""
@@ -41,7 +41,7 @@ class TestWapitiParser(TestCase):
         self.assertEqual(5, len(findings))
         finding = findings[2]
         self.assertEqual("HTTP Secure Headers: X-XSS-Protection is not set", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
 
     def test_parse_cwe(self):
         """File to test CWE"""
@@ -51,5 +51,5 @@ class TestWapitiParser(TestCase):
         self.assertEqual(1, len(findings))
         finding = findings[0]
         self.assertEqual("Cross Site Request Forgery: CSP is not set", finding.title)
-        self.assertEqual("High", finding.severity)
+        self.assertEqual("Low", finding.severity)
         self.assertEqual(352, finding.cwe)
