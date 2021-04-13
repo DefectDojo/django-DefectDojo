fa87795c5db470465c70bb4c8036b284e24ee686
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-04-13 13:38:50 +0200

    Fix: flake8

--- dojo/management/commands/dump_scanner_list.py
+++ dojo/management/commands/dump_scanner_list.py
@@ -4,28 +4,28 @@ from dojo.tools import factory
 
 
 class Command(BaseCommand):
-    """
+        """
         Dumps json file of supported scanners
         """
 
-    def add_arguments(self, parser):
-        parser.add_argument('file_path')
+        def add_arguments(self, parser):
+            parser.add_argument('file_path')
 
-    def handle(self, *args, **options):
-        counter = 30
-        model = "dojo.Tool_Type"
-        dump = []
-        for parser in factory.PARSERS:
-            dump.append(
-                {'model': model,
-                 'pk': counter,
-                 'fields': {
-                     'name': factory.PARSERS[parser].get_scan_types()[0],
-                     'description': factory.PARSERS[parser].get_description_for_scan_types("mock")
-                 }
-                 }
-            )
-            counter += 1
-        file = open(options['file_path'], "a")
-        file.write(json.dumps(dump))
-        file.close()
+        def handle(self, *args, **options):
+            counter = 30
+            model = "dojo.Tool_Type"
+            dump = []
+            for parser in factory.PARSERS:
+              dump.append(
+                  {   'model': model,
+                      'pk': counter,
+                      'fields': {
+                          'name': factory.PARSERS[parser].get_scan_types()[0],
+                          'description': factory.PARSERS[parser].get_description_for_scan_types("mock")
+                      }
+                  }
+              )
+              counter+=1
+            file = open(options['file_path'], "a")
+            file.write(json.dumps(dump))
+            file.close()
