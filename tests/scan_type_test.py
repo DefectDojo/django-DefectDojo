1779f74646becd64f0d95d3f4e1910bd79ed25f5
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-04-13 13:27:00 +0200

    Add: Tool type delete message

--- tests/scan_type_test.py
+++ tests/scan_type_test.py
@@ -33,59 +33,15 @@ class RegulationTest(BaseTestCase):
     def test_delete_scanner(self):
         driver = self.driver
         driver.get(self.base_url + "tool_type")
-        driver.find_element_by_xpath("/table[@id='products']/tbody/tr/td[4]/div/a[2]").click()
-
-
-    def test_enable_scanner(self):
-        pass
-
-    # def test_create_regulation(self):
-    #     driver = self.driver
-    #     driver.get(self.base_url + "regulations")
-    #     driver.find_element_by_link_text("Regulations").click()
-    #     driver.find_element_by_id("dropdownMenu1").click()
-    #     driver.find_element_by_link_text("Add regulation").click()
-    #     driver.find_element_by_id("id_name").clear()
-    #     driver.find_element_by_id("id_name").send_keys("PSA_TEST")
-    #     driver.find_element_by_id("id_acronym").clear()
-    #     driver.find_element_by_id("id_acronym").send_keys("PSA_TEST")
-    #     driver.find_element_by_css_selector("option:nth-child(6)").click()
-    #     driver.find_element_by_id("id_jurisdiction").clear()
-    #     driver.find_element_by_id("id_jurisdiction").send_keys("Europe")
-    #     driver.find_element_by_id("id_description").clear()
-    #     driver.find_element_by_id("id_description").send_keys("Few words about PSA")
-    #     driver.find_element_by_id("id_reference").clear()
-    #     driver.find_element_by_id("id_reference").send_keys("http://www.psa.eu")
-    #     driver.find_element_by_css_selector(".col-sm-offset-2 > .btn").click()
-    #
-    #     self.assertTrue(self.is_success_message_present(text='Regulation Successfully Created.'))
-    #
-    # def test_edit_regulation(self):
-    #     driver = self.driver
-    #     driver.get(self.base_url + "regulations")
-    #     driver.find_element_by_link_text("Regulations").click()
-    #     driver.find_element_by_link_text("PSA_TEST").click()
-    #     driver.find_element_by_id("id_name").clear()
-    #     driver.find_element_by_id("id_name").send_keys("Edited PSA test")
-    #     driver.find_element_by_id("submit").click()
-    #     self.assertTrue(self.is_success_message_present(text='Regulation Successfully Updated.'))
-    #
-    # def test_delete_regulation(self):
-    #     driver = self.driver
-    #     driver.get(self.base_url + "regulations")
-    #     driver.find_element_by_link_text("Regulations").click()
-    #     driver.find_element_by_link_text("Edited PSA test").click()
-    #     driver.find_element_by_id("delete").click()
-    #
-    #     self.assertTrue(self.is_success_message_present(text='Regulation Deleted.'))
+        driver.find_element_by_xpath("//table[@id='products']/tbody/tr/td[4]/div/a[2]").click()
+        self.assertTrue(self.is_success_message_present(text="Tool Type Successfully Deleted."))
 
 
 def suite():
     suite = unittest.TestSuite()
     suite.addTest(BaseTestCase('test_login'))
     suite.addTest(RegulationTest('test_disable_scanner'))
-    # suite.addTest(RegulationTest('test_edit_regulation'))
-    # suite.addTest(RegulationTest('test_delete_regulation'))
+    suite.addTest(RegulationTest('test_delete_scanner'))
     return suite
 
 
