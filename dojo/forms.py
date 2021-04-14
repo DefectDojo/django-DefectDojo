f63d50e56ff93df3bd18a09793b0af353a68ca69
Valentijn Scholten <valentijnscholten@gmail.com>
2021-04-14 09:03:29 +0200

    fix Nonetype when adding from template

--- dojo/forms.py
+++ dojo/forms.py
@@ -2280,7 +2280,7 @@ class JIRAFindingForm(forms.Form):
 
         self.fields['jira_issue'].widget = forms.TextInput(attrs={'placeholder': 'Leave empty and check push to jira to create a new JIRA issue'})
 
-        if self.instance.has_jira_group_issue:
+        if self.instance and self.instance.has_jira_group_issue:
             self.fields['push_to_jira'].widget.attrs['checked'] = 'checked'
             self.fields['jira_issue'].help_text = 'Changing the linked JIRA issue for finding groups is not (yet) supported.'
             self.initial['jira_issue'] = self.instance.finding_group.jira_issue.jira_key
