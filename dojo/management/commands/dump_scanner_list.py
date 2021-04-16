d71a9dd6327d2f6b5a6730c15ec24c441d6ea9ff
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-04-16 12:09:36 +0200

    Adding command to resqnc scanner

--- dojo/management/commands/dump_scanner_list.py
+++ dojo/management/commands/dump_scanner_list.py
@@ -1,15 +1,18 @@
 import json
 from django.core.management.base import BaseCommand
 from dojo.tools import factory
+from dojo.models import  Tool_Type
 
 
 class Command(BaseCommand):
     """
-        Dumps json file of supported scanners
+        Dumps json file of supported scanners or directly re-sync it to the DB
+        this is quick and dirty, TODO: refactor
         """
 
     def add_arguments(self, parser):
-        parser.add_argument('file_path')
+        parser.add_argument('-f','--file_path', required=False)
+        parser.add_argument('-r', '--resync', action='store_true')
 
     def handle(self, *args, **options):
         counter = 30
@@ -25,7 +28,17 @@ class Command(BaseCommand):
                  }
                  }
             )
+            if options['resync']:
+                try:
+                    print('Managing {0}'.format(factory.PARSERS[parser].get_scan_types()[0]))
+                    scanner = Tool_Type.objects.create( pk = counter, name = factory.PARSERS[parser].get_scan_types()[0], description = factory.PARSERS[parser].get_description_for_scan_types("mock"))
+                    print('Entered {0}'.format(factory.PARSERS[parser].get_scan_types()[0]))
+                    scanner.save()
+                except:
+                    print('Already exists {0}'.format(factory.PARSERS[parser].get_scan_types()[0]))
+                    pass
             counter += 1
-        file = open(options['file_path'], "a")
-        file.write(json.dumps(dump))
-        file.close()
+        if options['file_path']:
+            file = open(options['file_path'], "a")
+            file.write(json.dumps(dump))
+            file.close()
