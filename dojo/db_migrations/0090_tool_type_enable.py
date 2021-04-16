485655a273230d03abb3f6a661916fa7f58df17e
damiencarol <damien.carol@gmail.com>
2021-04-16 13:29:49 +0200

    Fix migration

--- dojo/db_migrations/0090_tool_type_enable.py
+++ dojo/db_migrations/0090_tool_type_enable.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0088_import_history_cicd'),
+        ('dojo', '0089_unprotect_jira_project'),
     ]
 
     operations = [
