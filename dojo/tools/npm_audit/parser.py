e1e3e53bb58db2771ea5720a3f4153d22fc52a2f
valentijnscholten <valentijnscholten@gmail.com>
2021-04-17 07:18:28 +0200

    update npm audit description (#4286)

--- dojo/tools/npm_audit/parser.py
+++ dojo/tools/npm_audit/parser.py
@@ -16,7 +16,7 @@ class NpmAuditParser(object):
         return scan_type  # no custom label for now
 
     def get_description_for_scan_types(self, scan_type):
-        return "NPM Audit Scan output file can be imported in JSON format."
+        return "NPM Audit Scan json output up to v6 can be imported in JSON format."
 
     def get_findings(self, json_output, test):
         tree = self.parse_json(json_output)
