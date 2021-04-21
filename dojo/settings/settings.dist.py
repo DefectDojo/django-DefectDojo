37d4a88fefa43699dc149e6533fe09b743c1b254
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-04-21 09:09:34 +0200

    added line number to hash_code

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -821,7 +821,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Trivy Scan': ['title', 'severity', 'cve', 'cwe'],
     'Snyk Scan': ['vuln_id_from_tool', 'file_path', 'component_name', 'component_version'],
     'GitLab Dependency Scanning Report': ['title', 'cve', 'file_path', 'component_name', 'component_version'],
-    'SpotBugs Scan': ['cwe', 'severity', 'file_path'],
+    'SpotBugs Scan': ['cwe', 'severity', 'file_path', 'line'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
