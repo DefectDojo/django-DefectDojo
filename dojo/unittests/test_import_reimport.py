4c5e3cdbbfffbcc8e0294da67c869805832e327c
Jannik Jürgens <jannik.juergens@deutschebahn.com>
2021-04-21 17:51:12 +0200

    tests: add unit test for import with parameter endpoint_to_add

--- dojo/unittests/test_import_reimport.py
+++ dojo/unittests/test_import_reimport.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from dojo.models import User, Test
+from dojo.models import User, Test, Finding
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from django.test.client import Client
@@ -65,6 +65,10 @@ class ImportReimportMixin(object):
         self.veracode_same_unique_id_different_hash_code = self.scans_path + 'veracode/many_findings_same_unique_id_different_hash_code.xml'
         self.veracode_different_hash_code_different_unique_id = self.scans_path + 'veracode/many_findings_different_hash_code_different_unique_id.xml'
         self.scan_type_veracode = 'Veracode Scan'
+
+        self.clair_many_findings = self.scans_path + 'clair/many_vul.json'
+        self.clair_empty = self.scans_path + 'clair/empty.json'
+        self.scan_type_clair = 'Clair Scan'
 
     # import zap scan, testing:
     # - import
@@ -985,6 +989,31 @@ class ImportReimportMixin(object):
                 count = count + 1
 
         self.assertEqual(5, count)
+
+    # currenty break when using the parameter endpoint_to_add
+    def test_import_param_close_old_findings_with_additional_endpoint(self):
+        logger.debug('importing clair report with additional endpoint')
+        with assertTestImportModelsCreated(self, imports=1, affected_findings=24, created=24):
+            import0 = self.import_scan_with_params(self.clair_many_findings, scan_type=self.scan_type_clair, close_old_findings=True, endpoint_to_add=1)
+
+        test_id = import0['test']
+        test = self.get_test(test_id)
+        findings = self.get_test_findings_api(test_id)
+        self.log_finding_summary_json_api(findings)
+        # imported count must match count in the report
+        self.assert_finding_count_json(24, findings)
+
+        # imported findings should be active in the engagement
+        engagement_findings_count = Finding.objects.filter(test__engagement_id=1, test__test_type=test.test_type, active=True, is_Mitigated=False).count()
+        self.assertEqual(engagement_findings_count, 24)
+
+        # reimport exact same report
+        with assertTestImportModelsCreated(self, imports=1, affected_findings=24, closed=24):
+            self.import_scan_with_params(self.clair_empty, scan_type=self.scan_type_clair, close_old_findings=True, endpoint_to_add=1)
+
+        # all findings from import0 should be closed now
+        engagement_findings_count = Finding.objects.filter(test__engagement_id=1, test__test_type=test.test_type, active=True, is_Mitigated=False).count()
+        self.assertEqual(engagement_findings_count, 0)
 
 
 @override_settings(TRACK_IMPORT_HISTORY=True)
