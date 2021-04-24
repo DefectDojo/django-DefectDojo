f1727ff71bd23bac1f0ccbecf1a67b0ed0f19ed2
Valentijn Scholten <valentijnscholten@gmail.com>
2021-04-24 16:49:29 +0200

    add auto_group_by to api

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -976,8 +976,8 @@ LOGGING = {
     },
     'loggers': {
         'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
+            'handlers': ['mail_admins', 'console'],
+            'level': 'WARN',
             'propagate': True,
         },
         'django.security': {
