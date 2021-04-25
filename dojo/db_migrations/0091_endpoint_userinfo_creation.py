830bbd589c0d6cd2351d182b4130df6fa4658138
Tomas Kubla <tomas@kubla.sk>
2021-04-25 10:15:43 +0100

    Fix unittest

--- dojo/db_migrations/0091_endpoint_userinfo_creation.py
+++ dojo/db_migrations/0091_endpoint_userinfo_creation.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('dojo', '0089_unprotect_jira_project'),
+        ('dojo', '0090_index_duplicate_finding'),
     ]
 
     operations = [
