df6630af816d9ff0c23c33c9435b4123e24fcd04
Valentijn Scholten <valentijnscholten@gmail.com>
2021-05-01 19:02:12 +0200

    Remove S_Severity naming display option

--- dojo/db_migrations/0092_remove_system_settings_s_finding_severity_naming.py
+++ dojo/db_migrations/0092_remove_system_settings_s_finding_severity_naming.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0071_product_type_enhancement'),
+        ('dojo', '0091_npm_audit_path_censoring'),
     ]
 
     operations = [
