f6cdc7537699cd440e8f4b1dd223449a0c11cb1f
Fred Blaise <madchap@users.noreply.github.com>
2021-05-02 23:31:15 +0200

    Update and rename 0093_remove_system_settings_s_finding_severity_naming.py to 0094_remove_system_settings_s_finding_severity_naming.py

--- dojo/db_migrations/0094_remove_system_settings_s_finding_severity_naming.py
+++ dojo/db_migrations/0094_remove_system_settings_s_finding_severity_naming.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0092_is_mitigated'),
+        ('dojo', '0093_django_tagging_removal'),
     ]
 
     operations = [
