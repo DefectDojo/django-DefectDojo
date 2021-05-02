fd1220e3c78291868e558133242cbae788e9172b
Fred Blaise <madchap@users.noreply.github.com>
2021-05-02 23:44:05 +0200

    Update and rename 0093_remove_old_product_contact_fields.py to 0095_remove_old_product_contact_fields.py

--- dojo/db_migrations/0095_remove_old_product_contact_fields.py
+++ dojo/db_migrations/0095_remove_old_product_contact_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0093_remove_system_settings_s_finding_severity_naming'),
+        ('dojo', '0094_remove_system_settings_s_finding_severity_naming'),
     ]
 
     operations = [
