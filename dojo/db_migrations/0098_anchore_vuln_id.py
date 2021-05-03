14cdd2094dfed919afae9f594894c5a1bb098d5d
Fred Blaise <fred.blaise@protonmail.com>
2021-05-03 12:19:20 +0200

    add extra protection in query

--- dojo/db_migrations/0098_anchore_vuln_id.py
+++ dojo/db_migrations/0098_anchore_vuln_id.py
@@ -7,7 +7,8 @@ class Migration(migrations.Migration):
         finding_model = apps.get_model('dojo', 'Finding')
         test_type_model = apps.get_model('dojo', 'Test_Type')
         anchore_scan, _ = test_type_model.objects.get_or_create(name='Anchore Engine Scan')
-        findings = finding_model.objects.filter(test__test_type=anchore_scan)
+        # extra protection, only migrate for the findings that have a non-null unique_id_from_tool
+        findings = finding_model.objects.filter(test__test_type=anchore_scan, unique_id_from_tool__isnull=False)
 
         # assign value from unique_id_from tool to vuln_id_from_tool
         findings.update(vuln_id_from_tool=F('unique_id_from_tool'))
@@ -19,7 +20,7 @@ class Migration(migrations.Migration):
         finding_model = apps.get_model('dojo', 'Finding')
         test_type_model = apps.get_model('dojo', 'Test_Type')
         anchore_scan, _ = test_type_model.objects.get_or_create(name='Anchore Engine Scan')
-        findings = finding_model.objects.filter(test__test_type=anchore_scan)
+        findings = finding_model.objects.filter(test__test_type=anchore_scan, vuln_id_from_tool__isnull=False)
 
         # assign value from vuln_id_from tool to unique_id_from_tool
         findings.update(unique_id_from_tool=F('vuln_id_from_tool'))
