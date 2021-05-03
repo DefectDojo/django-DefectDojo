7ba84b5581c2cac1687416e64be0382cf03218a6
Fred Blaise <fred.blaise@protonmail.com>
2021-05-03 11:58:56 +0200

    add migration

---
+++ dojo/db_migrations/0098_anchore_vuln_id.py
@@ -0,0 +1,34 @@
+from django.db import migrations
+from django.db.models import F
+
+
+class Migration(migrations.Migration):
+    def move_to_vuln_id(apps, schema_editor):
+        finding_model = apps.get_model('dojo', 'Finding')
+        test_type_model = apps.get_model('dojo', 'Test_Type')
+        anchore_scan, _ = test_type_model.objects.get_or_create(name='Anchore Engine Scan')
+        findings = finding_model.objects.filter(test__test_type=anchore_scan)
+
+        # assign value from unique_id_from tool to vuln_id_from_tool
+        findings.update(vuln_id_from_tool=F('unique_id_from_tool'))
+
+        # reset unique_id_from_tool
+        findings.update(unique_id_from_tool=None)
+
+    def reverse_move_to_vuln_id(apps, schema_editor):
+        finding_model = apps.get_model('dojo', 'Finding')
+        test_type_model = apps.get_model('dojo', 'Test_Type')
+        anchore_scan, _ = test_type_model.objects.get_or_create(name='Anchore Engine Scan')
+        findings = finding_model.objects.filter(test__test_type=anchore_scan)
+
+        # assign value from vuln_id_from tool to unique_id_from_tool
+        findings.update(unique_id_from_tool=F('vuln_id_from_tool'))
+
+        # reset vuln_id_from_tool
+        findings.update(vuln_id_from_tool=None)
+
+    dependencies = [
+        ('dojo', '0097_engagement_type'),
+    ]
+
+    operations = [migrations.RunPython(move_to_vuln_id, reverse_move_to_vuln_id)]
