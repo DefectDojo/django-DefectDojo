591e1be7b465732949c993df0179fbc4e449e95c
Yassine Ilmi <9211413+yilmi@users.noreply.github.com>
2021-05-10 13:57:53 +0100

    Swap ptvsd for debugpy, ptvsd deprecated #4115 (#4322)
    * Merging ptvsd into dev profile
    
    * Documentation minor update and script cleanup
    
    * Updating helm charts to remove mentioned of ptvsd
    
    * Updating docker documentation
    
    * Adding comments in code
    
    * pep8 fixes
    
    * Update docker-compose dev to get port from env
    
    * Keeping debug profile, with same entrypoint as dev
    
    * Documentation adjustments
    
    * Setting variable in helmchart to enable debug
    
    * Final docs changes
    
    * Update helm/defectdojo/templates/django-deployment.yaml
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>
    
    * Update requirements.txt
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>
    
    * Bump chart version, and rename value
    
    * Rebasing from dev
    
    * pep8 fixes
    
    * Rebasing
    
    Co-authored-by: Damien Carol <damien.carol@gmail.com>

--- docker/entrypoint-uwsgi-dev.sh
+++ docker/entrypoint-uwsgi-dev.sh
@@ -9,6 +9,16 @@ cd /app
 #   cp dojo/settings/settings.dist.py dojo/settings/settings.py
 # fi
 
+# Full list of uwsgi options: https://uwsgi-docs.readthedocs.io/en/latest/Options.html
+# --lazy-apps required for debugging --> https://uwsgi-docs.readthedocs.io/en/latest/articles/TheArtOfGracefulReloading.html?highlight=lazy-apps#preforking-vs-lazy-apps-vs-lazy
+
+
+if [ ! -z ${DD_DEBUG} ];then
+  echo "Debug mode enabled, reducing # of processes and threads to 1"
+  DD_UWSGI_NUM_OF_PROCESSES=1
+  DD_UWSGI_NUM_OF_THREADS=1
+fi
+
 exec uwsgi \
   "--${DD_UWSGI_MODE}" "${DD_UWSGI_ENDPOINT}" \
   --protocol uwsgi \
@@ -19,4 +29,6 @@ exec uwsgi \
   --reload-mercy 1 \
   --worker-reload-mercy 1 \
   --py-autoreload 1 \
-  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-8192}"
+  --buffer-size="${DD_UWSGI_BUFFER_SIZE:-8192}" \
+  --lazy-apps
+
