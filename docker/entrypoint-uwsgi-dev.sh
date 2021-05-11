47b5aac87bb9425d378d1e3b101f7dbb896a338d
Fred Blaise <madchap@users.noreply.github.com>
2021-05-11 14:39:20 +0200

    Fix erroneous comparison causing dev env to also be single process (#4487)
    * fix erroneous if causing dev to also be single process
    
    * remove traceback import to the profit of logging.exception(e)

--- docker/entrypoint-uwsgi-dev.sh
+++ docker/entrypoint-uwsgi-dev.sh
@@ -1,19 +1,13 @@
 #!/bin/sh
 
 
-# Copy settings.py (settings.py copied to allow for legacy installs and customizations)
 cd /app
-# TARGET_SETTINGS_FILE=dojo/settings/settings.py
-# if [ ! -f ${TARGET_SETTINGS_FILE} ]; then
-#   echo "Creating settings.py"
-#   cp dojo/settings/settings.dist.py dojo/settings/settings.py
-# fi
 
 # Full list of uwsgi options: https://uwsgi-docs.readthedocs.io/en/latest/Options.html
 # --lazy-apps required for debugging --> https://uwsgi-docs.readthedocs.io/en/latest/articles/TheArtOfGracefulReloading.html?highlight=lazy-apps#preforking-vs-lazy-apps-vs-lazy
 
 
-if [ ! -z ${DD_DEBUG} ];then
+if [ ${DD_DEBUG} == "True" ]; then
   echo "Debug mode enabled, reducing # of processes and threads to 1"
   DD_UWSGI_NUM_OF_PROCESSES=1
   DD_UWSGI_NUM_OF_THREADS=1
@@ -31,4 +25,3 @@ exec uwsgi \
   --py-autoreload 1 \
   --buffer-size="${DD_UWSGI_BUFFER_SIZE:-8192}" \
   --lazy-apps
-
