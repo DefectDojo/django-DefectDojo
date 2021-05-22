b803c381695362b3e5b590e75f14807225e3ec0c
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-05-22 08:19:52 +0200

    rename db migration after rebase

--- dojo/db_migrations/0104_global_roles.py
+++ dojo/db_migrations/0104_global_roles.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0102_dojo_group'),
+        ('dojo', '0103_report_notification'),
     ]
 
     operations = [
