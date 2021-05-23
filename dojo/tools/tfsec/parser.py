672e7c77fa1926478070057e44e612cc25d1db12
Andrey Sidorov <andrey.sidorov@arrival.com>
2021-05-23 13:23:22 +0300

    fix flake8

--- dojo/tools/tfsec/parser.py
+++ dojo/tools/tfsec/parser.py
@@ -30,44 +30,44 @@ class TFSecParser(object):
         if 'results' not in data:
             raise ValueError("Incorrect TFSec scan, missing attribute 'results'")
         for item in data.get('results', []):
-                rule_id = item.get('rule_id')
-                rule_description = item.get('rule_description')
-                rule_provider = item.get('rule_provider')
-                file = item.get('location').get('filename')
-                start_line = item.get('location').get('start_line')
-                end_line = item.get('location').get('end_line')
-                description = '\n'.join([item.get('description'), item.get('link')])
-                impact = item.get('impact')
-                resolution = item.get('resolution')
-                if item.get('passed') is False:
-                    active = True
-                else:
-                    active = False
-                if item.get('severity') in self.SEVERITY:
-                    severity = self.SEVERITY[item.get('severity')]
-                else:
-                    severity = "Low"
+            rule_id = item.get('rule_id')
+            rule_description = item.get('rule_description')
+            rule_provider = item.get('rule_provider')
+            file = item.get('location').get('filename')
+            start_line = item.get('location').get('start_line')
+            end_line = item.get('location').get('end_line')
+            description = '\n'.join([item.get('description'), item.get('link')])
+            impact = item.get('impact')
+            resolution = item.get('resolution')
+            if item.get('passed') is False:
+                active = True
+            else:
+                active = False
+            if item.get('severity') in self.SEVERITY:
+                severity = self.SEVERITY[item.get('severity')]
+            else:
+                severity = "Low"
 
-                dupe_key = hashlib.sha256(
-                    (rule_provider + rule_id + file + str(start_line) + str(end_line)).encode('utf-8')
-                ).hexdigest()
+            dupe_key = hashlib.sha256(
+                (rule_provider + rule_id + file + str(start_line) + str(end_line)).encode('utf-8')
+            ).hexdigest()
 
-                if dupe_key in dupes:
-                    finding = dupes[dupe_key]
-                    finding.nb_occurences += 1
-                else:
-                    finding = Finding(
-                        title=f"{rule_description}",
-                        test=test,
-                        severity=severity,
-                        description=description,
-                        active=active,
-                        mitigation=resolution,
-                        impact=impact,
-                        file_path=file,
-                        line=start_line,
-                        vuln_id_from_tool=rule_id,
-                        nb_occurences=1,
-                    )
-                    dupes[dupe_key] = finding
+            if dupe_key in dupes:
+                finding = dupes[dupe_key]
+                finding.nb_occurences += 1
+            else:
+                finding = Finding(
+                    title=f"{rule_description}",
+                    test=test,
+                    severity=severity,
+                    description=description,
+                    active=active,
+                    mitigation=resolution,
+                    impact=impact,
+                    file_path=file,
+                    line=start_line,
+                    vuln_id_from_tool=rule_id,
+                    nb_occurences=1,
+                )
+                dupes[dupe_key] = finding
         return list(dupes.values())
