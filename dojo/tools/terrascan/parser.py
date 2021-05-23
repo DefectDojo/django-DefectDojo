a482eb62722bec216a75d5b73a046a31e5efe1b7
Damien Carol <damien.carol@gmail.com>
2021-05-23 14:55:57 +0200

    Fix error when results are none

--- dojo/tools/terrascan/parser.py
+++ dojo/tools/terrascan/parser.py
@@ -29,6 +29,8 @@ class TerrascanParser(object):
         dupes = {}
         if 'results' not in data and 'violations' not in data.get('results'):
             raise ValueError("missing mandatory attribute 'results'")
+        if data.get('results') is None:
+            return list()
         for item in data.get('results').get('violations'):
             rule_name = item.get('rule_name')
             description = item.get('description')
