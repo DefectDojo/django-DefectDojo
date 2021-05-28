51f27a2be4f48e64850875f161ac163a28080f34
Valentijn Scholten <valentijnscholten@gmail.com>
2021-05-28 18:58:30 +0200

    add openapi v3 schema validation to unit tests

--- docker/entrypoint-unit-tests-devDocker.sh
+++ docker/entrypoint-unit-tests-devDocker.sh
@@ -13,6 +13,9 @@ unset DD_DATABASE_URL
 python3 manage.py makemigrations dojo
 python3 manage.py migrate
 
+# will print any warnings to stderr
+python3 manage.py spectacular > /dev/null
+
 echo "Swagger Schema Tests - Broken"
 echo "------------------------------------------------------------"
 python3 manage.py test dojo.unittests -v 3 --no-input --tag broken && true
