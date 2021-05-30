6cb295d96a573c45b9837eac98adcb2e86cf101b
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-05-30 23:52:48 +0200

    fix: test

--- dojo/urls.py
+++ dojo/urls.py
@@ -156,7 +156,7 @@ urlpatterns = [
     url(r'^%shistory/(?P<cid>\d+)/(?P<oid>\d+)$' % get_system_setting('url_prefix'), views.action_history,
         name='action_history'),
     url(r'^%s' % get_system_setting('url_prefix'), include(ur)),
-    url(r'^%sapi/v2/api-token-auth/' % get_system_setting('url_prefix'), ObtainAuthTokenExtended.as_view()),
+    url(r'^%sapi/v2/api-token-auth/' % get_system_setting('url_prefix'), ObtainAuthTokenExtended.as_view(), name='api_token_auth'),
     url(r'^%sapi/v2/doc/' % get_system_setting('url_prefix'), schema_view.with_ui('swagger', cache_timeout=0), name='api_v2_schema'),
     url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow: /", content_type="text/plain"), name="robots_file"),
     url(r'^manage_files/(?P<oid>\d+)/(?P<obj_type>\w+)$', views.manage_files, name='manage_files'),
