c6d7e0a4ff99b9a895cef7ee579fb809a07390e0
Nicolas Landais <nlandais@flexion.us>
2021-06-01 21:56:01 -0400

    Full ARN

--- dojo/unittests/tools/test_awssecurityhub.py
+++ dojo/unittests/tools/test_awssecurityhub.py
@@ -33,5 +33,5 @@ class TestAwsSecurityHubParser(TestCase):
         with open(sample_path("one_finding.json")) as test_file:
             parser = AwsSecurityHubParser()
             findings = parser.get_findings(test_file, Test())
-            self.assertEqual("de861909-2d26-4e45-bd86-19d2ab6ceef1",
+            self.assertEqual("arn:aws:securityhub:us-east-1:012345678912:subscription/aws-foundational-security-best-practices/v/1.0.0/IAM.5/finding/de861909-2d26-4e45-bd86-19d2ab6ceef1",
                 findings[0].unique_id_from_tool)
