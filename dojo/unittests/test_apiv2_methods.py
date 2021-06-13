f785e4a896ceae6173c4a47ab74afd12b996d1ac
Tomas Kubla <tomas@kubla.sk>
2021-06-13 14:08:26 +0100

    Migrate to OpenAPI3

--- dojo/unittests/test_apiv2_methods.py
+++ dojo/unittests/test_apiv2_methods.py
@@ -1,27 +1,15 @@
-from rest_framework.test import APIRequestFactory
-from rest_framework.views import APIView
-from rest_framework.test import APITestCase, force_authenticate
-from drf_yasg.generators import OpenAPISchemaGenerator
-from drf_yasg.openapi import Info
-from dojo.models import Dojo_User
 from dojo.urls import v2_api
+from django.test import TestCase
+from dojo.unittests.test_rest_framework import get_open_api3_json_schema, BASE_API_URL
 
-SWAGGER_SCHEMA_GENERATOR = OpenAPISchemaGenerator(Info("defectdojo", "v2"))
 
-
-class ApiEndpointMethods(APITestCase):
+class ApiEndpointMethods(TestCase):
     fixtures = ['dojo_testdata.json']
 
     def setUp(self):
         super().setUp()
-        testuser = Dojo_User.objects.get(username='admin')
 
-        factory = APIRequestFactory()
-        request = factory.get('/')
-        force_authenticate(request, user=testuser)
-        request = APIView().initialize_request(request)
-
-        self.schema = SWAGGER_SCHEMA_GENERATOR.get_schema(request, public=True)
+        self.schema = get_open_api3_json_schema()
 
         self.registry = v2_api.registry
 
@@ -32,9 +20,9 @@ class ApiEndpointMethods(APITestCase):
                 continue
 
             for method in ['get', 'post']:
-                self.assertIsNotNone(self.schema["paths"]['/' + reg + '/'].get(method),
+                self.assertIsNotNone(self.schema["paths"][BASE_API_URL + '/' + reg + '/'].get(method),
                                      "Endpoint: {}, Method: {}".format(reg, method))
 
             for method in ['get', 'put', 'patch', 'delete']:
-                self.assertIsNotNone(self.schema["paths"]['/' + reg + '/{id}/'].get(method),
+                self.assertIsNotNone(self.schema["paths"][BASE_API_URL + '/' + reg + '/{id}/'].get(method),
                                      "Endpoint: {}, Method: {}".format(reg, method))
