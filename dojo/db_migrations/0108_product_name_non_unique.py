f5340a207025b022640079656a7761046659f4f9
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-06-15 11:02:41 +0200

    Fix: migrations

--- dojo/db_migrations/0108_product_name_non_unique.py
+++ dojo/db_migrations/0108_product_name_non_unique.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0103_report_notification'),
+        ('dojo', '0107_global_role'),
     ]
 
     operations = [
