16f7050107fa8c3451a676784d618aaa0eccebe7
Dubravko Sever <dubravko.sever@pan-net.eu>
2021-06-15 12:21:40 +0200

    fix: validation after rebase

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -440,14 +440,6 @@ class ProductGroupSerializer(serializers.ModelSerializer):
         return data
 
 
-    def validate(self, data):
-        if self.context['request'].method in ['POST', 'PATCH', 'PUT']:
-            products = Product.objects.filter(prod_type=data.get('prod_type'), name=data.get('name'))
-            if products.count() > 0:
-                raise ValidationError('Product name with the same type already exists')
-        return data
-
-
 class ProductTypeMemberSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -1128,6 +1120,13 @@ class ProductSerializer(TaggitSerializer, serializers.ModelSerializer):
             }
         else:
             exclude = ['tid', 'updated', 'authorized_users']
+
+    def validate(self, data):
+        if self.context['request'].method in ['POST', 'PATCH', 'PUT']:
+            products = Product.objects.filter(prod_type=data.get('prod_type'), name=data.get('name'))
+            if products.count() > 0:
+                raise ValidationError('Product name for the same type already exists')
+        return data
 
     def get_findings_count(self, obj) -> int:
         return obj.findings_count
