7f5ca17fd21baa2a4f6556fa6e8ae7d2f31c26cc
Damien Carol <damien.carol@gmail.com>
2021-06-16 23:30:07 +0200

    Update dojo/tools/whitehat_sentinel/parser.py

--- dojo/tools/whitehat_sentinel/parser.py
+++ dojo/tools/whitehat_sentinel/parser.py
@@ -26,9 +26,6 @@ class WhiteHatSentinelParser(object):
 
     def get_findings(self, file, test):
 
-        # Exit if file is not provided
-        if file is None:
-            return []
 
         findings_collection = json.load(file)
 
