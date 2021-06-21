445ea2e1d29695223406f4f1427ec4a055aceb2c
SupaJuke <supa7412@gmail.com>
2021-06-21 13:40:03 +0700

    add files to branch

---
+++ dojo/tools/trustwave_fusion_api/parser.py
@@ -0,0 +1,109 @@
+import json
+import hashlib
+from datetime import datetime
+from dojo.models import Finding, Endpoint
+
+
+class TrustwaveFusionAPIParser(object):
+    """
+    Import Trustwave Fusion Report from its API in JSON format
+    """
+
+    def get_scan_types(self):
+        return ["Trustwave Fusion API Scan"]
+
+    def get_label_for_scan_types(self, scan_type):
+        return "Trustwave Fusion API Scan"
+
+    def get_description_for_scan_types(self, scan_type):
+        return "Trustwave Fusion API report file can be imported in JSON format"
+
+    def get_findings(self, file, test):
+        if file is None:
+            return None
+
+        data = file.read()
+        tree = json.loads(data)
+
+        if tree:
+            return self.get_items(tree, test)
+
+    def get_items(self, tree, test):
+        items = {}
+
+        # iterating through each vulnerability
+        for node in tree['items']:
+            item = get_item(node, test)
+
+            item_key = hashlib.sha256("|".join([
+                item.severity,
+                item.title,
+                item.description
+            ]).encode()).hexdigest()
+
+            if item_key in items:
+                items[item_key].unsaved_endpoints.extend(item.unsaved_endpoints)
+                items[item_key].nb_occurences += 1
+            else:
+                items[item_key] = item
+
+        return list(items.values())
+
+    def convert_severity(self, num_severity):
+        """Convert severity value"""
+        if num_severity >= -10:
+            return "Low"
+        elif -11 >= num_severity > -26:
+            return "Medium"
+        elif num_severity <= -26:
+            return "High"
+        else:
+            return "Info"
+
+
+def get_item(vuln, test):
+    finding = Finding(
+        test=test,
+        unique_id_from_tool=vuln['id'],
+        nb_occurences=1,
+    )
+
+    # Endpoint
+    if vuln['location']['url'] != 'None':
+        endpoint = Endpoint.from_uri(vuln['location']['url'])
+        finding.unsaved_endpoints = [endpoint]
+    else:  # fallback to using old way of creating endpoints
+        endpoint = Endpoint(
+            protocol=vuln['location']['applicationProtocol'],
+            host=str(vuln['location']['ip']),
+            port=vuln['location']['port']
+        )
+        finding.unsaved_endpoints = [endpoint]
+
+    finding.title = vuln['name']
+
+    # Description + CVEs
+    description = vuln['classification']
+    cves = "no match"
+    if "CVE-NO-MATCH" not in vuln['kb']['cves']:
+        cve = vuln['kb']['cves'][0]
+        finding.cve = int(cve[9:])
+
+        cves = ""
+        for cve in vuln['kb']['cves']:
+            cves += f"{cve}, "
+        cves = cves[:len(cves) - 2]  # removing the comma and the blank space
+
+    finding.description = description + "; CVEs: " + cves
+    finding.severity = vuln['severity'].title()
+
+    # Date
+    date_str = vuln['updatedOn']
+    date_str = date_str[:len(date_str) - 3] + date_str[-2:]
+    finding.date = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+    if "CVE-NO-MATCH" not in vuln['kb']['cves']:
+        cve = vuln['kb']['cves'][0]
+        finding.cve = int(cve[9:])
+
+    return finding
---
+++ dojo/unittests/tools/test_trustwave_fusion_api_parser.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from dojo.tools.trustwave_fusion_api.parser import TrustwaveFusionAPIParser
+from dojo.models import Test
+
+
+class TestTrustwaveFusionAPIParser(TestCase):
+
+    def test_parse_file_with_no_vuln_has_no_findings(self):
+        testfile = open("dojo/unittests/scans/trustwave_fusion_api/trustwave_fusion_api_zero_vul.json")
+        parser = TrustwaveFusionAPIParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(0, len(findings))
+
+    def test_vuln_with_valid_cve(self):
+        testfile = open("dojo/unittests/scans/trustwave_fusion_api/test_cve.json")
+        parser = TrustwaveFusionAPIParser()
+        findings = parser.get_findings(testfile, Test())
+
+        finding = findings[0]
+        self.assertEqual(7529, finding.cve)
+        self.assertEqual("Vulnerability/Missing Patch; CVEs: CVE-2017-7529",
+            finding.description)
+
+        finding = findings[1]
+        self.assertEqual(2566, finding.cve)  # We use the first cve
+        self.assertEqual("Cryptography/Weak Cryptography; CVEs: CVE-2013-2566, CVE-2015-2808",
+            finding.description)
+
+    def test_parse_file_with_multiple_vuln_has_multiple_findings(self):
+        testfile = open("dojo/unittests/scans/trustwave_fusion_api/trustwave_fusion_api_many_vul.json")
+        parser = TrustwaveFusionAPIParser()
+        findings = parser.get_findings(testfile, Test())
+
+        self.assertEqual(2, len(findings))
+
+        # endpoint validation
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
+
+        finding = findings[0]
+        self.assertEqual("0123456:id", finding.unique_id_from_tool)
+        # vulnerability does not have a name: fallback to using id as a title
+        self.assertEqual("Website Detected", finding.title)
+        self.assertEqual("Information/Service Discovery; CVEs: no match", finding.description)
+
+        date = finding.date.strftime("%Y-%m-%dT%H:%M:%S.%f%z")
+        self.assertEqual("2021-06-15T07:48:08.727000+0000", date)
+
+        self.assertEqual("Info", finding.severity)
+
+        self.assertIsNone(finding.cve)  # should be none since CVE is "CVE-NO-MATCH"
