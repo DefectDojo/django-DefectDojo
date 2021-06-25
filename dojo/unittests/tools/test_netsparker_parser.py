d24f4bf3dde8ff06db92d015cab4d5cea8f3eb6a
Muhammed Kılıç <3473393+kiliczsh@users.noreply.github.com>
2021-06-25 10:12:27 +0000

    Fixed scan data filename and converted cwe values to string.

--- dojo/unittests/tools/test_netsparker_parser.py
+++ dojo/unittests/tools/test_netsparker_parser.py
@@ -13,20 +13,20 @@ class TestNetsparkerParser(TestCase):
         with self.subTest(i=0):
             finding = findings[0]
             self.assertEqual("Medium", finding.severity)
-            self.assertEqual(16, finding.cwe)
+            self.assertEqual('16', finding.cwe)
             self.assertIsNotNone(finding.description)
             self.assertGreater(len(finding.description), 0)
             self.assertEqual("5.5", finding.cvssv3)
 
     def test_parse_file_with_multiple_finding(self):
-        testfile = open("dojo/unittests/scans/netsparker/netsparker_many_finding.json")
+        testfile = open("dojo/unittests/scans/netsparker/netsparker_many_findings.json")
         parser = NetsparkerParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(40, len(findings))
         with self.subTest(i=0):
             finding = findings[0]
             self.assertEqual("Medium", finding.severity)
-            self.assertEqual(16, finding.cwe)
+            self.assertEqual('16', finding.cwe)
             self.assertIsNotNone(finding.description)
             self.assertGreater(len(finding.description), 0)
             self.assertEqual("5.5", finding.cvssv3)
@@ -34,7 +34,7 @@ class TestNetsparkerParser(TestCase):
         with self.subTest(i=1):
             finding = findings[1]
             self.assertEqual("Critical", finding.severity)
-            self.assertEqual(89, finding.cwe)
+            self.assertEqual('89', finding.cwe)
             self.assertIsNotNone(finding.description)
             self.assertGreater(len(finding.description), 0)
             self.assertEqual("10.0", finding.cvssv3)
@@ -42,7 +42,7 @@ class TestNetsparkerParser(TestCase):
         with self.subTest(i=2):
             finding = findings[2]
             self.assertEqual("Medium", finding.severity)
-            self.assertEqual(205, finding.cwe)
+            self.assertEqual('205', finding.cwe)
             self.assertIsNotNone(finding.description)
             self.assertGreater(len(finding.description), 0)
             self.assertEqual("4.1", finding.cvssv3)
