8e7a84b8679b538c93562fde9efdcc6ea8cfef80
valentijnscholten <valentijnscholten@gmail.com>
2021-06-29 19:22:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='DefectDojo',
-    version='2.0.0',
+    version='2.1.0-dev',
     author='Greg Anderson',
     description="Tool for managing vulnerability engagements",
     install_requires=[
