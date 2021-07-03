effa339f5616edd81e3d5057d4f9753b286d603a
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-07-03 09:11:36 +0200

    integration test for group members

--- tests/group_test.py
+++ tests/group_test.py
@@ -1,6 +1,12 @@
 import unittest
 import sys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from base_test_class import BaseTestCase
+from user_test import UserTest
+from product_test import ProductTest
 
 
 class GroupTest(BaseTestCase):
@@ -23,7 +29,7 @@ class GroupTest(BaseTestCase):
         # Assert status is success
         self.assertTrue(self.is_success_message_present(text='Group was added successfully.'))
 
-    def test_group_edit_name(self):
+    def test_group_edit_name_and_global_role(self):
         # Login to the site. Password will have to be modified
         # to match an admin password in your own container
         driver = self.driver
@@ -44,11 +50,80 @@ class GroupTest(BaseTestCase):
         # Edit name
         driver.find_element_by_id("id_name").clear()
         driver.find_element_by_id("id_name").send_keys("Another Name")
+        # Select the role 'Reader'
+        Select(driver.find_element_by_id("id_role")).select_by_visible_text("Reader")
         # "Click" the submit button to complete the transaction
         driver.find_element_by_css_selector("input.btn.btn-primary").click()
 
         # Assert status is success
         self.assertTrue(self.is_success_message_present(text='Group saved successfully.'))
+
+    def test_add_group_member(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.driver
+        # Navigate to Group Management page
+        driver.get(self.base_url + "group")
+        # Select and click on the particular group to view
+        driver.find_element_by_link_text("Another Name").click()
+        # Open the menu to add users and click the 'Add' button
+        driver.find_element_by_id("dropdownMenuAddGroupMember").click()
+        driver.find_element_by_id("addGroupMember").click()
+        # Select the user 'propersahm'
+        try:
+            WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'id_users_chosen')))
+        except TimeoutException:
+            self.fail('Timed out waiting for products dropdown to initialize ')
+        driver.execute_script("document.getElementsByName('users')[0].style.display = 'inline'")
+        element = driver.find_element_by_xpath("//select[@name='users']")
+        user_option = element.find_elements_by_tag_name('option')[0]
+        Select(element).select_by_value(user_option.get_attribute("value"))
+        # Select the role 'Reader'
+        Select(driver.find_element_by_id("id_role")).select_by_visible_text("Reader")
+        # "Click" the submit button to complete the transaction
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        # Assert the message to determine success status
+        self.assertTrue(self.is_success_message_present(text='Group members added successfully.'))
+        # Query the site to determine if the member has been added
+        self.assertEqual(driver.find_elements_by_name("member_user")[1].text, "Proper Samuel (propersahm)")
+        self.assertEqual(driver.find_elements_by_name("member_role")[1].text, "Reader")
+
+    def test_edit_group_member(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.driver
+        # Navigate to Group Management page
+        driver.get(self.base_url + "group")
+        # Select and click on the particular group to view
+        driver.find_element_by_link_text("Another Name").click()
+        # Open the menu to manage members and click the 'Edit' button
+        driver.find_elements_by_name("dropdownManageGroupMembers")[1].click()
+        driver.find_elements_by_name("editGroupMember")[1].click()
+        # Select the role 'Maintainer'
+        Select(driver.find_element_by_id("id_role")).select_by_visible_text("Maintainer")
+        # "Click" the submit button to complete the transaction
+        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        # Assert the message to determine success status
+        self.assertTrue(self.is_success_message_present(text='Group member updated successfully'))
+        # Query the site to determine if the member has been edited
+        self.assertEqual(driver.find_elements_by_name("member_user")[1].text, "Proper Samuel (propersahm)")
+        self.assertEqual(driver.find_elements_by_name("member_role")[1].text, "Maintainer")
+
+    def test_delete_group_member(self):
+        # Login to the site. Password will have to be modified
+        # to match an admin password in your own container
+        driver = self.driver
+        # Navigate to Group Management page
+        driver.get(self.base_url + "group")
+        # Select and click on the particular group to view
+        driver.find_element_by_link_text("Another Name").click()
+        # Open the menu to manage members and click the 'Delete' button
+        driver.find_elements_by_name("dropdownManageGroupMembers")[1].click()
+        driver.find_elements_by_name("deleteGroupMember")[1].click()
+        # "Click" the submit button to complete the transaction
+        driver.find_element_by_css_selector("input.btn.btn-danger").click()
+        # Assert the message to determine success status
+        self.assertTrue(self.is_success_message_present(text='Group member deleted successfully.'))
 
     def test_group_delete(self):
         # Login to the site. Password will have to be modified
@@ -80,9 +155,16 @@ def suite():
     # Add each test the the suite to be run
     # success and failure is output by the test
     suite.addTest(BaseTestCase('test_login'))
+    suite.addTest(ProductTest('test_create_product'))
+    suite.addTest(UserTest('test_create_user'))
     suite.addTest(GroupTest('test_create_group'))
-    suite.addTest(GroupTest('test_group_edit_name'))
+    suite.addTest(GroupTest('test_group_edit_name_and_global_role'))
+    suite.addTest(GroupTest('test_add_group_member'))
+    suite.addTest(GroupTest('test_edit_group_member'))
+    suite.addTest(GroupTest('test_delete_group_member'))
     suite.addTest(GroupTest('test_group_delete'))
+    suite.addTest(UserTest('test_user_delete'))
+    suite.addTest(ProductTest('test_delete_product'))
 
     return suite
 
