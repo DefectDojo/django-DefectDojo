e67db66809b39b37958e3d89f984862b31b4aa10
Pablo Santiago López <pablosantiagolopez1@gmail.com>
2021-07-09 15:15:28 +0200

    Syntax correction

--- dojo/tools/detect_secrets/parser.py
+++ dojo/tools/detect_secrets/parser.py
@@ -25,7 +25,7 @@ class DetectSecretsParser(object):
             find_date = dateutil.parser.parse(data.get('generated_at'))
         for detect_file in data.get('results'):
             for item in data.get('results').get(detect_file):
-                if 'is_secret' in item and item['is_secret'] == False:
+                if 'is_secret' in item and item['is_secret'] is False:
                     continue
                 type = item.get('type')
                 file = item.get('filename')
