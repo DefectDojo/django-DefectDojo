65a403c5c2081cfccd485bfccd75492a34b84ccf
Blake Owens <blake.owens@10security.com>
2021-07-12 03:20:26 -0500

    Attempt to fix migration

--- dojo/db_migrations/0117_usercontactinfo_force_password_reset.py
+++ dojo/db_migrations/0117_usercontactinfo_force_password_reset.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0117_cyclonedx_vuln_uniqu'),
+        ('dojo', '0116_test_type_active'),
     ]
 
     operations = [
