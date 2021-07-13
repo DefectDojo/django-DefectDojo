17fdc7b53a609a79732a93caaefd053085643d0a
Tomas Kubla <tomas@kubla.sk>
2021-07-13 10:37:44 +0200

    fix db_mig

--- dojo/db_migrations/0119_sonarqube_test_and_clean.py
+++ dojo/db_migrations/0119_sonarqube_test_and_clean.py
@@ -23,7 +23,7 @@ def sq_clean(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0116_test_type_active'),
+        ('dojo', '0118_remove_finding_images'),
     ]
 
     operations = [
