60edeb1334a1fefe322bee08a200975c57eb2f3f
Arthur Wolf <46627109+VeepsFR@users.noreply.github.com>
2021-07-15 10:46:52 +0200

    Update semgrep parser.py
    Updated semgrep parser.py because the Info severity tag was not recognised

--- dojo/tools/semgrep/parser.py
+++ dojo/tools/semgrep/parser.py
@@ -67,5 +67,7 @@ class SemgrepParser(object):
             return "Low"
         elif "ERROR" == val.upper():
             return "High"
+        elif "INFO" == val.upper():
+            return "Informational"
         else:
             raise ValueError(f"Unknown value for severity: {val}")
