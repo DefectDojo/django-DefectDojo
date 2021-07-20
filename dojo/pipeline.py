2c38bc8e599269f29ae1a7ea74ba3e1ab9e37359
bgoareguer <43874676+bgoareguer@users.noreply.github.com>
2021-07-20 22:41:35 +0200

    Authorization V2: fix Gitlab import (#4839)
    * Fix roles when importing Gitlab projects
    
    * Fix roles when importing Gitlab projects

--- dojo/pipeline.py
+++ dojo/pipeline.py
@@ -3,10 +3,10 @@ import gitlab
 from django.conf import settings
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from dojo.models import Engagement, Product, Product_Member, Product_Type, Test
+from dojo.models import Engagement, Product, Product_Member, Product_Type, Test, Role
 from social_core.backends.azuread_tenant import AzureADTenantOAuth2
 from social_core.backends.google import GoogleOAuth2
-from dojo.authorization.roles_permissions import Permissions
+from dojo.authorization.roles_permissions import Permissions, Roles
 from dojo.product.queries import get_authorized_products
 
 
@@ -88,11 +88,7 @@ def update_product_access(backend, uid, user=None, social=None, *args, **kwargs)
                     product.authorized_users.add(user)
                     product.save()
                 else:
-                    product_member, created = Product_Member.objects.get_or_create(product=product, user=user)
-                    if created:
-                        # Make product member an Owner of the product
-                        product_member.role = 4
-                        product_member.save()
+                    product_member, created = Product_Member.objects.get_or_create(product=product, user=user, defaults={'role': Role.objects.get(id=Roles.Owner)})
 
         # For each product: if user is not project member any more, remove him from product's authorized users
         for product_name in user_product_names:
