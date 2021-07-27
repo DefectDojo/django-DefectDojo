a45ec12a59b5507e19468f1fe71b5afc205abd36
Dmitriy Tishkin <dtishkin@rvision.pro>
2021-07-28 02:14:30 +0300

    impruvments of cargo audit parser

--- dojo/tools/cargo_audit/parser.py
+++ dojo/tools/cargo_audit/parser.py
@@ -24,11 +24,16 @@ class CargoAuditParser(object):
             for item in data.get('vulnerabilities').get('list'):
                 advisory = item.get('advisory')
                 vuln_id = advisory.get('id')
-                title = advisory.get('title')
-                description = "\n".join([
-                    f"**Description:** `{advisory.get('description')}`",
-                    f"\n**Read more:** `{advisory.get('url')}`",
-                ])
+                if "categories" in advisory:
+                    categories = f"**Categories:** {', '.join(advisory['categories'])}"
+                else:
+                    categories = ''
+                description = categories + f"\n**Description:** `{advisory.get('description')}`"
+                if "functions" in item["affected"]:
+                    affected_func = [f'{func}: {", ".join(versions)}'
+                                     for func, versions in item["affected"]["functions"].items()]
+                    description += f"\n**Affected functions**: {', '.join(affected_func)}"
+                references = f"{advisory.get('url')}\n" + '\n'.join(advisory['references'])
                 date = advisory.get('date')
                 if len(advisory.get('aliases')) != 0:
                     cve = advisory.get('aliases')[0]
@@ -36,12 +41,16 @@ class CargoAuditParser(object):
                     cve = None
                 package_name = item.get('package').get('name')
                 package_version = item.get('package').get('version')
+                title = f"[{package_name} {package_version}] {advisory.get('title')}"
                 severity = "High"
                 if 'keywords' in advisory:
                     tags = advisory.get('keywords')
                 else:
                     tags = []
-
+                try:
+                    mitigation = f"**Update {package_name} to** {', '.join(item['versions']['patched'])}"
+                except KeyError:
+                    mitigation = "No information about patched version"
                 dupe_key = hashlib.sha256(
                     (vuln_id + str(cve) + date + package_name + package_version).encode('utf-8')
                 ).hexdigest()
@@ -62,6 +71,8 @@ class CargoAuditParser(object):
                         vuln_id_from_tool=vuln_id,
                         publish_date=date,
                         nb_occurences=1,
+                        references=references,
+                        mitigation=mitigation
                     )
                     dupes[dupe_key] = finding
         return list(dupes.values())
