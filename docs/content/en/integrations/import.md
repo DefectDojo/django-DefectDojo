7f5acfb4fc14317d54c43633c96b69a7692a66b7
kiblik <tomas@kubla.sk>
2021-08-03 04:15:20 +0200

    SonarQube: support multiple SQ projects for one product (#4676)
    * WIP
    
    * Remove SQ as subform of add/edit_product, add "add", "view", "edit", "delete" of SQ configuration - for product
    
    * sq: logic
    
    * flake8 + db_mig
    
    * flake8
    
    * typos
    
    * Cleanup
    
    * Typo
    
    * Filters
    
    * Fix client.find_project call
    
    * Move db_mig
    
    * Unittests
    
    * fix db_mig
    
    * --allow-empty-comment
    
    * fix db_mig
    
    * rerun tests
    
    * SQ Test connection - Tool Config
    
    * SQ Test connection - SQ config in Product
    
    * Update migration
    
    Co-authored-by: Cody Maffucci <46459665+Maffooch@users.noreply.github.com>

--- docs/content/en/integrations/import.md
+++ docs/content/en/integrations/import.md
@@ -796,20 +796,29 @@ Version: \>= 1.1.0
 SonarQube API will be accessed to gather the report. No report file
 required.
 
-Follow below steps to setup API Import:
+Follow the below steps to setup API Import:
 
 1.  Configure the Sonarqube Authentication details by navigating to
     Configuration-\>Tool Configuration. Note the url should be in the
-    formation of <http(s)://>\<sonarqube\_hostname\>/api. Select the tool
-    type to SonarQube. By default tool will import vulnerabilities only,
-    but additional filters can be setup using Extras field separated by commas (e.g. BUG,VULNERABILITY,CODE_SMELL)
+    formation of `<http(s)://>\<sonarqube\_hostname\>/api`. Select the tool
+    type to SonarQube. By default the tool will import vulnerabilities only,
+    but additional filters can be setup using the Extras field separated by 
+    commas (e.g. BUG,VULNERABILITY,CODE_SMELL)
 2.  In the Product settings fill the details for the SonarQube Project
     Key (Key name can be found by navigating to a specific project and
     selecting the value from the url
-    <http(s)://>\<sonarqube\_host\>/dashboard?id=\<key\_name\>
-3.  Once all of the above setting are made, the API Import should be
-    able to auto import all vulnerability information from the SonarQube
-    instance.
+    `<http(s)://>\<sonarqube\_host\>/dashboard?id=\<key\_name\>`.
+    In case you will not provide SonarQube Project Key, DefectDojo will 
+    try to use Product as Project name in SonarQube. If you would like to 
+    collect findings from multiple projects you can specify multiple Keys as 
+    separated SonarQube Configuration in the Product settings.
+3.  Once all of the above settings are made, the API Import should be
+    able to auto-import all vulnerability information from the SonarQube 
+    instance. During setting import, you can select which SonarQube 
+    Configuration (which Project key) should be used. If you will not choose 
+    any, DefectDojo will try to do the best guess :) (if you defined only one 
+    Product SonarQube Configuration or only one SonarQube Tool Configuration).
+
 
 **NOTE**: If `https` is in use for the SonarQube than certificate should be
 trusted by DD instance.
--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -1176,6 +1176,8 @@ class ImportScanSerializer(serializers.Serializer):
     build_id = serializers.CharField(required=False)
     branch_tag = serializers.CharField(required=False)
     commit_hash = serializers.CharField(required=False)
+    sonarqube_config = serializers.PrimaryKeyRelatedField(allow_null=True, default=None,
+                                                          queryset=Sonarqube_Product.objects.all())
 
     test = serializers.IntegerField(read_only=True)  # not a modelserializer, so can't use related fields
 
@@ -1195,6 +1197,7 @@ class ImportScanSerializer(serializers.Serializer):
         build_id = data.get('build_id', None)
         branch_tag = data.get('branch_tag', None)
         commit_hash = data.get('commit_hash', None)
+        sonarqube_config = data.get('sonarqube_config', None)
 
         environment_name = data.get('environment', 'Development')
         environment = Development_Environment.objects.get(name=environment_name)
@@ -1222,7 +1225,8 @@ class ImportScanSerializer(serializers.Serializer):
                                                                              commit_hash=commit_hash,
                                                                              push_to_jira=push_to_jira,
                                                                              close_old_findings=close_old_findings,
-                                                                             group_by=group_by)
+                                                                             group_by=group_by,
+                                                                             sonarqube_config=sonarqube_config)
         # convert to exception otherwise django rest framework will swallow them as 400 error
         # exceptions are already logged in the importer
         except SyntaxError as se:
@@ -1276,6 +1280,8 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
     build_id = serializers.CharField(required=False)
     branch_tag = serializers.CharField(required=False)
     commit_hash = serializers.CharField(required=False)
+    sonarqube_config = serializers.PrimaryKeyRelatedField(allow_null=True, default=None,
+                                                          queryset=Sonarqube_Product.objects.all())
 
     group_by = serializers.ChoiceField(required=False, choices=Finding_Group.GROUP_BY_OPTIONS, help_text='Choose an option to automatically group new findings by the chosen option.')
 
@@ -1294,6 +1300,7 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
         build_id = data.get('build_id', None)
         branch_tag = data.get('branch_tag', None)
         commit_hash = data.get('commit_hash', None)
+        sonarqube_config = data.get('sonarqube_config', None)
 
         scan = data.get('file', None)
         endpoints_to_add = [endpoint_to_add] if endpoint_to_add else None
@@ -1309,7 +1316,7 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
                                             version=version, branch_tag=branch_tag, build_id=build_id,
                                             commit_hash=commit_hash, push_to_jira=push_to_jira,
                                             close_old_findings=close_old_findings,
-                                            group_by=group_by)
+                                            group_by=group_by, sonarqube_config=sonarqube_config)
         # convert to exception otherwise django rest framework will swallow them as 400 error
         # exceptions are already logged in the importer
         except SyntaxError as se:
