a1b93c5feefd451d261c2c8dff9abf5daef1e4b3
Michael Chen <michaelchen@Michaels-MacBook-Pro.local>
2021-08-09 15:34:06 -0700

    Fix for flake8

--- dojo/product/views.py
+++ dojo/product/views.py
@@ -317,7 +317,6 @@ def finding_querys(request, prod):
     # risk_acceptances = Risk_Acceptance.objects.filter(engagement__in=Engagement.objects.filter(product=prod)).prefetch_related('accepted_findings')
     # filters['accepted'] = [finding for ra in risk_acceptances for finding in ra.accepted_findings.all()]
 
-    from dojo.finding.helper import ACCEPTED_FINDINGS_QUERY
     filters['accepted'] = findings_qs.filter(date__range=[start_date, end_date],
                                             risk_accepted=True)
     filters['verified'] = findings_qs.filter(date__range=[start_date, end_date],
