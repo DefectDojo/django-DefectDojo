2c2884b0421ecb6b7376e5bc9872990fb6653e8c
jhewi <87027856+jhewi@users.noreply.github.com>
2021-08-19 14:50:58 -0400

    Create startupdedupe.py

---
+++ dojo/management/commands/startupdedupe.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from dojo.models import Product, Product_Type, Tool_Type,
+
+
+"""
+Author: Josh Hewing and Michael Chen 
+This script will solve issue #3666 by checking for dedupulication configuration at startup:
+"""
+
+class Command(BaseCommand):
+  
+  For each_var in HASHCODE_FIELDS_PER_SCANNER:
+	for each_ind in each_var:
+		if each_ind not in HASHCODE_ALLOWED_FIELDS:
+			deduplicationLogger.debug(
+				"compute_hash_code - configuration error: some elements of HASHCODE_FIELDS_PER_SCANNER are not in the allowed list HASHCODE_ALLOWED_FIELDS. "
+                      		 "Using default fields")
+
+
+  
+
+
+
+
+
+
