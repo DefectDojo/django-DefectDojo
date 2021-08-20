89bc6cf4b1f9d2e9b8023901cd8fe51aa923e6e8
jhewi <87027856+jhewi@users.noreply.github.com>
2021-08-20 18:03:28 -0400

    Update validatededupeconfig.py

--- dojo/management/commands/validatededupeconfig.py
+++ dojo/management/commands/validatededupeconfig.py
@@ -3,12 +3,12 @@ from django.core.management.base import BaseCommand
 
 """
 Author: Josh Hewing and Michael Chen 
-This script will solve issue #3666 by checking for deduplication configuration at startup:
+This script will solve issue #3666 by checking for deduplication configuration at startup
 """
+
 
 class Command(BaseCommand):
     for each_var in HASHCODE_FIELDS_PER_SCANNER:
         for each_ind in each_var:
             if each_ind not in HASHCODE_ALLOWED_FIELDS:
-                deduplicationLogger.info(
-		    "compute_hash_code - configuration error: some elements of HASHCODE_FIELDS_PER_SCANNER are not in the allowed list HASHCODE_ALLOWED_FIELDS. " "Using default fields")
+                deduplicationLogger.info("compute_hash_code - configuration error: some elements of HASHCODE_FIELDS_PER_SCANNER are not in the allowed list HASHCODE_ALLOWED_FIELDS. " "Using default fields")
