e1646d362f0c48f3ea3f45c53e319d0e2ec10284
alles-klar <jannik-j@t-online.de>
2021-09-07 13:23:26 +0200

    fix(rest-api): fix some warings from drf (#5031)
    On behalf of DB Systel GmbH
    
    Co-authored-by: valentijnscholten <valentijnscholten@gmail.com>

--- dojo/benchmark/urls.py
+++ dojo/benchmark/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)$', views.benchmark_view, name='view_product_benchmark'),
-    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/category/(?P<cat>\d+)', views.benchmark_view, name='view_product_benchmark'),
-    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/category/(?P<cat>\d+)/edit/(?P<bid>\d+)', views.benchmark_view, name='edit_benchmark'),
-    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/delete', views.delete, name='delete_product_benchmark')
+    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/category/(?P<cat>\d+)$', views.benchmark_view, name='view_product_benchmark'),
+    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/category/(?P<cat>\d+)/edit/(?P<bid>\d+)$', views.benchmark_view, name='edit_benchmark'),
+    url(r'^benchmark/(?P<pid>\d+)/type/(?P<type>\d+)/delete$', views.delete, name='delete_product_benchmark')
 ]
