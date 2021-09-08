8822f858b8098942206291164fd517acdf7d108b
valentijnscholten <valentijnscholten@gmail.com>
2021-09-08 20:19:57 +0200

    quickfix error logging REST framework (#5059)

--- dojo/api_v2/exception_handler.py
+++ dojo/api_v2/exception_handler.py
@@ -2,6 +2,9 @@ from django.db.models.deletion import RestrictedError
 from rest_framework.response import Response
 from rest_framework.status import HTTP_409_CONFLICT, HTTP_500_INTERNAL_SERVER_ERROR
 from rest_framework.views import exception_handler
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def custom_exception_handler(exc, context):
@@ -14,6 +17,7 @@ def custom_exception_handler(exc, context):
         response.status_code = HTTP_500_INTERNAL_SERVER_ERROR
         response.data = {}
         response.data['message'] = 'Internal server error, check logs for details'
+        logger.exception('Error in call to REST framework')
 
     if isinstance(exc, RestrictedError):
         response.status_code = HTTP_409_CONFLICT
