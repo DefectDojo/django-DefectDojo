b02ab9f525676defeb496e11036f83701f0a11c5
damiencarol <damien.carol@gmail.com>
2021-09-09 14:34:26 +0200

    Fix errors with Spotbugs 4.4.x

--- dojo/unittests/tools/test_spotbugs_parser.py
+++ dojo/unittests/tools/test_spotbugs_parser.py
@@ -68,3 +68,25 @@ class TestSpotbugsParser(TestCase):
             "[OWASP: Top 10 2013-A1-Injection](https://www.owasp.org/index.php/Top_10_2013-A1-Injection)",
             test_finding.references.splitlines()[1]
         )
+
+    def test_version_4_4(self):
+        """There was a big difference between version < 4.4.x and after
+        The dictionnary is not in the report anymore
+        """
+        testfile = open("dojo/unittests/scans/spotbugs/version_4.4.0.xml")
+        parser = SpotbugsParser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        self.assertEqual(9, len(findings))
+        with self.subTest(i=0):
+            finding = findings[0]
+            self.assertEqual("DMI_HARDCODED_ABSOLUTE_FILENAME", finding.title)
+            self.assertEqual("Boot.java", finding.file_path)
+            self.assertEqual(23, finding.line)
+            self.assertEqual("High", finding.severity)
+        with self.subTest(i=8):
+            finding = findings[8]
+            self.assertEqual("NM_METHOD_NAMING_CONVENTION", finding.title)
+            self.assertIsNone(finding.file_path)  # manage special case where file = 'N/A'
+            self.assertIsNone(finding.line)
+            self.assertEqual("Medium", finding.severity)
