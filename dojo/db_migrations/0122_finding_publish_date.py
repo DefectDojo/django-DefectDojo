3a1a2321512fc182d60e987a36729563e1711e9c
damiencarol <damien.carol@gmail.com>
2021-09-10 10:18:46 +0200

    Fix db migration

--- dojo/db_migrations/0122_finding_publish_date.py
+++ dojo/db_migrations/0122_finding_publish_date.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("dojo", "0121_user_restrict"),
+        ("dojo", "0122_cobaltio_product"),
     ]
 
     operations = [
