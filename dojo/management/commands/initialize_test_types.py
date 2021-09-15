01f9b8c8ba6df1f7bd2ff0e16d46928ebb65aa40
damiencarol <damien.carol@gmail.com>
2021-09-15 12:17:52 +0200

    Fix initializer

--- dojo/management/commands/initialize_test_types.py
+++ dojo/management/commands/initialize_test_types.py
@@ -1,9 +1,13 @@
 from django.core.management.base import BaseCommand
-from dojo.tools.factory import initialize_test_types
+from dojo.tools.factory import PARSERS
 
 
 class Command(BaseCommand):
     help = 'Initializes Test_Types'
 
     def handle(self, *args, **options):
-        initialize_test_types()
+        # called by the initializer to fill the table with test_types
+        for scan_type in PARSERS:
+            test_type, created = Test_Type.objects.get_or_create(name=scan_type)
+            if created:
+                test_type.save()
