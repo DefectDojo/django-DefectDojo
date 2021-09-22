DefectDojo/django-DefectDojo

commit 12c01d8b88f6209dd5d7dcb026ccb41d0be73105

fix error on merging findings due to django3 changes (#5064)
* added fix for django3 change from keyOrder to field_order

* removed erronously added file

Co-authored-by: Chris Fort <Chris.Fort@lexisnexis.com>

dojo/forms.py
@@ -591,7 +591,7 @@ class MergeFindings(forms.ModelForm):
             queryset=findings, required=True, label="Findings to Merge",
             widget=forms.widgets.SelectMultiple(attrs={'size': 10}),
             help_text=('Select the findings to merge.'))
-        self.fields.keyOrder = ['finding_to_merge_into', 'findings_to_merge', 'append_description', 'add_endpoints', 'append_reference']
+        self.fields.field_order = ['finding_to_merge_into', 'findings_to_merge', 'append_description', 'add_endpoints', 'append_reference']
 
     class Meta:
         model = Finding

commit d0e055a0c94c95b96014541947fafdef567888a2

Fixes issue introduced in #5064
Fixes an issue introduced in #5064, which was supposed to fix merging. Tested and merging now works.

dojo/forms.py
@@ -592,7 +592,7 @@ class MergeFindings(forms.ModelForm):
             queryset=findings, required=True, label="Findings to Merge",
             widget=forms.widgets.SelectMultiple(attrs={'size': 10}),
             help_text=('Select the findings to merge.'))
-        self.fields.field_order = ['finding_to_merge_into', 'findings_to_merge', 'append_description', 'add_endpoints', 'append_reference']
+        self.field_order = ['finding_to_merge_into', 'findings_to_merge', 'append_description', 'add_endpoints', 'append_reference']
 
     class Meta:
         model = Finding