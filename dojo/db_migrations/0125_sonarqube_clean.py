93baa3138b289316bcd209530dd495bbbfdacff1
Tomas Kubla <tomas@kubla.sk>
2021-10-04 11:32:22 +0000

    Fix is null

--- dojo/db_migrations/0125_sonarqube_clean.py
+++ dojo/db_migrations/0125_sonarqube_clean.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 def sq_clean(apps, schema_editor):
     Sonarqube_Product_model = apps.get_model('dojo', 'Sonarqube_Product')
     Sonarqube_Product_model.objects.filter(
-        sonarqube_project_key__is_null=True,
-        sonarqube_tool_config__is_null=True
+        sonarqube_project_key__isnull=True,
+        sonarqube_tool_config__isnull=True
     ).delete()
 
 
