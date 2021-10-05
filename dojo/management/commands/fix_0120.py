49da8d04dc309a6e6ea486da8959a3179d61e353
Tomas Kubla <tomas@kubla.sk>
2021-10-05 10:27:25 +0000

    fix fix 0120

--- dojo/management/commands/fix_0120.py
+++ dojo/management/commands/fix_0120.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from dojo.models import Test
 from django.db.migrations.executor import MigrationExecutor
 from django.db import connections, DEFAULT_DB_ALIAS
+from django.db.utils import OperationalError
 
 import logging
 
@@ -16,19 +17,20 @@ class Command(BaseCommand):
         connection = connections[DEFAULT_DB_ALIAS]
         connection.prepare_database()
         executor = MigrationExecutor(connection)
-        dojo_last_mig = filter(lambda a: a[0] == 'dojo', executor.loader.graph.leaf_nodes()).__next__()[1]
-        if dojo_last_mig == '0119_default_group_is_staff':
+        if not (executor.migration_plan([('dojo','0119_default_group_is_staff')])):
+        # this means that '0119_default_group_is_staff' was last successful migration
             logger.warning('This command will remove field "sonarqube_config" in model "Test" to be able to finish migration 0120_sonarqube_test_and_clean')
             try:
-                schema_editor.remove_field(
-                    model=Test,
-                    field=Test._meta.get_field('sonarqube_config'),
-                )
-            except django.db.utils.OperationalError:
+                with connection.schema_editor() as schema_editor:
+                    schema_editor.remove_field(
+                        model=Test,
+                        field=Test._meta.get_field('sonarqube_config'),
+                    )
+            except OperationalError:
                 # We expact exception like:
                 #   django.db.utils.OperationalError: (1091, "Can't DROP 'sonarqube_config_id'; check that column/key exists")
-                logger.info('Database fixed')
-            else:
                 logger.info('There was nothing to fix')
+            else:
+                logger.info('Database fixed')
         else:
             logger.error('Only migrations stacked in front of 0120 can be fixed by this command')
