971c66921d9a0bce1ca04c5e777e64aeb98ae705
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-10-09 09:38:24 +0200

    rename db migration after rebase

--- dojo/db_migrations/0129_finding_deprecated_fields.py
+++ dojo/db_migrations/0129_finding_deprecated_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0127_remove_hashes'),
+        ('dojo', '0128_pytz_update'),
     ]
 
     operations = [
