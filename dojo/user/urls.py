365e1adc2a96a010e73ee77fc910339bc9192aac
Tomas Kubla <tomas@kubla.sk>
2021-10-19 21:37:28 +0000

    Define reset password process

--- dojo/user/urls.py
+++ dojo/user/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from dojo.user import views
 
@@ -29,5 +31,18 @@ urlpatterns = [
     url(r'^user/(?P<uid>\d+)/add_product_member$', views.add_product_member,
         name='add_product_member_user'),
     url(r'^user/(?P<uid>\d+)/add_group_member$', views.add_group_member,
-        name='add_group_member_user')
+        name='add_group_member_user'),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    path('password_reset', auth_views.PasswordResetView.as_view(
+        template_name='dojo/password_reset.html',
+    ), name="password_reset"),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='dojo/password_reset_done.html',
+    ), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='dojo/password_reset_confirm.html',
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='dojo/password_reset_complete.html',
+    ), name='password_reset_complete'),
 ]
