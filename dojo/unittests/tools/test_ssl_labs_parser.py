87edc1548e28b4e2b4c94b2806508ded93983e1e
kiblik <tomas@kubla.sk>
2021-10-19 03:29:20 +0000

    SSLlabs: fix endpoints (#5296)

--- dojo/unittests/tools/test_ssl_labs_parser.py
+++ dojo/unittests/tools/test_ssl_labs_parser.py
@@ -14,9 +14,12 @@ class TestSslLabsParser(TestCase):
         parser = SslLabsParser()
         with open("dojo/unittests/scans/ssl_labs/ssl_labs_ok_v1.5.0.json") as test_file:
             findings = parser.get_findings(test_file, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         self.assertEqual(1, len(findings))
         self.assertEqual(findings[0].title, "TLS Grade 'A+' for defectdojo.mevitae.com")
-        self.assertEqual(findings[0].url["host"], "defectdojo.mevitae.com")
+        self.assertEqual(findings[0].unsaved_endpoints[0].host, "defectdojo.mevitae.com")
         self.assertEqual(findings[0].cwe, 310)
         self.assertEqual(findings[0].severity, "Info")
         self.assertTrue("TLS" in findings[0].description)
@@ -25,9 +28,13 @@ class TestSslLabsParser(TestCase):
         parser = SslLabsParser()
         with open("dojo/unittests/scans/ssl_labs/ssl_labs_dh1024_v1.5.0.json") as test_file:
             findings = parser.get_findings(test_file, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         self.assertEqual(1, len(findings))
         self.assertEqual(findings[0].title, "TLS Grade 'B' for dh1024.badssl.com")
-        self.assertEqual(findings[0].url["host"], "dh1024.badssl.com")
+        self.assertEqual(len(findings[0].unsaved_endpoints), 3)
+        self.assertEqual(findings[0].unsaved_endpoints[0].host, "dh1024.badssl.com")
         self.assertEqual(findings[0].cwe, 310)
         self.assertEqual(findings[0].severity, "Medium")
         self.assertTrue("TLS" in findings[0].description)
@@ -36,9 +43,13 @@ class TestSslLabsParser(TestCase):
         parser = SslLabsParser()
         with open("dojo/unittests/scans/ssl_labs/ssl_labs_3des_v1.5.0.json") as test_file:
             findings = parser.get_findings(test_file, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         self.assertEqual(1, len(findings))
         self.assertEqual(findings[0].title, "TLS Grade 'C' for 3des.badssl.com")
-        self.assertEqual(findings[0].url["host"], "3des.badssl.com")
+        self.assertEqual(len(findings[0].unsaved_endpoints), 3)
+        self.assertEqual(findings[0].unsaved_endpoints[0].host, "3des.badssl.com")
         self.assertEqual(findings[0].cwe, 310)
         self.assertEqual(findings[0].severity, "High")
         self.assertTrue("TLS" in findings[0].description)
@@ -47,9 +58,13 @@ class TestSslLabsParser(TestCase):
         parser = SslLabsParser()
         with open("dojo/unittests/scans/ssl_labs/ssl_labs_revoked_v1.5.0.json") as test_file:
             findings = parser.get_findings(test_file, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         self.assertEqual(1, len(findings))
         self.assertEqual(findings[0].title, "TLS Grade 'T' for revoked.badssl.com")
-        self.assertEqual(findings[0].url["host"], "revoked.badssl.com")
+        self.assertEqual(len(findings[0].unsaved_endpoints), 3)
+        self.assertEqual(findings[0].unsaved_endpoints[0].host, "revoked.badssl.com")
         self.assertEqual(findings[0].cwe, 310)
         self.assertEqual(findings[0].severity, "Critical")
         self.assertTrue("TLS" in findings[0].description)
@@ -58,6 +73,9 @@ class TestSslLabsParser(TestCase):
         parser = SslLabsParser()
         with open("dojo/unittests/scans/ssl_labs/ssl_labs_multiple_v1.5.0.json") as test_file:
             findings = parser.get_findings(test_file, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         self.assertEqual(3, len(findings))
 
         # Track the results we've found
@@ -70,15 +88,18 @@ class TestSslLabsParser(TestCase):
             self.assertEqual(finding.cwe, 310)
             if finding.severity == "Info":
                 self.assertEqual(finding.title, "TLS Grade 'A+' for defectdojo.mevitae.com")
-                self.assertEqual(finding.url["host"], "defectdojo.mevitae.com")
+                self.assertEqual(len(finding.unsaved_endpoints), 4)
+                self.assertEqual(finding.unsaved_endpoints[0].host, "defectdojo.mevitae.com")
                 foundInfo = True
             elif finding.severity == "Medium":
                 self.assertEqual(finding.title, "TLS Grade 'B' for dh1024.badssl.com")
-                self.assertEqual(finding.url["host"], "dh1024.badssl.com")
+                self.assertEqual(len(finding.unsaved_endpoints), 3)
+                self.assertEqual(finding.unsaved_endpoints[0].host, "dh1024.badssl.com")
                 foundMedium = True
             elif finding.severity == "Critical":
                 self.assertEqual(finding.title, "TLS Grade 'T' for revoked.badssl.com")
-                self.assertEqual(finding.url["host"], "revoked.badssl.com")
+                self.assertEqual(len(finding.unsaved_endpoints), 3)
+                self.assertEqual(finding.unsaved_endpoints[0].host, "revoked.badssl.com")
                 foundCritical = True
             else:
                 self.fail("unexpected finding result")
