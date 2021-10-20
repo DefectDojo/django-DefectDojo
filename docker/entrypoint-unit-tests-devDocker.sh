84f753771e2b037b60afb488582ecda0e55771a2
Tomas Kubla <tomas@kubla.sk>
2021-10-20 14:17:45 +0200

    add tests to test_rest_framework

--- docker/entrypoint-unit-tests-devDocker.sh
+++ docker/entrypoint-unit-tests-devDocker.sh
@@ -40,19 +40,19 @@ EOF
     python3 manage.py spectacular > /dev/null
 }
 
-echo "Swagger Schema Tests - Broken"
-echo "------------------------------------------------------------"
-python3 manage.py test dojo.unittests -v 3 --keepdb --no-input --tag broken && true
-
-echo "Unit Tests"
-echo "------------------------------------------------------------"
-python3 manage.py test dojo.unittests -v 3 --keepdb --no-input --exclude-tag broken
+#echo "Swagger Schema Tests - Broken"
+#echo "------------------------------------------------------------"
+#python3 manage.py test dojo.unittests -v 3 --keepdb --no-input --tag broken && true
+#
+#echo "Unit Tests"
+#echo "------------------------------------------------------------"
+#python3 manage.py test dojo.unittests -v 3 --keepdb --no-input --exclude-tag broken
 
 # you can select a single file to "test" unit tests
 # python3 manage.py test dojo.unittests.tools.test_npm_audit_scan_parser.TestNpmAuditParser --keepdb -v 3
 
 # or even a single method
-# python3 manage.py test dojo.unittests.tools.test_npm_audit_scan_parser.TestNpmAuditParser.test_npm_audit_parser_many_vuln_npm7 --keepdb -v 3
+python3 manage.py test dojo.unittests.test_rest_framework.UserContactInfoTest --keepdb -v 3
 
 echo "End of tests. Leaving the container up"
 tail -f /dev/null
