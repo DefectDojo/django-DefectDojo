09401396becb719e12c29e16a8db85e0de1707db
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-11-08 22:00:17 +0100

    make recursive call safe

--- dojo/group/util.py
+++ dojo/group/util.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import Group
 
 
 def get_auth_group_name(group, attempt):
+    if attempt > 999:
+        raise Exception(f'Cannot find name for authorization group for Dojo_Group {group.name}, aborted after 999 attempts.')
     if attempt == 0:
         auth_group_name = group.name
     else:
@@ -10,5 +12,5 @@ def get_auth_group_name(group, attempt):
     try:
         auth_group = Group.objects.get(name=auth_group_name)
         return get_auth_group_name(group, attempt + 1)
-    except:
+    except Group.DoesNotExist:
         return auth_group_name
