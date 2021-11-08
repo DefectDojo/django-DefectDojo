06119c5ea526dac01563f2d7b30eaa2a8ad824cf
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-11-08 17:58:16 +0100

    Auth V2 - Remove legacy authorization part 1: Remove legacy auth from templates (#5382)
    * remove legacy auth from templates
    
    * remove legacy unit test

--- dojo/templatetags/authorization_tags.py
+++ dojo/templatetags/authorization_tags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.conf import settings
 from crum import get_current_user
 from dojo.authorization.roles_permissions import Permissions
 from dojo.authorization.authorization import user_has_permission
@@ -9,8 +8,4 @@ register = template.Library()
 
 @register.filter
 def has_object_permission(obj, permission):
-
-    if settings.FEATURE_AUTHORIZATION_V2:
-        return user_has_permission(get_current_user(), obj, Permissions[permission])
-    else:
-        return False
+    return user_has_permission(get_current_user(), obj, Permissions[permission])
