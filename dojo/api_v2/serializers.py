ef4ce962b9627129103aa3d29c4f143ba52e5cf4
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-11-09 18:34:30 +0100

    auth_group in API

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -351,7 +351,7 @@ class DojoGroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Dojo_Group
-        fields = '__all__'
+        exclude = ['auth_group']
 
 
 class DojoGroupMemberSerializer(serializers.ModelSerializer):
