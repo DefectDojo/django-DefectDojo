b819d00cea0e7acb2002cd4e41c69fe3e748cd7a
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-11-11 17:33:28 +0100

    fixes for unit tests

--- dojo/api_v2/permissions.py
+++ dojo/api_v2/permissions.py
@@ -46,9 +46,9 @@ class UserHasAppAnalysisPermission(permissions.BasePermission):
 class UserHasDojoGroupPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
-            return user_has_configuration_permission(request.user, 'auth.view_group')
+            return user_has_configuration_permission(request.user, 'auth.view_group', 'staff')
         elif request.method == 'POST':
-            return user_has_configuration_permission(request.user, 'auth.create_group')
+            return user_has_configuration_permission(request.user, 'auth.create_group', 'staff')
         else:
             return True
 
@@ -56,7 +56,7 @@ class UserHasDojoGroupPermission(permissions.BasePermission):
         if request.method == 'GET':
             # Users need to be authorized to view groups in general and only the groups they are a member of
             # because with the group they can see user information that might be considered as confidential
-            return user_has_configuration_permission(request.user, 'auth.view_group') and user_has_permission(request.user, obj, Permissions.Group_View)
+            return user_has_configuration_permission(request.user, 'auth.view_group', 'staff') and user_has_permission(request.user, obj, Permissions.Group_View)
         else:
             return check_object_permission(request, obj, Permissions.Group_View, Permissions.Group_Edit, Permissions.Group_Delete)
 
