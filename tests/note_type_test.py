0a3d43ffa963b7bcf2ff2a29bc0a103924329912
CharlieSears <charlie.sears@gmail.com>
2021-11-15 02:48:52 -0500

    Integration test warning cleanup (#5445)
    * Update all deprecated selenium get_element(s)_by*
    
    * Remove trailing whitespace from user_test.py

--- tests/note_type_test.py
+++ tests/note_type_test.py
@@ -1,6 +1,7 @@
 import unittest
 import sys
 from base_test_class import BaseTestCase
+from selenium.webdriver.common.by import By
 
 
 class NoteTypeTest(BaseTestCase):
@@ -8,40 +9,40 @@ class NoteTypeTest(BaseTestCase):
     def test_create_note_type(self):
         driver = self.driver
         driver.get(self.base_url + "note_type")
-        driver.find_element_by_id("dropdownMenu1").click()
-        driver.find_element_by_link_text("Add Note Type").click()
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("test note type")
-        driver.find_element_by_id("id_description").clear()
-        driver.find_element_by_id("id_description").send_keys("Test note type description")
-        driver.find_element_by_id("id_is_single").click()
-        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        driver.find_element(By.ID, "dropdownMenu1").click()
+        driver.find_element(By.LINK_TEXT, "Add Note Type").click()
+        driver.find_element(By.ID, "id_name").clear()
+        driver.find_element(By.ID, "id_name").send_keys("test note type")
+        driver.find_element(By.ID, "id_description").clear()
+        driver.find_element(By.ID, "id_description").send_keys("Test note type description")
+        driver.find_element(By.ID, "id_is_single").click()
+        driver.find_element(By.CSS_SELECTOR, "input.btn.btn-primary").click()
 
         self.assertTrue(self.is_success_message_present(text='Note Type added successfully.'))
 
     def test_edit_note_type(self):
         driver = self.driver
         driver.get(self.base_url + "note_type")
-        driver.find_element_by_link_text("Edit Note Type").click()
-        driver.find_element_by_id("id_name").clear()
-        driver.find_element_by_id("id_name").send_keys("Edited test note type")
-        driver.find_element_by_css_selector("input.btn.btn-primary").click()
+        driver.find_element(By.LINK_TEXT, "Edit Note Type").click()
+        driver.find_element(By.ID, "id_name").clear()
+        driver.find_element(By.ID, "id_name").send_keys("Edited test note type")
+        driver.find_element(By.CSS_SELECTOR, "input.btn.btn-primary").click()
 
         self.assertTrue(self.is_success_message_present(text='Note type updated successfully.'))
 
     def test_disable_note_type(self):
         driver = self.driver
         driver.get(self.base_url + "note_type")
-        driver.find_element_by_link_text("Disable Note Type").click()
-        driver.find_element_by_css_selector("input.btn.btn-danger").click()
+        driver.find_element(By.LINK_TEXT, "Disable Note Type").click()
+        driver.find_element(By.CSS_SELECTOR, "input.btn.btn-danger").click()
 
         self.assertTrue(self.is_success_message_present(text='Note type Disabled successfully.'))
 
     def test_enable_note_type(self):
         driver = self.driver
         driver.get(self.base_url + "note_type")
-        driver.find_element_by_link_text("Enable Note Type").click()
-        driver.find_element_by_css_selector("input.btn.btn-success").click()
+        driver.find_element(By.LINK_TEXT, "Enable Note Type").click()
+        driver.find_element(By.CSS_SELECTOR, "input.btn.btn-success").click()
 
         self.assertTrue(self.is_success_message_present(text='Note type Enabled successfully.'))
 
