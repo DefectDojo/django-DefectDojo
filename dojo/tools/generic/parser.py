127d6ed2a665486ba5eaa0aabc01446261ca8436
damiencarol <damien.carol@gmail.com>
2021-11-26 20:28:38 +0100

    Add support for files in generic parser

--- dojo/tools/generic/parser.py
+++ dojo/tools/generic/parser.py
@@ -35,6 +35,11 @@ class GenericParser(object):
             if "endpoints" in item:
                 unsaved_endpoints = item["endpoints"]
                 del item["endpoints"]
+            # remove files of the dictionnary
+            unsaved_files = None
+            if "files" in item:
+                unsaved_files = item["files"]
+                del item["files"]
 
             finding = Finding(**item)
             # manage active/verified overrride
@@ -57,6 +62,9 @@ class GenericParser(object):
                     else:
                         endpoint = Endpoint(**item)
                     finding.unsaved_endpoints.append(endpoint)
+
+            if unsaved_files:
+                finding.unsaved_files = unsaved_files
             findings.append(finding)
         return findings
 
