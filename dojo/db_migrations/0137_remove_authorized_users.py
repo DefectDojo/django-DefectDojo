6f45a983f771bf83ee72570376869d061cddcbe6
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-11-29 07:25:46 +0100

    rename db migration after rebase

--- dojo/db_migrations/0137_remove_authorized_users.py
+++ dojo/db_migrations/0137_remove_authorized_users.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0135_email_from'),
+        ('dojo', '0136_default_group_help_text'),
     ]
 
     operations = [
