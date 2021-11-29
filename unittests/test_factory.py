bd1c8cf6d94c1c7b25f9071dabb0bf565fa0ebc2
Valentijn Scholten <valentijnscholten@gmail.com>
2021-11-29 19:42:44 +0100

    move unittests outside dojo folder

---
+++ unittests/test_factory.py
@@ -0,0 +1,54 @@
+from dojo.tools.factory import get_parser
+from dojo.models import Test, Test_Type
+from unittests.dojo_test_case import DojoTestCase
+
+
+class TestFactory(DojoTestCase):
+    def test_get_parser(self):
+        with self.subTest(scan_type="Acunetix Scan"):
+            scan_type = "Acunetix Scan"
+            testfile = open(self.unit_test_folder + "/scans/acunetix/one_finding.xml")
+            parser = get_parser(scan_type)
+            findings = parser.get_findings(testfile, Test())
+            testfile.close()
+        with self.subTest(scan_type="Anchore Engine Scan"):
+            scan_type = "Anchore Engine Scan"
+            testfile = open(self.unit_test_folder + "/scans/anchore/one_vuln.json")
+            parser = get_parser(scan_type)
+            findings = parser.get_findings(testfile, Test())
+            testfile.close()
+        with self.subTest(scan_type="Nessus Scan"):
+            scan_type = "Nessus Scan"
+            testfile = open(self.unit_test_folder + "/scans/nessus/nessus_v_unknown.xml")
+            parser = get_parser(scan_type)
+            findings = parser.get_findings(testfile, Test())
+            testfile.close()
+        with self.subTest(scan_type="ZAP Scan"):
+            scan_type = "ZAP Scan"
+            testfile = open(self.unit_test_folder + "/scans/zap/some_2.9.0.xml")
+            parser = get_parser(scan_type)
+            findings = parser.get_findings(testfile, Test())
+            testfile.close()
+
+    def test_get_parser_error(self):
+        with self.assertRaises(ValueError):
+            scan_type = "type_that_doesn't_exist"
+            get_parser(scan_type)
+
+    def test_get_parser_test_active_in_db(self):
+        """This test is designed to validate that the factory take into account the falg 'active' in DB"""
+        scan_type = "ZAP Scan"
+        # desactivate the parser
+        Test_Type.objects.update_or_create(
+            name=scan_type,
+            defaults={"active": False},
+        )
+        with self.assertRaises(ValueError):
+            get_parser(scan_type)
+        # activate the parser
+        test_type, created = Test_Type.objects.update_or_create(
+            name=scan_type,
+            defaults={"active": True},
+        )
+        parser = get_parser(scan_type)
+        self.assertIsNotNone(parser)
---
+++ unittests/tools/test_veracode_parser.py
@@ -0,0 +1,123 @@
+import datetime
+
+from django.test import SimpleTestCase
+from dojo.tools.veracode.parser import VeracodeParser
+from dojo.models import Test
+
+
+class TestVeracodeScannerParser(SimpleTestCase):
+
+    def test_parse_file_with_one_finding(self):
+        testfile = open("unittests/scans/veracode/one_finding.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+
+    def test_parse_file_many_findings_different_hash_code_different_unique_id(self):
+        testfile = open("unittests/scans/veracode/many_findings_different_hash_code_different_unique_id.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(4, len(findings))
+        finding = findings[0]
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual(123, finding.cwe)
+        self.assertEqual("catname", finding.title)
+        self.assertFalse(finding.is_mitigated)
+        self.assertEqual("sourcefilepathMyApp2.java", finding.file_path)
+        self.assertEqual(2, finding.line)
+        self.assertEqual("app-12345_issue-1", finding.unique_id_from_tool)
+        finding = findings[1]
+        self.assertEqual("Medium", finding.severity)
+        self.assertTrue(finding.dynamic_finding)
+        finding = findings[2]
+        self.assertEqual("High", finding.severity)
+        self.assertIsNone(finding.cwe)
+        self.assertEqual("CVE-1234-1234", finding.cve)
+        self.assertEqual("Vulnerable component: library:1234", finding.title)
+        self.assertFalse(finding.is_mitigated)
+
+    def test_parse_file_with_multiple_finding(self):
+        testfile = open("unittests/scans/veracode/many_findings.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(4, len(findings))
+        finding = findings[0]
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual(123, finding.cwe)
+        self.assertEqual("catname", finding.title)
+        self.assertTrue(finding.static_finding)
+        self.assertFalse(finding.is_mitigated)
+        self.assertEqual("sourcefilepathMyApp.java", finding.file_path)
+        self.assertEqual(2, finding.line)
+        self.assertEqual("app-1234_issue-1", finding.unique_id_from_tool)
+        finding = findings[1]
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual(456, finding.cwe)
+        self.assertTrue(finding.dynamic_finding)
+        finding = findings[2]
+        self.assertEqual("High", finding.severity)
+        self.assertIsNone(finding.cwe)
+        self.assertEqual("CVE-1234-1234", finding.cve)
+        self.assertEqual("Vulnerable component: library:1234", finding.title)
+        self.assertFalse(finding.is_mitigated)
+        finding = findings[3]
+        self.assertEqual("High", finding.severity)
+        self.assertEqual("CVE-5678-5678", finding.cve)
+        self.assertEqual("Vulnerable component: library1:1234", finding.title)
+        self.assertFalse(finding.is_mitigated)
+
+    def test_parse_file_with_multiple_finding2(self):
+        testfile = open("unittests/scans/veracode/veracode_scan.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(7, len(findings))
+        finding = findings[0]
+        self.assertEqual("Information Exposure Through Sent Data", finding.title)
+        self.assertEqual("Low", finding.severity)
+        self.assertEqual(201, finding.cwe)
+        self.assertEqual(datetime.datetime(2018, 2, 17, 0, 35, 18), finding.date)  # date_first_occurrence="2018-02-17 00:35:18 UTC"
+        finding = findings[1]
+        self.assertEqual("Low", finding.severity)
+        self.assertEqual(201, finding.cwe)
+        self.assertEqual("/devTools/utility.jsp", finding.file_path)
+        self.assertEqual(361, finding.line)
+        self.assertIsNone(finding.component_name)
+        self.assertIsNone(finding.component_version)
+        # finding 6
+        finding = findings[6]
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual("CVE-2012-6153", finding.cve)
+        self.assertEqual(20, finding.cwe)
+        self.assertEqual("commons-httpclient", finding.component_name)
+        self.assertEqual("3.1", finding.component_version)
+        self.assertEqual("CVE-2012-6153", finding.unique_id_from_tool)
+
+    def test_parse_file_with_mitigated_finding(self):
+        testfile = open("unittests/scans/veracode/mitigated_finding.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+        finding = findings[0]
+        self.assertEqual("Medium", finding.severity)
+        self.assertTrue(finding.is_mitigated)
+        self.assertEqual(datetime.datetime(2020, 6, 1, 10, 2, 1), finding.mitigated)
+        self.assertEqual("app-1234_issue-1", finding.unique_id_from_tool)
+
+    def test_parse_file_with_dynamic_finding(self):
+        testfile = open("unittests/scans/veracode/dynamic_finding.xml")
+        parser = VeracodeParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+        finding = findings[0]
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual(456, finding.cwe)
+        self.assertTrue(finding.dynamic_finding)
+        self.assertEqual("catname", finding.title)
+        self.assertEqual("Description", finding.description)
+        self.assertFalse(finding.is_mitigated)
+        self.assertEqual(datetime.datetime(2021, 9, 3, 10, 0, 0), finding.date)
+        self.assertEqual(1, len(finding.unsaved_endpoints))
+        endpoint = finding.unsaved_endpoints[0]
+        self.assertEqual('https', endpoint.protocol)
+        self.assertEqual('www.example.com', endpoint.host)
+        self.assertEqual('index.html', endpoint.path)
