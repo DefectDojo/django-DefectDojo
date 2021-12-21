b06f261bd7d6671d2865c29fa6996dbdb6490f71
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-12-21 06:33:54 +0100

    fix copy and paste error

--- dojo/tools/gitleaks/parser.py
+++ dojo/tools/gitleaks/parser.py
@@ -35,7 +35,7 @@ class GitleaksParser(object):
             elif issue.get('Description'):
                 self.get_finding_current(issue, test, dupes)
             else:
-                raise Exception('Format is not recognized for Trufflehog3')
+                raise Exception('Format is not recognized for Gitleaks')
 
         return list(dupes.values())
 
