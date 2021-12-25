f97d99c86b993dcb666b9552d461040b2f78aa6d
Manuel Sommer <sommermanuel@gmx.de>
2021-12-25 21:30:16 +0100

    meta-format accoring to damiencarols comment

--- dojo/tools/rusty_hog/parser.py
+++ dojo/tools/rusty_hog/parser.py
@@ -1,5 +1,5 @@
 import json
-
+from dojo.tools.parser_test import ParserTest
 from dojo.models import Finding
 
 
@@ -27,63 +27,101 @@ class RustyhogParser(object):
                 tree = json.loads(data)
         except:
             raise Exception("Invalid format")
-
         return tree
 
     def get_items(self, tree, test):
         items = {}
         for node in tree:
-            item = get_item(node, test)
+            item = self.get_item(node, test)
             unique_key = "Finding {}".format(tree.index(node))
             items[unique_key] = item
-
         return list(items.values())
 
+    def get_tests(self, scan_type, handle):
+            tree = json.load(handle)
+            tests = list()
+            parsername = "Rusty Hog"
+            for node in tree:
+                if 'commit' in node or 'commitHash' in node or 'parent_commit_hash' in node or 'old_file_id' in node or 'new_file_id' in node:
+                    parsername = "Choctaw Hog"
+                    break
+                if 'issue_id' in node or 'location' in node or 'url' in node:
+                    parsername = "Gottingen Hog"
+                    break
+            #for run in tree.get('runs', list()):
+            test = ParserTest(
+                name=parsername,
+                type=parsername,
+                version="",
+            )
+            if parsername == "Rusty Hog":
+                test.description = "The exact scanner within Rusty Hog could not be determined due to missing information within the scan result."
+            else:
+                test.description = parsername
+            test.findings = self.__getitem(vulnerabilities=tree, scanner=parsername)
+            tests.append(test)
+            return tests
 
-def get_item(vulnerability, test):
-    cwe = 200
-    description = "**This string was found:** {}".format(vulnerability.get('stringsFound'))
-    if vulnerability.get('commit') is not None:
-        description += "\n**Commit message:** {}".format(vulnerability.get('commit'))
-    if vulnerability.get('commitHash') is not None:
-        description += "\n**Commit hash:** {}".format(vulnerability.get('commitHash'))
-    if vulnerability.get('parent_commit_hash') is not None:
-        description += "\n**Parent commit hash:** {}".format(vulnerability.get('parent_commit_hash'))
-    if vulnerability.get('date') is not None:
-        description += "\n**Date:** {}".format(vulnerability.get('date'))
-    if vulnerability.get('old_file_id') is not None and vulnerability.get('new_file_id') is not None:
-        description += "\n**Old and new file IDs:** {} - {}".format(
-                        vulnerability.get('old_file_id'),
-                        vulnerability.get('new_file_id'))
-    if vulnerability.get('old_line_num') is not None and vulnerability.get('new_line_num') is not None:
-        description += "\n**Old and new line numbers:** {} - {}".format(
-                        vulnerability.get('old_line_num'),
-                        vulnerability.get('new_line_num'))
-    if vulnerability.get('issue_id') is not None:
-        description += "\n**JIRA Issue ID:** {}".format(vulnerability.get('issue_id'))
-    if vulnerability.get('location') is not None:
-        description += "\n**JIRA location:** {}".format(vulnerability.get('location'))
-    if vulnerability.get('url') is not None:
-        description += "\n**JIRA url:** {}".format(vulnerability.get('url'))
-    file_path = vulnerability.get('path')
-    title = "{} found in {} ({})".format(
-            vulnerability.get('reason'),
-            vulnerability.get('path'),
-            vulnerability.get('commitHash'))
-
-    # create the finding object
-    finding = Finding(
-        title=title,
-        test=test,
-        severity='High',
-        cwe=cwe,
-        description=description,
-        mitigation="Please ensure no secret material nor confidential information is kept in clear within git repositories.",
-        file_path=file_path,
-        static_finding=True,
-        dynamic_finding=False
-    )
-
-    finding.description = finding.description.strip()
-
-    return finding
+    def __getitem(self, vulnerabilities, scanner):
+        findings = []
+        line = ""
+        cwe = 200
+        for vulnerability in vulnerabilities:
+            if scanner == "Rusty Hog":
+                break
+            elif scanner == "Choctaw Hog":
+                """Choctaw Hog"""
+                description = "**This string was found:** {}".format(vulnerability.get('stringsFound'))
+                if vulnerability.get('commit') is not None:
+                    description += "\n**Commit message:** {}".format(vulnerability.get('commit'))
+                if vulnerability.get('commitHash') is not None:
+                    description += "\n**Commit hash:** {}".format(vulnerability.get('commitHash'))
+                if vulnerability.get('parent_commit_hash') is not None:
+                    description += "\n**Parent commit hash:** {}".format(vulnerability.get('parent_commit_hash'))
+                if vulnerability.get('old_file_id') is not None and vulnerability.get('new_file_id') is not None:
+                    description += "\n**Old and new file IDs:** {} - {}".format(
+                                    vulnerability.get('old_file_id'),
+                                    vulnerability.get('new_file_id'))
+                if vulnerability.get('old_line_num') is not None and vulnerability.get('new_line_num') is not None:
+                    description += "\n**Old and new line numbers:** {} - {}".format(
+                                    vulnerability.get('old_line_num'),
+                                    vulnerability.get('new_line_num'))
+            elif scanner == "Gottingen Hog":
+                """Gottingen Hog"""
+                description = "**This string was found:** {}".format(vulnerability.get('stringsFound'))
+                if vulnerability.get('issue_id') is not None:
+                    description += "\n**JIRA Issue ID:** {}".format(vulnerability.get('issue_id'))
+                if vulnerability.get('location') is not None:
+                    description += "\n**JIRA location:** {}".format(vulnerability.get('location'))
+                if vulnerability.get('url') is not None:
+                    description += "\n**JIRA url:** {}".format(vulnerability.get('url'))
+            """General - for all Rusty Hogs"""
+            file_path = vulnerability.get('path')
+            if vulnerability.get('date') is not None:
+                description += "\n**Date:** {}".format(vulnerability.get('date'))
+            """Finding Title"""
+            if scanner == "Choctaw Hog":
+                title = "{} found in Git path {} ({})".format(
+                        vulnerability.get('reason'),
+                        vulnerability.get('path'),
+                        vulnerability.get('commitHash'))
+            elif scanner == "Gottingen Hog":
+                title = "{} found in Jira ID {}".format(
+                        vulnerability.get('reason'),
+                        vulnerability.get('issue_id'),
+                        vulnerability.get('location'))
+            # create the finding object
+            finding = Finding(
+                title=title,
+                #test=test,
+                severity='High',
+                cwe=cwe,
+                description=description,
+                mitigation="Please ensure no secret material nor confidential information is kept in clear within git repositories.",
+                file_path=file_path,
+                static_finding=True,
+                dynamic_finding=False
+            )
+            finding.description = finding.description.strip()
+            findings.append(finding)
+        return findings
