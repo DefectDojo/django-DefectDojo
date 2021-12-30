445074bf9af65048b4498dd6a0fea2d4f1bd3d2e
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2021-12-30 12:07:15 +0100

    rename db migration after rebase

--- dojo/db_migrations/0143_objects.py
+++ dojo/db_migrations/0143_objects.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0141_enable_user_profile_editable'),
+        ('dojo', '0142_environment_delete'),
     ]
 
     operations = [
