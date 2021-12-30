3f0d35a9d5a7e5b034862d3f5c8e9b764372b807
pabloalexsandroalmeida83 <57901549+pabloalexsandroalmeida83@users.noreply.github.com>
2021-12-30 12:50:58 -0300

    Add files via upload

---
+++ dojo/tools/nuclei/parser.py
@@ -0,0 +1,76 @@
+import json
+import hashlib
+from dojo.models import Finding, Endpoint
+
+
+class NucleiParser(object):
+    """
+    A class that can be used to parse the nuclei (https://github.com/projectdiscovery/nuclei) JSON report file
+    """
+
+    def get_scan_types(self): 
+        return ["Nuclei Scan"]
+
+    def get_label_for_scan_types(self, scan_type):
+        return "Nuclei Scan"
+
+    def get_description_for_scan_types(self, scan_type):
+        return "Import JSON output for nuclei scan report."
+
+    def get_findings(self, filename, test):
+        data = [json.loads(line) for line in filename]
+        if len(data) == 0:
+            return []
+        else:
+            dupes = {}
+            for item in data:
+                template_id = item.get('template-id')
+                info = item.get('info')
+                name = info.get('name')
+                severity = info.get('severity').title()
+                type = item.get('type')
+                matched = item.get('matched-at')
+                if '://' in matched:
+                    endpoint = Endpoint.from_uri(matched)
+                else:
+                    endpoint = Endpoint.from_uri('//' + matched)
+
+                finding = Finding(
+                    title=f"{name}",
+                    test=test,
+                    severity=severity,
+                    nb_occurences=1,
+                    vuln_id_from_tool=template_id,
+                )
+               
+                if info.get('description'):
+                    finding.description = info.get('description')
+                if info.get('tags'):
+                    finding.unsaved_tags = info.get('tags')
+                if info.get('reference'):
+                    finding.references = info.get('reference')
+                if info.get("severity-justification"):
+                    finding.severity_justification = info.get("severity-justification")
+                if item.get('mitigation'):
+                    finding.mitigation = item.get('mitigation')
+                if item.get('impact'):
+                    finding.impact = item.get('impact')
+                if item.get('line'):
+                    finding.line = item.get('line')
+                if item.get('file_path'):
+                    finding.file_path = item.get('file_path')
+
+                finding.unsaved_endpoints.append(endpoint)
+
+                dupe_key = hashlib.sha256(
+                    (template_id + type).encode('utf-8')
+                ).hexdigest()
+
+                if dupe_key in dupes:
+                    finding = dupes[dupe_key]
+                    if endpoint not in finding.unsaved_endpoints:
+                        finding.unsaved_endpoints.append(endpoint)
+                    finding.nb_occurences += 1
+                else:
+                    dupes[dupe_key] = finding
+            return list(dupes.values())
