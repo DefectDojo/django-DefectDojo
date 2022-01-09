896cd516168134f8c7868e4274b34e8ed7d5b7ee
damiencarol <damien.carol@gmail.com>
2022-01-09 11:29:55 +0100

    Add unit tests file with many vuln

--- dojo/tools/rubocop/parser.py
+++ dojo/tools/rubocop/parser.py
@@ -7,7 +7,20 @@ class RubocopParser:
 
     ID = "Rubocop Scan"
 
-    SEVERITY_MAPPING = {"convention": "Info"}
+    # possible values are:
+    # `:info`, `:refactor`, `:convention`, `:warning`, `:error` or `:fatal`.
+    # see https://github.com/rubocop/rubocop/blob/master/lib/rubocop/cop/severity.rb
+    # TODO change when the tool support it (not now 1.24.1, always "conventionnal")
+    # current version (1.13.0) always populate severity to "conventionnal"
+    # so we force it to 'Medium'
+    SEVERITY_MAPPING = {
+        "info": "Info",
+        "refactor": "Medium",
+        "convention": "Medium",  # see the note
+        "warning": "Medium",
+        "error": "High",
+        "fatal": "Critical",
+    }
 
     def get_scan_types(self):
         return [self.ID]
@@ -22,6 +35,7 @@ class RubocopParser:
         return True
 
     def get_findings(self, scan_file, test):
+        """Load a file as JSON file and create findings"""
         data = json.load(scan_file)
         findings = list()
         for vuln_file in data.get("files", []):
@@ -31,11 +45,13 @@ class RubocopParser:
                 if not offense["cop_name"].lower().startswith("security"):
                     continue
                 line = int(offense["location"]["start_line"])
-                description = "\n".join([
-                    f"**Message**: {offense.get('message')}",
-                    f"**Is correctable?**: `{offense.get('correctable')}`",
-                    f"**Location**: `{'-'.join(offense['location'])}`",
-                ])
+                description = "\n".join(
+                    [
+                        f"**Message**: {offense.get('message')}",
+                        f"**Is correctable?**: `{offense.get('correctable')}`",
+                        f"**Location**: `{'-'.join(offense['location'])}`",
+                    ]
+                )
                 finding = Finding(
                     test=test,
                     title=offense.get("message"),
