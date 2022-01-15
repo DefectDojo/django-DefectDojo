d1477ec45d79ad563359ab2f5b66ee740b8e1e51
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2022-01-15 08:07:09 +0100

    Configuration authorization 3: Finish making more configuration permissions editable (#5713)
    * initial commit
    
    * menu for product add
    
    * more configuration permissions
    
    * remove AUTHORIZATION_STAFF_OVERRIDE
    
    * intermediate commit
    
    * intermediate commit
    
    * next intermediate commit
    
    * flake8
    
    * remove sys.exit
    
    * initialization of non-standard django permissions
    
    * initialization of non-standard Django permissions
    
    * fix unit tests
    
    * editing of configuration permissions only for superusers
    
    * flake8
    
    * fix unit tests
    
    * reformat permission fields
    
    * Product Type Add permission according to flag
    
    * fix for groups list
    
    * fix for add products
    
    * fix for view test
    
    * fix for finding list

--- dojo/api_v2/permissions.py
+++ dojo/api_v2/permissions.py
@@ -542,3 +542,23 @@ class UserHasConfigurationPermissionStaff(permissions.DjangoModelPermissions):
             return super().has_permission(request, view)
         else:
             return request.user.is_staff
+
+
+class UserHasConfigurationPermissionSuperuser(permissions.DjangoModelPermissions):
+
+    # Override map to also provide 'view' permissions
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
+    def has_permission(self, request, view):
+        if settings.FEATURE_CONFIGURATION_AUTHORIZATION:
+            return super().has_permission(request, view)
+        else:
+            return request.user.is_superuser
