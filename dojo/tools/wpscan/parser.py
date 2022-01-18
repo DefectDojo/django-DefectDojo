c6b4a13d5819f09fcf3df76f79f4079bc241cc7c
prakashar11 <prakashar@outlook.com>
2022-01-18 10:29:50 -0800

    Update wpscan data upload (#5562)
    * update wpscan data upload
    
    * update based on review comments
    
    * update test case name
    
    * update wpscan parser
    
    * dev branch changes
    
    * update unittest wpscan scan file location

--- dojo/tools/wpscan/parser.py
+++ dojo/tools/wpscan/parser.py
@@ -18,6 +18,54 @@ class WpscanParser(object):
     def get_description_for_scan_types(self, scan_type):
         return "Import JSON report"
 
+    def get_vulnerabilities(self, report_date, vulnerabilities, dupes, node=None, plugin=None):
+        for vul in vulnerabilities:
+            description = "\n".join([
+                '**Title:** `' + vul['title'] + "`\n"
+            ])
+
+            if node and 'location' in node:
+                description += '**Location:** `' + "".join(node['location']) + "`\n"
+
+            if plugin:
+                description += '**Plugin:** `' + "".join(plugin) + "`\n"
+
+            finding = Finding(
+                title=vul['title'],
+                description=description,
+                severity='Medium',
+                cwe=1035,
+                references=self.generate_references(vul['references']),
+                dynamic_finding=True,
+                static_finding=False,
+                unique_id_from_tool=vul['references']['wpvulndb'][0],
+                nb_occurences=1,
+            )
+            if plugin:
+                finding.component_name = plugin
+            # manage version
+            if node and 'version' in node and node['version'] is not None:
+                finding.component_version = node['version'].get('number')
+            # manage date of finding with report date
+            if report_date:
+                finding.date = report_date
+            # if there is a fixed version fill mitigation
+            if 'fixed_in' in vul and vul['fixed_in']:
+                finding.mitigation = 'fixed in : ' + vul['fixed_in']
+            # manage CVE
+            if 'cve' in vul['references']:
+                finding.cve = "CVE-" + vul['references']['cve'][0]
+
+            # internal de-duplication
+            dupe_key = hashlib.sha256(str(finding.unique_id_from_tool).encode('utf-8')).hexdigest()
+            if dupe_key in dupes:
+                find = dupes[dupe_key]
+                if finding.references:
+                    dupes[dupe_key].references += finding.references
+                find.nb_occurences += finding.nb_occurences
+            else:
+                dupes[dupe_key] = finding
+
     def get_findings(self, file, test):
         tree = json.load(file)
 
@@ -26,50 +74,15 @@ class WpscanParser(object):
             report_date = datetime.utcfromtimestamp(tree.get('start_time'))
 
         dupes = dict()
+        # manage plugin findings
         for plugin in tree.get('plugins', []):
             node = tree['plugins'][plugin]
-            for vul in node.get('vulnerabilities'):
+            self.get_vulnerabilities(report_date, node.get('vulnerabilities'), dupes, node, plugin)
 
-                description = "\n".join([
-                    '**Title:** `' + vul['title'] + "`\n",
-                    '**Location:** `' + node['location'] + "`\n",
-                    '**Plugin:** `' + plugin + "`\n",
-                ])
-
-                finding = Finding(
-                    title=vul['title'],
-                    description=description,
-                    severity='Medium',
-                    cwe=1035,
-                    references=self.generate_references(vul['references']),
-                    dynamic_finding=True,
-                    static_finding=False,
-                    unique_id_from_tool=vul['references']['wpvulndb'][0],
-                    nb_occurences=1,
-                )
-                # manage version
-                finding.component_name = plugin
-                if 'version' in node and node['version'] is not None:
-                    finding.component_version = node['version'].get('number')
-                # manage date of finding with report date
-                if report_date:
-                    finding.date = report_date
-                # if there is a fixed version fill mitigation
-                if 'fixed_in' in vul:
-                    finding.mitigation = 'fixed in : ' + vul['fixed_in']
-                # manage CVE
-                if 'cve' in vul['references']:
-                    finding.cve = "CVE-" + vul['references']['cve'][0]
-
-                # internal de-duplication
-                dupe_key = hashlib.sha256(str(finding.unique_id_from_tool).encode('utf-8')).hexdigest()
-                if dupe_key in dupes:
-                    find = dupes[dupe_key]
-                    if finding.references:
-                        dupes[dupe_key].references += finding.references
-                    find.nb_occurences += finding.nb_occurences
-                else:
-                    dupes[dupe_key] = finding
+        # manage Wordpress version findings
+        if 'vulnerabilities' in tree.get('version', []):
+            if tree.get('version', [])['vulnerabilities']:
+                self.get_vulnerabilities(report_date, tree.get('version', [])['vulnerabilities'], dupes)
 
         # manage interesting interesting_findings
         for interesting_finding in tree.get('interesting_findings', []):
@@ -78,6 +91,8 @@ class WpscanParser(object):
                 '**Type:** `' + interesting_finding.get('type') + "`\n",
                 '**Url:** `' + interesting_finding['url'] + "`\n",
             ])
+            if interesting_finding['interesting_entries']:
+                description += '**Details:** `' + " ".join(interesting_finding['interesting_entries']) + "`\n"
             finding = Finding(
                 title=f"Interesting finding: {interesting_finding.get('to_s')}",
                 description=description,
