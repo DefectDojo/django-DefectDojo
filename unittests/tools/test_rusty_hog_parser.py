7580a2b24181a01a8cd7e7be11cc3a10690b39b1
Manuel Sommer <sommermanuel@gmx.de>
2022-01-19 10:47:03 +0100

    flake8

--- unittests/tools/test_rusty_hog_parser.py
+++ unittests/tools/test_rusty_hog_parser.py
@@ -68,19 +68,19 @@ class TestRustyhogParser(DojoTestCase):
         findings = parser.get_items(testfile, "Rusty Hog", Test())  # The outputfile is empty. A subscanner can't be classified
         self.assertEqual(0, len(findings))
 
-def test_parse_file_with_one_vuln_has_one_finding_essexhog(self):
+    def test_parse_file_with_one_vuln_has_one_finding_essexhog(self):
         testfile = open("unittests/scans/rusty_hog/essexhog_one_vuln.json")
         parser = RustyhogParser()
         findings = parser.get_items(testfile, "Essex Hog", Test())
         self.assertEqual(1, len(findings))
 
-def test_parse_file_with_multiple_vuln_has_multiple_finding_essexhog(self):
+    def test_parse_file_with_multiple_vuln_has_multiple_finding_essexhog(self):
         testfile = open("unittests/scans/rusty_hog/essexhog_many_vulns.json")
         parser = RustyhogParser()
         findings = parser.get_items(testfile, "Essex Hog", Test())
         self.assertEqual(10, len(findings))
 
-def test_parse_file_with_multiple_vuln_has_multiple_finding_essexhog_content(self):
+    def test_parse_file_with_multiple_vuln_has_multiple_finding_essexhog_content(self):
         testfile = open("unittests/scans/rusty_hog/essexhog_many_vulns.json")
         parser = RustyhogParser()
         findings = parser.get_items(testfile, "Essex Hog", Test())
