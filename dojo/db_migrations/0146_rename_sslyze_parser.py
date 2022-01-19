1d10ddc24689e3d4b154d533542573d5637b0ad2
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2022-01-19 10:43:38 +0100

    rename db migration after rebase

--- dojo/db_migrations/0146_rename_sslyze_parser.py
+++ dojo/db_migrations/0146_rename_sslyze_parser.py
@@ -15,7 +15,7 @@ def rename_sslyze_parser(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0144_import_action_untouched'),
+        ('dojo', '0145_system_settings_default_group_email_pattern'),
     ]
 
     operations = [
