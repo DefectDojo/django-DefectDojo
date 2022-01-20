19c38a24b6591be0eda65781866fd7f405e90496
Manuel Sommer <sommermanuel@gmx.de>
2022-01-20 14:53:35 +0100

    updated json.load

--- dojo/tools/rusty_hog/parser.py
+++ dojo/tools/rusty_hog/parser.py
@@ -19,7 +19,7 @@ class RustyhogParser(object):
         return self.get_items(tree, test)
 
     def parse_json(self, json_output):
-        tree = json.loads(json_output.read())
+        tree = json.load(json_output)
         return tree
 
     def get_items(self, json_output, scanner, test):
