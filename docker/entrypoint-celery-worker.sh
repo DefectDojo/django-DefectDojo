b2a14de58eb9825d58265f3eb331298fae1a2068
Maffooch <46459665+Maffooch@users.noreply.github.com>
2022-01-21 00:09:55 -0600

    Add support for including multiple setting files via extra_settings

--- docker/entrypoint-celery-worker.sh
+++ docker/entrypoint-celery-worker.sh
@@ -37,6 +37,18 @@ if test -f "$FILE"; then
     cp "$FILE" /app/dojo/settings/local_settings.py
 fi
 
+# Allow for bind-mount multiple settings.py overrides
+FILES=$(ls -I README.md /app/docker/extra_settings)
+NUM_FILES=$(echo "$FILES" | wc -l)
+if [ "$NUM_FILES" -gt "1" ]; then
+    COMMA_LIST=$(echo $FILES | tr -s '[:blank:]' ', ')
+    echo "============================================================"
+    echo "     Overriding DefectDojo's local_settings.py with multiple"
+    echo "     Files: $COMMA_LIST"
+    echo "============================================================"
+    cp /app/docker/extra_settings/* /app/dojo/settings/
+fi
+
 if [ "${DD_CELERY_WORKER_POOL_TYPE}" = "prefork" ]; then
   EXTRA_PARAMS="--autoscale=${DD_CELERY_WORKER_AUTOSCALE_MAX},${DD_CELERY_WORKER_AUTOSCALE_MIN}
     --prefetch-multiplier=${DD_CELERY_WORKER_PREFETCH_MULTIPLIER}"
--- docker/entrypoint-uwsgi.sh
+++ docker/entrypoint-uwsgi.sh
@@ -27,6 +27,18 @@ if test -f "$FILE"; then
     cp "$FILE" /app/dojo/settings/local_settings.py
 fi
 
+# Allow for bind-mount multiple settings.py overrides
+FILES=$(ls -I README.md /app/docker/extra_settings)
+NUM_FILES=$(echo "$FILES" | wc -l)
+if [ "$NUM_FILES" -gt "1" ]; then
+    COMMA_LIST=$(echo $FILES | tr -s '[:blank:]' ', ')
+    echo "============================================================"
+    echo "     Overriding DefectDojo's local_settings.py with multiple"
+    echo "     Files: $COMMA_LIST"
+    echo "============================================================"
+    cp /app/docker/extra_settings/* /app/dojo/settings/
+fi
+
 umask 0002
 
 # do the check with Django stack
