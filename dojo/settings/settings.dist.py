aa111aec649b5732aabd0be00608894a8a80d997
Manuel Sommer <manuel.sommer@tradebyte.com>
2022-01-27 09:50:01 +0100

    update

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1058,6 +1058,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'pip-audit Scan': ['vuln_id_from_tool', 'component_name', 'component_version'],
     'Rubocop Scan': ['vuln_id_from_tool', 'file_path', 'line'],
     'JFrog Xray Scan': ['title', 'description', 'component_name', 'component_version'],
+    'Rusty Hog Scan': ['title', 'description', 'cwe', 'severity'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1183,6 +1184,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'pip-audit Scan': DEDUPE_ALGO_HASH_CODE,
     'Rubocop Scan': DEDUPE_ALGO_HASH_CODE,
     'JFrog Xray Scan': DEDUPE_ALGO_HASH_CODE,
+    'Rusty Hog Scan': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
