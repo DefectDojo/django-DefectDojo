5bdc82ca76e7687f3e78c81b3bb93656648bddb3
Unsuspecting user <49907666+kibernautas@users.noreply.github.com>
2022-01-28 18:48:03 +0200

    feat: Rework sslyze parser for version 5 (#5689)
    * feat: Rework sslyze parser for version 5
    
    * feat: Rework sslyze parser for ven 5
    
    * Added Test data for new version, updated parser to handle both old and new versions.
    
    * Lint fix
    
    * Reverted defect_dojo_sample_data
    
    * rename test type and update settings for deduplication
    
    * revert formatting of sample data
    
    * rename db migration after rebase
    
    * add upgrade notes
    
    * rename db migration after rebase
    
    * work on unit tests tbc.
    
    * more work on unit tests
    
    Co-authored-by: Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1060,6 +1060,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Rubocop Scan': ['vuln_id_from_tool', 'file_path', 'line'],
     'JFrog Xray Scan': ['title', 'description', 'component_name', 'component_version'],
     'CycloneDX Scan': ['vuln_id_from_tool', 'component_name', 'component_version'],
+    'SSLyze Scan (JSON)': ['title', 'description'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1188,6 +1189,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Rubocop Scan': DEDUPE_ALGO_HASH_CODE,
     'JFrog Xray Scan': DEDUPE_ALGO_HASH_CODE,
     'CycloneDX Scan': DEDUPE_ALGO_HASH_CODE,
+    'SSLyze Scan (JSON)': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
