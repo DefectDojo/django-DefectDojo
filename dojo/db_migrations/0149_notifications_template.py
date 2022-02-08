a863552cc29fdcbc63e41f02f5acfd8a98326bef
Dubravko Sever <dubravko.sever@pan-net.eu>
2022-02-08 21:37:26 +0100

    Update: migration

--- dojo/db_migrations/0149_notifications_template.py
+++ dojo/db_migrations/0149_notifications_template.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0147_rename_sslyze_parser'),
+        ('dojo', '0148_default_notifications'),
     ]
 
     operations = [
