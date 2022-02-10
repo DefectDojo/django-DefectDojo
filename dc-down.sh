ea64d2e12dc4da09f23d0680f1ddcd2bf65e2bf8
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2022-02-10 10:00:33 +0100

    documentation

--- dc-down.sh
+++ dc-down.sh
@@ -1,7 +1,12 @@
 #/bin/bash
 
-echo "Stopping docker compose ..."
+if [ $# -eq 0 ]
+then
+    echo "Stopping docker compose and removing containers"
+else
+    echo "Stopping docker compose and removing containers with additional parameter $1 ..."
+fi
 
 # Stopping containers for all configurations
 # The environment must be provided but it doesn't make a difference which one
-docker-compose --profile mysql-rabbitmq --profile postgres-redis --env-file ./docker/environments/mysql-rabbitmq.env down
+docker-compose --profile mysql-rabbitmq --profile postgres-redis --env-file ./docker/environments/mysql-rabbitmq.env down $1
