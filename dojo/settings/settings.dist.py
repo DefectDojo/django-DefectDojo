2a458c29bff72f195f126d21dd8e1e26fd2fb268
Manuel Sommer <sommermanuel@gmx.de>
2022-02-17 13:50:42 +0100

    updated according to comments

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1067,7 +1067,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'JFrog Xray Scan': ['title', 'description', 'component_name', 'component_version'],
     'CycloneDX Scan': ['vuln_id_from_tool', 'component_name', 'component_version'],
     'SSLyze Scan (JSON)': ['title', 'description'],
-    'Harbor Vulnerability Scan': ['title', 'severity', 'description'],
+    'Harbor Vulnerability Scan': ['title'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
