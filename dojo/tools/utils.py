a4f6e834e3742ccc54231c0d6a9fd77a7984577d
Valentijn Scholten <valentijnscholten@gmail.com>
2022-03-11 17:49:07 +0100

    npm audit: fix cwe parsing AGAIN for empty cwe lists

--- dojo/tools/utils.py
+++ dojo/tools/utils.py
@@ -7,21 +7,24 @@ logger = logging.getLogger(__name__)
 def get_npm_cwe(item_node):
     """
         possible values:
-            "cwe": ["CWE-173", "CWE-200","CWE-601"]
+            "cwe": ["CWE-173", "CWE-200","CWE-601"]  (or [])
             "cwe": "CWE-1234"
-            "cwe": "[\"CWE-173\",\"CWE-200\",\"CWE-601\"]"
+            "cwe": "[\"CWE-173\",\"CWE-200\",\"CWE-601\"]" (or "[]")
     """
     if 'cwe' in item_node:
         cwe_node = item_node['cwe']
+        print(cwe_node)
         if type(cwe_node) == list:
-            if cwe_node[0]:
+            if cwe_node:
                 return int(cwe_node[0][4:])
         elif cwe_node.startswith('CWE-'):
             cwe_string = cwe_node[4:]
             if cwe_string:
                 return int(cwe_string)
         elif cwe_node.startswith('['):
-            return int(json.loads(cwe_node)[0][4:])
+            cwe = json.loads(cwe_node)
+            if cwe:
+                return int(cwe[0][4:])
 
     # Use CWE-1035 as fallback (vulnerable third party component)
     return 1035
