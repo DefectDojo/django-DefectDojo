39bd42437455eece0e2d210bca62e5914c3f9ff8
Valentijn Scholten <valentijnscholten@gmail.com>
2022-03-11 14:24:20 +0100

    npm audit: fix cwe parsing

--- unittests/tools/test_npm_audit_parser.py
+++ unittests/tools/test_npm_audit_parser.py
@@ -18,6 +18,7 @@ class TestNpmAuditParser(DojoTestCase):
         findings = parser.get_findings(testfile, Test())
         testfile.close()
         self.assertEqual(1, len(findings))
+        self.assertEqual(94, findings[0].cwe)
         self.assertEqual("growl", findings[0].component_name)
         self.assertEqual("1.9.2", findings[0].component_version)
 
@@ -37,6 +38,14 @@ class TestNpmAuditParser(DojoTestCase):
         # TODO ordering seems to be different in ci compared to local, so disable for now
         # self.assertEqual('mime', findings[4].component_name)
         # self.assertEqual('1.3.4', findings[4].component_version)
+
+    def test_npm_audit_parser_multiple_cwes_per_finding(self):
+        testfile = open(path.join(path.dirname(__file__), "../scans/npm_audit_sample/multiple_cwes.json"))
+        parser = NpmAuditParser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        self.assertEqual(1, len(findings))
+        self.assertEqual(346, findings[0].cwe)
 
     def test_npm_audit_parser_empty_with_error(self):
         with self.assertRaises(ValueError) as context:
