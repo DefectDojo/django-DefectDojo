da979a7383463fc321a9f33935933836e9dc4b3f
Ludovic Courgnaud <ludovic.courgnaud@gmail.com>
2022-03-25 06:00:53 +0100

    [FIX] Wrong Endpoint 'Date Discovered' (#6079)
    When creating a new Finding in the past for an Endpoint (new or already existing one), the 'Date Discovered' of this Endpoint (for the corresponding Finding) is always set to the today's date as defaulted in models.py files.
    This date should be fixed to the same value than the Finding's date, because the 'Date Discovered' is linked between the Endpoint and the Finding

--- dojo/finding/helper.py
+++ dojo/finding/helper.py
@@ -553,6 +553,6 @@ def add_endpoints(new_finding, form):
     for endpoint in new_finding.endpoints.all():
         eps, created = Endpoint_Status.objects.get_or_create(
             finding=new_finding,
-            endpoint=endpoint)
+            endpoint=endpoint, defaults={'date': form.cleaned_data['date'] or now})
         endpoint.endpoint_status.add(eps)
         new_finding.endpoint_status.add(eps)
