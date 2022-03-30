943bc42a021e7257a02e40d530807402437ead48
Igor Gomes <igor.gomes@zup.com.br>
2022-03-30 09:21:42 -0700

    Update Nuclei importer maintaining backwards compatibility

--- dojo/tools/nuclei/parser.py
+++ dojo/tools/nuclei/parser.py
@@ -25,14 +25,14 @@ class NucleiParser(object):
             dupes = {}
             for item in data:
                 template_id = item.get('templateID')
-                if template_id == None:
+                if template_id is None:
                     template_id = item.get('template-id')
                 info = item.get('info')
                 name = info.get('name')
                 severity = info.get('severity').title()
                 type = item.get('type')
                 matched = item.get('matched')
-                if matched == None:
+                if matched is None:
                     matched = item.get('matched-at')
                 if '://' in matched:
                     endpoint = Endpoint.from_uri(matched)
