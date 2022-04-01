5d7e9862e570185b83e8be3492218c73ad0e3f08
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2022-04-01 21:19:47 +0200

    show file selection dialogue in openapi3 (#6098)

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -689,7 +689,9 @@ SPECTACULAR_SETTINGS = {
     # OTHER SETTINGS
     # the following set to False could help some client generators
     # 'ENUM_ADD_EXPLICIT_BLANK_NULL_CHOICE': False,
-    'POSTPROCESSING_HOOKS': ['dojo.api_v2.prefetch.schema.prefetch_postprocessing_hook']
+    'POSTPROCESSING_HOOKS': ['dojo.api_v2.prefetch.schema.prefetch_postprocessing_hook'],
+    # show file selection dialogue, see https://github.com/tfranzel/drf-spectacular/issues/455
+    "COMPONENT_SPLIT_REQUEST": True,
 }
 
 # ------------------------------------------------------------------------------
