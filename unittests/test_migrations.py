36f7950bd9b0a005795d803ed6d51fbde55dd4d6
kiblik <tomas@kubla.sk>
2022-04-19 21:07:27 +0000

    Unittests speedup - skip TestRemoveEndpointMitigatedMigration (#6135)

--- unittests/test_migrations.py
+++ unittests/test_migrations.py
@@ -247,8 +247,7 @@ class TestEndpointStatusMigration(MigratorTestCase):
         self.assertEqual(eps.count(), 1)
 
 
-# TODO: These tests can be skipped in 2.10.x or later
-# @skip("Outdated - Any future changes of code should not affect these tests")
+@skip("Outdated - Any future changes of code should not affect these tests")
 class TestRemoveEndpointMitigatedMigration(MigratorTestCase):
     migrate_from = ('dojo', '0152_notifications_template')
     migrate_to = ('dojo', '0153_migrate_endpoint_mitigated')
