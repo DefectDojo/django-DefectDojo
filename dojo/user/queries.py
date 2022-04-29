48e6060fbe1ab0740635e843ab11e9723f7b185a
Stefan Fleckenstein <stefan.fleckenstein@maibornwolff.de>
2022-04-29 09:02:38 +0200

    performance optimization

--- dojo/user/queries.py
+++ dojo/user/queries.py
@@ -11,11 +11,13 @@ from dojo.request_cache import cache_for_request
 def get_authorized_users_for_product_type(users, product_type, permission):
     roles = get_roles_for_permission(permission)
     product_type_members = Product_Type_Member.objects \
-        .filter(product_type=product_type, role__in=roles)
+        .filter(product_type=product_type, role__in=roles) \
+        .select_related('user')
     product_type_groups = Product_Type_Group.objects \
         .filter(product_type=product_type, role__in=roles)
-    group_members = Dojo_Group_Member.objects. \
-        filter(group__in=[ptg.group for ptg in product_type_groups])
+    group_members = Dojo_Group_Member.objects \
+        .filter(group__in=[ptg.group for ptg in product_type_groups]) \
+        .select_related('user')
     return users.filter(Q(id__in=[ptm.user.id for ptm in product_type_members]) |
         Q(id__in=[gm.user.id for gm in group_members]) |
         Q(global_role__role__in=roles) |
@@ -28,16 +30,20 @@ def get_authorized_users_for_product_and_product_type(users, product, permission
 
     roles = get_roles_for_permission(permission)
     product_members = Product_Member.objects \
-        .filter(product=product, role__in=roles)
+        .filter(product=product, role__in=roles) \
+        .select_related('user')
     product_type_members = Product_Type_Member.objects \
-        .filter(product_type=product.prod_type, role__in=roles)
+        .filter(product_type=product.prod_type, role__in=roles) \
+        .select_related('user')
     product_groups = Product_Group.objects \
         .filter(product=product, role__in=roles)
     product_type_groups = Product_Type_Group.objects \
         .filter(product_type=product.prod_type, role__in=roles)
-    group_members = Dojo_Group_Member.objects.filter(
-        Q(group__in=[pg.group for pg in product_groups]) |
-        Q(group__in=[ptg.group for ptg in product_type_groups]))
+    group_members = Dojo_Group_Member.objects \
+        .filter(
+            Q(group__in=[pg.group for pg in product_groups]) |
+            Q(group__in=[ptg.group for ptg in product_type_groups])) \
+        .select_related('user')
     return users.filter(Q(id__in=[pm.user.id for pm in product_members]) |
         Q(id__in=[ptm.user.id for ptm in product_type_members]) |
         Q(id__in=[gm.user.id for gm in group_members]) |
@@ -64,16 +70,20 @@ def get_authorized_users(permission, user=None):
 
     roles = get_roles_for_permission(permission)
     product_members = Product_Member.objects \
-        .filter(product_id__in=authorized_products, role__in=roles)
+        .filter(product_id__in=authorized_products, role__in=roles) \
+        .select_related('user')
     product_type_members = Product_Type_Member.objects \
-        .filter(product_type_id__in=authorized_product_types, role__in=roles)
+        .filter(product_type_id__in=authorized_product_types, role__in=roles) \
+        .select_related('user')
     product_groups = Product_Group.objects \
         .filter(product_id__in=authorized_products, role__in=roles)
     product_type_groups = Product_Type_Group.objects \
         .filter(product_type_id__in=authorized_product_types, role__in=roles)
-    group_members = Dojo_Group_Member.objects.filter(
-        Q(group__in=[pg.group for pg in product_groups]) |
-        Q(group__in=[ptg.group for ptg in product_type_groups]))
+    group_members = Dojo_Group_Member.objects \
+        .filter(
+            Q(group__in=[pg.group for pg in product_groups]) |
+            Q(group__in=[ptg.group for ptg in product_type_groups])) \
+        .select_related('user')
     return users.filter(Q(id__in=[pm.user.id for pm in product_members]) |
         Q(id__in=[ptm.user.id for ptm in product_type_members]) |
         Q(id__in=[gm.user.id for gm in group_members]) |
