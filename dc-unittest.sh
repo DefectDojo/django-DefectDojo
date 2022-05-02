f169c49f48d3221e848307731586d5e131050ba5
Dubravko Sever <dubravko.sever@pan-net.eu>
2022-05-02 12:59:09 +0200

    Add: option to check docker-compose version

--- dc-unittest.sh
+++ dc-unittest.sh
@@ -3,6 +3,23 @@
 unset PROFILE
 unset TEST_CASE
 
+main=`docker-compose  version  --short | cut -d '.' -f 1`
+minor=`docker-compose  version  --short | cut -d '.' -f 2`
+current=`docker-compose  version  --short`
+
+echo 'Checking docker-compose version'
+if [[ $main -lt 1 ]]; then
+  echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+  exit 1
+elif [[ $main -eq 1 ]]; then
+  if [[ $minor -lt 28 ]]; then
+    echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+    exit 1
+  fi
+fi
+
+echo 'Supported docker-compose version'
+
 usage() {
   echo
   echo "This script helps with running unit tests."
--- dc-up-d.sh
+++ dc-up-d.sh
@@ -2,6 +2,24 @@
 
 unset PROFILE
 
+main=`docker-compose  version  --short | cut -d '.' -f 1`
+minor=`docker-compose  version  --short | cut -d '.' -f 2`
+current=`docker-compose  version  --short`
+
+echo 'Checking docker-compose version'
+if [[ $main -lt 1 ]]; then
+  echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+  exit 1
+elif [[ $main -eq 1 ]]; then
+  if [[ $minor -lt 28 ]]; then
+    echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+    exit 1
+  fi
+fi
+
+echo 'Supported docker-compose version'
+
+
 if [ $# -eq 0 ]
 then
     if [ -z $DD_PROFILE ]
