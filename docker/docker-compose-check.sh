4514e996c74adb744bd26fed9c049131109f91ca
Sever <dubravko.sever@pan-net.eu>
2022-05-03 19:15:37 +0200

    dc command improvements (#6227)
    * Add: option to check docker-compose version
    
    * Add: default profile if not specified
    
    * refactoring
    
    * Update doc

---
+++ docker/docker-compose-check.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+
+main=`docker-compose  version  --short | cut -d '.' -f 1`
+minor=`docker-compose  version  --short | cut -d '.' -f 2`
+current=`docker-compose  version  --short`
+
+echo 'Checking docker-compose version'
+if [[ $main -lt 1 ]]; then
+  echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+  exit 1
+elif [[ $main -eq 1 ]]; then
+  if [[ $minor -lt 28 ]]; then
+    echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
+    exit 1
+  fi
+fi
+
+echo 'Supported docker-compose version'
