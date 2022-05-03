2d9aa827847dc577e62678b9fbb82963944f56e4
Timo Pagel <github-commit@timo-pagel.de>
2022-05-03 17:15:00 +0200

    chore: update db migrations

--- dojo/db_migrations/0161_dojo_group_is_azure.py
+++ dojo/db_migrations/0161_dojo_group_is_azure.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0157_vulnerability_reference'),
+        ('dojo', '0160_set_notnull_endpoint_statuses'),
     ]
 
     operations = [
