82d0a70ac97426319ca96ca208ce6e4069acf5f4
Chris Fort <chris.fort@lexisnexis.com>
2022-05-10 15:00:24 -0400

    added tests for Qualys CSV parsers, fixed some logic and bugs

--- dojo/tools/qualys/csv_parser.py
+++ dojo/tools/qualys/csv_parser.py
@@ -1,8 +1,12 @@
 import csv
 import io
+import logging
 from datetime import datetime
+import re
 
 from dojo.models import Finding, Endpoint
+
+_logger = logging.getLogger(__name__)
 
 
 def parse_csv(csv_file) -> [Finding]:
@@ -13,17 +17,53 @@ def parse_csv(csv_file) -> [Finding]:
     Returns:
     """
 
-    default_keys = ['IP', 'Network', 'DNS', 'NetBIOS', 'Tracking Method', 'OS', 'IP Status', 'QID', 'Title',
-                    'Vuln Status', 'Type', 'Severity', 'Port', 'Protocol', 'FQDN', 'SSL', 'First Detected',
-                    'Last Detected', 'Times Detected', 'Date Last Fixed', 'CVE ID', 'Vendor Reference', 'Bugtraq ID',
-                    'CVSS3', 'CVSS3 Base', 'CVSS3 Temporal', 'Threat', 'Impact', 'Solution', 'Exploitability',
-                    'Associated Malware', 'PCI Vuln', 'Ticket State', 'Instance', 'OS CPE', 'Category',
-                    'Associated Tags']
+    default_keys = [
+        'IP',
+        'Network',
+        'DNS',
+        'NetBIOS',
+        'Tracking Method',
+        'OS',
+        'IP Status',
+        'QID',
+        'Title',
+        'Vuln Status',
+        'Type',
+        'Severity',
+        'Port',
+        'Protocol',
+        'FQDN',
+        'SSL',
+        'First Detected',
+        'Last Detected',
+        'Times Detected',
+        'Date Last Fixed',
+        'CVE ID',
+        'Vendor Reference',
+        'Bugtraq ID',
+        'CVSS3',
+        'CVSS3 Base',
+        'CVSS3 Temporal',
+        'Threat',
+        'Impact',
+        'Solution',
+        'Exploitability',
+        'Associated Malware',
+        'PCI Vuln',
+        'Ticket State',
+        'Instance',
+        'OS CPE',
+        'Category',
+        'Associated Tags']
 
     content = csv_file.read()
-    if type(content) is bytes:
+    if isinstance(content, bytes):
         content = content.decode('utf-8')
-    csv_reader = csv.DictReader(io.StringIO(content), delimiter=',', quotechar='"', fieldnames=default_keys)
+    csv_reader = csv.DictReader(
+        io.StringIO(content),
+        delimiter=',',
+        quotechar='"',
+        fieldnames=default_keys)
 
     report_findings = get_report_findings(csv_reader)
     dojo_findings = build_findings_from_dict(report_findings)
@@ -58,19 +98,57 @@ def get_references(cve_list):
         return None
 
 
-def build_findings_from_dict(report_findings) -> [Finding]:
+def _extract_cvss_vectors(cvss_base, cvss_temporal):
     """
-    Takes a Dict built from CSV and creates a Finding object
+    Parses the CVSS3 Vectors from the CVSS3 Base and CVSS3 Temporal fields and returns as a single string.
+
+    This is done because the raw values come with additional characters that cannot be parsed with the cvss library.
+        Example: 6.7 (AV:L/AC:L/PR:H/UI:N/S:U/C:H/I:H/A:H)
+    Args:
+        cvss_base:
+        cvss_temporal:
+    Returns:
+        A CVSS3 Vector including both Base and Temporal if available
+    """
+
+    vector_pattern = r'^\d.\d \((.*)\)'
+
+    if cvss_base:
+        try:
+            cvss_base_vector = re.search(vector_pattern, cvss_base).group(1)
+        except IndexError:
+            _logger.error(f'CVSS3 Base Vector not found in {cvss_base}')
+            return None
+        if cvss_temporal:
+            try:
+                cvss_temporal_vector = re.search(vector_pattern, cvss_temporal).group(1)
+                return f'CVSS:3.0/{cvss_base_vector}/{cvss_temporal_vector}'
+            except IndexError:
+                _logger.error(f'CVSS3 Temporal Vector not found in {cvss_base}')
+                return f'CVSS:3.0/{cvss_base_vector}'
+        else:
+            return cvss_base_vector
+    else:
+        return None
+
+
+def build_findings_from_dict(report_findings:[dict]) -> [Finding]:
+    """
+    Takes a list of Dictionaries built from CSV and creates a Finding object
     Args:
         report_findings:
     Returns:
 
     """
-    severity_lookup = {'1': 'Info', '2': 'Low', '3': 'Medium', '4': 'High', '5': 'Critical'}
+    severity_lookup = {
+        '1': 'Info',
+        '2': 'Low',
+        '3': 'Medium',
+        '4': 'High',
+        '5': 'Critical'}
     dojo_findings = []
 
     for report_finding in report_findings:
-
         if report_finding.get('FQDN'):
             endpoint = Endpoint.from_uri(report_finding.get('FQDN'))
         else:
@@ -83,13 +161,12 @@ def build_findings_from_dict(report_findings) -> [Finding]:
             severity=severity_lookup.get(report_finding['Severity'], 'Info'),
             references=get_references(report_finding['CVE ID']),
             impact=report_finding['Impact'],
-            date=datetime.strptime(report_finding['Last Detected'], "%m/%d/%Y %H:%M:%S"),
+            date=datetime.strptime(report_finding['Last Detected'], "%m/%d/%Y %H:%M:%S").date(),
             vuln_id_from_tool=report_finding['QID'],
-            cvssv3_score=report_finding['CVSS3']
-        )
+            cvssv3=_extract_cvss_vectors(report_finding['CVSS3 Base'], report_finding['CVSS3 Temporal']))
 
         if report_finding['Date Last Fixed']:
-            finding.mitigated = datetime.strptime(report_finding['Date Last Fixed'], "%m/%d/%Y %H:%M:%S")
+            finding.mitigated = datetime.strptime(report_finding['Date Last Fixed'], "%m/%d/%Y %H:%M:%S").date()
             finding.is_Mitigated = True
         else:
             finding.is_Mitigated = False
