755bef053f1f14b4920b9ac35f60e816503b9193
damiencarol <damien.carol@gmail.com>
2022-05-10 11:05:22 +0200

    Fix Flake8

--- unittests/tools/test_fortify_parser.py
+++ unittests/tools/test_fortify_parser.py
@@ -1,7 +1,7 @@
-from ..dojo_test_case import DojoTestCase, get_unit_tests_path
-from dojo.tools.fortify.parser import FortifyParser
 from dojo.models import Test
-from datetime import datetime
+from dojo.tools.fortify.parser import FortifyParser
+
+from ..dojo_test_case import DojoTestCase, get_unit_tests_path
 
 
 class TestFortifyParser(DojoTestCase):
