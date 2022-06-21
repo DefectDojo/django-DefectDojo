64565d316000081ac28173cdfb6e25d1ccb3826e
kiblik <tomas@kubla.sk>
2022-06-21 15:36:54 +0000

    APIv2: Add support for configuration_permissions (#6424)
    * APIv2: Add support for configuration_permissions
    
    * Add tests

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -27,6 +27,7 @@ from django.conf import settings
 from rest_framework import serializers
 from django.core.exceptions import ValidationError, PermissionDenied
 from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.models import Permission
 from django.utils import timezone
 from django.db.utils import IntegrityError
 import six
@@ -41,6 +42,7 @@ from dojo.importers.reimporter.reimporter import DojoDefaultReImporter as ReImpo
 from dojo.authorization.authorization import user_has_permission
 from dojo.authorization.roles_permissions import Permissions
 from dojo.finding.helper import save_vulnerability_ids, save_vulnerability_ids_template
+from dojo.user.utils import get_configuration_permissions_codenames
 
 
 logger = logging.getLogger(__name__)
@@ -350,24 +352,67 @@ class UserSerializer(serializers.ModelSerializer):
     last_login = serializers.DateTimeField(read_only=True)
     password = serializers.CharField(write_only=True, style={'input_type': 'password'}, required=False,
                                      validators=[validate_password])
+    configuration_permissions = serializers.PrimaryKeyRelatedField(
+         allow_null=True,
+         queryset=Permission.objects.filter(codename__in=get_configuration_permissions_codenames()),
+         many=True,
+         required=False,
+         source='user_permissions')
 
     class Meta:
         model = Dojo_User
         if settings.FEATURE_CONFIGURATION_AUTHORIZATION:
-            fields = ('id', 'username', 'first_name', 'last_name', 'email', 'last_login', 'is_active', 'is_superuser', 'password')
+            fields = ('id', 'username', 'first_name', 'last_name', 'email', 'last_login', 'is_active', 'is_superuser', 'password', 'configuration_permissions')
         else:
-            fields = ('id', 'username', 'first_name', 'last_name', 'email', 'last_login', 'is_active', 'is_staff', 'is_superuser', 'password')
+            fields = ('id', 'username', 'first_name', 'last_name', 'email', 'last_login', 'is_active', 'is_staff', 'is_superuser', 'password', 'configuration_permissions')
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        # This will show only "configuration_permissions" even if user has also other permissions
+        all_permissions = set(ret['configuration_permissions'])
+        allowed_configuration_permissions = set(self.fields['configuration_permissions'].child_relation.queryset.values_list('id', flat=True))
+        ret['configuration_permissions'] = list(all_permissions.intersection(allowed_configuration_permissions))
+
+        return ret
+
+    def update(self, instance, validated_data):
+        new_configuration_permissions = None
+        if 'user_permissions' in validated_data:  # This field was renamed from "configuration_permissions" in the meantime
+            new_configuration_permissions = set(validated_data.pop('user_permissions'))
+
+        instance = super().update(instance, validated_data)
+
+        # This will update only Permissions from category "configuration_permissions". Others will be untouched
+        if new_configuration_permissions:
+            allowed_configuration_permissions = set(self.fields['configuration_permissions'].child_relation.queryset.all())
+            non_configuration_permissions = set(instance.user_permissions.all()) - allowed_configuration_permissions
+            new_permissions = non_configuration_permissions.union(new_configuration_permissions)
+            instance.user_permissions.set(new_permissions)
+
+        return instance
 
     def create(self, validated_data):
         if 'password' in validated_data:
             password = validated_data.pop('password')
         else:
             password = None
+
+        new_configuration_permissions = None
+        if 'user_permissions' in validated_data:  # This field was renamed from "configuration_permissions" in the meantime
+            new_configuration_permissions = set(validated_data.pop('user_permissions'))
+
         user = Dojo_User.objects.create(**validated_data)
+
         if password:
             user.set_password(password)
         else:
             user.set_unusable_password()
+
+        # This will create only Permissions from category "configuration_permissions". There are no other Permissions.
+        if new_configuration_permissions:
+            user.user_permissions.set(new_configuration_permissions)
+
         user.save()
         return user
 
@@ -409,9 +454,55 @@ class RoleSerializer(serializers.ModelSerializer):
 
 class DojoGroupSerializer(serializers.ModelSerializer):
 
+    configuration_permissions = serializers.PrimaryKeyRelatedField(
+         allow_null=True,
+         queryset=Permission.objects.filter(codename__in=get_configuration_permissions_codenames()),
+         many=True,
+         required=False,
+         source='auth_group.permissions')
+
     class Meta:
         model = Dojo_Group
         exclude = ['auth_group']
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+
+        # This will show only "configuration_permissions" even if user has also other permissions
+        all_permissions = set(ret['configuration_permissions'])
+        allowed_configuration_permissions = set(self.fields['configuration_permissions'].child_relation.queryset.values_list('id', flat=True))
+        ret['configuration_permissions'] = list(all_permissions.intersection(allowed_configuration_permissions))
+
+        return ret
+
+    def create(self, validated_data):
+        new_configuration_permissions = None
+        if 'auth_group' in validated_data and 'permissions' in validated_data['auth_group']:  # This field was renamed from "configuration_permissions" in the meantime
+            new_configuration_permissions = set(validated_data.pop('auth_group')['permissions'])
+
+        instance = super().create(validated_data)
+
+        # This will update only Permissions from category "configuration_permissions". There are no other Permissions.
+        if new_configuration_permissions:
+            instance.auth_group.permissions.set(new_configuration_permissions)
+
+        return instance
+
+    def update(self, instance, validated_data):
+        new_configuration_permissions = None
+        if 'auth_group' in validated_data and 'permissions' in validated_data['auth_group']:  # This field was renamed from "configuration_permissions" in the meantime
+            new_configuration_permissions = set(validated_data.pop('auth_group')['permissions'])
+
+        instance = super().update(instance, validated_data)
+
+        # This will update only Permissions from category "configuration_permissions". Others will be untouched
+        if new_configuration_permissions:
+            allowed_configuration_permissions = set(self.fields['configuration_permissions'].child_relation.queryset.all())
+            non_configuration_permissions = set(instance.auth_group.permissions.all()) - allowed_configuration_permissions
+            new_permissions = non_configuration_permissions.union(new_configuration_permissions)
+            instance.auth_group.permissions.set(new_permissions)
+
+        return instance
 
 
 class DojoGroupMemberSerializer(serializers.ModelSerializer):
@@ -1952,3 +2043,9 @@ class DeletePreviewSerializer(serializers.Serializer):
     model = serializers.CharField(read_only=True)
     id = serializers.IntegerField(read_only=True, allow_null=True)
     name = serializers.CharField(read_only=True)
+
+
+class ConfigurationPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Permission
+        exclude = ['content_type']
