e54ae73da5a8dcd2d0caf67d3746c2e63a5bfee5
Jacob Hoopes <jake.hoopes@gmail.com>
2022-07-14 12:22:59 -0600

    Clean feature branch

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1121,6 +1121,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'StackHawk HawkScan': ['vuln_id_from_tool', 'component_name', 'component_version'],
     'Hydra Scan': ['title', 'description'],
     'DrHeader JSON Importer': ['title', 'description'],
+    'PWN SAST': ['title', 'description'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1265,6 +1266,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Rusty Hog Scan': DEDUPE_ALGO_HASH_CODE,
     'Hydra Scan': DEDUPE_ALGO_HASH_CODE,
     'DrHeader JSON Importer': DEDUPE_ALGO_HASH_CODE,
+    'PWN SAST': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
