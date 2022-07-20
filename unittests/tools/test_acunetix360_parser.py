7b158cf632f49dce27387074766cf5d90dd16649
Colm O hEigeartaigh <coheigea@users.noreply.github.com>
2022-07-20 04:14:24 +0100

    Parse Acunetix360 State field for risk acceptance and false positive findings (#6581)

--- unittests/tools/test_acunetix360_parser.py
+++ unittests/tools/test_acunetix360_parser.py
@@ -24,6 +24,46 @@ class TestAcunetix360Parser(DojoTestCase):
             endpoint = finding.unsaved_endpoints[0]
             self.assertEqual(str(endpoint), "http://php.testsparker.com/auth/login.php")
 
+    def test_parse_file_with_one_finding_false_positive(self):
+        testfile = open("unittests/scans/acunetix360/acunetix360_one_finding_false_positive.json")
+        parser = Acunetix360Parser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
+        with self.subTest(i=0):
+            finding = findings[0]
+            self.assertEqual("Medium", finding.severity)
+            self.assertEqual(16, finding.cwe)
+            self.assertIsNotNone(finding.description)
+            self.assertGreater(len(finding.description), 0)
+            self.assertEqual("CVSS:3.0/AV:N/AC:L/PR:L/UI:R/S:U/C:H/I:N/A:N/E:H/RL:O/RC:C", finding.cvssv3)
+            self.assertEqual(1, len(finding.unsaved_endpoints))
+            endpoint = finding.unsaved_endpoints[0]
+            self.assertEqual(str(endpoint), "http://php.testsparker.com/auth/login.php")
+            self.assertTrue(finding.false_p)
+
+    def test_parse_file_with_one_finding_risk_accepted(self):
+        testfile = open("unittests/scans/acunetix360/acunetix360_one_finding_accepted_risk.json")
+        parser = Acunetix360Parser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
+        with self.subTest(i=0):
+            finding = findings[0]
+            self.assertEqual("Medium", finding.severity)
+            self.assertEqual(16, finding.cwe)
+            self.assertIsNotNone(finding.description)
+            self.assertGreater(len(finding.description), 0)
+            self.assertEqual("CVSS:3.0/AV:N/AC:L/PR:L/UI:R/S:U/C:H/I:N/A:N/E:H/RL:O/RC:C", finding.cvssv3)
+            self.assertEqual(1, len(finding.unsaved_endpoints))
+            endpoint = finding.unsaved_endpoints[0]
+            self.assertEqual(str(endpoint), "http://php.testsparker.com/auth/login.php")
+            self.assertTrue(finding.risk_accepted)
+
     def test_parse_file_with_multiple_finding(self):
         testfile = open("unittests/scans/acunetix360/acunetix360_many_findings.json")
         parser = Acunetix360Parser()
