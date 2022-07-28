eacc4670b1d111e38daf7f3fad9ebddc5411118d
sjs6776 <103534016+sjs6776@users.noreply.github.com>
2022-07-28 11:39:49 -0400

    Update settings.dist.py
    Added the requested lines for Blackduck Hub Deduplication #6161

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1120,6 +1120,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Hydra Scan': ['title', 'description'],
     'DrHeader JSON Importer': ['title', 'description'],
     'PWN SAST': ['title', 'description'],
+    'Blackduck Hub Scan': ['title', 'cve', 'component_name'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1266,6 +1267,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Hydra Scan': DEDUPE_ALGO_HASH_CODE,
     'DrHeader JSON Importer': DEDUPE_ALGO_HASH_CODE,
     'PWN SAST': DEDUPE_ALGO_HASH_CODE,
+    'Blackduck Hub Scan': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
