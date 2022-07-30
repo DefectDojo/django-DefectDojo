389f6de3a674e0b398be33e3c9a4c637fca87933
adeptex <adeptex@users.noreply.github.com>
2022-07-30 21:03:05 +0200

    De-duplication

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1120,6 +1120,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Hydra Scan': ['title', 'description'],
     'DrHeader JSON Importer': ['title', 'description'],
     'PWN SAST': ['title', 'description'],
+    'Whispers': ['vuln_id_from_tool'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1266,6 +1267,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Hydra Scan': DEDUPE_ALGO_HASH_CODE,
     'DrHeader JSON Importer': DEDUPE_ALGO_HASH_CODE,
     'PWN SAST': DEDUPE_ALGO_HASH_CODE,
+    'Whispers': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
--- dojo/tools/whispers/parser.py
+++ dojo/tools/whispers/parser.py
@@ -3,27 +3,26 @@ import json
 
 from dojo.models import Endpoint, Finding
 
-SEVERITY_MAP = {
-    "BLOCKER": "Critical",
-    "CRITICAL": "High",
-    "MAJOR": "Medium",
-    "MINOR": "Low",
-    "INFO": "Info",
-}
-
-
-def _mask(text, n_plain=4):
-    length = len(text)
-    if length <= n_plain:
-        n_plain = 0
-
-    return text[:n_plain] + ("*" * (length - n_plain))
-
-
 class WhispersParser(object):
     """
     Identify hardcoded secrets in static structured text
     """
+
+    SEVERITY_MAP = {
+        "BLOCKER": "Critical",
+        "CRITICAL": "High",
+        "MAJOR": "Medium",
+        "MINOR": "Low",
+        "INFO": "Info",
+    }
+
+    @staticmethod
+    def _mask(text, n_plain=4):
+        length = len(text)
+        if length <= n_plain:
+            n_plain = 0
+
+        return text[:n_plain] + ("*" * (length - n_plain))
 
     def get_scan_types(self):
         return ["Whispers Scan"]
@@ -36,16 +35,14 @@ class WhispersParser(object):
 
     def get_findings(self, file, test):
         tree = json.load(file)
-        dupes = dict()
-
+        findings = []
         for vuln in tree:
             summary = (
                 f'Hardcoded {vuln.get("message")} "{vuln.get("key")}" '
                 f'in {vuln.get("file")}:{vuln.get("line")}'
             )
-            description = f'{summary} `{_mask(vuln.get("value"))}`'
-
-            finding = Finding(
+            description = f'{summary} `{self._mask(vuln.get("value"))}`'
+            findings.append(Finding(
                 title=summary,
                 description=description,
                 mitigation=(
@@ -58,24 +55,13 @@ class WhispersParser(object):
                     "https://cwe.mitre.org/data/definitions/798.html"
                 ),
                 cwe=798,
-                severity=SEVERITY_MAP.get(vuln.get("severity"), "Info"),
+                severity=self.SEVERITY_MAP.get(vuln.get("severity"), "Info"),
                 file_path=vuln.get("file"),
                 line=int(vuln.get("line")),
-                uniq_id_from_tool=hashlib.sha256(str(description).encode("utf-8")).hexdigest(),
+                vuln_id_from_tool=hashlib.sha256(str(description).encode("utf-8")).hexdigest(),
                 static_finding=True,
                 dynamic_finding=False,
                 test=test,
-            )
+            ))
 
-            # internal de-duplication
-            dupe_key = finding.hash_code
-            if dupe_key in dupes:
-                find = dupes[dupe_key]
-                if finding.description:
-                    find.description += "\n" + finding.description
-                find.unsaved_endpoints.extend(finding.unsaved_endpoints)
-                dupes[dupe_key] = find
-            else:
-                dupes[dupe_key] = finding
-
-        return list(dupes.values())
+        return findings
