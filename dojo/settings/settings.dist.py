a03a94016c68b8912e6ae5605c7e91a866953cee
DVSutyagin <dvsutyagin@greenatom.ru>
2022-08-05 11:57:01 +0300

    modified dedupe config

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1123,7 +1123,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Hydra Scan': ['title', 'description'],
     'DrHeader JSON Importer': ['title', 'description'],
     'PWN SAST': ['title', 'description'],
-    'docker-bench-security Scan': ['title', 'description'],
+    'docker-bench-security Scan': ['vuln_id_from_tool'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
