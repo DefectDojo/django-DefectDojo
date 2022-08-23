4dff6af1c03f740e2328e94ebf053dc14eeadc21
Dubravko Sever <dubravko.sever@pan-net.eu>
2022-08-23 12:00:46 +0200

    Sorting by severity after add/update

--- dojo/notifications/helper.py
+++ dojo/notifications/helper.py
@@ -381,6 +381,13 @@ def notify_test_created(test):
 
 
 def notify_scan_added(test, updated_count, new_findings, findings_mitigated=[], findings_reactivated=[], findings_untouched=[]):
+    logger.debug("Scan added notifications")
+
+    new_findings.sort(key=lambda x: x.numerical_severity)
+    findings_mitigated.sort(key=lambda x: x.numerical_severity)
+    findings_reactivated.sort(key=lambda x: x.numerical_severity)
+    findings_untouched.sort(key=lambda x: x.numerical_severity)
+
     title = 'Created/Updated ' + str(updated_count) + " findings for " + str(test.engagement.product) + ': ' + str(test.engagement.name) + ': ' + str(test)
     create_notification(event='scan_added', title=title, findings_new=new_findings, findings_mitigated=findings_mitigated, findings_reactivated=findings_reactivated,
                         finding_count=updated_count, test=test, engagement=test.engagement, product=test.engagement.product, findings_untouched=findings_untouched,
