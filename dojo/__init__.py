8fa151b463e218c47ed2b73b709a93e61cd49cae
gmarquet <gmarquet@talend.com>
2022-09-13 17:19:33 +0200

    Align to dev branch

--- dojo/__init__.py
+++ dojo/__init__.py
@@ -1,9 +1,7 @@
-
-
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 from .celery import app as celery_app  # noqa
 
-__version__ = '2.14.0'
-__url__ = 'https://github.com/DefectDojo/django-DefectDojo'
-__docs__ = 'https://defectdojo.github.io/django-DefectDojo'
+__version__ = "2.14.0-dev"
+__url__ = "https://github.com/DefectDojo/django-DefectDojo"
+__docs__ = "https://defectdojo.github.io/django-DefectDojo"
