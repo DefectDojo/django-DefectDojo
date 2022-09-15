766097e7273ea380f78d4a75b7069a8da7202553
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2022-09-15 15:33:55 -0500

    Correct unit tests

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -884,7 +884,7 @@ class EndpointSerializer(TaggitSerializer, serializers.ModelSerializer):
             path = data.get('path', self.instance.path)
             query = data.get('query', self.instance.query)
             fragment = data.get('fragment', self.instance.fragment)
-            if data.get('product') != self.instance.product:
+            if 'product' in data and data['product'] != self.instance.product:
                 raise serializers.ValidationError('Change of product is not possible')
             product = self.instance.product
 
