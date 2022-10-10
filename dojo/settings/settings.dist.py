9bccdda92a87e174b00cd5f208aeb5a7106e4e97
Pavel <107552671+pna-nca@users.noreply.github.com>
2022-10-10 16:54:07 +0200

    dojo: tool: introduce NeuVector compliance scans import support (#6947)
    This commit makes DefectDojo to support compliance scans performed by
    NeuVector. Such scan results can only be fetched via REST API using
    endpoints like /v1/scan/workload/{id} and /v1/host/{id}/compliance. The
    latter one returns the results in a slightly different format. Both of
    them are supported.

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1203,6 +1203,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'Veracode SourceClear Scan': ['title', 'vulnerability_ids', 'component_name', 'component_version'],
     'Twistlock Image Scan': ['title', 'severity', 'component_name', 'component_version'],
     'NeuVector (REST)': ['title', 'severity', 'component_name', 'component_version'],
+    'NeuVector (compliance)': ['title', 'vuln_id_from_tool', 'description'],
 }
 
 # This tells if we should accept cwe=0 when computing hash_code with a configurable list of fields from HASHCODE_FIELDS_PER_SCANNER (this setting doesn't apply to legacy algorithm)
@@ -1366,6 +1367,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'docker-bench-security Scan': DEDUPE_ALGO_HASH_CODE,
     'Twistlock Image Scan': DEDUPE_ALGO_HASH_CODE,
     'NeuVector (REST)': DEDUPE_ALGO_HASH_CODE,
+    'NeuVector (compliance)': DEDUPE_ALGO_HASH_CODE,
 }
 
 DUPE_DELETE_MAX_PER_RUN = env('DD_DUPE_DELETE_MAX_PER_RUN')
