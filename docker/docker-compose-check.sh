cb919221844789b316709e10f97fb4db0924e0bf
wzf <1019088083@qq.com>
2022-12-05 19:04:43 +0800

    change script for docker-compose v2

--- docker/docker-compose-check.sh
+++ docker/docker-compose-check.sh
@@ -5,6 +5,11 @@ minor=`docker-compose  version  --short | cut -d '.' -f 2`
 current=`docker-compose  version  --short`
 
 echo 'Checking docker-compose version'
+if [[ $main = v2 ]]; then
+  echo 'Supported docker-compose version'
+  exit 0
+fi
+
 if [[ $main -lt 1 ]]; then
   echo "$current is not supported docker-compose version, please upgrade to minimal supported version:1.28"
   exit 1
