3c2f6a457d2a10c8fcd6a1398038e7c5860516ac
Colm O hEigeartaigh <coheigea@users.noreply.github.com>
2022-12-06 17:39:40 +0000

    Adding API switch close_old_findings_product_scope to allow closing findings of the same type in the entire product (#7219)

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -1514,6 +1514,9 @@ class ImportScanSerializer(serializers.Serializer):
     close_old_findings = serializers.BooleanField(required=False, default=False,
         help_text="Select if old findings no longer present in the report get closed as mitigated when importing. "
                   "If service has been set, only the findings for this service will be closed.")
+    close_old_findings_product_scope = serializers.BooleanField(required=False, default=False,
+        help_text="Select if close_old_findings applies to all findings of the same type in the product. "
+                  "By default, it is false meaning that only old findings of the same type in the engagement are in scope.")
     push_to_jira = serializers.BooleanField(default=False)
     environment = serializers.CharField(required=False)
     version = serializers.CharField(required=False, help_text="Version that was scanned.")
@@ -1543,6 +1546,7 @@ class ImportScanSerializer(serializers.Serializer):
     def save(self, push_to_jira=False):
         data = self.validated_data
         close_old_findings = data.get('close_old_findings')
+        close_old_findings_product_scope = data.get('close_old_findings_product_scope')
         active = data.get('active')
         verified = data.get('verified')
         minimum_severity = data.get('minimum_severity')
@@ -1586,6 +1590,7 @@ class ImportScanSerializer(serializers.Serializer):
                                                                                             commit_hash=commit_hash,
                                                                                             push_to_jira=push_to_jira,
                                                                                             close_old_findings=close_old_findings,
+                                                                                            close_old_findings_product_scope=close_old_findings_product_scope,
                                                                                             group_by=group_by,
                                                                                             api_scan_configuration=api_scan_configuration,
                                                                                             service=service,
@@ -1662,6 +1667,10 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
     # also for ReImport.
     close_old_findings = serializers.BooleanField(required=False, default=True,
                                                   help_text="Select if old findings no longer present in the report get closed as mitigated when importing.")
+    close_old_findings_product_scope = serializers.BooleanField(required=False, default=False,
+        help_text="Select if close_old_findings applies to all findings of the same type in the product. "
+                  "By default, it is false meaning that only old findings of the same type in the engagement are in scope. "
+                  "Note that this only applies on the first call to reimport-scan.")
     version = serializers.CharField(required=False, help_text="Version that will be set on existing Test object. Leave empty to leave existing value in place.")
     build_id = serializers.CharField(required=False, help_text="ID of the build that was scanned.")
     branch_tag = serializers.CharField(required=False, help_text="Branch or Tag that was scanned.")
@@ -1699,6 +1708,7 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
         minimum_severity = data.get('minimum_severity')
         scan_date = data.get('scan_date', None)
         close_old_findings = data.get('close_old_findings')
+        close_old_findings_product_scope = data.get('close_old_findings_product_scope')
         verified = data.get('verified')
         active = data.get('active')
         do_not_reactivate = data.get('do_not_reactivate', False)
@@ -1762,6 +1772,7 @@ class ReImportScanSerializer(TaggitSerializer, serializers.Serializer):
                                                                                                 commit_hash=commit_hash,
                                                                                                 push_to_jira=push_to_jira,
                                                                                                 close_old_findings=close_old_findings,
+                                                                                                close_old_findings_product_scope=close_old_findings_product_scope,
                                                                                                 group_by=group_by,
                                                                                                 api_scan_configuration=api_scan_configuration,
                                                                                                 service=service,
