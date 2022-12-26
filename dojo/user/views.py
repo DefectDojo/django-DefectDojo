7852ab80d66b0a422285aab5600a16e22b0b8ce2
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2022-12-26 12:07:14 -0600

    Fix grammar issue [skip actions]

--- dojo/user/views.py
+++ dojo/user/views.py
@@ -58,7 +58,7 @@ class DojoLoginView(LoginView):
         messages.add_message(
             self.request,
             messages.SUCCESS,
-            _(f'Hello {name}! Your last login was on {naturaltime(last_login)} ({last_login.strftime("%Y-%m-%d %I:%M:%S %p")})'),
+            _(f'Hello {name}! Your last login was {naturaltime(last_login)} ({last_login.strftime("%Y-%m-%d %I:%M:%S %p")})'),
             extra_tags='alert-success')
         return response
 
