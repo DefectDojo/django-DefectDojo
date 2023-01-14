cf7edbdab77e1fa5462934ca0f9bbe7b8a3bcb31
Tomas Kubla <tomas@kubla.sk>
2023-01-14 20:44:00 +0000

    fix missed

--- unittests/test_parsers.py
+++ unittests/test_parsers.py
@@ -11,46 +11,49 @@ class TestParsers(DojoTestCase):
             if parser_dir.is_file() or parser_dir.name == '__pycache__':
                 continue  # this is not parser dir but some support file
 
-            with self.subTest(parser_dir.name):
-                if parser_dir.name.startswith("api_"):
-                    doc_name = parser_dir.name[4:]
-                    category = 'api'
-                else:
-                    doc_name = parser_dir.name
-                    category = 'file'
+            if parser_dir.name.startswith("api_"):
+                doc_name = parser_dir.name[4:]
+                category = 'api'
+            else:
+                doc_name = parser_dir.name
+                category = 'file'
 
-                if doc_name not in [
-                    'checkmarx_osa',  # it is documented in 'checkmarx'
-                ]:
+            if doc_name not in [
+                'checkmarx_osa',  # it is documented in 'checkmarx'
+            ]:
+                with self.subTest(parser=parser_dir.name,category='docs'):
                     doc_file = os.path.join(basedir, 'docs', 'content', 'en', 'integrations', 'parsers', category, f"{doc_name}.md")
                     self.assertTrue(
                         os.path.isfile(doc_file),
                         f"Documentation file '{doc_file}' is missing or using different name"
                     )
 
-                if parser_dir.name not in [
-                    # there is not exception for now
-                ]:
+            if parser_dir.name not in [
+                # there is not exception for now
+            ]:
+                with self.subTest(parser=parser_dir.name,category='parser'):
                     parser_test_file = os.path.join(basedir, 'unittests', 'tools', f"test_{parser_dir.name}_parser.py")
                     self.assertTrue(
                         os.path.isfile(parser_test_file),
                         f"Unittest of parser '{parser_test_file}' is missing or using different name"
                     )
 
-                if parser_dir.name not in [
-                    'vcg',  # content of the sample report is string the directly in unittest
-                ]:
+            if parser_dir.name not in [
+                'vcg',  # content of the sample report is string the directly in unittest
+            ]:
+                with self.subTest(parser=parser_dir.name,category='testfiles'):
                     scan_dir = os.path.join(basedir, 'unittests', 'scans', parser_dir.name)
                     self.assertTrue(
                         os.path.isdir(scan_dir),
                         f"Test files for unittest of parser '{scan_dir}' are missing or using different name"
                     )
 
-                if category == 'api':
-                    if parser_dir.name not in [
-                        'api_blackduck',  # TODO
-                        'api_vulners',  # TODO
-                    ]:
+            if category == 'api':
+                if parser_dir.name not in [
+                    'api_blackduck',  # TODO
+                    'api_vulners',  # TODO
+                ]:
+                    with self.subTest(parser=parser_dir.name,category='importer'):
                         importer_test_file = os.path.join(basedir, 'unittests', 'tools', f"test_{parser_dir.name}_importer.py")
                         self.assertTrue(
                             os.path.isfile(importer_test_file),
