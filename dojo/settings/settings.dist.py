003eaebe917067b676ac59a6beddc1bbd1816de5
kiblik <kiblik@gjh.sk>
2023-01-19 14:19:28 +0000

    Use different approach

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -407,7 +407,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    # e.g.: os.path.join(os.path.dirname(DOJO_ROOT), 'components', 'node_modules'),
+    os.path.join(os.path.dirname(DOJO_ROOT), 'components', 'node_modules'),
 )
 
 # List of finder classes that know how to find static files in
