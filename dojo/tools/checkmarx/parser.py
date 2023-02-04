1a4e5c9d6b1e10c42f8e90c92218480f40fde1f3
damiencarol <damien.carol@gmail.com>
2023-02-04 21:52:15 +0100

    First version

--- dojo/tools/checkmarx/parser.py
+++ dojo/tools/checkmarx/parser.py
@@ -285,37 +285,91 @@ class CheckmarxParser(object):
         else:
             return self._get_findings_xml(file, test)
 
+    def _parse_date(self, value):
+        if isinstance(value, str):
+            return parser.parse(value)
+        elif isinstance(value, dict):
+            return 3
+        else:
+            return None
+
     def _get_findings_json(self, file, test):
         """"""
         data = json.load(file)
         findings = []
         results = data.get("scanResults", [])
         for result_type in results:
-            for language in results[result_type].get("languages"):
-                for query in language.get("queries", []):
-                    descriptiondetails = query.get("description", "")
-                    group = ""
-                    title = query.get("queryName").replace("_", " ")
-                    if query.get('groupName'):
-                        group = query.get('groupName').replace('_', ' ')
-                    for vulnerability in query.get("vulnerabilities", []):
+            # manage sca part
+            if result_type == "sast":
+                for language in results[result_type].get("languages", []):
+                    for query in language.get("queries", []):
+                        descriptiondetails = query.get("description", "")
+                        group = ""
+                        title = query.get("queryName").replace("_", " ")
+                        if query.get('groupName'):
+                            group = query.get('groupName').replace('_', ' ')
+                        for vulnerability in query.get("vulnerabilities", []):
+                            finding = Finding(
+                                description=descriptiondetails,
+                                title=title,
+                                date=self._parse_date(vulnerability.get("firstFoundDate")),
+                                severity=vulnerability.get("severity").title(),
+                                active=(vulnerability.get("status") != "Not exploitable"),
+                                verified=(vulnerability.get("status") != "To verify"),
+                                test=test,
+                                cwe=vulnerability.get("cweId"),
+                                static_finding=True,
+                            )
+                            if vulnerability.get("id"):
+                                finding.unique_id_from_tool = vulnerability.get("id")
+                            # get the last node and set some values
+                            if vulnerability.get('nodes'):
+                                last_node = vulnerability['nodes'][-1]
+                                finding.file_path = last_node.get("fileName")
+                                finding.line = last_node.get("line")
+                            finding.unsaved_tags = [result_type]
+                            findings.append(finding)
+            if result_type == "sca":
+                for package in results[result_type].get("packages", []):
+                    component_name = package.get("name").split("-")[-2]
+                    component_version = package.get("name").split("-")[-1]
+                    for vulnerability in package.get('vulnerabilities', []):
+                        cve = vulnerability.get('cveId')
                         finding = Finding(
-                            description=descriptiondetails,
-                            title=title,
-                            date=parser.parse(vulnerability.get("firstFoundDate")),
+                            title=f'{component_name}:{component_version} | {cve}',
+                            description=vulnerability.get("description"),
+                            date=self._parse_date(vulnerability.get("firstFoundDate")),
                             severity=vulnerability.get("severity").title(),
                             active=(vulnerability.get("status") != "Not exploitable"),
-                            verified=(vulnerability.get("status") != "To verify"),
+                            verified=(vulnerability.get("state") != "To verify"),
+                            component_name=component_name,
+                            component_version=component_version,
                             test=test,
-                            cwe=vulnerability.get("cweId"),
-                            static_finding=(result_type == "sast"),
-                            unique_id_from_tool=vulnerability.get("id"),
+                            cwe=int(vulnerability.get("cwe", 0)),
+                            static_finding=True,
                         )
-                        # get the last node and set some values
-                        if vulnerability.get('nodes'):
-                            last_node = vulnerability['nodes'][-1]
-                            finding.file_path = last_node.get("fileName")
-                            finding.line = last_node.get("line")
-                        finding.unsaved_tags = [result_type]
+                        if vulnerability.get("cveId"):
+                            finding.unsaved_vulnerability_ids = [vulnerability.get("cveId")]
+                        if vulnerability.get("id"):
+                            finding.unique_id_from_tool = vulnerability.get("id")
+                        findings.append(finding)
+            if result_type == "kics":
+                for kics_type in results[result_type].get("results", []):
+                    name = kics_type.get('name')
+                    for vulnerability in kics_type.get('vulnerabilities', []):
+                        finding = Finding(
+                            title=f'{name} | {vulnerability.get("issueType")}',
+                            description=vulnerability.get("description"),
+                            date=self._parse_date(vulnerability.get("firstFoundDate")),
+                            severity=vulnerability.get("severity").title(),
+                            active=(vulnerability.get("status") != "Not exploitable"),
+                            verified=(vulnerability.get("state") != "To verify"),
+                            file_path=vulnerability.get("fileName"),
+                            line=vulnerability.get("line"),
+                            test=test,
+                            static_finding=True,
+                        )
+                        if vulnerability.get("id"):
+                            finding.unique_id_from_tool = vulnerability.get("id")
                         findings.append(finding)
         return findings
