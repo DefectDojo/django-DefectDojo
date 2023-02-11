faff74d49a28915975f2b43b9f2c912b566e4d06
DVSutyagin <dvsutyagin@greenatom.ru>
2023-02-11 21:07:15 +0300

    codechecker - fix parse_json

--- dojo/tools/codechecker/parser.py
+++ dojo/tools/codechecker/parser.py
@@ -26,7 +26,8 @@ class CodeCheckerParser(object):
         if tree:
             return self.get_items(tree)
 
-    def parse_json(self, data):
+    def parse_json(self, json_output):
+        data = json_output.read()
         return json.loads(data)
 
     def get_items(self, tree):
