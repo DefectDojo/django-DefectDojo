d46d4deb531328d5bccbc1f95ba5b20266ef2327
drJabber <dJabber@gmail.com>
2023-02-13 08:30:22 +0300

    Update dojo/tools/codechecker/parser.py
    Co-authored-by: Damien Carol <damien.carol@gmail.com>

--- dojo/tools/codechecker/parser.py
+++ dojo/tools/codechecker/parser.py
@@ -97,7 +97,6 @@ def get_item(vuln):
     finding = Finding(title=title,
                       description=description,
                       severity=severity,
-                      mitigation=mitigation,
                       unique_id_from_tool=unique_id_from_tool,
                       references=references,
                       file_path=file_path,
