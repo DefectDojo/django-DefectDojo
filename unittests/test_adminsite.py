af32ee3b22123673e95fe9288213a21ad72b4b07
Tomas Kubla <tomas@kubla.sk>
2023-03-02 13:59:58 +0000

    Fix some missings, add tagulous, flake8

--- unittests/test_adminsite.py
+++ unittests/test_adminsite.py
@@ -1,12 +1,20 @@
 from .dojo_test_case import DojoTestCase
 from django.contrib import admin
-from django.db import models
+import django.apps
+import tagulous
+
 
 class AdminSite(DojoTestCase):
     fixtures = ['dojo_testdata.json']
 
     def test_is_model_defined(self):
-        for subclass in models.Model.__subclasses__():
+        for subclass in django.apps.apps.get_models():
+            if subclass._meta.proxy:
+                continue
             if subclass.__module__ == 'dojo.models':
-                with self.subTest(subclass=subclass):
-                    self.assertIn(subclass, admin.site._registry.keys(), "{} is not registered in 'admin.site' in models.py".format(subclass))
+                if not ((subclass.__name__[:9] == "Tagulous_") and (subclass.__name__[-5:] != "_tags")):
+                    with self.subTest(type="base", subclass=subclass):
+                        self.assertIn(subclass, admin.site._registry.keys(), "{} is not registered in 'admin.site' in models.py".format(subclass))
+                else:        
+                    with self.subTest(type="tag", subclass=subclass):
+                        self.assertIn(subclass, admin.site._registry.keys(), "{} is not registered in 'tagulous.admin' in models.py".format(subclass))
