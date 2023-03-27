e10ad9991e69e37f52381855bceb25dc34996cdf
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2023-03-27 11:57:22 -0500

    Clean up fixtures

--- dojo/forms.py
+++ dojo/forms.py
@@ -2493,7 +2493,7 @@ class SystemSettingsForm(forms.ModelForm):
 
     class Meta:
         model = System_Settings
-        exclude = ['product_grade', 'credentials', 'column_widths', 'drive_folder_ID']
+        exclude = ['product_grade']
 
 
 class BenchmarkForm(forms.ModelForm):
