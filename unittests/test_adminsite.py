4db25012ab9362846f626ec04397180ac78d3519
kiblik <kiblik@gjh.sk>
2023-03-30 15:58:37 +0000

    Fix condition

--- unittests/test_adminsite.py
+++ unittests/test_adminsite.py
@@ -11,7 +11,7 @@ class AdminSite(DojoTestCase):
             if subclass._meta.proxy:
                 continue
             if subclass.__module__ == 'dojo.models':
-                if not ((subclass.__name__[:9] == "Tagulous_") and (subclass.__name__[-5:] != "_tags")):
+                if not ((subclass.__name__[:9] == "Tagulous_") and (subclass.__name__[-5:] == "_tags")):
                     with self.subTest(type="base", subclass=subclass):
                         self.assertIn(subclass, admin.site._registry.keys(), "{} is not registered in 'admin.site' in models.py".format(subclass))
                 else:
