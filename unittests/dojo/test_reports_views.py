b02166a9560b87714913032b726bfbc962c02740
root <root@SANTIAGO-BUITRAGO>
2023-04-13 18:45:45 -0500

    test_reports_views corrected

--- unittests/dojo/test_reports_views.py
+++ unittests/dojo/test_reports_views.py
@@ -1,20 +1,25 @@
+
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dojo.settings.settings')
-
 import django
 django.setup()
 
-import unittest
-from django.test import RequestFactory
-from dojo.reports.views import down
-from django.contrib.auth.models import User
-from django.test import TestCase
 
-class TestDownView(TestCase):
-    
+from dojo.reports.views import down
+from django.test import RequestFactory
+from django.contrib.auth.models import User
+import unittest
+import unittest.mock
+
+
+
+django.setup()
+
+
+class TestDownView(unittest.TestCase):
     def setUp(self):
         self.factory = RequestFactory()
-    
+
     def test_down_view(self):
         request = self.factory.get('/dojo/reports/views/down/')
         username = 'testuser'
@@ -22,5 +27,4 @@ class TestDownView(TestCase):
         request.user = user
         response = down(request)
         self.assertEqual(response.status_code, 200)
-        #self.assertContains(response, 'disabled.html')
         # Add more assertions as necessary
