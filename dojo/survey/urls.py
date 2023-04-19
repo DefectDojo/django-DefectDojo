56379e0f781a57b8916f70f156b87d50be816c50
renejal <beimarjal@unicauca.edu.co>
2023-04-19 16:19:45 -0500

    fix pep8 of guideline in dojo/survey/

--- dojo/survey/urls.py
+++ dojo/survey/urls.py
@@ -1,78 +1,102 @@
-'''
+"""
 Created on Feb 18, 2015
 
 @author: jay7958
-'''
+"""
 from django.urls import re_path
 from django.contrib import admin
 from django.apps import apps
 from dojo.survey import views
+
 if not apps.ready:
     apps.get_models()
 
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r'^questionnaire$',
-        views.questionnaire,
-        name='questionnaire'),
-    re_path(r'^questionnaire/create$',
+    re_path(r"^questionnaire$", views.questionnaire, name="questionnaire"),
+    re_path(
+        r"^questionnaire/create$",
         views.create_questionnaire,
-        name='create_questionnaire'),
-    re_path(r'^questionnaire/(?P<sid>\d+)/edit$',
+        name="create_questionnaire",
+    ),
+    re_path(
+        r"^questionnaire/(?P<sid>\d+)/edit$",
         views.edit_questionnaire,
-        name='edit_questionnaire'),
-    re_path(r'^questionnaire/(?P<sid>\d+)/delete',
+        name="edit_questionnaire",
+    ),
+    re_path(
+        r"^questionnaire/(?P<sid>\d+)/delete",
         views.delete_questionnaire,
-        name='delete_questionnaire'),
-    re_path(r'^questionnaire/(?P<sid>\d+)/edit/questions$',
+        name="delete_questionnaire",
+    ),
+    re_path(
+        r"^questionnaire/(?P<sid>\d+)/edit/questions$",
         views.edit_questionnaire_questions,
-        name='edit_questionnaire_questions'),
-    re_path(r'^questions$',
-        views.questions,
-        name='questions'),
-    re_path(r'^questions/add$',
-        views.create_question,
-        name='create_question'),
-    re_path(r'^questions/(?P<qid>\d+)/edit$',
+        name="edit_questionnaire_questions",
+    ),
+    re_path(r"^questions$", views.questions, name="questions"),
+    re_path(r"^questions/add$", views.create_question, name="create_question"),
+    re_path(
+        r"^questions/(?P<qid>\d+)/edit$",
         views.edit_question,
-        name='edit_question'),
-    re_path(r'^choices/add$',
-        views.add_choices,
-        name='add_choices'),
-    re_path(r'^engagement/(?P<eid>\d+)/add_questionnaire$',
+        name="edit_question",
+    ),
+    re_path(r"^choices/add$", views.add_choices, name="add_choices"),
+    re_path(
+        r"^engagement/(?P<eid>\d+)/add_questionnaire$",
         views.add_questionnaire,
-        name='add_questionnaire'),
-    re_path(r'^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/answer',
+        name="add_questionnaire",
+    ),
+    re_path(
+        r"^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/answer",
         views.answer_questionnaire,
-        name='answer_questionnaire'),
-    re_path(r'^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/delete',
+        name="answer_questionnaire",
+    ),
+    re_path(
+        r"^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/delete",
         views.delete_engagement_survey,
-        name='delete_engagement_survey'),
-    re_path(r'^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)$',
+        name="delete_engagement_survey",
+    ),
+    re_path(
+        r"^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)$",
         views.view_questionnaire,
-        name='view_questionnaire'),
-    re_path(r'^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/assign',
+        name="view_questionnaire",
+    ),
+    re_path(
+        r"^engagement/(?P<eid>\d+)/questionnaire/(?P<sid>\d+)/assign",
         views.assign_questionnaire,
-        name='assign_questionnaire'),
-
+        name="assign_questionnaire",
+    ),
     # Questionnaires without an engagemnet
-    re_path(r'^empty_questionnaire$',
+    re_path(
+        r"^empty_questionnaire$",
         views.add_empty_questionnaire,
-        name='add_empty_questionnaire'),
-    re_path(r'^empty_questionnaire/(?P<esid>\d+)$',
+        name="add_empty_questionnaire",
+    ),
+    re_path(
+        r"^empty_questionnaire/(?P<esid>\d+)$",
         views.view_empty_survey,
-        name='view_empty_survey'),
-    re_path(r'^empty_questionnaire/(?P<esid>\d+)/delete$',
+        name="view_empty_survey",
+    ),
+    re_path(
+        r"^empty_questionnaire/(?P<esid>\d+)/delete$",
         views.delete_empty_questionnaire,
-        name='delete_empty_questionnaire'),
-    re_path(r'^general_questionnaire/(?P<esid>\d+)/delete$',
+        name="delete_empty_questionnaire",
+    ),
+    re_path(
+        r"^general_questionnaire/(?P<esid>\d+)/delete$",
         views.delete_general_questionnaire,
-        name='delete_general_questionnaire'),
-    re_path(r'^empty_questionnaire/(?P<esid>\d+)/answer$',
+        name="delete_general_questionnaire",
+    ),
+    re_path(
+        r"^empty_questionnaire/(?P<esid>\d+)/answer$",
         views.answer_empty_survey,
-        name='answer_empty_survey'),
-    re_path(r'^empty_questionnaire/(?P<esid>\d+)/new_engagement$',
+        name="answer_empty_survey",
+    ),
+    re_path(
+        r"^empty_questionnaire/(?P<esid>\d+)/new_engagement$",
         views.engagement_empty_survey,
-        name='engagement_empty_survey'),
+        name="engagement_empty_survey",
+    ),
 ]
