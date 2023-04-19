9351ee98be6a85173b813e731857280e5763030c
renejal <beimarjal@unicauca.edu.co>
2023-04-19 10:24:20 -0500

    fix pep8 of guidelines in dojo/github_issue_link/ and disable hook-of-file-fixer

--- dojo/github_issue_link/urls.py
+++ dojo/github_issue_link/urls.py
@@ -2,8 +2,12 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^github-webhook', views.webhook, name='github_web_hook'),
-    re_path(r'^github/add', views.new_github, name='add_github'),
-    re_path(r'^github/(?P<tid>\d+)/delete$', views.delete_github,
-        name='delete_github'),
-    re_path(r'^github$', views.github, name='github')]
+    re_path(r"^github-webhook", views.webhook, name="github_web_hook"),
+    re_path(r"^github/add", views.new_github, name="add_github"),
+    re_path(
+        r"^github/(?P<tid>\d+)/delete$",
+        views.delete_github,
+        name="delete_github",
+    ),
+    re_path(r"^github$", views.github, name="github"),
+]
--- dojo/github_issue_link/views.py
+++ dojo/github_issue_link/views.py
@@ -15,85 +15,114 @@ from github import Github
 from dojo.forms import GITHUBForm, DeleteGITHUBConfForm
 from dojo.models import GITHUB_Conf
 from dojo.utils import add_breadcrumb
-from dojo.authorization.authorization_decorators import user_is_configuration_authorized
+from dojo.authorization.authorization_decorators import (
+    user_is_configuration_authorized,
+)
 
 logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
 def webhook(request):
-    return HttpResponse('')
+    return HttpResponse("")
 
 
-@user_is_configuration_authorized('dojo.add_github_conf')
+@user_is_configuration_authorized("dojo.add_github_conf")
 def new_github(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         gform = GITHUBForm(request.POST, instance=GITHUB_Conf())
         if gform.is_valid():
             try:
-                api_key = gform.cleaned_data.get('api_key')
+                api_key = gform.cleaned_data.get("api_key")
                 g = Github(api_key)
                 user = g.get_user()
-                logger.debug('Using user ' + user.login)
+                logger.debug("Using user " + user.login)
 
                 new_j = gform.save(commit=False)
                 new_j.api_key = api_key
                 new_j.save()
-                messages.add_message(request,
-                                     messages.SUCCESS,
-                                     'GitHub Configuration Successfully Created.',
-                                     extra_tags='alert-success')
-                return HttpResponseRedirect(reverse('github', ))
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    "GitHub Configuration Successfully Created.",
+                    extra_tags="alert-success",
+                )
+                return HttpResponseRedirect(
+                    reverse(
+                        "github",
+                    )
+                )
             except Exception as info:
                 logger.error(info)
-                messages.add_message(request,
-                                     messages.ERROR,
-                                     'Unable to authenticate on GitHub.',
-                                     extra_tags='alert-danger')
-                return HttpResponseRedirect(reverse('github', ))
+                messages.add_message(
+                    request,
+                    messages.ERROR,
+                    "Unable to authenticate on GitHub.",
+                    extra_tags="alert-danger",
+                )
+                return HttpResponseRedirect(
+                    reverse(
+                        "github",
+                    )
+                )
     else:
         gform = GITHUBForm()
-        add_breadcrumb(title="New GitHub Configuration", top_level=False, request=request)
-        return render(request, 'dojo/new_github.html',
-                    {'gform': gform})
+        add_breadcrumb(
+            title="New GitHub Configuration", top_level=False, request=request
+        )
+        return render(request, "dojo/new_github.html", {"gform": gform})
 
 
-@user_is_configuration_authorized('dojo.view_github_conf')
+@user_is_configuration_authorized("dojo.view_github_conf")
 def github(request):
     confs = GITHUB_Conf.objects.all()
-    add_breadcrumb(title="GitHub List", top_level=not len(request.GET), request=request)
-    return render(request,
-                  'dojo/github.html',
-                  {'confs': confs,
-                   })
+    add_breadcrumb(
+        title="GitHub List", top_level=not len(request.GET), request=request
+    )
+    return render(
+        request,
+        "dojo/github.html",
+        {
+            "confs": confs,
+        },
+    )
 
 
-@user_is_configuration_authorized('dojo.delete_github_conf')
+@user_is_configuration_authorized("dojo.delete_github_conf")
 def delete_github(request, tid):
     github_instance = get_object_or_404(GITHUB_Conf, pk=tid)
     # eng = test.engagement
     # TODO Make Form
     form = DeleteGITHUBConfForm(instance=github_instance)
 
-    if request.method == 'POST':
-        if 'id' in request.POST and str(github_instance.id) == request.POST['id']:
+    if request.method == "POST":
+        if (
+            "id" in request.POST
+            and str(github_instance.id) == request.POST["id"]
+        ):
             form = DeleteGITHUBConfForm(request.POST, instance=github_instance)
             if form.is_valid():
                 github_instance.delete()
-                messages.add_message(request,
-                                     messages.SUCCESS,
-                                     'GitHub Conf and relationships removed.',
-                                     extra_tags='alert-success')
-                return HttpResponseRedirect(reverse('github'))
+                messages.add_message(
+                    request,
+                    messages.SUCCESS,
+                    "GitHub Conf and relationships removed.",
+                    extra_tags="alert-success",
+                )
+                return HttpResponseRedirect(reverse("github"))
 
     collector = NestedObjects(using=DEFAULT_DB_ALIAS)
     collector.collect([github_instance])
     rels = collector.nested()
 
     add_breadcrumb(title="Delete", top_level=False, request=request)
-    return render(request, 'dojo/delete_github.html',
-                  {'inst': github_instance,
-                   'form': form,
-                   'rels': rels,
-                   'deletable_objects': rels,
-                   })
+    return render(
+        request,
+        "dojo/delete_github.html",
+        {
+            "inst": github_instance,
+            "form": form,
+            "rels": rels,
+            "deletable_objects": rels,
+        },
+    )
