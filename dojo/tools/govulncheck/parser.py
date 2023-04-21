a16369aac13b42b3f1580c77442b85ef9dc84669
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2023-04-21 18:44:24 -0500

    Add dedupe settings

--- dojo/tools/govulncheck/parser.py
+++ dojo/tools/govulncheck/parser.py
@@ -42,15 +42,17 @@ class GovulncheckParser:
                 list_vulns = data['Vulns']
                 for cve, elems in groupby(list_vulns, key=lambda vuln: vuln['OSV']['aliases'][0]):
                     try:
-                        d = dict()
                         first_elem = list(islice(elems, 1))
-                        d['cve'] = cve
-                        d['severity'] = SEVERITY
-                        d['title'] = first_elem[0]['OSV']['id']
-                        d['component_name'] = first_elem[0]['OSV']['affected'][0]['package']['name']
-                        d['component_version'] = self.get_version(data, first_elem[0]['RequireSink'])
+                        d = {
+                            'cve': cve,
+                            'severity': SEVERITY,
+                            'title': first_elem[0]['OSV']['id'],
+                            'component_name': first_elem[0]['OSV']['affected'][0]['package']['name'],
+                            'component_version': self.get_version(data, first_elem[0]['RequireSink']),
+                        }
                         d['references'] = first_elem[0]['OSV']['references'][0]['url']
                         d['url'] = first_elem[0]['OSV']['affected'][0]['database_specific']['url']
+                        d['unique_id_from_tool'] = first_elem[0]['OSV']['id']
                         vuln_methods = set(
                             first_elem[0]['OSV']['affected'][0]['ecosystem_specific']['imports'][0]['symbols'])
                         impact = set(self.get_location(data, first_elem[0]['CallSink']))
