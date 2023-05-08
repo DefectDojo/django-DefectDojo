78f10aa72d58c52a066022b1f190dccf675f14d0
Alejandro Tortolero <ajtortolero@outlook.com>
2023-05-08 05:22:39 -0500

    Update files in folder dojo/tools/anchorectl_policies with PEP8 standars.

--- dojo/tools/anchorectl_policies/parser.py
+++ dojo/tools/anchorectl_policies/parser.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 
 class AnchoreCTLPoliciesParser:
-
     def get_scan_types(self):
         return ["AnchoreCTL Policies Report"]
 
@@ -23,7 +22,7 @@ class AnchoreCTLPoliciesParser:
     def get_findings(self, filename, test):
         content = filename.read()
         try:
-            data = json.loads(str(content, 'utf-8'))
+            data = json.loads(str(content, "utf-8"))
         except (JSONDecodeError, TypeError):
             data = json.loads(content)
 
@@ -31,18 +30,18 @@ class AnchoreCTLPoliciesParser:
         items = list()
         try:
             for image in data:
-                for result in image['detail']:
+                for result in image["detail"]:
                     try:
-                        gate = result['gate']
-                        description = result['description']
-                        policy_id = result['policyId']
-                        status = result['status']
-                        image_name = result['tag']
-                        trigger_id = result['triggerId']
-                        repo, tag = image_name.split(':', 2)
+                        gate = result["gate"]
+                        description = result["description"]
+                        policy_id = result["policyId"]
+                        status = result["status"]
+                        image_name = result["tag"]
+                        trigger_id = result["triggerId"]
+                        repo, tag = image_name.split(":", 2)
                         severity = map_gate_action_to_severity(status)
                         vulnerability_id = extract_vulnerability_id(trigger_id)
-                        title = policy_id + ' - gate|' + gate + ' - trigger|' + trigger_id
+                        title = policy_id + " - gate|" + gate + " - trigger|" + trigger_id
                         find = Finding(
                             title=title,
                             test=test,
@@ -54,7 +53,8 @@ class AnchoreCTLPoliciesParser:
                             component_version=tag,
                             date=find_date,
                             static_finding=True,
-                            dynamic_finding=False)
+                            dynamic_finding=False,
+                        )
                         if vulnerability_id:
                             find.unsaved_vulnerability_ids = [vulnerability_id]
                         items.append(find)
@@ -62,31 +62,31 @@ class AnchoreCTLPoliciesParser:
                         raise ValueError("Invalid format: {} key not found".format(err))
         except AttributeError as err:
             # import empty policies without error (e.g. policies or images objects are not a dictionary)
-            logger.warning('Exception at %s', 'parsing anchore policy', exc_info=err)
+            logger.warning("Exception at %s", "parsing anchore policy", exc_info=err)
         return items
 
 
 def map_gate_action_to_severity(gate):
     gate_action_to_severity = {
-        'stop': 'Critical',
-        'warn': 'Medium',
+        "stop": "Critical",
+        "warn": "Medium",
     }
     if gate in gate_action_to_severity:
         return gate_action_to_severity[gate]
-    return 'Low'
+    return "Low"
 
 
 def policy_name(policies, policy_id):
     for policy in policies:
-        if policy_id == policy['id']:
-            return policy['name']
+        if policy_id == policy["id"]:
+            return policy["name"]
     return "unknown"
 
 
 def extract_vulnerability_id(trigger_id):
     try:
-        vulnerability_id, _ = trigger_id.split('+', 2)
-        if vulnerability_id.startswith('CVE'):
+        vulnerability_id, _ = trigger_id.split("+", 2)
+        if vulnerability_id.startswith("CVE"):
             return vulnerability_id
         return None
     except ValueError:
@@ -94,7 +94,7 @@ def extract_vulnerability_id(trigger_id):
 
 
 def search_filepath(text):
-    match = re.search(r' (/[^/ ]*)+/?', text)
+    match = re.search(r" (/[^/ ]*)+/?", text)
     path = ""
     if match:
         try:
