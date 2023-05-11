7d3eeb3a1e9c1801e0dfba8bf5a4c7b1455e904e
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2023-05-11 17:12:22 -0500

    Update All GitLab parsers to accomodate v15 Schema

--- dojo/tools/gitlab_container_scan/parser.py
+++ dojo/tools/gitlab_container_scan/parser.py
@@ -20,6 +20,42 @@ class GitlabContainerScanParser(object):
     def get_description_for_scan_types(self, scan_type):
         return "GitLab Container Scan report file can be imported in JSON format (option --json)."
 
+    def _get_dependency_version(self, dependency):
+        return dependency["version"] if "version" in dependency else ""
+    
+    def _get_dependency_name(self, dependency):
+        if "package" in dependency and "name" in dependency["package"]:
+            return dependency["package"]["name"]
+        return ""
+    
+    def _get_identifier_cve(self, identifier):
+        return identifier["value"] if identifier.get("type", "no-type") == "cve" else None
+
+    def _get_identifier_cwe(self, identifier):
+        return identifier["value"] if identifier.get("type", "no-type") == "cwe" else None
+
+    def _get_first_cve(self, identifiers):
+        cwe = ""
+        # Find the first match of a cve
+        for identifier in identifiers:
+            cve = self._get_identifier_cve(identifier)
+            # query the cwe as a backup
+            cwe = self._get_identifier_cwe(identifier)
+            if cve:
+                return cve
+        # Looks like a cve was not found, try to use the cwe, otherwise "fail"
+        return f"CWE-{cwe}" if cwe else None
+
+    def _get_package_string(self, dependency):
+        dependency_name = self._get_dependency_name(dependency)
+        dependency_version = self._get_dependency_version(dependency)
+        if dependency_name:
+            if dependency_version:
+                return f"{dependency_name}-{dependency_version}"
+            return dependency_name
+        # check if name is missing, but at least version is here
+        return f"unknown-package-{dependency_version}" if dependency_version else None
+
     def get_findings(self, file, test):
         findings = []
         data = json.load(file)
@@ -31,10 +67,16 @@ class GitlabContainerScanParser(object):
         # Vulnerabilities is stored on vulnerabilities key
         vulnerabilities = data["vulnerabilities"]
         for vulnerability in vulnerabilities:
-            title = vulnerability["message"]
+            title = vulnerability.get("message")
+            dependency = vulnerability["location"]["dependency"]
+            identifiers = vulnerability["identifiers"]
+            # In new versiona, the message field is no longer in report, so build the title from other parts
+            if not title:
+                issue_string = self._get_first_cve(identifiers)
+                location_string = self._get_package_string(dependency)
+                title = f"{issue_string} in {location_string}"
             description = vulnerability["description"]
             severity = self.normalise_severity(vulnerability["severity"])
-            dependency = vulnerability["location"]["dependency"]
             finding = Finding(
                 title=title,
                 date=date,
@@ -47,25 +89,25 @@ class GitlabContainerScanParser(object):
             )
 
             # Add component fields if not empty
-            unsaved_vulnerability_ids = list()
-            for id in vulnerability["identifiers"]:
-                if "type" in id:
-                    if id.get("type") == "cve":
-                        unsaved_vulnerability_ids.append(id["value"])
-                    if id.get("type") == "cwe":
-                        finding.cwe = id["value"]
+            unsaved_vulnerability_ids = []
+            for identifier in identifiers:
+                cve = self._get_identifier_cve(identifier)
+                if cve:
+                    unsaved_vulnerability_ids.append(cve)
+                cwe = self._get_identifier_cwe(identifier)
+                if cwe:
+                    finding.cwe = cwe
             if unsaved_vulnerability_ids:
                 finding.unsaved_vulnerability_ids = unsaved_vulnerability_ids
 
             # Check package key before name as both is optional on GitLab schema
-            if "package" in dependency:
-                if "name" in dependency["package"]:
-                    finding.component_name = textwrap.shorten(
-                        dependency["package"]["name"], width=190, placeholder="..."
-                    )
+            dependency_name = self._get_dependency_name(dependency)
+            if dependency_name:
+                finding.component_name = textwrap.shorten(dependency_name, width=190, placeholder="...")
 
-            if "version" in dependency:
-                finding.component_version = textwrap.shorten(dependency["version"], width=90, placeholder="...")
+            dependency_version = self._get_dependency_version(dependency)
+            if dependency_version:
+                finding.component_version = textwrap.shorten(dependency_version, width=90, placeholder="...")
 
             if "solution" in vulnerability:
                 finding.mitigation = vulnerability["solution"]
@@ -79,6 +121,4 @@ class GitlabContainerScanParser(object):
         Normalise GitLab's severity to DefectDojo's
         (Critical, High, Medium, Low, Unknown, Info) -> (Critical, High, Medium, Low, Info)
         """
-        if severity == "Unknown":
-            return "Info"
-        return severity
+        return "Info" if severity == "Unknown" else severity
--- unittests/tools/test_gitlab_dast_parser.py
+++ unittests/tools/test_gitlab_dast_parser.py
@@ -10,8 +10,8 @@ class TestGitlabDastParser(DojoTestCase):
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(0, len(findings))
 
-    def test_parse_file_with_one_vuln_has_one_finding(self):
-        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_one_vul.json")
+    def test_parse_file_with_one_vuln_has_one_finding_v14(self):
+        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_one_vul_v14.json")
         parser = GitlabDastParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(1, len(findings))
@@ -38,8 +38,36 @@ class TestGitlabDastParser(DojoTestCase):
 
         self.assertEqual(359, finding.cwe)
 
-    def test_parse_file_with_multiple_vuln_has_multiple_findings(self):
-        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_many_vul.json")
+    def test_parse_file_with_one_vuln_has_one_finding_v15(self):
+        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_one_vul_v15.json")
+        parser = GitlabDastParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
+        finding = findings[0]
+
+        # endpoint validation
+        for endpoint in finding.unsaved_endpoints:
+            endpoint.clean()
+
+        self.assertEqual(
+            "5ec00bbc-2e53-44cb-83e9-3d35365277e3", finding.unique_id_from_tool
+        )
+        self.assertEqual(None, finding.scanner_confidence)
+        # vulnerability does not have a name: fallback to using id as a title
+        self.assertEqual("5ec00bbc-2e53-44cb-83e9-3d35365277e3", finding.title)
+        self.assertIsInstance(finding.description, str)
+
+        date = finding.date.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual("2021-04-23T15:46:40.615000", date)
+        self.assertIsNone(finding.references)  # should be None as there are no links
+
+        self.assertEqual("High", finding.severity)
+        self.assertEqual("", finding.mitigation)  # no solution proposed
+
+        self.assertEqual(359, finding.cwe)
+
+    def test_parse_file_with_multiple_vuln_has_multiple_findings_v14(self):
+        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_many_vul_v14.json")
         parser = GitlabDastParser()
         findings = parser.get_findings(testfile, Test())
 
@@ -54,6 +82,44 @@ class TestGitlabDastParser(DojoTestCase):
         finding = findings[1]
         # must-have fields
         self.assertEqual(3, finding.scanner_confidence)
+        self.assertTrue("Content Security Policy (CSP)" in finding.description)
+        self.assertEqual(False, finding.static_finding)
+        self.assertEqual(True, finding.dynamic_finding)
+
+        # conditional fields
+        date = finding.date.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual("2021-04-23T15:46:40.644000", date)
+        self.assertEqual(
+            "87e98ddf-7d75-444a-be6d-45400151a0fe", finding.unique_id_from_tool
+        )
+        # vulnerability does not have a name: fallback to using id as a title
+        self.assertEqual(finding.unique_id_from_tool, finding.title)
+        self.assertEqual(16, finding.cwe)
+        self.assertTrue("http://www.w3.org/TR/CSP/" in finding.references)
+        self.assertEqual("Medium", finding.severity)
+        endpoint = finding.unsaved_endpoints[0]
+        self.assertEqual(str(endpoint), "http://api-server/v1/tree/10")
+        self.assertEqual(endpoint.host, "api-server")  # host port path
+        self.assertEqual(endpoint.port, 80)
+        self.assertEqual(endpoint.path, "v1/tree/10")
+        self.assertTrue("Ensure that your web server," in finding.mitigation)
+
+    def test_parse_file_with_multiple_vuln_has_multiple_findings_v15(self):
+        testfile = open("unittests/scans/gitlab_dast/gitlab_dast_many_vul_v15.json")
+        parser = GitlabDastParser()
+        findings = parser.get_findings(testfile, Test())
+
+        self.assertEqual(10, len(findings))
+
+        # endpoint validation
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
+
+        # the first one is done above
+        finding = findings[1]
+        # must-have fields
+        self.assertEqual(None, finding.scanner_confidence)
         self.assertTrue("Content Security Policy (CSP)" in finding.description)
         self.assertEqual(False, finding.static_finding)
         self.assertEqual(True, finding.dynamic_finding)
