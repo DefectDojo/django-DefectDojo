d0feba3687ff9cb6f08da3acefdee075d15acdad
root <root@SANTIAGO-BUITRAGO>
2023-05-29 01:39:38 -0500

    test_reports_views added

---
+++ unittests/test_reports_views.py
@@ -0,0 +1,53 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dojo.settings.settings')
+import django
+django.setup()
+
+import unittest
+from unittest.mock import MagicMock, patch
+from django.test import RequestFactory
+from django.contrib.auth.models import AnonymousUser
+from django.http import HttpResponse
+from dojo.reports import views
+
+
+class ReportsViewsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_down(self):
+        request = self.factory.get('/down')
+        request.user = AnonymousUser()
+
+        with patch('dojo.reports.views.render') as mock_render:
+            mock_render.return_value = HttpResponse(status=200)
+            response = views.down(request)
+            mock_render.assert_called_with(request, 'disabled.html')
+            self.assertEqual(response.status_code, 200)
+
+    def test_report_url_resolver_with_forwarded_headers(self):
+        request = self.factory.get('/')
+        request.META = {
+            'HTTP_X_FORWARDED_PROTO': 'https',
+            'HTTP_X_FORWARDED_FOR': 'example.com',
+            'SERVER_PORT': '8000'
+        }
+        expected_url_resolver = 'https://example.com:8000'
+        url_resolver = views.report_url_resolver(request)
+        self.assertEqual(url_resolver, expected_url_resolver)
+
+    def test_report_url_resolver_without_forwarded_headers(self):
+        request = self.factory.get('/')
+        request.META = {
+            'HTTP_HOST': 'example.com:8000',
+            'SERVER_PORT': '8000',
+            'scheme': 'http'
+        }
+        expected_url_resolver = 'http://example.com:8000'
+        url_resolver = views.report_url_resolver(request)
+        self.assertEqual(url_resolver, expected_url_resolver)
+
+
+if __name__ == '__main__':
+    unittest.main()
