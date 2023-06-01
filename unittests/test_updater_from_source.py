20fb197124cc8978d2c5eb46cf752ef304b0f56f
root <root@SANTIAGO-BUITRAGO>
2023-05-31 22:54:40 -0500

    test_updater_from_source added

--- unittests/test_updater_from_source.py
+++ unittests/test_updater_from_source.py
@@ -1,131 +1,48 @@
-import django
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dojo.settings.settings')
+import django
 django.setup()
 
-from dojo.tools.api_sonarqube.updater_from_source import *
-from dojo.risk_acceptance.helper import remove_finding_from_risk_acceptance
-from dojo.models import User, Finding, Test, Sonarqube_Issue, Development_Environment, Engagement, Product, Product_Type, Test_Type, Dojo_User
-from django.utils import timezone
-from unittest.mock import MagicMock
 import unittest
 from unittest.mock import patch
+from django.test import TestCase
+from django.utils import timezone
+from dojo.models import Finding, Sonarqube_Issue
+from dojo.tools.api_sonarqube.updater_from_source import *
+from unittest.mock import MagicMock
 
 
-
-class TestSonarQubeApiUpdaterFromSource(unittest.TestCase):
+class TestSonarQubeApiUpdater(TestCase):
 
     def setUp(self):
-        self.updater = SonarQubeApiUpdaterFromSource()
+        self.sonarqube_issue = Sonarqube_Issue(key="ABC123", status="Open", type="Bug")
+        self.finding1 = Finding(active=True, sonarqube_issue=self.sonarqube_issue)
+        self.finding2 = Finding(active=True, sonarqube_issue=None)
+        self.finding3 = Finding(active=False, sonarqube_issue=self.sonarqube_issue)
+        self.finding4 = Finding(active=True, sonarqube_issue=self.sonarqube_issue)
 
-        self.development_environment = Development_Environment(name="Production")
-        self.development_environment.save()
-        product_type, _ = Product_Type.objects.get_or_create(name="Product Type")
+        self.finding = MagicMock()
+        self.finding.sonarqube_issue = MagicMock()
+        self.finding.test = MagicMock()
 
-        # Obtain or create an instance of Dojo_User
-        
-        self.user, _ = User.objects.get_or_create(username="User 1")
     
 
-        # Create a new instance of Product and assign the product_manager
-        self.product = Product.objects.create(
-            name="Product_",
-            prod_type=product_type,
-        )
 
-        
+    def test_get_findings_to_update(self):
+        findings = [self.finding1, self.finding2, self.finding3, self.finding4]
 
-        self.product.save()
+        mock_query = MagicMock()
+        mock_query.select_related.return_value = findings
 
-        self.engagement = Engagement(
-            name="Engagement 1",
-            target_start=timezone.now(),
-            target_end=timezone.now(),
-            description="Engagement description",
-            product=self.product,
-        )
-        self.engagement.save()
-        self.test_type, _ = Test_Type.objects.get_or_create(name="Test type")
+        with patch.object(Finding.objects, 'filter', return_value=mock_query) as mock_filter:
+            updated_findings = SonarQubeApiUpdaterFromSource.get_findings_to_update()
 
-        self.test = Test(
-            title="Test 1",
-            target_start=timezone.now(),
-            target_end=timezone.now(),
-            environment=self.development_environment,
-            engagement=self.engagement,
-            test_type=self.test_type,
-        )
-        self.test.save()
-
-        self.sonarqube_issue, _ = Sonarqube_Issue.objects.get_or_create(
-            key="test_key",
-            status="Open",
-            type="Vulnerability",
-        )
-        self.sonarqube_issue.save()
-
-        self.finding = Finding(
-            test=self.test,
-            sonarqube_issue=self.sonarqube_issue,
-            active=True,
-            verified=True,
-            false_p=False,
-            mitigated=None,
-            is_mitigated=False,
-            reporter=self.user
-        )
-        self.finding.save()
-
-    def tearDown(self):
-        self.finding.delete()
-        self.sonarqube_issue.delete()
-        self.test.delete()
-        self.engagement.delete()
-        self.development_environment.delete()
-        self.product.delete()
-
-    """def test_get_findings_to_update(self):
-        self.assertEqual(len(self.updater.get_findings_to_update()), 1)
-
-        finding_no_issue = Finding(
-            test=self.test,
-            active=True,
-            verified=True,
-            false_p=False,
-            mitigated=None,
-            is_mitigated=False,
-            reporter=self.user
-        )
-        finding_no_issue.save()
-
-        self.assertEqual(len(self.updater.get_findings_to_update()), 1)"""
-
-    """
-    @patch('dojo.tools.api_sonarqube.updater_from_source.SonarQubeApiImporter.prepare_client')
-    
-    def test_update(self, mock_prepare_client):
-        # Mock data
-        sonarqube_issue = Sonarqube_Issue(key='ABC-123')
-        self.finding.sonarqube_issue = sonarqube_issue
-        issue = {'resolution': 'FIXED'}
-
-        # Mock the prepare_client method
-        mock_client = mock_prepare_client.return_value
-        mock_client.get_issue.return_value = issue
-
-        # Configure the return value of mock_prepare_client
-        mock_prepare_client.return_value = (mock_client, "some_other_value")
-
-        # Create an instance of SonarQubeApiUpdaterFromSource
-        updater = SonarQubeApiUpdaterFromSource()
-
-        # Call the update method
-        updater.update(self.finding)
-
-        # Assert that the update_finding_status method is called with the correct arguments
-        mock_client.get_issue.assert_called_once_with('ABC-123')
-        mock_client.update_finding_status.assert_called_once_with(self.finding, 'FIXED')
-    """
+            mock_filter.assert_called_with(
+                sonarqube_issue__isnull=False,
+                active=True,
+            )
+            mock_query.select_related.assert_called_with('sonarqube_issue')
+            self.assertEqual(updated_findings, findings)
 
 
 
