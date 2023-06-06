c04dcf9b8f0f158427bf6e5738a6f6efe92b13f9
renejal <beimarjal@unicauca.edu.co>
2023-06-06 01:30:10 -0500

    add extended_information

--- dojo/tools/jfrog_xray/parser.py
+++ dojo/tools/jfrog_xray/parser.py
@@ -63,18 +63,66 @@ def get_servery(vulnerability):
     return severity
 
 
-def get_mitigacion_and_component_key(vulnerability):
+def get_references(vulnerability):
+    ref = ""
+    if "references" in vulnerability:
+        references = vulnerability["references"]
+        for reference in references:
+            ref += reference + "\n"
+    return ref
+
+
+def get_remediation(extended_information):
+    remediation = ""
+    if "remediation" in extended_information:
+        remediation = "\n**Remediation**\n"
+        remediation += extended_information["remediation"] + "\n"
+    return remediation
+       
+        
+def get_severity_justification(vulnerability):
+    severity_desc = ""
+    remediation = ""
+    extended_information = vulnerability.get("extended_information")
+    if extended_information:
+        remediation += get_remediation(extended_information)
+        if "short_description" in extended_information:
+            severity_desc = "**short description**\n"
+            severity_desc += extended_information["short_description"] + "\n"
+            severity_desc = "**full description**\n"
+            severity_desc += extended_information["full_description"] + "\n"
+            severity_desc = "**jfrog research severity**\n"
+            severity_desc += extended_information["jfrog_research_severity"] + "\n"
+            if "jfrog_research_severity_reasons" in extended_information:
+                severity_desc = "**jfrog research severity reasons**\n"
+                for item in extended_information["jfrog_research_severity_reasons"]:
+                    severity_desc += item["name"] + "\n" if item.get("name") else ""
+                    severity_desc += item["description"] + "\n" if item.get("description") else ""
+    return severity_desc, remediation 
+
+def get_component(vulnerability):
+    mitigation = ""
+    gav = ""
+    impact = "**Impact paths**\n"
     if "components" in vulnerability:
         components = vulnerability["components"]
-        key = next(iter(components))
-        print("***key***", key)
-        component = components[key]
+        gav = next(iter(components))
+        print("***key***", gav)
+        component = components[gav]
         fixed_versions = component.get("fixed_versions")
         if fixed_versions:
             mitigation = "**Versions containing a fix:**\n"
             mitigation = mitigation + "\n".join(fixed_versions)
-            return mitigation, key
-        return "None", key
+        if "impact_paths" in component:
+            impact_paths = component["impact_paths"][0]
+            for item in impact_paths:
+                if "component_id" in item:
+                    component_id = item["component_id"]
+                    impact = impact + "\n" + component_id
+                if "full_path" in item:
+                    full_path = item["full_path"]
+                    impact = impact + "\n" + full_path
+        return gav, mitigation, impact
 
 
 def get_version_vulnerability(vulnerability):
@@ -108,13 +156,13 @@ def get_cve(vulnerability):
 
 
 def get_item(vulnerability, test):
-    print("**********", vulnerability)
+    severity_justification, remediation = get_severity_justification(vulnerability)
     severity = get_servery(vulnerability)
+    references = get_references(vulnerability)
     vulnerability_ids = list()
     cwe = None
     cvssv3 = None
     cvss_v3 = "No CVSS v3 score."
-    mitigation = None
     extra_desc = ""
     # Some entries have no CVE entries, despite they exist. Example CVE-2017-1000502.
     cves = get_cve(vulnerability)
@@ -131,7 +179,7 @@ def get_item(vulnerability, test):
             cvssv3 = CVSS3.from_rh_vector(cvss_v3).clean_vector()
 
     extra_desc += get_provider(vulnerability)
-    mitigation, component_name = get_mitigacion_and_component_key(vulnerability)
+    component_name, mitigation, impact = get_component(vulnerability)
     component_version = get_etx(vulnerability)
 
     # The 'id' field is empty? (at least in my sample file)
@@ -151,11 +199,14 @@ def get_item(vulnerability, test):
         title=title,
         cwe=cwe,
         test=test,
+        severity_justification=severity_justification,
         severity=severity,
         description=(vulnerability['summary'] + extra_desc).strip(),
-        mitigation=mitigation,
+        mitigation=mitigation + remediation,
         component_name=component_name,
         component_version=component_version,
+        impact=impact,
+        references=references,
         file_path=vulnerability.get('source_comp_id'),
         static_finding=True,
         dynamic_finding=False,
