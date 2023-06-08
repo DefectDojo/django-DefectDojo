07989afbd3c3befdc8f9cad071b0eaebaf578016
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2023-06-08 09:11:27 -0500

    Forgot a place

--- docker/entrypoint-initializer.sh
+++ docker/entrypoint-initializer.sh
@@ -16,7 +16,7 @@ initialize_data()
 create_announcement_banner() 
 {
 # Load the announcement banner
-if [ -z "$CREATE_INITIAL_BANNER" ]; then
+if [ -z "$DD_CREATE_INITIAL_BANNER" ]; then
 echo "Creating Announcement Banner"
 cat <<EOD | python3 manage.py shell
 from dojo.models import Announcement, UserAnnouncement, Dojo_User
