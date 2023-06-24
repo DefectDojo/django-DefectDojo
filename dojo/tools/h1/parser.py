3a5529461a6fe1ca9c05a055659ceb91c36e8218
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-24 13:35:22 -0500

    Update files in folder dojo/tools/h1 with PEP8 standars.

--- dojo/tools/h1/parser.py
+++ dojo/tools/h1/parser.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from dojo.models import Finding
 
-__author__ = 'Kirill Gotsman'
+__author__ = "Kirill Gotsman"
 
 
 class H1Parser(object):
@@ -29,15 +29,17 @@ class H1Parser(object):
         # Load the contents of the JSON file into a dictionary
         data = file.read()
         try:
-            tree = json.loads(str(data, 'utf-8'))
-        except:
+            tree = json.loads(str(data, "utf-8"))
+        except BaseException:
             tree = json.loads(data)
         # Convert JSON  report to DefectDojo format
         dupes = dict()
         for content in tree["data"]:
             # Get all relevant data
-            date = content['attributes']['created_at']
-            date = datetime.strftime(datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ"), "%Y-%m-%d")
+            date = content["attributes"]["created_at"]
+            date = datetime.strftime(
+                datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ"), "%Y-%m-%d"
+            )
             # Build the title of the Dojo finding
             title = "#" + content["id"] + " " + content["attributes"]["title"]
 
@@ -45,21 +47,31 @@ class H1Parser(object):
 
             # References
             try:
-                issue_tracker_id = content['attributes']['issue_tracker_reference_id']
-                issue_tracker_url = content['attributes']['issue_tracker_reference_url']
-                references = "[{}]({})\n".format(issue_tracker_id, issue_tracker_url)
-            except:
+                issue_tracker_id = content["attributes"][
+                    "issue_tracker_reference_id"
+                ]
+                issue_tracker_url = content["attributes"][
+                    "issue_tracker_reference_url"
+                ]
+                references = "[{}]({})\n".format(
+                    issue_tracker_id, issue_tracker_url
+                )
+            except BaseException:
                 references = ""
 
             # Build the severity of the Dojo finding
             try:
-                severity = content["relationships"]["severity"]["data"]["attributes"]["rating"].capitalize()
+                severity = content["relationships"]["severity"]["data"][
+                    "attributes"
+                ]["rating"].capitalize()
                 if severity not in ["Low", "Medium", "High", "Critical"]:
                     severity = "Info"
-            except:
+            except BaseException:
                 severity = "Info"
             # Build the references of the Dojo finding
-            ref_link = "https://hackerone.com/reports/{}".format(content.get("id"))
+            ref_link = "https://hackerone.com/reports/{}".format(
+                content.get("id")
+            )
             references += "[{}]({})".format(ref_link, ref_link)
 
             # Set active state of the Dojo finding
@@ -70,11 +82,17 @@ class H1Parser(object):
 
             # Set CWE of the Dojo finding
             try:
-                cwe = int(content["relationships"]["weakness"]["data"]["attributes"]["external_id"][4:])
-            except:
+                cwe = int(
+                    content["relationships"]["weakness"]["data"]["attributes"][
+                        "external_id"
+                    ][4:]
+                )
+            except BaseException:
                 cwe = 0
 
-            dupe_key = hashlib.md5(str(references + title).encode('utf-8')).hexdigest()
+            dupe_key = hashlib.md5(
+                str(references + title).encode("utf-8")
+            ).hexdigest()
             if dupe_key in dupes:
                 finding = dupes[dupe_key]
                 if finding.references:
@@ -96,43 +114,60 @@ class H1Parser(object):
                     impact="No impact provided",
                     references=references,
                     cwe=cwe,
-                    dynamic_finding=False,)
+                    dynamic_finding=False,
+                )
                 finding.unsaved_endpoints = list()
                 dupes[dupe_key] = finding
         return dupes.values()
 
     def build_description(self, content):
-        date = content['attributes']['created_at']
-        date = datetime.strftime(datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ"), "%Y-%m-%d")
-        reporter = content['relationships']['reporter']['data']['attributes']['username']
-        triaged_date = content['attributes']['triaged_at']
+        date = content["attributes"]["created_at"]
+        date = datetime.strftime(
+            datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ"), "%Y-%m-%d"
+        )
+        reporter = content["relationships"]["reporter"]["data"]["attributes"][
+            "username"
+        ]
+        triaged_date = content["attributes"]["triaged_at"]
 
         # Build the description of the Dojo finding
-        description = "#" + content['attributes']['title']
+        description = "#" + content["attributes"]["title"]
         description += "\nSubmitted: {}\nBy: {}\n".format(date, reporter)
 
         # Add triaged date
         if triaged_date is not None:
             triaged_date = datetime.strftime(
-                datetime.strptime(triaged_date, "%Y-%m-%dT%H:%M:%S.%fZ"), "%Y-%m-%d")
+                datetime.strptime(triaged_date, "%Y-%m-%dT%H:%M:%S.%fZ"),
+                "%Y-%m-%d",
+            )
             description += "Triaged: {}\n".format(triaged_date)
 
         # Try to grab CVSS
         try:
-            cvss = content['relationships']['severity']['data']['attributes']['score']
+            cvss = content["relationships"]["severity"]["data"]["attributes"][
+                "score"
+            ]
             description += "CVSS: {}\n".format(cvss)
-        except:
+        except BaseException:
             pass
 
         # Build rest of description meat
-        description += "##Report: \n{}\n".format(content["attributes"]["vulnerability_information"])
+        description += "##Report: \n{}\n".format(
+            content["attributes"]["vulnerability_information"]
+        )
 
         # Try to grab weakness if it's there
         try:
-            weakness_title = content['relationships']['weakness']['data']['attributes']['name']
-            weakness_desc = content['relationships']['weakness']['data']['attributes']['description']
-            description += "\n##Weakness: {}\n{}".format(weakness_title, weakness_desc)
-        except:
+            weakness_title = content["relationships"]["weakness"]["data"][
+                "attributes"
+            ]["name"]
+            weakness_desc = content["relationships"]["weakness"]["data"][
+                "attributes"
+            ]["description"]
+            description += "\n##Weakness: {}\n{}".format(
+                weakness_title, weakness_desc
+            )
+        except BaseException:
             pass
 
         return description
