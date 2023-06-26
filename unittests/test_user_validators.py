0993b32a8667dbbf65cec931e7bce987b310c78e
Tomas Kubla <tomas@kubla.sk>
2023-06-27 00:03:19 +0200

    Flake8, pep8

--- unittests/test_user_validators.py
+++ unittests/test_user_validators.py
@@ -5,28 +5,32 @@ from dojo.models import System_Settings, Dojo_User
 
 class TestUserValidators(DojoTestCase):
 
-    def form_test(self, password, confirm_password = None, current_password = None):
-        if confirm_password == None:
+    def form_test(
+            self,
+            password,
+            confirm_password=None,
+            current_password=None):
+        if confirm_password is None:
             confirm_password = password
-        if current_password == None:
+        if current_password is None:
             current_password = self.current_password
         return ChangePasswordForm({
-                "current_password": current_password,
-                "new_password": password,
-                "confirm_password": confirm_password,
-            },
-            user = self.user,
+            "current_password": current_password,
+            "new_password": password,
+            "confirm_password": confirm_password,
+        },
+            user=self.user,
         )
 
     def set_policy(
         self,
-        minimum_password_length = 0,
-        maximum_password_length = 50,
-        number_character_required = False,
-        special_character_required = False,
-        lowercase_character_required = False,
-        uppercase_character_required = False,
-        non_common_password_required = False,    
+        minimum_password_length=0,
+        maximum_password_length=50,
+        number_character_required=False,
+        special_character_required=False,
+        lowercase_character_required=False,
+        uppercase_character_required=False,
+        non_common_password_required=False,
     ):
         self.system_settings = System_Settings.objects.get()
         self.system_settings.minimum_password_length = minimum_password_length
@@ -43,8 +47,8 @@ class TestUserValidators(DojoTestCase):
         self.current_password = "Or1gina1P@ssw0rd"
         self.user = Dojo_User.objects.create(username="password_tester")
         self.user.set_password(self.current_password)
-        self.user.save() 
-    
+        self.user.save()
+
     def test_validator_minimum_password_length(self):
         with self.subTest(policy='minimum_password_length 1≥0'):
             self.set_policy(minimum_password_length=0)
@@ -56,7 +60,9 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(minimum_password_length=2)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'Password must be at least 2 characters long.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'Password must be at least 2 characters long.')
 
     def test_validator_maximum_password_length(self):
         with self.subTest(policy='maximum_password_length 1≤2'):
@@ -69,8 +75,10 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(maximum_password_length=0)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'Password must be less than 0 characters long.')
-    
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'Password must be less than 0 characters long.')
+
     def test_validator_number_character_required(self):
         with self.subTest(policy='number_character_required=False'):
             self.set_policy(number_character_required=False)
@@ -79,7 +87,9 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(number_character_required=True)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'Password must contain at least 1 digit, 0-9.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'Password must contain at least 1 digit, 0-9.')
 
     def test_validator_special_character_required(self):
         with self.subTest(policy='special_character_required=False'):
@@ -89,7 +99,9 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(special_character_required=True)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'The password must contain at least 1 special character, ()[]{}|\`~!@#$%^&*_-+=;:\'",<>./?.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'The password must contain at least 1 special character, ()[]{}|\\`~!@#$%^&*_-+=;:\'",<>./?.')
 
     def test_validator_lowercase_character_required(self):
         with self.subTest(policy='lowercase_character_required=False'):
@@ -99,7 +111,9 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(lowercase_character_required=True)
             form = self.form_test('X')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'Password must contain at least 1 lowercase letter, a-z.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'Password must contain at least 1 lowercase letter, a-z.')
 
     def test_validator_uppercase_character_required(self):
         with self.subTest(policy='uppercase_character_required=False'):
@@ -109,7 +123,9 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(uppercase_character_required=True)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'Password must contain at least 1 uppercase letter, A-Z.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'Password must contain at least 1 uppercase letter, A-Z.')
 
     def test_validator_non_common_password_required(self):
         with self.subTest(policy='non_common_password_required=False'):
@@ -119,19 +135,25 @@ class TestUserValidators(DojoTestCase):
             self.set_policy(non_common_password_required=True)
             form = self.form_test('x')
             self.assertFalse(form.is_valid())
-            self.assertEqual(form.errors['new_password'][0], 'This password is too common.')
+            self.assertEqual(
+                form.errors['new_password'][0],
+                'This password is too common.')
 
     def test_form_invalid_current_pass(self):
         self.set_policy()
         form = self.form_test('x', current_password='not current password')
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['__all__'][0], 'Current password is incorrect.')
+        self.assertEqual(
+            form.errors['__all__'][0],
+            'Current password is incorrect.')
 
     def test_form_same_pass_as_before(self):
         self.set_policy()
         form = self.form_test(self.current_password)
         self.assertFalse(form.is_valid())
-        self.assertEqual(form.errors['__all__'][0], 'New password must be different from current password.')
+        self.assertEqual(
+            form.errors['__all__'][0],
+            'New password must be different from current password.')
 
     def test_form_diff_confirm_password(self):
         self.set_policy()
