12bdaf1811d6be8425427a0863218c18ed4b6906
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-26 01:13:49 -0500

    Update files in folder dojo/tools/php_security_audit_v2 with PEP8 standars.

--- dojo/tools/php_security_audit_v2/parser.py
+++ dojo/tools/php_security_audit_v2/parser.py
@@ -5,7 +5,6 @@ from dojo.models import Finding
 
 
 class PhpSecurityAuditV2Parser(object):
-
     def get_scan_types(self):
         return ["PHP Security Audit v2"]
 
@@ -18,8 +17,8 @@ class PhpSecurityAuditV2Parser(object):
     def get_findings(self, filename, test):
         tree = filename.read()
         try:
-            data = json.loads(str(tree, 'utf-8'))
-        except:
+            data = json.loads(str(tree, "utf-8"))
+        except BaseException:
             data = json.loads(tree)
         dupes = dict()
 
@@ -36,9 +35,16 @@ class PhpSecurityAuditV2Parser(object):
                     findingdetail += "Rule Source: " + issue["source"] + "\n"
                     findingdetail += "Details: " + issue["message"] + "\n"
 
-                    sev = PhpSecurityAuditV2Parser.get_severity_word(issue["severity"])
+                    sev = PhpSecurityAuditV2Parser.get_severity_word(
+                        issue["severity"]
+                    )
 
-                    dupe_key = title + filepath + str(issue["line"]) + str(issue["column"])
+                    dupe_key = (
+                        title
+                        + filepath
+                        + str(issue["line"])
+                        + str(issue["column"])
+                    )
 
                     if dupe_key in dupes:
                         find = dupes[dupe_key]
@@ -57,7 +63,7 @@ class PhpSecurityAuditV2Parser(object):
                         )
 
                         dupes[dupe_key] = find
-                        findingdetail = ''
+                        findingdetail = ""
 
         return list(dupes.values())
 
@@ -66,10 +72,10 @@ class PhpSecurityAuditV2Parser(object):
         sev = math.ceil(severity / 2)
 
         if sev == 5:
-            return 'Critical'
+            return "Critical"
         elif sev == 4:
-            return 'High'
+            return "High"
         elif sev == 3:
-            return 'Medium'
+            return "Medium"
         else:
-            return 'Low'
+            return "Low"
