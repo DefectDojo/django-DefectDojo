fb9f5aaf39a0665cbafe045d659306741e1ef048
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-27 13:26:46 -0500

    Update files in folder dojo/tools/trufflehog3 and trustwave with PEP8 standars.

--- dojo/tools/trustwave/parser.py
+++ dojo/tools/trustwave/parser.py
@@ -6,7 +6,6 @@ from dojo.models import Endpoint, Finding
 
 
 class TrustwaveParser(object):
-
     def get_scan_types(self):
         return ["Trustwave Scan (CSV)"]
 
@@ -17,18 +16,19 @@ class TrustwaveParser(object):
         return "CSV output of Trustwave vulnerability scan."
 
     def get_findings(self, filename, test):
-
         content = filename.read()
-        if type(content) is bytes:
-            content = content.decode('utf-8')
-        reader = csv.DictReader(io.StringIO(content), delimiter=',', quotechar='"')
+        if isinstance(content, bytes):
+            content = content.decode("utf-8")
+        reader = csv.DictReader(
+            io.StringIO(content), delimiter=",", quotechar='"'
+        )
 
         severity_mapping = {
-            'I': 'Info',
-            'L': 'Low',
-            'M': 'Medium',
-            'H': 'High',
-            'C': 'Critical',
+            "I": "Info",
+            "L": "Low",
+            "M": "Medium",
+            "H": "High",
+            "C": "Critical",
         }
 
         dupes = {}
@@ -37,31 +37,33 @@ class TrustwaveParser(object):
                 test=test,
                 nb_occurences=1,
             )
-            host = row.get('Domain')
-            if host is None or host == '':
-                host = row.get('IP')
+            host = row.get("Domain")
+            if host is None or host == "":
+                host = row.get("IP")
             finding.unsaved_endpoints = [Endpoint(host=host)]
-            if row.get('Port') is not None and not "" == row.get('Port'):
-                finding.unsaved_endpoints[0].port = int(row['Port'])
-            if row.get('Protocol') is not None and not "" == row.get('Protocol'):
-                finding.unsaved_endpoints[0].protocol = row['Protocol']
-            finding.title = row['Vulnerability Name']
-            finding.description = row['Description']
-            finding.references = row.get('Evidence')
-            finding.mitigation = row.get('Remediation')
+            if row.get("Port") is not None and not "" == row.get("Port"):
+                finding.unsaved_endpoints[0].port = int(row["Port"])
+            if row.get("Protocol") is not None and not "" == row.get(
+                "Protocol"
+            ):
+                finding.unsaved_endpoints[0].protocol = row["Protocol"]
+            finding.title = row["Vulnerability Name"]
+            finding.description = row["Description"]
+            finding.references = row.get("Evidence")
+            finding.mitigation = row.get("Remediation")
 
             # manage severity
-            if row['Severity'] in severity_mapping:
-                finding.severity = severity_mapping[row['Severity']]
+            if row["Severity"] in severity_mapping:
+                finding.severity = severity_mapping[row["Severity"]]
             else:
-                finding.severity = 'Low'
-            finding.unsaved_vulnerability_ids = [row.get('CVE')]
+                finding.severity = "Low"
+            finding.unsaved_vulnerability_ids = [row.get("CVE")]
 
-            dupes_key = hashlib.sha256("|".join([
-                finding.severity,
-                finding.title,
-                finding.description
-            ]).encode()).hexdigest()
+            dupes_key = hashlib.sha256(
+                "|".join(
+                    [finding.severity, finding.title, finding.description]
+                ).encode()
+            ).hexdigest()
 
             if dupes_key in dupes:
                 dupes[dupes_key].nb_occurences += 1
