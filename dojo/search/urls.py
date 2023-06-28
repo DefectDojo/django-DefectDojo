19d130cbda73bdc5e14a130b538dc56a61a78d64
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-27 19:59:42 -0500

    Update files in folder dojo/search with PEP8 standars.

--- dojo/search/urls.py
+++ dojo/search/urls.py
@@ -4,6 +4,5 @@ from dojo.search import views
 
 urlpatterns = [
     #  search
-    re_path(r'^simple_search$', views.simple_search,
-        name='simple_search'),
+    re_path(r"^simple_search$", views.simple_search, name="simple_search"),
 ]
