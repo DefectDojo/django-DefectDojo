ef7d5098e11082e703328c7bcdc65f74bb2356c4
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-27 19:54:40 -0500

    Update files in folder dojo/sla_config with PEP8 standars.

--- dojo/sla_config/urls.py
+++ dojo/sla_config/urls.py
@@ -2,7 +2,11 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^sla_config/add', views.new_sla_config, name='new_sla_config'),
-    re_path(r'^sla_config/(?P<slaid>\d+)/edit$', views.edit_sla_config, name='edit_sla_config'),
-    re_path(r'^sla_config$', views.sla_config, name='sla_config'),
+    re_path(r"^sla_config/add", views.new_sla_config, name="new_sla_config"),
+    re_path(
+        r"^sla_config/(?P<slaid>\d+)/edit$",
+        views.edit_sla_config,
+        name="edit_sla_config",
+    ),
+    re_path(r"^sla_config$", views.sla_config, name="sla_config"),
 ]
