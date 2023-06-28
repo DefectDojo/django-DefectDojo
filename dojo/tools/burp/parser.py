648bae4cefb35630789e7c2dc3d566e5a1efba46
root <root@SANTIAGO-BUITRAGO>
2023-06-28 14:42:41 -0500

    Vulnerability fix 083 (XML Injection)

--- dojo/tools/burp/parser.py
+++ dojo/tools/burp/parser.py
@@ -2,7 +2,8 @@ import base64
 import logging
 import re
 import html2text
-from defusedxml import ElementTree as etree
+#from defusedxml import ElementTree as etree
+from defusedxml import defuse_parse as etree
 from dojo.models import Endpoint, Finding
 
 logger = logging.getLogger(__name__)
@@ -29,7 +30,7 @@ class BurpParser(object):
         )
 
     def get_findings(self, xml_output, test):
-        tree = etree.parse(xml_output, etree.XMLParser())
+        tree = etree.parse(xml_output, etree.DefuseXMLParser())
         return self.get_items(tree, test)
 
     def get_items(self, tree, test):
