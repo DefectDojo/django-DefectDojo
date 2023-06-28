e0ce024768c321443429776ab29af6f386b5d265
Alejandro Tortolero <ajtortolero@outlook.com>
2023-06-27 19:58:03 -0500

    Update files in folder dojo/settings with PEP8 standars.

--- dojo/settings/settings.py
+++ dojo/settings/settings.py
@@ -3,7 +3,4 @@ from split_settings.tools import optional, include
 # See https://documentation.defectdojo.com/getting_started/configuration/ for options
 # how to tune the configuration to your needs.
 
-include(
-    'settings.dist.py',
-    optional('local_settings.py')
-)
+include("settings.dist.py", optional("local_settings.py"))
