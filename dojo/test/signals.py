ad9a8e2029468c3f3fe8dd04fc7a3e6cb43961a4
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2023-07-07 15:35:47 -0500

    Update signals.py

--- dojo/test/signals.py
+++ dojo/test/signals.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(signals.pre_save, sender=Test)
-def update_found_byt_for_findings(sender, instance, **kwargs):
+def update_found_by_for_findings(sender, instance, **kwargs):
     with contextlib.suppress(sender.DoesNotExist):
         obj = sender.objects.get(pk=instance.pk)
         # Check if the test type has changed
