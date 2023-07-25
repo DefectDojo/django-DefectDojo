a0e18e337bd838c38803908c1c594529ea42e6e4
kiblik <tomas@kubla.sk>
2023-07-25 23:07:05 +0000

    Fix invalid OpenAPI schema + Add integration test (#8253)
    * Interation test: Test OpenAPI schema
    
    * Fix DateRangeFilter empty option
    
    * Apply suggestions from code review
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Replace also others `''` to `None`
    
    * Fix build
    
    ---------
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>

--- dojo/filters.py
+++ dojo/filters.py
@@ -111,7 +111,7 @@ class FindingStatusFilter(ChoiceFilter):
         return qs.filter(UNDER_REVIEW_QUERY)
 
     options = {
-        '': (_('Any'), any),
+        None: (_('Any'), any),
         0: (_('Open'), open),
         1: (_('Verified'), verified),
         2: (_('Out Of Scope'), out_of_scope),
@@ -138,7 +138,7 @@ class FindingStatusFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](self, qs, self.field_name)
 
 
@@ -364,7 +364,7 @@ class FindingFilterWithTags(DojoFilter):
 
 class DateRangeFilter(ChoiceFilter):
     options = {
-        '': (_('Any date'), lambda qs, name: qs.all()),
+        None: (_('Any date'), lambda qs, name: qs.all()),
         1: (_('Today'), lambda qs, name: qs.filter(**{
             '%s__year' % name: now().year,
             '%s__month' % name: now().month,
@@ -404,13 +404,13 @@ class DateRangeFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](qs, self.field_name)
 
 
 class DateRangeOmniFilter(ChoiceFilter):
     options = {
-        '': (_('Any date'), lambda qs, name: qs.all()),
+        None: (_('Any date'), lambda qs, name: qs.all()),
         1: (_('Today'), lambda qs, name: qs.filter(**{
             '%s__year' % name: now().year,
             '%s__month' % name: now().month,
@@ -466,13 +466,13 @@ class DateRangeOmniFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](qs, self.field_name)
 
 
 class ReportBooleanFilter(ChoiceFilter):
     options = {
-        '': (_('Either'), lambda qs, name: qs.all()),
+        None: (_('Either'), lambda qs, name: qs.all()),
         1: (_('Yes'), lambda qs, name: qs.filter(**{
             '%s' % name: True
         })),
@@ -490,7 +490,7 @@ class ReportBooleanFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](qs, self.field_name)
 
 
@@ -510,7 +510,7 @@ class ReportRiskAcceptanceFilter(ChoiceFilter):
         return qs.filter(WAS_ACCEPTED_FINDINGS_QUERY)
 
     options = {
-        '': (_('Either'), any),
+        None: (_('Either'), any),
         1: (_('Yes'), accepted),
         2: (_('No'), not_accepted),
         3: (_('Was'), was_accepted),
@@ -525,7 +525,7 @@ class ReportRiskAcceptanceFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](self, qs, self.field_name)
 
 
@@ -581,7 +581,7 @@ class MetricsDateRangeFilter(ChoiceFilter):
         return self.past_x_days(qs, name, 365)
 
     options = {
-        '': (_('Past 30 days'), past_thirty_days),
+        None: (_('Past 30 days'), past_thirty_days),
         1: (_('Past 7 days'), past_seven_days),
         2: (_('Past 90 days'), past_ninety_days),
         3: (_('Current month'), current_month),
@@ -609,7 +609,7 @@ class MetricsDateRangeFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](self, qs, self.field_name)
 
 
@@ -2270,7 +2270,7 @@ class QuestionTypeFilter(ChoiceFilter):
         return qs.filter(polymorphic_ctype=ContentType.objects.get_for_model(ChoiceQuestion))
 
     options = {
-        '': (_('Any'), any),
+        None: (_('Any'), any),
         1: (_('Text Question'), text_question),
         2: (_('Choice Question'), choice_question),
     }
@@ -2284,7 +2284,7 @@ class QuestionTypeFilter(ChoiceFilter):
         try:
             value = int(value)
         except (ValueError, TypeError):
-            value = ''
+            value = None
         return self.options[value][1](self, qs, self.options[value][0])
 
 
