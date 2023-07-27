98a1edf7d07da8f6e349ae07a8c0c6776f82ecf0
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2023-07-27 12:13:39 -0500

    add configuration opentelemetry uwsgi and otlp

--- manage.py
+++ manage.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 import os
 import sys
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dojo.settings.settings")
+
+    resource = Resource(attributes={"service.name": "defectdojo"})
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    tracer = trace.get_tracer(__name__)
+
+    otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+
+    span_processor = BatchSpanProcessor(otlp_exporter)
+    trace.get_tracer_provider().add_span_processor(span_processor)
 
     from django.core.management import execute_from_command_line
 
