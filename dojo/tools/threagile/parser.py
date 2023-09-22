5eeeaf3164a1f41c0441be59035223f446b6d9cb
avlachopoulos <avlachopoulos@omilia.com>
2023-09-22 11:36:54 +0300

    Feat: Add Threagile parser

---
+++ dojo/tools/threagile/parser.py
@@ -0,0 +1,95 @@
+import json
+
+from dojo.models import Finding
+
+RISK_TO_CWE_MAP = {
+    "accidental-secret-leak": 200,
+    "code-backdooring": 912,
+    "container-baseimage-backdooring": 912,
+    "container-platform-escape": 1008,
+    "cross-site-request-forgery": 352,
+    "cross-site-scripting": 79,
+    "dos-risky-access-across-trust-boundary": 400,
+    "incomplete-model": 1008,
+    "ldap-injection": 90,
+    "missing-authentication-second-factor": 308,
+    "missing-authentication": 306,
+    "missing-build-infrastructure": 1127,
+    "missing-cloud-hardening": 1008,
+    "missing-file-validation": 434,
+    "missing-hardening": 16,
+    "missing-identity-propagation": 204,
+    "missing-identity-provider-isolation": 1008,
+    "missing-identity-store": 287,
+    "missing-network-segmentation": 1008,
+    "missing-vault-isolation": 1008,
+    "missing-vault": 522,
+    "missing-waf": 1008,
+    "mixed-target-on-shared-runtime": 1008,
+    "path-traversal": 22,
+    "push-instead-of-pull-deployment": 1127,
+    "search-query-injection": 74,
+    "server-side-request-forgery": 918,
+    "service-registry-poisoning": 693,
+    "sql-injection-rule": 89,
+    "unchecked-deployment": 1127,
+    "unencrypted-asset": 311,
+    "unencrypted-communication": 319,
+    "unguarded-access-from-internet": 501,
+    "unguarded-direct-datastore-access": 501,
+    "unnecessary-communication-link": 1008,
+    "unnecessary-data-asset": 1008,
+    "unnecessary-data-transfer": 1008,
+    "unnecessary-technical-asset": 1008,
+    "untrusted-deserialization": 502,
+    "wrong-communication-link": 1008,
+    "wrong-trust-boudnary-content": 1008,
+    "xml-external-entity": 611
+}
+
+
+class ThreagileParser(object):
+    """
+    Import ThreaAgile threatmodel risk finding in JSON format
+    """
+
+    REQUIRED_FIELDS = ["category", "title", "severity", "synthetic_id","exploitation_impact"]
+
+    def get_scan_types(self):
+        return ["ThreaAgile risks report"]
+
+    def get_label_for_scan_types(self, scan_type):
+        return "ThreAgile risks report"
+
+    def get_description_for_scan_types(self, scan_type):
+        return "ThreAgile Risks Report in JSON format (risks.json)."
+
+    def get_findings(self, file, test):
+        if file is None:
+            return None
+
+        return self.get_items(json.load(file), test)
+
+    def get_items(self, tree, test):
+        if not isinstance(tree, list):
+            raise ValueError("Invalid ThreAgile risks file")
+        if not tree:
+            return list()
+        findings = []
+        for item in tree:
+            for key in item.keys():
+                if key not in self.REQUIRED_FIELDS:
+                    raise ValueError(f"Invalid ThreAgile risks file, missing field {key}")
+            severity = item.get("severity").capitalize()
+            severity = severity if severity != "Elevated" else "High"
+            finding = Finding(
+                title=item.get("category", ""),
+                cwe=RISK_TO_CWE_MAP.get(item.get("category"), None),
+                description=item.get("title"),
+                impact=item.get("exploitation_impact"),
+                severity= severity,
+                test=test,
+                unique_id_from_tool=item.get("synthetic_id")
+            )
+            findings.append(finding)
+        return findings
---
+++ unittests/tools/test_threagile_parser.py
@@ -0,0 +1,33 @@
+from dojo.models import Test
+from dojo.tools.threagile.parser import ThreagileParser
+from unittests.dojo_test_case import DojoTestCase
+
+
+class TestThreAgileParser(DojoTestCase):
+    def test_non_threagile_file_raises_error(self):
+        with open("unittests/scans/threagile/bad_formatted_risks_file.json") as testfile:
+            parser = ThreagileParser()
+            with self.assertRaises(ValueError) as exc_context:
+                parser.get_findings(testfile, Test())
+            exc = exc_context.exception
+            self.assertEqual("Invalid ThreAgile risks file", str(exc))
+
+    def test_empty_file_returns_no_findings(self):
+        with open("unittests/scans/threagile/empty_file_no_risks.json") as testfile:
+            parser = ThreagileParser()
+            findings = parser.get_findings(testfile, Test())
+            self.assertEqual(0, len(findings))
+
+    def test_file_with_vulnerabilities_returns_correct_findings(self):
+        with open("unittests/scans/threagile/risks.json") as testfile:
+            parser = ThreagileParser()
+            findings = parser.get_findings(testfile, Test())
+            self.assertEqual(4, len(findings))
+            finding = findings[0]
+            self.assertEqual("unguarded-direct-datastore-access", finding.title)
+            self.assertEqual("\u003cb\u003eUnguarded Direct Datastore Access\u003c/b\u003e of \u003cb\u003ePoliciesRegoStorage\u003c/b\u003e by \u003cb\u003eEnergon\u003c/b\u003e via \u003cb\u003eEnergonToPolicyRegoFileStorage\u003c/b\u003e", finding.description)
+            self.assertEqual("High", finding.severity)
+            self.assertEqual("unguarded-direct-datastore-access@energon-ta>energontopolicyregofilestorage@energon-ta@policies-rego-storage-ta", finding.unique_id_from_tool)
+            self.assertEqual("501", finding.cwe)
+            self.assertEqual("medium", finding.impact)
+
