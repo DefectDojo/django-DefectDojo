50f4d6c82fad96e9426afd36e65b8d02344acbfa
Vlad Donose <vlad.donose@ezugi.com>
2023-09-27 18:35:01 +0300

    Update ASFF parser to create endpoints
    Modified the ASFF parser to ensure that endpoints are created based solely on the EC2 instance's private IP address. It ensures proper private IP address validation without other lib (e.g. ipaddress)

--- dojo/tools/asff/parser.py
+++ dojo/tools/asff/parser.py
@@ -1,19 +1,29 @@
 import json
-
 import dateutil
+from dojo.models import Endpoint, Finding, Test
 
-from dojo.models import Finding
-
-# https://docs.aws.amazon.com/securityhub/1.0/APIReference/API_Severity.html
 SEVERITY_MAPPING = {
-    "INFORMATIONAL": "Info",  # No issue was found.
-    "LOW": "Low",  # The issue does not require action on its own.
-    "MEDIUM": "Medium",  # The issue must be addressed but not urgently.
-    "HIGH": "High",  # The issue must be addressed as a priority.
-    # The issue must be remediated immediately to avoid it escalating.
+    "INFORMATIONAL": "Info",
+    "LOW": "Low",
+    "MEDIUM": "Medium",
+    "HIGH": "High",
     "CRITICAL": "Critical",
 }
 
+def is_private_ipv4(ip):
+    parts = ip.split('.')
+    if len(parts) != 4:
+        return False
+    # 10.0.0.0 to 10.255.255.255
+    if parts[0] == '10':
+        return True
+    # 172.16.0.0 to 172.31.255.255
+    if parts[0] == '172' and 16 <= int(parts[1]) <= 31:
+        return True
+    # 192.168.0.0 to 192.168.255.255
+    if parts[0] == '192' and parts[1] == '168':
+        return True
+    return False
 
 class AsffParser(object):
     def get_scan_types(self):
@@ -28,29 +38,36 @@ class AsffParser(object):
 
     def get_findings(self, file, test):
         data = json.load(file)
-        result = list()
+        findings = []
+
         for item in data:
-            result.append(
-                Finding(
-                    title=item.get("Title"),
-                    description=item.get("Description"),
-                    date=dateutil.parser.parse(item.get("CreatedAt")),
-                    severity=self.get_severity(item.get("Severity")),
-                    active=True,  # TODO manage attribute 'RecordState'
-                    unique_id_from_tool=item.get("Id"),
-                )
+            finding = Finding(
+                title=item.get("Title"),
+                description=item.get("Description"),
+                date=dateutil.parser.parse(item.get("CreatedAt")),
+                severity=self.get_severity(item.get("Severity")),
+                active=(item.get("RecordState") == "ACTIVE"),
+                unique_id_from_tool=item.get("Id"),
+                test=test
             )
-        return result
+
+            if "Resources" in item:
+                endpoints = []
+                for resource in item["Resources"]:
+                    if resource["Type"] == "AwsEc2Instance" and "Details" in resource:
+                        details = resource["Details"]["AwsEc2Instance"]
+                        for ip in details.get("IpV4Addresses", []):
+                            if is_private_ipv4(ip):
+                                endpoints.append(Endpoint(host=ip))
+                finding.unsaved_endpoints = endpoints
+
+            findings.append(finding)
+        return findings
 
     def get_severity(self, data):
         if data.get("Label"):
             return SEVERITY_MAPPING[data.get("Label")]
         elif isinstance(data.get("Normalized"), int):
-            # 0 - INFORMATIONAL
-            # 1–39 - LOW
-            # 40–69 - MEDIUM
-            # 70–89 - HIGH
-            # 90–100 - CRITICAL
             if data.get("Normalized") > 89:
                 return "Critical"
             elif data.get("Normalized") > 69:
