979df5e8243a5fd81629360e760f7022e4d256e1
vd0n <vd0n@vd0n.com>
2023-09-27 22:12:05 +0300

    Update ASFF parser to create endpoints
    Modified the ASFF parser to ensure that endpoints are created based solely on the EC2 instance's private IP address. It ensures private IP address validation.

--- dojo/tools/asff/parser.py
+++ dojo/tools/asff/parser.py
@@ -2,18 +2,30 @@ import json
 
 import dateutil
 
-from dojo.models import Finding
+from dojo.models import Endpoint, Finding
 
-# https://docs.aws.amazon.com/securityhub/1.0/APIReference/API_Severity.html
 SEVERITY_MAPPING = {
-    "INFORMATIONAL": "Info",  # No issue was found.
-    "LOW": "Low",  # The issue does not require action on its own.
-    "MEDIUM": "Medium",  # The issue must be addressed but not urgently.
-    "HIGH": "High",  # The issue must be addressed as a priority.
-    # The issue must be remediated immediately to avoid it escalating.
+    "INFORMATIONAL": "Info",
+    "LOW": "Low",
+    "MEDIUM": "Medium",
+    "HIGH": "High",
     "CRITICAL": "Critical",
 }
 
+def is_private_ipv4(ip):
+    parts = ip.split('.')
+    if len(parts) != 4:
+        return False
+    # 10.0.0.0 to 10.255.255.255
+    if parts[0] == '10':
+        return True
+    # 172.16.0.0 to 172.31.255.255
+    if parts[0] == '172' and 16 <= int(parts[1]) <= 31:
+        return True
+    # 192.168.0.0 to 192.168.255.255
+    if parts[0] == '192' and parts[1] == '168':
+        return True
+    return False
 
 class AsffParser(object):
     def get_scan_types(self):
@@ -29,6 +41,7 @@ class AsffParser(object):
     def get_findings(self, file, test):
         data = json.load(file)
         result = list()
+
         for item in data:
             if item.get("Remediation"):
                 mitigation = item.get("Remediation").get("Recommendation").get("Text")
@@ -36,18 +49,30 @@ class AsffParser(object):
             else:
                 mitigation = None
                 references = None
-            result.append(
-                Finding(
-                    title=item.get("Title"),
-                    description=item.get("Description"),
-                    date=dateutil.parser.parse(item.get("CreatedAt")),
-                    mitigation=mitigation,
-                    references=references,
-                    severity=self.get_severity(item.get("Severity")),
-                    active=True,  # TODO manage attribute 'RecordState'
-                    unique_id_from_tool=item.get("Id"),
-                )
+
+            finding = Finding(
+                title=item.get("Title"),
+                description=item.get("Description"),
+                date=dateutil.parser.parse(item.get("CreatedAt")),
+                mitigation=mitigation,
+                references=references,
+                severity=self.get_severity(item.get("Severity")),
+                active=True,  # TODO: manage attribute 'RecordState'
+                unique_id_from_tool=item.get("Id"),
+                test=test
             )
+
+            if "Resources" in item:
+                endpoints = []
+                for resource in item["Resources"]:
+                    if resource["Type"] == "AwsEc2Instance" and "Details" in resource:
+                        details = resource["Details"]["AwsEc2Instance"]
+                        for ip in details.get("IpV4Addresses", []):
+                            if is_private_ipv4(ip):
+                                endpoints.append(Endpoint(host=ip))
+                finding.unsaved_endpoints = endpoints
+
+            result.append(finding)
         return result
 
     def get_severity(self, data):
