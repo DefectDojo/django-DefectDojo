ea930942c9f2b7c31e4665da5d3194e3d10e7aeb
Manuel Sommer <sommermanuel@gmx.de>
2023-10-05 08:55:46 +0200

    more unittests

--- unittests/tools/test_openvas_xml_parser.py
+++ unittests/tools/test_openvas_xml_parser.py
@@ -25,6 +25,10 @@ class TestOpenVASUploadXMLParser(DojoTestCase):
                 for endpoint in finding.unsaved_endpoints:
                     endpoint.clean()
             self.assertEqual(1, len(findings))
+            with self.subTest(i=0):
+                finding = findings[0]
+                self.assertEqual("Mozilla Firefox Security Update (Mfsa_2023-32_2023-36) - Windows_10.0.101.2_general/tcp", finding.title)
+                self.assertEqual("Critical", finding.severity)
 
     def test_openvas_xml_many_vuln(self):
         with open("unittests/scans/openvas_xml/many_vuln.xml") as f:
