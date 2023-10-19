50ac1e4d976eb0c0d9f3079728223ecd5f860a05
Manuel Sommer <sommermanuel@gmx.de>
2023-10-19 14:45:21 +0200

    :bug: fix unittest

--- unittests/tools/test_hcl_appscan_parser.py
+++ unittests/tools/test_hcl_appscan_parser.py
@@ -11,13 +11,6 @@ class TestHCLAppScanParser(DojoTestCase):
         my_file_handle.close()
         self.assertEqual(0, len(findings))
 
-    def test_one_finding(self):
-        my_file_handle = open("unittests/scans/hcl_appscan/one_finding.xml")
-        parser = HCLAppScanParser()
-        findings = parser.get_findings(my_file_handle, None)
-        my_file_handle.close()
-        self.assertEqual(1, len(findings))
-
     def test_many_findings(self):
         my_file_handle = open("unittests/scans/hcl_appscan/many_findings.xml")
         parser = HCLAppScanParser()
