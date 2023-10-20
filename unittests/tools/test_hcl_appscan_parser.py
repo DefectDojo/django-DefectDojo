d32cb2034cf33f6c5e9d8a55c93c7198597979cf
Manuel Sommer <sommermanuel@gmx.de>
2023-10-20 09:05:42 +0200

    added unittests

--- unittests/tools/test_hcl_appscan_parser.py
+++ unittests/tools/test_hcl_appscan_parser.py
@@ -17,3 +17,8 @@ class TestHCLAppScanParser(DojoTestCase):
         findings = parser.get_findings(my_file_handle, None)
         my_file_handle.close()
         self.assertEqual(60, len(findings))
+        self.assertEqual(findings[0].title, "Unencrypted Login Request_mani-virtual-machine_/dvja-1.0-SNAPSHOT/register.action")
+        self.assertEqual(findings[1].title, "Unencrypted Login Request_mani-virtual-machine_/dvja-1.0-SNAPSHOT/login.action;jsessionid=AD12F9CF7835CC92885A381859462BAC")
+        self.assertEqual(findings[0].severity, "High")
+        self.assertEqual(findings[9].severity, "Medium")
+        self.assertEqual(findings[1].description, "Issue-Type-Name: Unencrypted Login Request\nLocation: http://mani-virtual-machine:9000/dvja-1.0-SNAPSHOT/login.action;jsessionid=AD12F9CF7835CC92885A381859462BAC\nDomain: mani-virtual-machine\nElement: password\nElementType: Parameter\nPath: /dvja-1.0-SNAPSHOT/login.action;jsessionid=AD12F9CF7835CC92885A381859462BAC\nScheme: http\nHost: mani-virtual-machine\nPort: 9000\n")
