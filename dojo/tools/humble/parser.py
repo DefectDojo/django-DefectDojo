15947586418209a6c29a315e0cd7a82f074b9227
Manuel Sommer <sommermanuel@gmx.de>
2023-11-15 11:49:42 +0100

    fix according to comment

--- dojo/tools/humble/parser.py
+++ dojo/tools/humble/parser.py
@@ -22,9 +22,12 @@ class HumbleParser(object):
             data = {}
         if data != {}:
             url = data['[0. Info]']['URL']
+            import re
+            host = re.compile(r"https?://(www\.)?")
+            host = host.sub('', url).strip().strip('/')
             for content in data['[1. Missing HTTP Security Headers]']:
                 if content != "Nothing to report, all seems OK!":
-                    finding = Finding(title=url + "_missing_" + str(content),
+                    finding = Finding(title=host + "_missing_" + str(content),
                         test=test,
                         description="This security Header is missing: " + content,
                         severity="Medium",
@@ -32,11 +35,11 @@ class HumbleParser(object):
                         dynamic_finding=True)
                     items.append(finding)
                     finding.unsaved_endpoints = list()
-                    endpoint = Endpoint(host=url)
+                    endpoint = Endpoint(host=host)
                     finding.unsaved_endpoints.append(endpoint)
             for content in data['[2. Fingerprint HTTP Response Headers]']:
                 if content != "Nothing to report, all seems OK!":
-                    finding = Finding(title=url + "_fingerprint_" + str(content),
+                    finding = Finding(title=host + "_fingerprint_" + str(content),
                         test=test,
                         description="This fingerprint HTTP Response Header is available. Please remove it: " + content,
                         severity="Medium",
@@ -44,11 +47,11 @@ class HumbleParser(object):
                         dynamic_finding=True)
                     items.append(finding)
                     finding.unsaved_endpoints = list()
-                    endpoint = Endpoint(host=url)
+                    endpoint = Endpoint(host=host)
                     finding.unsaved_endpoints.append(endpoint)
             for content in data['[3. Deprecated HTTP Response Headers/Protocols and Insecure Values]']:
                 if content != "Nothing to report, all seems OK!":
-                    finding = Finding(title=url + "_deprecatedheader_" + str(content),
+                    finding = Finding(title=host + "_deprecatedheader_" + str(content),
                         test=test,
                         description="This deprecated HTTP Response Header is available. Please remove it: " + content,
                         severity="Medium",
@@ -56,11 +59,11 @@ class HumbleParser(object):
                         dynamic_finding=True)
                     items.append(finding)
                     finding.unsaved_endpoints = list()
-                    endpoint = Endpoint(host=url)
+                    endpoint = Endpoint(host=host)
                     finding.unsaved_endpoints.append(endpoint)
             for content in data['[4. Empty HTTP Response Headers Values]']:
                 if content != "Nothing to report, all seems OK!":
-                    finding = Finding(title=url + "_emptyhttpresponse_" + str(content),
+                    finding = Finding(title=host + "_emptyhttpresponse_" + str(content),
                         test=test,
                         description="This empty HTTP Response Header value is available. Please remove it: " + content,
                         severity="Medium",
@@ -68,6 +71,6 @@ class HumbleParser(object):
                         dynamic_finding=True)
                     items.append(finding)
                     finding.unsaved_endpoints = list()
-                    endpoint = Endpoint(host=url)
+                    endpoint = Endpoint(host=host)
                     finding.unsaved_endpoints.append(endpoint)
         return items
--- unittests/tools/test_humble_parser.py
+++ unittests/tools/test_humble_parser.py
@@ -8,22 +8,29 @@ class TestHumbleParser(DojoTestCase):
         testfile = open("unittests/scans/humble/many_findings.json")
         parser = HumbleParser()
         findings = parser.get_findings(testfile, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         testfile.close()
         self.assertEqual(9, len(findings))
         finding = findings[0]
-        self.assertEqual(finding.unsaved_endpoints[0].host, "https://asdf.asf.hs")
-        self.assertEqual("https://asdf.asf.hs_missing_Clear-Site-Data", finding.title)
+        self.assertEqual(finding.unsaved_endpoints[0].host, "asdf.asf.hs")
+        self.assertEqual("asdf.asf.hs_missing_Clear-Site-Data", finding.title)
         finding = findings[7]
-        self.assertEqual("https://asdf.asf.hs_deprecatedheader_Strict-Transport-Security (Recommended Values)", finding.title)
+        self.assertEqual("asdf.asf.hs_deprecatedheader_Strict-Transport-Security (Recommended Values)", finding.title)
 
     def test_humble_parser_with_many_findings2(self):
         testfile = open("unittests/scans/humble/many_findings2.json")
         parser = HumbleParser()
         findings = parser.get_findings(testfile, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
         testfile.close()
         self.assertEqual(16, len(findings))
         finding = findings[0]
-        self.assertEqual("https://testestset.com_missing_Clear-Site-Data", finding.title)
+        self.assertEqual(finding.unsaved_endpoints[0].host, "testestset.com")
+        self.assertEqual("testestset.com_missing_Clear-Site-Data", finding.title)
         finding = findings[7]
-        self.assertEqual("https://testestset.com_missing_Referrer-Policy", finding.title)
+        self.assertEqual("testestset.com_missing_Referrer-Policy", finding.title)
         self.assertEqual("This security Header is missing: Referrer-Policy", finding.description)
