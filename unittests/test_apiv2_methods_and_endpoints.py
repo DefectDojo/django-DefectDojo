379642eccf33f61153a21ef687e93dc38ce74fe7
Tomas Kubla <tomas@kubla.sk>
2023-11-29 11:07:01 +0100

    Define exceptions for now

--- unittests/test_apiv2_methods_and_endpoints.py
+++ unittests/test_apiv2_methods_and_endpoints.py
@@ -3,6 +3,34 @@ from .dojo_test_case import DojoTestCase
 from .test_rest_framework import get_open_api3_json_schema, BASE_API_URL
 import django.apps
 from dojo.api_v2 import serializers
+from dojo.models import (
+    Contact,
+    Product_Line,
+    Report_Type,
+    CWE,
+    BurpRawRequestResponse,
+    FileAccessToken,
+    Announcement,
+    UserAnnouncement,
+    BannerConf,
+    GITHUB_Conf,
+    GITHUB_Issue,
+    GITHUB_Clone,
+    GITHUB_Details_Cache,
+    GITHUB_PKey,
+    Tool_Product_History,
+    Objects_Review,
+    Objects_Product,
+    Testing_Guide_Category,
+    Testing_Guide,
+    Benchmark_Type,
+    Benchmark_Category,
+    Benchmark_Requirement,
+    Benchmark_Product,
+    Benchmark_Product_Summary,
+    Choice,
+)
+
 
 class ApiEndpointMethods(DojoTestCase):
     fixtures = ['dojo_testdata.json']
@@ -58,6 +86,33 @@ class ApiEndpoints(DojoTestCase):
             if hasattr(serializer, 'Meta'):
                 if hasattr(serializer.Meta, 'model'):
                     self.used_models.append(serializer.Meta.model)
+        self.no_api_models = [  # TODO: these models are excluded from check for now but implementation is needed
+            Contact,
+            Product_Line,
+            Report_Type,
+            CWE,
+            BurpRawRequestResponse,
+            FileAccessToken,
+            Announcement,
+            UserAnnouncement,
+            BannerConf,
+            GITHUB_Conf,
+            GITHUB_Issue,
+            GITHUB_Clone,
+            GITHUB_Details_Cache,
+            GITHUB_PKey,
+            Tool_Product_History,
+            Objects_Review,
+            Objects_Product,
+            Testing_Guide_Category,
+            Testing_Guide,
+            Benchmark_Type,
+            Benchmark_Category,
+            Benchmark_Requirement,
+            Benchmark_Product,
+            Benchmark_Product_Summary,
+            Choice,
+        ]
 
     def test_is_defined(self):
         for subclass in django.apps.apps.get_models():
@@ -67,4 +122,7 @@ class ApiEndpoints(DojoTestCase):
                 if subclass.__name__ in ['Alerts']:
                     continue
                 with self.subTest(subclass=subclass):
-                    self.assertIn(subclass, self.used_models)
+                    if subclass in self.used_models:
+                        self.assertNotIn(subclass, self.no_api_models, "Thank you, you just implemented API endpoint for the model which was needed. Please remove it from exception list 'self.no_api_models'")
+                    if subclass not in self.no_api_models:
+                        self.assertIn(subclass, self.used_models, "API endpoint for the managing mentioned model is need")
