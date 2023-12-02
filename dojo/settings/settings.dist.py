b8c8d9db122f7edeec6532558d5861a67d5755ae
renejal <40049733+renejal@users.noreply.github.com>
2023-12-01 21:09:04 -0500

    Feature/parser jfrog xray binary scan (#9015)
    * new parser Jfrog Xray on Demand Binary Scan
    
    * new parser Jfrog Xray on Demand Binary Scan
    
    * delete blank line at end of file
    
    * rename function
    
    * More sample reports
    
    * Update docs/content/en/integrations/parsers/file/jfrog_xray_on_demand_binary_scan.md
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update docs/content/en/integrations/parsers/file/jfrog_xray_on_demand_binary_scan.md
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update docs/content/en/integrations/parsers/file/jfrog_xray_on_demand_binary_scan.md
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/settings/settings.dist.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/settings/settings.dist.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * First round of Improvements
    
    * Drop duplicates in component_id and full_path
    
    * Process per component
    
    * Visual improvements
    
    * Use+clean summary in Title, fix dedup, parse version, drop useless functions
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * Update dojo/tools/jfrog_xray_on_demand_binary_scan/parser.py
    
    Co-authored-by: kiblik <kiblik@gjh.sk>
    
    * fix test rename class
    
    * Last Improvements and tests
    
    * capitalization skills
    
    ---------
    
    Co-authored-by: Tomas Kubla <tomas@kubla.sk>
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    Co-authored-by: kiblik <kiblik@gjh.sk>

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1226,6 +1226,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     'GitLab Dependency Scanning Report': ['title', 'vulnerability_ids', 'file_path', 'component_name', 'component_version'],
     'SpotBugs Scan': ['cwe', 'severity', 'file_path', 'line'],
     'JFrog Xray Unified Scan': ['vulnerability_ids', 'file_path', 'component_name', 'component_version'],
+    'JFrog Xray On Demand Binary Scan': ["title", "component_name", "component_version"],
     'Scout Suite Scan': ['file_path', 'vuln_id_from_tool'],  # for now we use file_path as there is no attribute for "service"
     'AWS Security Hub Scan': ['unique_id_from_tool'],
     'Meterian Scan': ['cwe', 'component_name', 'component_version', 'description', 'severity'],
@@ -1423,6 +1424,7 @@ DEDUPLICATION_ALGORITHM_PER_PARSER = {
     'Checkov Scan': DEDUPE_ALGO_HASH_CODE,
     'SpotBugs Scan': DEDUPE_ALGO_HASH_CODE,
     'JFrog Xray Unified Scan': DEDUPE_ALGO_HASH_CODE,
+    'JFrog Xray On Demand Binary Scan': DEDUPE_ALGO_HASH_CODE,
     'Scout Suite Scan': DEDUPE_ALGO_HASH_CODE,
     'AWS Security Hub Scan': DEDUPE_ALGO_UNIQUE_ID_FROM_TOOL,
     'Meterian Scan': DEDUPE_ALGO_HASH_CODE,
