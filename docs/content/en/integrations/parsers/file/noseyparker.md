0ba7a9d9c75f5acb660d1c5eb92da99443e1e79f
tpat13 <32806320+tpat13@users.noreply.github.com>
2023-12-04 00:09:02 -0500

    Updated Parser

--- docs/content/en/integrations/parsers/file/noseyparker.md
+++ docs/content/en/integrations/parsers/file/noseyparker.md
@@ -2,4 +2,92 @@
 title: "Nosey Parker"
 toc_hide: true
 ---
-JSONLines Output of Nosey Parker. Supports version 0.15.0 of https://github.com/praetorian-inc/noseyparker 
+Input Type:
+-
+This parser takes JSON Lines Output from Nosey Parker. Supports version 0.15.0 of https://github.com/praetorian-inc/noseyparker 
+
+Things to note about the Nosey Parker Parser:
+- 
+- All findings are marked with a severity of 'High'
+- The deduplication algorithm marks a unique finding by the secret, filepath, and line number all together
+
+Acceptable JSON Lines file:
+-
+Each line of the JSON Lines file from NoseyParker is one secret, but it can have multiple matches within the repository. All properties are required by the parser.
+
+The following is an example of an acceptable JSON lines file:
+~~~
+{"type":"finding","rule_name":"Generic API Key","match_content":"32ui1ffdasfhu239b4df2ac6609a9919","num_matches":1,"matches":[{"provenance":[{"kind":"file","path":"./app/schema/config.py"},{"kind":"git_repo","repo_path":"./.git","commit_provenance":{"commit_kind":"first_seen","commit_metadata":{"commit_id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","committer_name":"Princess Leia","committer_email":"leia@test.com","committer_timestamp":"1685495256 +0000","author_name":"Princess Leia","author_email":"leia@test.com","author_timestamp":"1685495256 +0000","message":"framework\n"},"blob_path":"app/schema/config.py"}}],"blob_metadata":{"id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","num_bytes":664,"mime_essence":"text/plain","charset":null},"blob_id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","location":{"offset_span":{"start":617,"end":660},"source_span":{"start":{"line":16,"column":17},"end":{"line":16,"column":59}}},"capture_group_index":1,"match_content":"32ui1ffdasfhu239b4df2ac6609a9919","snippet":{"before":"E = \"https://testwebsite.com\"\n ","matching":"API_KEY = \"32ui1ffdasfhu239b4df2ac6609a9919","after":"\"\n\n\n"},"rule_name":"Generic API Key"}]}
+{"type":"finding","rule_name":"Generic Username and Password (unquoted)","match_content":"secret","num_matches":1,"matches":[{"provenance":[{"kind":"file","path":"./app/schema/config.py"},{"kind":"git_repo","repo_path":"./.git","commit_provenance":{"commit_kind":"first_seen","commit_metadata":{"commit_id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","committer_name":"Princess Leia","committer_email":"leia@test.com","committer_timestamp":"1685495256 +0000","author_name":"Princess Leia","author_email":"leia@test.com","author_timestamp":"1685495256 +0000","message":"framework\n"},"blob_path":"app/schema/config.py"}}],"blob_metadata":{"id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","num_bytes":664,"mime_essence":"text/plain","charset":null},"blob_id":"0ee84b84c29924b210e3576fe9d1e8632948bedc","location":{"offset_span":{"start":617,"end":660},"source_span":{"start":{"line":16,"column":17},"end":{"line":16,"column":59}}},"capture_group_index":1,"match_content":"secret","snippet":{"before":"E = \"https://testwebsite.com\"\n ","matching":"secret","after":"testing\"\n\n\n"},"rule_name":"Generic Username and Password (unquoted)"}]}
+
+~~~
+
+If the first line is expanded, it looks like this:
+
+~~~
+{
+    "type": "finding",
+    "rule_name": "Generic API Key",
+    "match_content": "32ui1ffdasfhu239b4df2ac6609a9919",
+    "num_matches": 1,
+    "matches": [
+        {
+            "provenance": [
+                {
+                    "kind": "file",
+                    "path": "./app/schema/config.py"
+                },
+                {
+                    "kind": "git_repo",
+                    "repo_path": "./.git",
+                    "commit_provenance": {
+                        "commit_kind": "first_seen",
+                        "commit_metadata": {
+                            "commit_id": "0ee84b84c29924b210e3576fe9d1e8632948bedc",
+                            "committer_name": "Princess Leia",
+                            "committer_email": "leia@test.com",
+                            "committer_timestamp": "1685495256 +0000",
+                            "author_name": "Princess Leia",
+                            "author_email": "leia@test.com",
+                            "author_timestamp": "1685495256 +0000",
+                            "message": "framework\n"
+                        },
+                        "blob_path": "app/schema/config.py"
+                    }
+                }
+            ],
+            "blob_metadata": {
+                "id": "0ee84b84c29924b210e3576fe9d1e8632948bedc",
+                "num_bytes": 664,
+                "mime_essence": "text/plain",
+                "charset": null
+            },
+            "blob_id": "0ee84b84c29924b210e3576fe9d1e8632948bedc",
+            "location": {
+                "offset_span": {
+                    "start": 617,
+                    "end": 660
+                },
+                "source_span": {
+                    "start": {
+                        "line": 16,
+                        "column": 17
+                    },
+                    "end": {
+                        "line": 16,
+                        "column": 59
+                    }
+                }
+            },
+            "capture_group_index": 1,
+            "match_content": "32ui1ffdasfhu239b4df2ac6609a9919",
+            "snippet": {
+                "before": "E = \"https://testwebsite.com\"\n ",
+                "matching": "API_KEY = \"32ui1ffdasfhu239b4df2ac6609a9919",
+                "after": "\"\n\n\n"
+            },
+            "rule_name": "Generic API Key"
+        }
+    ]
+}
+~~~
--- dojo/tools/noseyparker/parser.py
+++ dojo/tools/noseyparker/parser.py
@@ -1,7 +1,6 @@
 import hashlib
 import json
 
-
 from datetime import datetime
 from dojo.models import Finding
 
@@ -18,180 +17,86 @@ class NoseyParkerParser(object):
         return "Nosey Parker Scan"
 
     def get_description_for_scan_types(self, scan_type):
-        return "Nosey Parker report file can be imported in JSON Lines format (option --jsonl)."
+        return f"Nosey Parker report file can be imported in JSON Lines format (option --jsonl). " \
+               f"Supports v0.15.0 of https://github.com/praetorian-inc/noseyparker"
 
-    def get_findings(self, file, test, reporter):
-        """
-        Returns findings from jsonlines file
-        """
-        dupes = {}
-        # Turn JSONL file into DataFrame
-        if file.name.lower().endswith(".jsonl") or file.name.lower().endswith(".json"):
-            # Process jsonlines into Dict
-            data = [json.loads(line) for line in file]
-
-            # Check for empty file
-            if len(len(data)) == 0:
-                return []
-
-            # Parse through each secret of each Json line
-            for item in data:
-                # Set rule to the current secret type (e.g AWS S3 Bucket)
-                key = item['rule_name']
-                # Number of identical secret matches
-                num_matches = item['num_matches']
-                severity = "High"
-
-                # First finding in json list
-                first_finding = item['matches'][0]
-
-                # Set Finding details
-                title = f"Secret(s) Found in Repository with Commit ID {first_finding['blob_id']}"
-                description = f"Secret found of type:   {key} \n" \
-                              f"SECRET starts with:  {secret[:3]} on line number {line_num} \n" \
-                              f"This secret was found {num_matches} time(s) \n" \
-                              f"**Committer Name: ** {first_finding['provenance']['commit_provenance']['committer_name']}  \n" \
-                              f"**Committer Email: ** {first_finding['provenance']['commit_provenance']['committer_email']} \n"
-
-                line_num = first_finding['location']['source_span']['start']['line']
-                secret = item['match_content']
-                filepath = first_finding['provenance.path']
-                reproduce = f"**First Occurrence of secret: ** \n" \
-                            f"Snippet: {first_finding['snippet']['before']}***SECRET***{first_finding['snippet']['after']} \n" \
-                            f"Location: {filepath} line #{line_num}"
-                description += reproduce
-
-                # Internal de-duplication
-                dupe_key = hashlib.sha256(str(filepath + secret).encode('utf-8')).hexdigest()
-                if dupe_key in dupes:
-                    find = dupes[dupe_key]
-                    if finding.description:
-                        find.description += "\n" + finding.description
-                    finding.nb_occurences += 1
-                    dupes[dupe_key] = find
-                else:
-                    dupes[dupe_key] = True
-                    # Create Finding object
-                    finding = Finding(
-                        test=test,
-                        cwe=798,
-                        title=title,
-                        description=description,
-                        steps_to_reproduce=reproduce,
-                        severity=severity,
-                        mitigation="Please reset the account/token and remove ALL occurences of this secret from source code. "
-                                   "Store secrets/tokens/passwords in secret managers or secure vaults.",
-                        reporter=reporter,
-                        date=datetime.today().strftime("%Y-%m-%d"),
-                        verified='false',
-                        active='true',
-                        is_mitigated='false',
-                        file_path=filepath,
-                        line=line_num,
-                        static_finding=True,
-                        dynamic_finding=False
-
-                    )
-                    dupes[dupe_key] = finding
-            else:
-                raise ValueError("Format is not recognized for NoseyParker")
-
-
-
-    def get_findings(self, file, test, filter, reporter):
+    def get_findings(self, file, test):
         """
         Returns findings from jsonlines file and uses filter
         to skip findings and determine severity
         """
         dupes = {}
 
-        # Filter
-        filter_dict = self.parse_filter(filter)
-
         # Turn JSONL file into DataFrame
-        if file.name.lower().endswith(".jsonl") or file.name.lower().endswith(".json"):
-            # Process jsonlines into Dict
+        if file is None:
+            return
+        elif file.name.lower().endswith(".jsonl"):
+            # Process JSON lines into Dict
             data = [json.loads(line) for line in file]
 
             # Check for empty file
-            if len(len(data)) == 0:
+            if len(data[0]) == 0:
                 return []
 
-
-            # Parse through each secret of each Json line
-            for item in data:
-                # Set rule to the current secret type (e.g AWS S3 Bucket)
-                key = item['rule_name']
-                # Number of identical secret matches
-                num_matches = item['num_matches']
-                severity = "High"
-
-                # Check if Filter dictionary indicates to Skip finding
-                if key in filter_dict:
-                    if filter_dict[key]['Skip'] == "True":
-                        return []
-                    else:
-                        # Get severity from filter json
-                        severity = filter_dict[key]['Priority']
-
-                # First finding in json list
-                first_finding = item['matches'][0]
+            # Parse through each secret in each JSON line
+            for line in data:
+                # Set rule to the current secret type (e.g. AWS S3 Bucket)
+                try:
+                    rule_name = line['rule_name']
+                    secret = line['match_content']
+                except Exception:
+                    raise ValueError("Invalid Nosey Parker data, make sure to use Nosey Parker v0.15.0")
 
                 # Set Finding details
-                title = f"Secret(s) Found in Repository with Commit ID {first_finding['blob_id']}"
-                description = f"Secret found of type:   {key} \n" \
-                              f"SECRET starts with:  {secret[:3]} on line number {line_num} \n" \
-                              f"This secret was found {num_matches} time(s) \n" \
-                              f"**Committer Name: ** {first_finding['provenance']['commit_provenance']['committer_name']}  \n" \
-                              f"**Committer Email: ** {first_finding['provenance']['commit_provenance']['committer_email']} \n"
+                for match in line['matches']:
 
-                line_num = first_finding['location']['source_span']['start']['line']
-                secret = item['match_content']
-                filepath = first_finding['provenance.path']
-                reproduce = f"**First Occurrence of secret: ** \n" \
-                            f"Snippet: {first_finding['snippet']['before']}***SECRET***{first_finding['snippet']['after']} \n" \
-                            f"Location: {filepath} line #{line_num}"
-                description += reproduce
+                    title = f"Secret(s) Found in Repository with Commit ID {match['blob_id']}"
+                    filepath = match['provenance'][0]['path']
+                    line_num = match['location']['source_span']['start']['line']
+                    description = f"Secret found of type:   {rule_name} \n" \
+                                  f"SECRET starts with:  '{secret[:3]}' on line number {line_num} \n" \
+                                  f"Committer Name: {match['provenance'][1]['commit_provenance']['commit_metadata']['committer_name']}  \n" \
+                                  f"Committer Email: {match['provenance'][1]['commit_provenance']['commit_metadata']['committer_email']} \n \n"
 
-                # Internal de-duplication
-                dupe_key = hashlib.sha256(str(filepath + secret).encode('utf-8')).hexdigest()
-                if dupe_key in dupes:
-                    find = dupes[dupe_key]
-                    if finding.description:
-                        find.description += "\n" + finding.description
-                    finding.nb_occurences += 1
-                    dupes[dupe_key] = find
-                else:
-                    dupes[dupe_key] = True
-                    # Create Finding object
-                    finding = Finding(
-                        test=test,
-                        cwe=798,
-                        title=title,
-                        description=description,
-                        steps_to_reproduce=reproduce,
-                        severity=severity,
-                        mitigation="Please reset the account/token and remove ALL occurences of this secret from source code. "
-                                   "Store secrets/tokens/passwords in secret managers or secure vaults.",
-                        reporter=reporter,
-                        date=datetime.today().strftime("%Y-%m-%d"),
-                        verified='false',
-                        active='true',
-                        is_mitigated='false',
-                        file_path=filepath,
-                        line=line_num,
-                        static_finding=True,
-                        dynamic_finding=False
+                    reproduce = f"Location: {filepath} line #{line_num}" \
+                                f"Snippet: {match['snippet']['before']}***SECRET***{match['snippet']['after']} \n" \
 
-                    )
-                    dupes[dupe_key] = finding
+                    description += reproduce
+
+                    # Internal de-duplication
+                    key = hashlib.md5((filepath + "|" + secret + "|" + str(line_num)).encode("utf-8")).hexdigest()
+
+                    if key in dupes:
+                        finding = dupes[key]
+                        if finding.description:
+                            finding.description += "\n" + finding.description
+                        finding.nb_occurences += 1
+                        dupes[key] = finding
+                    else:
+                        dupes[key] = True
+                        # Create Finding object
+                        finding = Finding(
+                            test=test,
+                            cwe=798,
+                            title=title,
+                            description=description,
+                            steps_to_reproduce=reproduce,
+                            severity='High',
+                            mitigation=f"Reset the account/token and remove occurrences of this secret from source "
+                                       f"code. Store secrets/tokens/passwords in secret managers or secure vaults.",
+                            date=datetime.today().strftime("%Y-%m-%d"),
+                            verified=False,
+                            active=True,
+                            is_mitigated=False,
+                            file_path=filepath,
+                            line=line_num,
+                            static_finding=True,
+                            nb_occurences=1,
+                            dynamic_finding=False
+
+                        )
+                        dupes[key] = finding
         else:
-            raise ValueError("Format is not recognized for NoseyParker")
+            raise ValueError("JSON lines format not recognized. Make sure to use Nosey Parker v0.15.0")
 
         return list(dupes.values())
-
-    def parse_filter(self, filter_file):
-        # Parse Filter JSON file into Dictionary
-
-        filter_dict = json.load(filter_file)
-        return filter_dict
