526319fac9ab88012b7bbc0581c53667445f2dd8
tpat13 <32806320+tpat13@users.noreply.github.com>
2023-12-04 00:07:58 -0500

    Nosey Parker Test Cases

---
+++ unittests/tools/test_noseyparker_parser.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from dojo.tools.noseyparker.parser import NoseyParkerParser
+from dojo.models import Test
+
+
+class TestNoseyParkerParser(TestCase):
+
+    def test_noseyparker_parser__no_vulns(self):
+        testfile = open("unittests/scans/noseyparker/noseyparker_zero_vul.jsonl")
+        parser = NoseyParkerParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(0, len(findings))
+        testfile.close()
+
+    def test_noseyparker_parser_one_vuln(self):
+        testfile = open("unittests/scans/noseyparker/noseyparker_one_vul.jsonl")
+        parser = NoseyParkerParser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        finding = findings[0]
+        self.assertEqual("./app/schema/config.py", findings[0].file_path)
+        self.assertEqual("High", finding.severity)
+        self.assertEqual(798, finding.cwe)
+        self.assertEqual(1, len(findings))
+
+    def test_noseyparker_parser_many_vulns(self):
+        # Testfile contains 4 lines (Middle 2 are duplicates and last line has 2 of the same exact matches)
+        testfile = open("unittests/scans/noseyparker/noseyparker_many_vul.jsonl")
+        parser = NoseyParkerParser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        for finding in findings:
+            self.assertEqual("High", finding.severity)
+            self.assertEqual(798, finding.cwe)
+        self.assertEqual(3, len(findings))
+
+    def test_noseyparker_parser_error(self):
+        with self.assertRaises(ValueError) as context:
+            testfile = open("unittests/scans/noseyparker/empty_with_error.json")
+            parser = NoseyParkerParser()
+            findings = parser.get_findings(testfile, Test())
+            testfile.close()
+            self.assertTrue(
+                "Invalid Nosey Parker data, make sure to use Nosey Parker v0.15.0" in str(context.exception)
+            )
+            self.assertTrue("ECONNREFUSED" in str(context.exception))
