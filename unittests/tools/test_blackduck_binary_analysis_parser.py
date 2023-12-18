01fed4690bb3026422713149c2391a21caf1ca09
Jacob Hoopes <jake.hoopes@gmail.com>
2023-12-18 10:05:56 -0700

    Initial implementation of Black Duck Binary Analysis Parser - #bugfixes in unit tests.

--- unittests/tools/test_blackduck_binary_analysis_parser.py
+++ unittests/tools/test_blackduck_binary_analysis_parser.py
@@ -1,17 +1,18 @@
 from ..dojo_test_case import DojoTestCase
 from dojo.tools.blackduck_binary_analysis.parser import BlackduckBinaryAnalysisParser
 from dojo.models import Test
+from pathlib import Path
 
 
 class TestBlackduckBinaryAnalysisParser(DojoTestCase):
     def test_parse_no_vulns(self):
-        testfile = open("unittests/scans/blackduck_binary_analysis/no_vuln.csv")
+        testfile = Path(get_unit_tests_path() + "/scans/blackduck_binary_analysis/no_vuln.csv")
         parser = BlackduckBinaryAnalysisParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(0, len(findings))
 
     def test_parse_one_vuln(self):
-        testfile = open("unittests/scans/blackduck_binary_analysis/one_vuln.csv")
+        testfile = Path(get_unit_tests_path() + "/scans/blackduck_binary_analysis/one_vuln.csv")
         parser = BlackduckBinaryAnalysisParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(1, len(findings))
@@ -29,7 +30,7 @@ class TestBlackduckBinaryAnalysisParser(DojoTestCase):
             self.assertIsNotNone(finding.unique_id_from_tool)
 
     def test_parse_many_vulns(self):
-        testfile = open("unittests/scans/blackduck_binary_analysis/many_vulns.csv")
+        testfile = Path(get_unit_tests_path() + "/scans/blackduck_binary_analysis/many_vulns.csv")
         parser = BlackduckBinaryAnalysisParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(5, len(findings))
