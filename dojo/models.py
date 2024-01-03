38f4d228d96f20b3e582e5fa33d7eaaab80ac460
kiblik <tomas@kubla.sk>
2024-01-03 17:47:12 +0100

    API: Add Announcements (#9112)
    * Add Announcement to API
    
    * Add test_rest_framework
    
    * Add test_swagger_schema
    
    * Flake8
    
    * Fix count
    
    * Skip test
    
    * Inc db_mig
    
    * Use DojoModelViewSet
    
    * inc db_mig

--- dojo/models.py
+++ dojo/models.py
@@ -3502,9 +3502,14 @@ class Announcement(models.Model):
     message = models.CharField(max_length=500,
                                 help_text=_("This dismissable message will be displayed on all pages for authenticated users. It can contain basic html tags, for example <a href='https://www.fred.com' style='color: #337ab7;' target='_blank'>https://example.com</a>"),
                                 default='')
-    dismissable = models.BooleanField(default=False, null=True, blank=True)
     style = models.CharField(max_length=64, choices=ANNOUNCEMENT_STYLE_CHOICES, default='info',
                             help_text=_("The style of banner to display. (info, success, warning, danger)"))
+    dismissable = models.BooleanField(default=False,
+                                      null=False,
+                                      blank=True,
+                                      verbose_name=_('Dismissable?'),
+                                      help_text=_('Ticking this box allows users to dismiss the current announcement'),
+                                      )
 
 
 class UserAnnouncement(models.Model):
