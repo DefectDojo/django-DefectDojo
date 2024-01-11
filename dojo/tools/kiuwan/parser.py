c6c7e8142bde4e5f1cded83ae3e9b52bb6e5388d
Florian Markus <florian.markus@anic-gmbh.de>
2024-01-11 14:39:40 +0100

    Fix defects import

--- dojo/tools/kiuwan/parser.py
+++ dojo/tools/kiuwan/parser.py
@@ -62,7 +62,7 @@ class KiuwanParser(object):
                 + row["Software characteristic"]
                 + "\n\n"
                 + "**Vulnerability type** : "
-                + row["Vulnerability type"]
+                + (row["Vulnerability type"] if "Vulnerability type" in row else "")
                 + "\n\n"
                 + "**CWE Scope** : "
                 + row["CWE Scope"]
--- unittests/tools/test_kiuwan_parser.py
+++ unittests/tools/test_kiuwan_parser.py
@@ -6,7 +6,6 @@ from dojo.models import Test
 class TestKiuwanParser(DojoTestCase):
 
     def test_parse_file_with_no_vuln_has_no_findings(self):
-
         testfile = open("unittests/scans/kiuwan/kiuwan_no_vuln.csv")
         parser = KiuwanParser()
         findings = parser.get_findings(testfile, Test())
@@ -23,6 +22,12 @@ class TestKiuwanParser(DojoTestCase):
         parser = KiuwanParser()
         findings = parser.get_findings(testfile, Test())
         self.assertEqual(131, len(findings))
+
+    def test_parse_file_with_defects(self):
+        testfile = open("unittests/scans/kiuwan/kiuwan_defects.csv")
+        parser = KiuwanParser()
+        findings = parser.get_findings(testfile, Test())
+        self.assertEqual(1, len(findings))
 
     def test_parse_file_issue_9308(self):
         testfile = open("unittests/scans/kiuwan/issue_9308.csv")
