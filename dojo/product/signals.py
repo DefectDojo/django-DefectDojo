b7dfc74a319a5290b993d2d8cf47e512a1da70c6
Blake Owens <blake@defectdojo.com>
2024-01-10 21:43:04 -0600

    move product save check to signal

--- dojo/product/signals.py
+++ dojo/product/signals.py
@@ -9,6 +9,22 @@ from dojo.product import helpers as async_product_funcs
 logger = logging.getLogger(__name__)
 
 
+@receiver(signals.pre_save, sender=Product)
+def initial_product_save_sla_configuration(sender, instance, **kwargs):
+    sla_config = getattr(Product.objects.filter(id=instance.id).first(), 'sla_configuration', None)
+    if sla_config:
+        instance._old_sla_configuration = sla_config
+
+
+@receiver(signals.post_save, sender=Product)
+def post_product_save_sla_configuration(sender, instance, **kwargs):
+    old_sla_config = getattr(instance, '_old_sla_configuration', None)
+    if old_sla_config and old_sla_config != instance.sla_configuration:
+        for f in Finding.objects.filter(test__engagement__product=instance):
+            f.set_sla_expiration_date()
+            f.save()
+
+
 @receiver(signals.m2m_changed, sender=Product.tags.through)
 def product_tags_post_add_remove(sender, instance, action, **kwargs):
     if action in ["post_add", "post_remove"]:
