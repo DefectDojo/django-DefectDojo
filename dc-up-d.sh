b0a00597c2f1484fc989c8cf2dc045b309387d12
kiblik <tomas@kubla.sk>
2024-01-19 03:16:39 +0100

    Fix shellcheck (#9147)
    * Fix shellcheck
    
    * Update docker/entrypoint-unit-tests-devDocker.sh
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>

--- dc-up-d.sh
+++ dc-up-d.sh
@@ -7,7 +7,7 @@ if [[ $? -eq 1 ]]; then exit 1; fi
 
 if [ $# -eq 0 ]
 then
-    if [ -z $DD_PROFILE ]
+    if [ -z "$DD_PROFILE" ]
     then
         echo "No profile supplied, running default: postgres-redis"
         PROFILE="postgres-redis"
@@ -29,4 +29,4 @@ fi
 echo "Starting docker compose with profile $PROFILE in the background ..."
 
 # Compose V2 integrates compose functions into the Docker platform, continuing to support most of the previous docker-compose features and flags. You can run Compose V2 by replacing the hyphen (-) with a space, using docker compose, instead of docker-compose.
-docker compose --profile $PROFILE --env-file ./docker/environments/$PROFILE.env up --no-deps -d
+docker compose --profile "$PROFILE" --env-file "./docker/environments/$PROFILE.env" up --no-deps -d
--- docker/docker-compose-check.sh
+++ docker/docker-compose-check.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-main=`docker compose  version  --short | cut -d '.' -f 1`
-minor=`docker compose  version  --short | cut -d '.' -f 2`
-current=`docker compose  version  --short`
+main=$(docker compose  version  --short | cut -d '.' -f 1)
+minor=$(docker compose  version  --short | cut -d '.' -f 2)
+current=$(docker compose  version  --short)
 
 echo 'Checking docker compose version'
 if [[ $main -lt 2 ]]; then
