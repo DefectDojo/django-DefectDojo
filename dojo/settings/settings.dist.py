a54cd22f413f3f6cb8bab440ca6910c006738eb6
renejal <beimarjal@unicauca.edu.co>
2024-01-23 11:40:08 -0500

    add Permission-Policy

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -809,6 +809,33 @@ SESSION_COOKIE_SAMESITE = env("DD_SESSION_COOKIE_SAMESITE")
 CSRF_COOKIE_SECURE = env("DD_CSRF_COOKIE_SECURE")
 CSRF_COOKIE_SAMESITE = env("DD_CSRF_COOKIE_SAMESITE")
 
+# Content Policy
+PERMISSIONS_POLICY = {
+    "accelerometer": [],
+    "ambient-light-sensor": [],
+    "autoplay": [],
+    "camera": [],
+    "display-capture": [],
+    "document-domain": [],
+    "encrypted-media": [],
+    "fullscreen": [],
+    "gamepad": [],
+    "geolocation": [],
+    "gyroscope": [],
+    "magnetometer": [],
+    "microphone": [],
+    "midi": [],
+    "payment": [],
+    "picture-in-picture": [],
+    "publickey-credentials-get": [],
+    "speaker-selection": [],
+    "sync-xhr": [],
+    "usb": [],
+    "screen-wake-lock": [],
+    "web-share": [],
+    "xr-spatial-tracking": []
+}
+
 # Content Security Policy
 CSP_INCLUDE_NONCE_IN = ["script-src"]
 CSP_DEFAULT_SRC = "'self'"
@@ -1012,6 +1039,7 @@ DJANGO_MIDDLEWARE_CLASSES = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "django_permissions_policy.PermissionsPolicyMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
