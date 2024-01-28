14ce4a2a0a53eea1ae90048f0aab054b4dd7bebf
Manuel Sommer <sommermanuel@gmx.de>
2024-01-28 21:23:10 +0100

    add severity

--- dojo/tools/kubescape/parser.py
+++ dojo/tools/kubescape/parser.py
@@ -12,6 +12,14 @@ class KubescapeParser(object):
     def get_description_for_scan_types(self, scan_type):
         return "Import result of Kubescape JSON output."
 
+    def severity_mapper(self, input):
+        if input == 1:
+            return "Low"
+        elif input == 2:
+            return "Medium"
+        elif input == 3:
+            return "High"
+
     def get_findings(self, filename, test):
         findings = []
         try:
@@ -22,12 +30,22 @@ class KubescapeParser(object):
         for resource in data["resources"]:
             resourceid = resource["resourceID"]
             results = ([each for each in data["results"] if each.get('resourceID') == resourceid])
-            """TODO, PARSE THE RIGHT VALUES INTO THE FINDING"""
-            i+=1
-            find = Finding(title="title"+str(i),
-            test=test,
-            description="message",
-            severity="High",
-            static_finding=False)
-            findings.append(find)
+            controls = results[0].get("controls", [])
+            try:
+                prioritizedResource = results[0]["prioritizedResource"]["severity"]
+            except KeyError:
+                prioritizedResource = "Info"
+            for control in controls:
+                """TODO, PARSE THE RIGHT VALUES INTO THE FINDING"""
+                if self.severity_mapper(prioritizedResource) == None:
+                    severity = "Info"
+                else:
+                    severity = self.severity_mapper(prioritizedResource)
+                i+=1
+                find = Finding(title="title"+str(i),
+                test=test,
+                description="message",
+                severity=severity,
+                static_finding=False)
+                findings.append(find)
         return findings
