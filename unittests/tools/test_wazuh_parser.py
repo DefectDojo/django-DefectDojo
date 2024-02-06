710d498f1a6338509e1e5f562507fa549dbd4aef
Quirin Hardy Zießler <quirin.ziessler@outlook.com>
2024-02-07 00:11:53 +0100

    :tada: Improvements for wazuh importer (#9248)
    * improvement for wazuh importer
    
    * :wrench: change on dedupe for Wazuh
    
    * :wrench: change on dedupe for Wazuh
    
    * :memo:
    
    * :pencil2:
    
    * :memo:
    
    * :memo:
    
    * flake8
    
    * :tada: recoded wazuh importer to support endpoints
    
    * :white_check_mark: adjusted unittests
    
    * :memo:
    
    * :pencil2:
    
    * :pencil2:
    
    ---------
    
    Co-authored-by: Cody Maffucci <46459665+Maffooch@users.noreply.github.com>

--- unittests/tools/test_wazuh_parser.py
+++ unittests/tools/test_wazuh_parser.py
@@ -20,7 +20,10 @@ class TestWazuhParser(DojoTestCase):
                 endpoint.clean()
         self.assertEqual(1, len(findings))
         self.assertEqual("Medium", finding.severity)
-        self.assertEqual("CVE-1234-123123", finding.unsaved_vulnerability_ids[0])
+        self.assertEqual("CVE-1234-123123", finding.unsaved_vulnerability_ids)
+        self.assertEqual("asdf", finding.component_name)
+        self.assertEqual("4.3.1", finding.component_version)
+        self.assertEqual(5.5, finding.cvssv3_score)
 
     def test_parse_many_finding(self):
         testfile = open("unittests/scans/wazuh/many_findings.json")
@@ -30,3 +33,19 @@ class TestWazuhParser(DojoTestCase):
             for endpoint in finding.unsaved_endpoints:
                 endpoint.clean()
         self.assertEqual(6, len(findings))
+
+    def test_parse_one_finding_with_endpoint(self):
+        testfile = open("unittests/scans/wazuh/one_finding_with_endpoint.json")
+        parser = WazuhParser()
+        findings = parser.get_findings(testfile, Test())
+        for finding in findings:
+            for endpoint in finding.unsaved_endpoints:
+                endpoint.clean()
+        self.assertEqual(1, len(findings))
+        self.assertEqual("Medium", finding.severity)
+        self.assertEqual("CVE-1234-1234", finding.unsaved_vulnerability_ids)
+        self.assertEqual(6.5, finding.cvssv3_score)
+        endpoint = finding.unsaved_endpoints[0]
+        self.assertEqual("agent-1", endpoint.host)
+        self.assertEqual("asdf", finding.component_name)
+        self.assertEqual("1", finding.component_version)
