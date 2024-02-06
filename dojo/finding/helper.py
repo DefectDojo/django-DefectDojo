d103d85c969f25bde4a10300423479b47dcf1134
Sean Reid <sean@defectdojo.com>
2024-02-06 14:41:44 -0500

    sc-4117 Remove setting of epps to None in finding template helper method

--- dojo/finding/helper.py
+++ dojo/finding/helper.py
@@ -667,6 +667,3 @@ def save_vulnerability_ids_template(finding_template, vulnerability_ids):
         finding_template.cve = vulnerability_ids[0]
     else:
         finding_template.cve = None
-
-    # Remove stored EPSS info
-    finding_template.epss_score = finding_template.epss_percentile = None
