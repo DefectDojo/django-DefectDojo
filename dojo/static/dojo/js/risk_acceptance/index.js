8ef0cfd76239d77da2f04eefda141543f982667c
renejal <beimarjal@unicauca.edu.co>
2024-02-20 08:46:25 -0500

    disabled popup

--- dojo/static/dojo/js/risk_acceptance/index.js
+++ dojo/static/dojo/js/risk_acceptance/index.js
@@ -1,41 +1,50 @@
-$(document).ready(function(){
-    $(document).on("click", "#btn_accept_risk_reject", function(){
-        updateModal("Reject","\n Are you sure to reject the finding?\n",
-        "btn btn-warning",
-        "confirmed_action_risk_pending",
-        "id_modal_message",
-        "risk_accept_decline")
-        $('#confirmModal').modal('show');
+    $(document).ready(function(){
+        var form = null;
+        $(document).on("click", ".btn-warning", function(){
+            form = $(this).closest('form');
+            input = form.find('.action-tranfer-finding')
+            input.attr('name', 'risk_accept_decline');
+            updateModal("Reject","\n Are you sure to reject the finding?\n",
+            "btn btn-warning",
+            "#confirmed_action_risk_pending",
+            "#id_modal_message")
+            $('#confirmModal').modal('show');
+        });
+
+        $(document).on("click", ".btn-success", function(){
+            form = $(this).closest('form');
+            input = form.find('.action-tranfer-finding')
+            input.attr('name', 'risk_accept_pending');
+            updateModal("Accept",
+            "\n Are you sure to Accept the finding?\n",
+            "btn btn-success",
+            "#confirmed_action_risk_pending",
+            "#id_modal_message")
+            $('#confirmModal').modal('show');
+        });
+
+        $(document).on("click", ".btn-danger", function(){
+            form = $(this).closest('form');
+            input = form.find('action-tranfer-finding')
+            input.name = "remove_finding"
+            updateModal("Remove",
+            "\n Are you sure to remove the finding?\n",
+            "btn btn-danger",
+            "#confirmed_action_risk_pending",
+            "#id_modal_message")
+            $('#confirmModal').modal('show');
+        });
+
+        $(document).on('click','#confirmed_action_risk_pending', function(){
+            form.submit();
+        });
+
     });
 
-    $(document).on("click", "#btn_accept_risk_accept", function(){
-        updateModal("Accept",
-        "\n Are you sure to Accept the finding?\n",
-        "btn btn-success",
-        "confirmed_action_risk_pending",
-        "id_modal_message",
-        "risk_accept_pending",
-        "form_risk_pending_id")
-        $('#confirmModal').modal('show');
-    });
-
-    $(document).on("click", "#btn_accept_risk_remove", function(){
-        updateModal("Remove",
-        "\n Are you sure to remove the finding?\n",
-        "btn btn-danger",
-        "confirmed_action_risk_pending",
-        "id_modal_message",
-        "remove_finding")
-        $('#confirmModal').modal('show');
-    });
-
-});
-
-function updateModal(action, message, class_name, button_id, id_modal_message, action_name){
-    let buttonElement = document.getElementById(button_id);
-    let modalMessageElement = document.getElementById(id_modal_message);
-    buttonElement.className = class_name;
-    buttonElement.value = action;
-    buttonElement.name = action_name; 
-    modalMessageElement.innerHTML = message;
-}
+    function updateModal(action, message, class_name, button, id_modal_message){
+        buttonElement = document.getElementById(button);
+        let modalMessageElement = document.getElementById(id_modal_message);
+        buttonElement.addClass(class_name);
+        buttonElement.val(action); 
+        modalMessageElement.innerHTML = message;
+    }
