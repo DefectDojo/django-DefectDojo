3b5c0564c112d114a8fd6f8ef3c0ed1779b4b2e1
kiblik <tomas@kubla.sk>
2024-02-28 13:24:57 +0000

    Remove filterwarnings for RemovedInDjango50Warning (#9500)
    * Drop RemovedInDjango50Warning from filterwarnings
    
    * Remove CryptPasswordHasher from PASSWORD_HASHERS

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -490,7 +490,6 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedSHA1PasswordHasher',
     'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
-    'django.contrib.auth.hashers.CryptPasswordHasher',
 ]
 
 SOCIAL_AUTH_PIPELINE = (
@@ -1701,10 +1700,3 @@ USE_FIRST_SEEN = env('DD_USE_FIRST_SEEN')
 # Reference issue: https://github.com/jazzband/django-polymorphic/issues/229
 warnings.filterwarnings("ignore", message="polymorphic.base.ManagerInheritanceWarning.*")
 warnings.filterwarnings("ignore", message="PolymorphicModelBase._default_manager.*")
-
-
-# TODO - these warnings needs to be removed after all warnings have been removed
-if DEBUG:
-    from django.utils.deprecation import RemovedInDjango50Warning
-    warnings.filterwarnings("ignore", category=RemovedInDjango50Warning)
-    warnings.filterwarnings("ignore", message="unclosed file .+")
