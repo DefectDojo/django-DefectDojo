e0d06287e6b081839c9592ddc7e12d38db610f41
Felix Hernandez <ffhg_920522@hotmail.com>
2024-02-28 19:26:56 -0600

    fix unittests SnykCodeParser

--- unittests/tools/test_snyk_code_parser.py
+++ unittests/tools/test_snyk_code_parser.py
@@ -5,8 +5,15 @@ from dojo.tools.snyk_code.parser import SnykCodeParser
 
 class TestSnykCodeParser(DojoTestCase):
 
+    def test_snykParser_single_has_many_findings(self):
+        testfile = open("unittests/scans/snyk_code/single_project_many_vulns.json")
+        parser = SnykCodeParser()
+        findings = parser.get_findings(testfile, Test())
+        testfile.close()
+        self.assertEqual(206, len(findings))
+
     def test_snykcode_issue_9270(self):
-        with open("unittests/scans/snyk/snykcode_issue_9270.json") as testfile:
+        with open("unittests/scans/snyk_code/snykcode_issue_9270.json") as testfile:
             parser = SnykCodeParser()
             findings = parser.get_findings(testfile, Test())
             self.assertEqual(39, len(findings))
