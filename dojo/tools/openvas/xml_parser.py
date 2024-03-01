20def1d125c7656fa873c2ec1fad72f569a2e51f
Manuel Sommer <sommermanuel@gmx.de>
2024-03-01 09:41:03 +0100

    adapt csv parser

--- dojo/tools/openvas/xml_parser.py
+++ dojo/tools/openvas/xml_parser.py
@@ -1,3 +1,12 @@
+import csv
+import hashlib
+import io
+from dateutil.parser import parse
+from xml.dom import NamespaceErr
+from defusedxml import ElementTree as ET
+from dojo.models import Finding, Endpoint
+
+
 class OpenVASXMLParser(object):
     def __init__(self) -> None:
         pass
