a2d23cf53f4a0d6d40424bd09e877a32b5217115
renejal <beimarjal@unicauca.edu.co>
2024-03-05 08:16:52 -0500

    add transfer finding function

--- dojo/transfer_findings/helper.py
+++ dojo/transfer_findings/helper.py
@@ -1,5 +1,8 @@
+import copy
+import datetime
 from crum import get_current_user
 from dojo.risk_acceptance import risk_pending
+from dojo.models import Test, Finding, Product, Engagement, Test_Type
 from dojo.authorization.authorization import (
     user_has_global_permission,
     user_has_permission,
@@ -12,6 +15,17 @@ def get_permissions_tranfer_finding(permission):
     permission_result = user_has_global_permission(user, permission)
     roles = roles_permissions.get_roles_with_permissions()
 
+
+def transfer_finding(finding: Finding, engagement: Engagement):
+    test_type = Test_Type.objects.create(name='Transfer_finding', static_tool=True)
+    test = Test.objects.create(engagement=engagement, test_type=test_type,
+                                     target_start=datetime.date(2000, 2, 1), target_end=datetime.date(2000, 2, 1))
+    obj_finding = Finding(test=test, title=finding.title, cve=finding.cve, severity=finding.severity, verified=finding.verified,
+            description=finding.description, mitigation=finding.mitigation, impact=finding.impact,
+            reporter=finding.reporter, numerical_severity=finding.numerical_severity,
+            static_finding=finding.static_finding, dynamic_finding=finding.dynamic_finding
+            )
+    obj_finding.save()
 
 
 
