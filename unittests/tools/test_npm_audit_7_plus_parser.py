d3b4c791e9364b84261e44f7c9e4947f24f344e0
Jay Paz <jay@defectdojo.com>
2024-03-06 16:18:44 -0600

    added field validation

--- unittests/tools/test_npm_audit_7_plus_parser.py
+++ unittests/tools/test_npm_audit_7_plus_parser.py
@@ -18,6 +18,13 @@ class TestNpmAudit7PlusParser(DojoTestCase):
         findings = parser.get_findings(testfile, Test())
         testfile.close()
         self.assertEqual(1, len(findings))
+        with self.subTest(i=0):
+            finding = findings[0]
+            self.assertEqual("High", finding.severity)
+            self.assertEqual(400, finding.cwe)
+            self.assertIsNotNone(finding.description)
+            self.assertGreater(len(finding.description), 0)
+            self.assertEqual("CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:N/I:N/A:L", finding.cvssv3)
 
     def test_npm_audit_7_plus_parser_with_many_vuln_has_many_findings(self):
         testfile = open(path.join(path.dirname(__file__), "../scans/npm_audit_7_plus/many_vulns.json"))
@@ -25,3 +32,10 @@ class TestNpmAudit7PlusParser(DojoTestCase):
         findings = parser.get_findings(testfile, Test())
         testfile.close()
         self.assertEqual(6, len(findings))
+        with self.subTest(i=0):
+            finding = findings[0]
+            self.assertEqual("Medium", finding.severity)
+            self.assertEqual(1035, finding.cwe)
+            self.assertIsNotNone(finding.description)
+            self.assertGreater(len(finding.description), 0)
+            self.assertEqual("@Vercel/Fun", finding.title)
