139a70c2a3c908de6badfd0d53d6f94a249364f7
Jay Paz <jay.paz@gmail.com>
2024-03-06 20:57:41 -0600

    Update dojo/tools/npm_audit_7_plus/parser.py
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>

--- dojo/tools/npm_audit_7_plus/parser.py
+++ dojo/tools/npm_audit_7_plus/parser.py
@@ -220,7 +220,7 @@ def get_vuln_description(item_node, tree):
             else:
                 if tree[effect]["name"] != ev:
                     description = (description +
-                                   "  Depends on vulnearble versions of " +
+                                   "  Depends on vulnerable versions of " +
                                    ev + "\n")
         for en in tree[effect]["nodes"]:
             description = description + "  " + en + "\n"
