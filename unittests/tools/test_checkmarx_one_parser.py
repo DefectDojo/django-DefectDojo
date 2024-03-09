de14d6adc602838dad3f8dddd11aee628596a8a9
Matt Tesauro <mtesauro@gmail.com>
2024-03-09 02:45:13 -0600

    Make unit tests smaller

--- unittests/tools/test_checkmarx_one_parser.py
+++ unittests/tools/test_checkmarx_one_parser.py
@@ -10,7 +10,7 @@ class TestCheckmarxOneParser(DojoTestCase):
             parser = CheckmarxOneParser()
             findings = parser.get_findings(testfile, Test())
             testfile.close()
-            self.assertEqual(99, len(findings))
+            self.assertEqual(5, len(findings))
             with self.subTest(i=0):
                 for finding in findings:
                     self.assertIsNotNone(finding.unique_id_from_tool)
@@ -21,13 +21,13 @@ class TestCheckmarxOneParser(DojoTestCase):
                     self.assertIsNotNone(finding.description)
                 finding_test = findings[0]
                 self.assertEqual("Medium", finding_test.severity)
-                self.assertEqual("/src/helpers/ConstantsHelper.ts", finding_test.file_path)
+                self.assertEqual("/src/helpers/Helper.ts", finding_test.file_path)
 
-    def test_checkmarx_one_checkmarx_apigateway(self):
-        with open("unittests/scans/checkmarx_one/checkmarx_apigateway.json") as testfile:
+    def test_checkmarx_one_many_findings(self):
+        with open("unittests/scans/checkmarx_one/many_findings.json") as testfile:
             parser = CheckmarxOneParser()
             findings = parser.get_findings(testfile, Test())
-            self.assertEqual(156, len(findings))
+            self.assertEqual(6, len(findings))
             with self.subTest(i=0):
                 for finding in findings:
                     self.assertIsNotNone(finding.unique_id_from_tool)
@@ -37,22 +37,11 @@ class TestCheckmarxOneParser(DojoTestCase):
                     self.assertIsNotNone(finding.severity)
                     self.assertIsNotNone(finding.description)
                 finding_test = findings[0]
-                self.assertEqual("Low", finding_test.severity)
-                self.assertEqual("/docker-compose.yml", finding_test.file_path)
+                self.assertEqual("High", finding_test.severity)
+                self.assertEqual("/qe/testharness/Dockerfile", finding_test.file_path)
 
-    def test_checkmarx_one_checkmarx_gibraltar(self):
-        with open("unittests/scans/checkmarx_one/checkmarx_gibraltar.json") as testfile:
+    def test_checkmarx_one_no_findings(self):
+        with open("unittests/scans/checkmarx_one/no_findings.json") as testfile:
             parser = CheckmarxOneParser()
             findings = parser.get_findings(testfile, Test())
-            self.assertEqual(1205, len(findings))
-            with self.subTest(i=0):
-                for finding in findings:
-                    self.assertIsNotNone(finding.unique_id_from_tool)
-                    self.assertIsNotNone(finding.title)
-                    self.assertIsNotNone(finding.test)
-                    self.assertIsNotNone(finding.date)
-                    self.assertIsNotNone(finding.severity)
-                    self.assertIsNotNone(finding.description)
-                finding_test = findings[0]
-                self.assertEqual("Info", finding_test.severity)
-                self.assertEqual("/services/health-care-provider-api/src/providers/__test__/salesforce-connection.provider.test.ts", finding_test.file_path)
+            self.assertEqual(0, len(findings))
