9157bc04d2b169df73b63e45e6bf3d018b2e5fa1
Manuel Sommer <sommermanuel@gmx.de>
2024-03-14 09:47:45 +0100

    update

---
+++ dojo/tools/sonarqube/sonarqube_restapi_json.py
@@ -0,0 +1,122 @@
+from dojo.models import Finding
+
+
+class SonarQubeRESTAPIJSON(object):
+    def get_json_items(self, json_content, test, mode):
+        items = []
+        for issue in json_content.get("issues"):
+            if issue.get("type") == "BUG":
+                key = issue.get("key")
+                rule = issue.get("rule")
+                component = issue.get("component")
+                project = issue.get("project")
+                line = str(issue.get("line"))
+                textRange = str(issue.get("textRange"))
+                flows = str(issue.get("flows"))
+                status = issue.get("status")
+                message = issue.get("message")
+                tags = issue.get("tags")
+                type = issue.get("type")
+                scope = issue.get("scope")
+                quickFixAvailable = str(issue.get("quickFixAvailable"))
+                codeVariants = str(issue.get("codeVariants"))
+                description = ""
+                description += "**key:** " + key + "\n"
+                description += "**rule:** " + rule + "\n"
+                description += "**component:** " + component + "\n"
+                description += "**project:** " + project + "\n"
+                description += "**line:** " + line + "\n"
+                description += "**textRange:** " + textRange + "\n"
+                description += "**flows:** " + flows + "\n"
+                description += "**status:** " + status + "\n"
+                description += "**message:** " + message + "\n"
+                description += "**tags:** " + tags + "\n"
+                description += "**type:** " + type + "\n"
+                description += "**scope:** " + scope + "\n"
+                item = Finding(
+                    title="vuln_title",
+                    description="vuln_description",
+                    test=test,
+                    severity=self.severitytranslator(issue.get("severity")),
+                    static_finding=True,
+                    dynamic_finding=False,
+                )
+            elif issue.get("type") == "VULNERABILITY":
+                key = issue.get("key")
+                rule = issue.get("rule")
+                component = issue.get("component")
+                project = issue.get("project")
+                flows = str(issue.get("flows"))
+                status = issue.get("status")
+                message = issue.get("message")
+                scope = issue.get("scope")
+                quickFixAvailable = str(issue.get("quickFixAvailable"))
+                codeVariants = str(issue.get("codeVariants"))
+                tags = str(issue.get("tags"))
+                description = ""
+                description += "**key:** " + key + "\n"
+                description += "**rule:** " + rule + "\n"
+                description += "**component:** " + component + "\n"
+                description += "**project:** " + project + "\n"
+                description += "**flows:** " + flows + "\n"
+                description += "**status:** " + status + "\n"
+                description += "**message:** " + message + "\n"
+                description += "**scope:** " + scope + "\n"
+                description += "**quickFixAvailable:** " + quickFixAvailable + "\n"
+                description += "**codeVariants:** " + codeVariants + "\n"
+                description += "**tags:** " + tags + "\n"
+                item = Finding(
+                    title=rule + "_" + key,
+                    description=description,
+                    test=test,
+                    severity=self.severitytranslator(issue.get("severity")),
+                    static_finding=True,
+                    dynamic_finding=False,
+                )
+            elif issue.get("type") == "CODE_SMELL":
+                key = issue.get("key")
+                rule = issue.get("rule")
+                component = issue.get("component")
+                project = issue.get("project")
+                line = str(issue.get("line"))
+                textRange = str(issue.get("textRange"))
+                flows = str(issue.get("flows"))
+                status = issue.get("status")
+                message = issue.get("message")
+                tags = str(issue.get("tags"))
+                scope = issue.get("scope")
+                quickFixAvailable = str(issue.get("quickFixAvailable"))
+                codeVariants = str(issue.get("codeVariants"))
+                description = ""
+                description += "**rule:** " + rule + "\n"
+                description += "**component:** " + component + "\n"
+                description += "**project:** " + project + "\n"
+                description += "**line:** " + line + "\n"
+                description += "**textRange:** " + textRange + "\n"
+                description += "**flows:** " + flows + "\n"
+                description += "**status:** " + status + "\n"
+                description += "**message:** " + message + "\n"
+                description += "**tags:** " + tags + "\n"
+                description += "**scope:** " + scope + "\n"
+                description += "**quickFixAvailable:** " + quickFixAvailable + "\n"
+                description += "**codeVariants:** " + codeVariants + "\n"
+                item = Finding(
+                    title=rule + "_" + key,
+                    description=description,
+                    test=test,
+                    severity=self.severitytranslator(issue.get("severity")),
+                    static_finding=True,
+                    dynamic_finding=False,
+                )
+            items.append(item)
+        return items
+
+    def severitytranslator(self, severity):
+        if severity == "BLOCKER":
+            return "High"
+        elif severity == "MAJOR":
+            return "Medium"
+        elif severity == "MINOR":
+            return "Low"
+        else:
+            return severity.lower().capitalize()
