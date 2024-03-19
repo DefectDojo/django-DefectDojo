c757d1e6008ddbaa520104fd584f3c393f07cf28
Felix Hernandez <ffhg_920522@hotmail.com>
2024-03-19 13:09:55 -0600

    fixing exception handler in 500 errors

--- dojo/api_v2/exception_handler.py
+++ dojo/api_v2/exception_handler.py
@@ -14,27 +14,50 @@ logger = logging.getLogger(__name__)
 
 
 def custom_exception_handler(exc, context):
+    # Call REST framework's default exception handler first,
+    # to get the standard error response.
     response = exception_handler(exc, context)
 
     if isinstance(exc, RestrictedError):
-        logger.exception(exc)
+        # An object cannot be deleted because it has dependent objects.
         response = Response()
         response.status_code = HTTP_409_CONFLICT
-        response.data = {"message": str(exc)}
+        response.data = {}
+        response.data["message"] = str(exc)
     elif isinstance(exc, ValidationError):
-        logger.exception(exc)
         response = Response()
         response.status_code = HTTP_400_BAD_REQUEST
-        response.data = {"message": str(exc)}
+        response.data = {}
+        response.data["message"] = str(exc)
     else:
         if response is None:
+            if System_Settings.objects.get().api_expose_error_details:
+                exception_message = str(exc.args[0])
+            else:
+                exception_message = "Internal server error, check logs for details"
+            # There is no standard error response, so we assume an unexpected
+            # exception. It is logged but no details are given to the user,
+            # to avoid leaking internal technical information.
             logger.exception(exc)
             response = Response()
             response.status_code = HTTP_500_INTERNAL_SERVER_ERROR
             response.data = {}
-            if System_Settings.objects.get().api_expose_error_details:
-                response.data["message"] = str(exc.args[0])
+            response.data[
+                "message"
+            ] = exception_message
+        else:
+            if response.status_code < 500:
+                # HTTP status codes lower than 500 are no technical errors.
+                # They need not to be logged and we provide the exception
+                # message, if it is different from the detail that is already
+                # in the response.
+                if isinstance(response.data, dict) and str(
+                    exc
+                ) != response.data.get("detail", ""):
+                    response.data["message"] = str(exc)
             else:
-                response.data["message"] = "Internal server error, check logs for details"
+                # HTTP status code 500 or higher are technical errors.
+                # They get logged and we don't change the response.
+                logger.exception(exc)
 
     return response
