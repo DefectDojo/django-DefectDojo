a84a383ac36aa4fbc1f24edd02b8fd471efc2461
Manuel Sommer <sommermanuel@gmx.de>
2024-03-19 11:35:19 +0100

    adapt db migrations

--- dojo/db_migrations/0206_alter_sonarqube_issue_key.py
+++ dojo/db_migrations/0206_alter_sonarqube_issue_key.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0204_jira_project_epic_issue_type_name'),
+        ('dojo', '0205_jira_project_epic_issue_type_name'),
     ]
 
     operations = [
