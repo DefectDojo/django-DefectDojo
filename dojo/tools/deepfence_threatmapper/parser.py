126dba40dd5610d5d0f58f7ccf02972dc89cee15
Manuel Sommer <sommermanuel@gmx.de>
2024-03-28 11:13:21 +0100

    update deepfence threatmapper

--- dojo/tools/deepfence_threatmapper/parser.py
+++ dojo/tools/deepfence_threatmapper/parser.py
@@ -1,7 +1,5 @@
-import csv
-import sys
-import io
 from dojo.models import Finding
+from openpyxl import load_workbook
 
 
 class DeepfenceThreatmapperParser(object):
@@ -12,109 +10,111 @@ class DeepfenceThreatmapperParser(object):
         return scan_type
 
     def get_description_for_scan_types(self, scan_type):
-        return "Deepfence Threatmapper report in csv format."
+        return "Deepfence Threatmapper report in XLSX format."
 
     def get_findings(self, filename, test):
-        content = filename.read()
-        if isinstance(content, bytes):
-            content = content.decode("utf-8")
-        csv.field_size_limit(int(sys.maxsize / 10))  # the request/resp are big
-        reader = csv.DictReader(io.StringIO(content))
+        workbook = load_workbook(filename)
+        worksheet = workbook.active
         findings = []
-        for row in reader:
+        headers = dict()
+        first = True
+        for row in worksheet.iter_rows(min_row=1, values_only=True):
             description = ""
-            if row.get("Rule Name") and row.get("Class"):
-                Rule_Name = row.get("Rule Name")
-                Class = row.get("Class")
-                File_Name = row.get("File Name")
-                Summary = row.get("Summary")
-                Severity = row.get("Severity")
-                Node_Name = row.get("Node Name")
-                NodeType = row.get("NodeType")
-                Container_Name = row.get("Container Name")
-                Kubernetes_Cluster_Name = row.get("Kubernetes Cluster Name")
-                description += "**Summary:** " + Summary + "\n"
-                description += "**Rule Name:** " + Rule_Name + "\n"
-                description += "**Class:** " + Class + "\n"
-                description += "**File Name:** " + File_Name + "\n"
-                description += "**Node Name:** " + Node_Name + "\n"
-                description += "**NodeType:** " + NodeType + "\n"
-                description += "**Container Name:** " + Container_Name + "\n"
-                description += "**Kubernetes Cluster Name:** " + Kubernetes_Cluster_Name + "\n"
+            if first:
+                first = False
+                for i in range(len(row)):
+                    headers[row[i]] = i
+            elif headers.get("Rule Name") is not None and headers.get("Class") is not None:
+                Rule_Name = row[headers["Rule Name"]]
+                Class = row[headers["Class"]]
+                File_Name = row[headers["File Name"]]
+                Summary = row[headers["Summary"]]
+                Severity = row[headers["Severity"]]
+                Node_Name = row[headers["Node Name"]]
+                NodeType = row[headers["NodeType"]]
+                Container_Name = row[headers["Container Name"]]
+                Kubernetes_Cluster_Name = row[headers["Kubernetes Cluster Name"]]
+                description += "**Summary:** " + str(Summary) + "\n"
+                description += "**Rule Name:** " + str(Rule_Name) + "\n"
+                description += "**Class:** " + str(Class) + "\n"
+                description += "**File Name:** " + str(File_Name) + "\n"
+                description += "**Node Name:** " + str(Node_Name) + "\n"
+                description += "**NodeType:** " + str(NodeType) + "\n"
+                description += "**Container Name:** " + str(Container_Name) + "\n"
+                description += "**Kubernetes Cluster Name:** " + str(Kubernetes_Cluster_Name) + "\n"
                 findings.append(
                     Finding(
                         title=Rule_Name,
                         description=description,
                         file_path=File_Name,
-                        severity=Severity.capitalize(),
+                        severity=self.severity(Severity),
                         static_finding=False,
                         dynamic_finding=True,
                         test=test,
                     )
                 )
-            elif row.get("Filename") and row.get("Content"):
-                Filename = row.get("Filename")
-                Content = row.get("Content")
-                Name = row.get("Name")
-                Rule = row.get("Rule")
-                Severity = row.get("Severity")
-                Node_Name = row.get("Node Name")
-                Container_Name = row.get("Container Name")
-                Kubernetes_Cluster_Name = row.get("Kubernetes Cluster Name")
-                Signature = row.get("Signature")
-                description += "**Filename:** " + Filename + "\n"
-                description += "**Name:** " + Name + "\n"
-                description += "**Rule:** " + Rule + "\n"
-                description += "**Node Name:** " + Node_Name + "\n"
-                description += "**Container Name:** " + Container_Name + "\n"
-                description += "**Kubernetes Cluster Name:** " + Kubernetes_Cluster_Name + "\n"
-                description += "**Content:** " + Content + "\n"
-                description += "**Signature:** " + Signature + "\n"
-                findings.append(
-                    Finding(
-                        title=Name,
-                        description=description,
-                        file_path=Filename,
-                        severity=Severity.capitalize(),
-                        static_finding=False,
-                        dynamic_finding=True,
-                        test=test,
+            elif headers.get("Filename") is not None and headers.get("Content") is not None:
+                Filename = row[headers["Filename"]]
+                Content = row[headers["Content"]]
+                Name = row[headers["Name"]]
+                Rule = row[headers["Rule"]]
+                Severity = row[headers["Severity"]]
+                Node_Name = row[headers["Node Name"]]
+                Container_Name = row[headers["Container Name"]]
+                Kubernetes_Cluster_Name = row[headers["Kubernetes Cluster Name"]]
+                Signature = row[headers["Signature"]]
+                description += "**Filename:** " + str(Filename) + "\n"
+                description += "**Name:** " + str(Name) + "\n"
+                description += "**Rule:** " + str(Rule) + "\n"
+                description += "**Node Name:** " + str(Node_Name) + "\n"
+                description += "**Container Name:** " + str(Container_Name) + "\n"
+                description += "**Kubernetes Cluster Name:** " + str(Kubernetes_Cluster_Name) + "\n"
+                description += "**Content:** " + str(Content) + "\n"
+                description += "**Signature:** " + str(Signature) + "\n"
+                if Name is not None and Severity is not None:
+                    findings.append(
+                        Finding(
+                            title=str(Name),
+                            description=description,
+                            file_path=Filename,
+                            severity=self.severity(Severity),
+                            static_finding=False,
+                            dynamic_finding=True,
+                            test=test,
+                        )
                     )
-                )
-            elif row.get("@timestamp") and row.get("cve_attack_vector"):
-                cve_attack_vector = row.get("cve_attack_vector")
-                cve_caused_by_package = row.get("cve_caused_by_package")
-                cve_container_image = row.get("cve_container_image")
-                cve_container_image_id = row.get("cve_container_image_id")
-                cve_cvss_score = row.get("cve_cvss_score")
-                cve_description = row.get("cve_description")
-                cve_fixed_in = row.get("cve_fixed_in")
-                cve_id = row.get("cve_id")
-                cve_link = row.get("cve_link")
-                cve_severity = row.get("cve_severity")
-                cve_overall_score = row.get("cve_overall_score")
-                cve_type = row.get("cve_type")
-                host_name = row.get("host_name")
-                cloud_account_id = row.get("cloud_account_id")
-                masked = row.get("masked")
-                description += "**cve_attack_vector:** " + cve_attack_vector + "\n"
-                description += "**cve_caused_by_package:** " + cve_caused_by_package + "\n"
-                description += "**cve_container_image:** " + cve_container_image + "\n"
-                description += "**cve_container_image_id:** " + cve_container_image_id + "\n"
-                description += "**cve_description:** " + cve_description + "\n"
-                description += "**cve_severity:** " + cve_severity + "\n"
-                description += "**cve_overall_score:** " + cve_overall_score + "\n"
-                description += "**cve_type:** " + cve_type + "\n"
-                description += "**host_name:** " + host_name + "\n"
-                description += "**cloud_account_id:** " + cloud_account_id + "\n"
-                description += "**masked:** " + masked + "\n"
+            elif headers.get("@timestamp") is not None and headers.get("cve_attack_vector") is not None:
+                cve_attack_vector = row[headers["cve_attack_vector"]]
+                cve_caused_by_package = row[headers["cve_caused_by_package"]]
+                cve_container_image = row[headers["cve_container_image"]]
+                cve_container_image_id = row[headers["cve_container_image_id"]]
+                cve_description = row[headers["cve_description"]]
+                cve_fixed_in = row[headers["cve_fixed_in"]]
+                cve_id = row[headers["cve_id"]]
+                cve_link = row[headers["cve_link"]]
+                cve_severity = row[headers["cve_severity"]]
+                cve_overall_score = row[headers["cve_overall_score"]]
+                cve_type = row[headers["cve_type"]]
+                host_name = row[headers["host_name"]]
+                cloud_account_id = row[headers["cloud_account_id"]]
+                masked = row[headers["masked"]]
+                description += "**cve_attack_vector:** " + str(cve_attack_vector) + "\n"
+                description += "**cve_caused_by_package:** " + str(cve_caused_by_package) + "\n"
+                description += "**cve_container_image:** " + str(cve_container_image) + "\n"
+                description += "**cve_container_image_id:** " + str(cve_container_image_id) + "\n"
+                description += "**cve_description:** " + str(cve_description) + "\n"
+                description += "**cve_severity:** " + str(cve_severity) + "\n"
+                description += "**cve_overall_score:** " + str(cve_overall_score) + "\n"
+                description += "**cve_type:** " + str(cve_type) + "\n"
+                description += "**host_name:** " + str(host_name) + "\n"
+                description += "**cloud_account_id:** " + str(cloud_account_id) + "\n"
+                description += "**masked:** " + str(masked) + "\n"
                 findings.append(
                     Finding(
                         title="Threatmapper_Vuln_Report-" + cve_id,
                         description=description,
                         component_name=cve_caused_by_package,
-                        cvssv3_score=cve_cvss_score,
-                        severity=cve_severity.capitalize(),
+                        severity=self.severity(cve_severity),
                         static_finding=False,
                         dynamic_finding=True,
                         mitigation=cve_fixed_in,
@@ -123,35 +123,35 @@ class DeepfenceThreatmapperParser(object):
                         test=test,
                     )
                 )
-            elif row.get("@timestamp") and row.get("compliance_check_type"):
-                compliance_check_type = row.get("compliance_check_type")
-                count = row.get("count")
-                doc_id = row.get("doc_id")
-                host_name = row.get("host_name")
-                cloud_account_id = row.get("cloud_account_id")
-                masked = row.get("masked")
-                node_id = row.get("node_id")
-                node_name = row.get("node_name")
-                node_type = row.get("node_type")
-                status = row.get("status")
-                test_category = row.get("test_category")
-                test_desc = row.get("test_desc")
-                test_info = row.get("test_info")
-                test_number = row.get("test_number")
-                description += "**compliance_check_type:** " + compliance_check_type + "\n"
-                description += "**host_name:** " + host_name + "\n"
-                description += "**cloud_account_id:** " + cloud_account_id + "\n"
-                description += "**masked:** " + masked + "\n"
-                description += "**node_id:** " + node_id + "\n"
-                description += "**node_name:** " + node_name + "\n"
-                description += "**node_type:** " + node_type + "\n"
-                description += "**status:** " + status + "\n"
-                description += "**test_category:** " + test_category + "\n"
-                description += "**test_desc:** " + test_desc + "\n"
-                description += "**test_info:** " + test_info + "\n"
-                description += "**test_number:** " + test_number + "\n"
-                description += "**count:** " + count + "\n"
-                description += "**doc_id:** " + doc_id + "\n"
+            elif headers.get("@timestamp") is not None and headers.get("compliance_check_type") is not None:
+                compliance_check_type = row[headers["compliance_check_type"]]
+                count = row[headers["count"]]
+                doc_id = row[headers["doc_id"]]
+                host_name = row[headers["host_name"]]
+                cloud_account_id = row[headers["cloud_account_id"]]
+                masked = row[headers["masked"]]
+                node_id = row[headers["node_id"]]
+                node_name = row[headers["node_name"]]
+                node_type = row[headers["node_type"]]
+                status = row[headers["status"]]
+                test_category = row[headers["test_category"]]
+                test_desc = row[headers["test_desc"]]
+                test_info = row[headers["test_info"]]
+                test_number = row[headers["test_number"]]
+                description += "**compliance_check_type:** " + str(compliance_check_type) + "\n"
+                description += "**host_name:** " + str(host_name) + "\n"
+                description += "**cloud_account_id:** " + str(cloud_account_id) + "\n"
+                description += "**masked:** " + str(masked) + "\n"
+                description += "**node_id:** " + str(node_id) + "\n"
+                description += "**node_name:** " + str(node_name) + "\n"
+                description += "**node_type:** " + str(node_type) + "\n"
+                description += "**status:** " + str(status) + "\n"
+                description += "**test_category:** " + str(test_category) + "\n"
+                description += "**test_desc:** " + str(test_desc) + "\n"
+                description += "**test_info:** " + str(test_info) + "\n"
+                description += "**test_number:** " + str(test_number) + "\n"
+                description += "**count:** " + str(count) + "\n"
+                description += "**doc_id:** " + str(doc_id) + "\n"
                 findings.append(
                     Finding(
                         title="Threatmapper_Compliance_Report-" + test_number,
@@ -174,3 +174,9 @@ class DeepfenceThreatmapperParser(object):
         else:
             output = "Info"
         return output
+
+    def severity(self, input):
+        if input is None:
+            return "Info"
+        else:
+            return input.capitalize()
