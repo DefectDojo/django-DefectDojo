a9f462360d3713b16aa14d038f121abe33f4918d
Manuel Sommer <sommermanuel@gmx.de>
2024-04-03 15:22:02 +0200

    :bug: jake json output in cyclonedx not parsed

--- dojo/tools/cyclonedx/json_parser.py
+++ dojo/tools/cyclonedx/json_parser.py
@@ -49,21 +49,98 @@ class CycloneDXJSONParser(object):
                 references += "\n"
             # for each component affected we create a finding if the "affects"
             # node is here
-            for affect in vulnerability.get("affects", []):
-                reference = affect["ref"]  # required by the specification
-                component_name, component_version = Cyclonedxhelper()._get_component(
-                    components, reference
-                )
+            if vulnerability.get("affects"):
+                for affect in vulnerability.get("affects", []):
+                    reference = affect["ref"]  # required by the specification
+                    component_name, component_version = Cyclonedxhelper()._get_component(
+                        components, reference
+                    )
+                    if not description:
+                        description = "Description was not provided."
+                    finding = Finding(
+                        title=f"{component_name}:{component_version} | {vulnerability.get('id')}",
+                        test=test,
+                        description=description,
+                        severity=severity,
+                        mitigation=vulnerability.get("recommendation", ""),
+                        component_name=component_name,
+                        component_version=component_version,
+                        references=references,
+                        static_finding=True,
+                        dynamic_finding=False,
+                        vuln_id_from_tool=vulnerability.get("id"),
+                    )
+                    if report_date:
+                        finding.date = report_date
+                    ratings = vulnerability.get("ratings", [])
+                    for rating in ratings:
+                        if (
+                            rating.get("method") == "CVSSv3"
+                            or rating.get("method") == "CVSSv31"
+                        ):
+                            raw_vector = rating["vector"]
+                            cvssv3 = Cyclonedxhelper()._get_cvssv3(raw_vector)
+                            severity = rating.get("severity")
+                            if cvssv3:
+                                finding.cvssv3 = cvssv3.clean_vector()
+                                if severity:
+                                    finding.severity = Cyclonedxhelper().fix_severity(severity)
+                                else:
+                                    finding.severity = cvssv3.severities()[0]
+                    vulnerability_ids = list()
+                    # set id as first vulnerability id
+                    if vulnerability.get("id"):
+                        vulnerability_ids.append(vulnerability.get("id"))
+                    # check references to see if we have other vulnerability ids
+                    for reference in vulnerability.get("references", []):
+                        vulnerability_id = reference.get("id")
+                        if vulnerability_id:
+                            vulnerability_ids.append(vulnerability_id)
+                    if vulnerability_ids:
+                        finding.unsaved_vulnerability_ids = vulnerability_ids
+                    # if there is some CWE
+                    cwes = vulnerability.get("cwes")
+                    if cwes and len(cwes) > 1:
+                        # FIXME support more than one CWE
+                        LOGGER.debug(
+                            f"more than one CWE for a finding {cwes}. NOT supported by parser API"
+                        )
+                    if cwes and len(cwes) > 0:
+                        finding.cwe = cwes[0]
+                    # Check for mitigation
+                    analysis = vulnerability.get("analysis")
+                    if analysis:
+                        state = analysis.get("state")
+                        if state:
+                            if (
+                                "resolved" == state
+                                or "resolved_with_pedigree" == state
+                                or "not_affected" == state
+                            ):
+                                finding.is_mitigated = True
+                                finding.active = False
+                            elif "false_positive" == state:
+                                finding.false_p = True
+                                finding.active = False
+                            if not finding.active:
+                                detail = analysis.get("detail")
+                                if detail:
+                                    finding.mitigation = (
+                                        finding.mitigation
+                                        + "\n**This vulnerability is mitigated and/or suppressed:** {}\n".format(
+                                            detail
+                                        )
+                                    )
+                    findings.append(finding)
+            else:
                 if not description:
                     description = "Description was not provided."
                 finding = Finding(
-                    title=f"{component_name}:{component_version} | {vulnerability.get('id')}",
+                    title=vulnerability.get("id"),
                     test=test,
                     description=description,
                     severity=severity,
                     mitigation=vulnerability.get("recommendation", ""),
-                    component_name=component_name,
-                    component_version=component_version,
                     references=references,
                     static_finding=True,
                     dynamic_finding=False,
