5207707f0320e122e558faf02c5ad4a796c29751
kiblik <5609770+kiblik@users.noreply.github.com>
2024-04-05 10:50:22 +0200

    PreDjango 4.2 fixes

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -492,10 +492,7 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
     'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     'django.contrib.auth.hashers.BCryptPasswordHasher',
-    'django.contrib.auth.hashers.SHA1PasswordHasher',
     'django.contrib.auth.hashers.MD5PasswordHasher',
-    'django.contrib.auth.hashers.UnsaltedSHA1PasswordHasher',
-    'django.contrib.auth.hashers.UnsaltedMD5PasswordHasher',
 ]
 
 SOCIAL_AUTH_PIPELINE = (
