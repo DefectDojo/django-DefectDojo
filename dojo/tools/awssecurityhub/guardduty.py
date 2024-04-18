c809ebee937d489a6526d5731c0b9c7c9e33e755
testaccount90009 <122134756+testaccount90009@users.noreply.github.com>
2024-04-17 20:08:24 -0700

    Fix ResourceId add

--- dojo/tools/awssecurityhub/guardduty.py
+++ dojo/tools/awssecurityhub/guardduty.py
@@ -30,7 +30,7 @@ class GuardDuty:
             else:
                 mitigated = datetime.utcnow()
         description = f"This is a GuardDuty Finding\n{finding.get('Description', '')}"
-        description += f"\n Resource ID: {finding.get("Resources", {}).get("Id", "")}\n"
+        description += f"\n Resource ID: {finding.get('Resources', {}).get('Id', '')}\n"
         description += f"\n**AWS Finding ARN: ** {finding_id}\n"
         description += f"SourceURL: {finding.get('SourceUrl', '')}\n"
         description += f"AwsAccountId: {finding.get('AwsAccountId', '')}\n"
