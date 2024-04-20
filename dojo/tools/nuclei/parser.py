6ef1d0420afcddf26ca2d3ec41163554b42dce18
Vilius <viliuspovilaika@users.noreply.github.com>
2024-04-20 16:52:01 +0300

    Fix Nuclei parser
    Fix Nuclei JSON parser

--- dojo/tools/nuclei/parser.py
+++ dojo/tools/nuclei/parser.py
@@ -151,7 +151,7 @@ class NucleiParser(object):
             )
 
             dupe_key = hashlib.sha256(
-                (template_id + item_type + matcher + endpoint.host).encode(
+                (template_id + item_type + matcher + host).encode(
                     "utf-8"
                 )
             ).hexdigest()
