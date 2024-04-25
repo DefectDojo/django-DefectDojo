54e50d981179330f5c4ea74d3e8cf8f0b8559af6
Michael Wager <mail@mwager.de>
2024-04-25 10:52:31 +0200

    Add Kiuwan SCA parser implementation

---
+++ dojo/tools/kiuwan_sca/parser.py
@@ -0,0 +1,75 @@
+import json
+import hashlib
+import io
+
+from dojo.models import Finding
+
+__author__ = "mwager"
+
+class KiuwanSCAParser(object):
+    SEVERITY = {
+        "-" : "Low",
+				"LOW" : "Low",
+				"MEDIUM" : "Medium",
+				"HIGH" : "High",
+				"CRITICAL" : "Critical",
+				"Low" : "Low",
+				"Medium" : "Medium",
+				"High" : "High",
+				"Critical" : "Critical"
+    }
+
+    def get_scan_types(self):
+        return ["Kiuwan SCA Scan"]
+
+    def get_label_for_scan_types(self, scan_type):
+        return scan_type
+
+    def get_description_for_scan_types(self, scan_type):
+        return "Import Kiuwan Insights Scan in JSON format. Export as JSON using Kiuwan REST API."
+
+    def get_findings(self, filename, test):
+        data = json.load(filename)
+
+        dupes = dict()
+        for row in data:
+            # if a finding was "muted" in the Kiuwan UI, we ignore it (e.g. marked as false positive)
+            if row["muted"] == True:
+                continue
+
+            finding = Finding(test=test)
+            finding.title = "Kiuwan Insights finding: " + row["cve"]
+            finding.description = row["description"]
+            finding.severity = self.SEVERITY[row["securityRisk"]]
+
+            if "components" in row and len(row["components"]) > 0:
+                finding.component_name = row["components"][0]["artifact"]
+                finding.component_version = row["components"][0]["version"]
+
+            if "cve" in row:
+                finding.cve = row["cve"]
+
+            if "cwe" in row:
+                try:
+                    finding.cwe = int(row["cwe"].replace("CWE-", ""))
+                except Exception:
+                    pass
+
+            finding.references = "See Kiuwan Web UI"
+            finding.mitigation = "See Kiuwan Web UI"
+            finding.static_finding = True
+
+            key = hashlib.sha256(
+                (
+                    finding.cve
+                    + "|"
+                    + finding.severity
+                    + "|"
+                    + str(finding.cwe)
+                ).encode("utf-8")
+            ).hexdigest()
+
+            if key not in dupes:
+                dupes[key] = finding
+
+        return list(dupes.values())
