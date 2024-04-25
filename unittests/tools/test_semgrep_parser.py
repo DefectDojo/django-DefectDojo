a679ce4c9c614fe3ed3cd9a6a89fd519adba2495
Matt Tesauro <mtesauro@gmail.com>
2024-04-25 11:18:02 -0500

    Updates to semgrep parser

--- unittests/tools/test_semgrep_parser.py
+++ unittests/tools/test_semgrep_parser.py
@@ -2,7 +2,7 @@ from ..dojo_test_case import DojoTestCase
 from dojo.tools.semgrep.parser import SemgrepParser
 from dojo.models import Test
 
-
+# Test of semgrep parser
 class TestSemgrepParser(DojoTestCase):
 
     def test_parse_empty(self):
