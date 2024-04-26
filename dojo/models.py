511f58e5911865cbcf8ff369af8839098b0e7f14
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-04-26 17:02:23 -0500

    Similar Findings: Create Toggle

--- dojo/models.py
+++ dojo/models.py
@@ -421,6 +421,12 @@ class System_Settings(models.Model):
         verbose_name=_('Enable Remediation Advice'),
         help_text=_("Enables global remediation advice and matching on CWE and Title. The text will be replaced for mitigation, impact and references on a finding. Useful for providing consistent impact and remediation advice regardless of the scanner."))
 
+    enable_similar_findings = models.BooleanField(
+        default=True,
+        blank=False,
+        verbose_name=_("Enable Similar Findings"),
+        help_text=_("Enable the query of similar findings on the view finding page. This feature can involve potentially large queries and negatively impact performance"))
+
     engagement_auto_close = models.BooleanField(
         default=False,
         blank=False,
@@ -462,6 +468,7 @@ class System_Settings(models.Model):
         blank=False,
         verbose_name=_("Enable an exponential backoff strategy for SLA breach notifications."),
         help_text=_("Enable an exponential backoff strategy for SLA breach notifications, e.g. 1, 2, 4, 8, etc. Otherwise it alerts every day"))
+    
 
     allow_anonymous_survey_repsonse = models.BooleanField(
         default=False,
