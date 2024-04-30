a75719b9a9dc614b650b6a04f8621c0722549637
Harold Blankenship <36673698+hblankenship@users.noreply.github.com>
2024-04-30 09:11:13 -0500

    Unit Tests: Correct File Close Warnings (#10055)
    * files in unnittest
    
    * missed file open
    
    * fix linter issues from editing merges
    
    * indentation fix
    
    * remove whitespaces
    
    * fix flake issues

--- unittests/tools/test_h1_parser.py
+++ unittests/tools/test_h1_parser.py
@@ -6,19 +6,19 @@ from dojo.models import Test
 class TestHackerOneParser(DojoTestCase):
 
     def test_parse_file_with_no_vuln_has_no_finding(self):
-        testfile = open("unittests/scans/h1/data_empty.json")
-        parser = H1Parser()
-        findings = parser.get_findings(testfile, Test())
-        self.assertEqual(0, len(findings))
+        with open("unittests/scans/h1/data_empty.json") as testfile:
+            parser = H1Parser()
+            findings = parser.get_findings(testfile, Test())
+            self.assertEqual(0, len(findings))
 
     def test_parse_file_with_one_vuln_has_one_finding(self):
-        testfile = open("unittests/scans/h1/data_one.json")
-        parser = H1Parser()
-        findings = parser.get_findings(testfile, Test())
-        self.assertEqual(1, len(findings))
+        with open("unittests/scans/h1/data_one.json") as testfile:
+            parser = H1Parser()
+            findings = parser.get_findings(testfile, Test())
+            self.assertEqual(1, len(findings))
 
     def test_parse_file_with_multiple_vuln_has_multiple_finding(self):
-        testfile = open("unittests/scans/h1/data_many.json")
-        parser = H1Parser()
-        findings = parser.get_findings(testfile, Test())
-        self.assertEqual(2, len(findings))
+        with open("unittests/scans/h1/data_many.json") as testfile:
+            parser = H1Parser()
+            findings = parser.get_findings(testfile, Test())
+            self.assertEqual(2, len(findings))
