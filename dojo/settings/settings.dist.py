ccbaa5394c6f893eb53b887d89db329116474a54
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-05-01 04:44:21 +0200

    remove aws scout2 parser  (#9894)
    * remove scout2 content
    
    * remove aws_scout2 components

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -220,7 +220,7 @@ env = environ.FileAwareEnv(
     # regular expression to exclude one or more parsers
     # could be usefull to limit parser allowed
     # AWS Scout2 Scan Parser is deprecated (see https://github.com/DefectDojo/django-DefectDojo/pull/5268)
-    DD_PARSER_EXCLUDE=(str, 'AWS Scout2 Scan'),
+    DD_PARSER_EXCLUDE=(str, ''),
     # when enabled in sytem settings,  every minute a job run to delete excess duplicates
     # we limit the amount of duplicates that can be deleted in a single run of that job
     # to prevent overlapping runs of that job from occurrring
