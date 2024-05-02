097730ca1bf3700cd4c151bb1a151677245bbf12
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-05-02 05:50:52 +0200

    add test description for AWS SecurityHub Scan (#9904)
    * add test description for AWS SecurityHub Scan
    
    * ruff, W293
    
    * Update dojo/tools/awssecurityhub/parser.py
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>
    
    * ruff
    
    ---------
    
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>

--- dojo/tools/awssecurityhub/parser.py
+++ dojo/tools/awssecurityhub/parser.py
@@ -3,9 +3,11 @@ import json
 from dojo.tools.awssecurityhub.compliance import Compliance
 from dojo.tools.awssecurityhub.guardduty import GuardDuty
 from dojo.tools.awssecurityhub.inspector import Inspector
+from dojo.tools.parser_test import ParserTest
 
 
 class AwsSecurityHubParser:
+    ID = "AWS Security Hub"
 
     def get_scan_types(self):
         return ["AWS Security Hub Scan"]
@@ -15,6 +17,26 @@ class AwsSecurityHubParser:
 
     def get_description_for_scan_types(self, scan_type):
         return "AWS Security Hub exports in JSON format."
+
+    def get_tests(self, scan_type, scan):
+        data = json.load(scan)
+        findings = data.get("Findings", data.get("findings", None))
+        if not isinstance(findings, list):
+            msg = "Incorrect Security Hub report format"
+            raise TypeError(msg)
+        prod = []
+        aws_acc = []
+        for finding in findings:
+            prod.append(finding.get("ProductName", "AWS Security Hub Ruleset"))
+            aws_acc.append(finding.get("AwsAccountId"))
+        report_date = data.get("createdAt")
+        test = ParserTest(
+            name=self.ID, type=self.ID, version=""
+        )
+        test.description = "**AWS Accounts:** " + ', '.join(set(aws_acc)) + "\n"
+        test.description += "**Finding Origins:** " + ', '.join(set(prod)) + "\n"
+        test.findings = self.get_items(data, report_date)
+        return [test]
 
     def get_findings(self, filehandle, test):
         tree = json.load(filehandle)
