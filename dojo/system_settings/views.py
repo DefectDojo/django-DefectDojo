5e2fe841276a0a4fac4fa1132f189a6e68c3a262
David H <davidhernandeze@gmail.com>
2024-05-02 12:28:23 -0600

    add sections in system settings

--- dojo/system_settings/views.py
+++ dojo/system_settings/views.py
@@ -16,6 +16,62 @@ logger = logging.getLogger(__name__)
 @user_passes_test(lambda u: u.is_superuser)
 def system_settings(request):
     system_settings_obj = System_Settings.objects.get(no_cache=True)
+    form = SystemSettingsForm(instance=system_settings_obj)
+
+    sections = [
+        {
+            'name': 'Deduplication and Finding Settings',
+            'fields': {'enable_deduplication', 'delete_duplicates', 'max_dupes', 'false_positive_history',
+                       'retroactive_false_positive_history', 'risk_acceptance_form_default_days',
+                       'risk_acceptance_notify_before_expiration', 'enable_finding_groups',
+                       'enable_endpoint_metadata_import', 'enable_template_match'}
+        },
+        {
+            'name': 'Finding Service Level Agreement (SLA) Settings',
+            'fields': {'enable_finding_sla', 'enable_notify_sla_active', 'enable_notify_sla_active_verified',
+                       'enable_notify_sla_jira_only', 'enable_notify_sla_exponential_backoff',
+                       'enable_similar_findings'}
+        },
+        {
+            'name': 'Jira Integration Settings',
+            'fields': {'enable_jira', 'jira_labels', 'add_vulnerability_id_to_jira_label', 'enable_jira_web_hook',
+                       'disable_jira_webhook_secret', 'jira_webhook_secret', 'jira_minimum_severity'}
+        },
+        {
+            'name': 'Integration Settings',
+            'fields': {'enable_github', 'enable_slack_notifications', 'slack_channel', 'slack_token', 'slack_username',
+                       'enable_msteams_notifications', 'msteams_url', 'email_from', 'enable_mail_notifications',
+                       'mail_notifications_to'}
+        },
+        {
+            'name': 'Product Settings',
+            'fields': {'enable_product_grade', 'product_grade_a', 'product_grade_b', 'product_grade_c',
+                       'product_grade_d', 'product_grade_e', 'product_grade_f', 'enable_benchmark',
+                       'enable_product_tag_inheritance', 'enable_product_tracking_files'}
+        },
+        {
+            'name': 'Engagement Settings',
+            'fields': {'engagement_auto_close', 'engagement_auto_close_days'}
+        },
+        {
+            'name': 'Application Settings',
+            'fields': {'enable_credentials', 'credentials', 'disclaimer', 'url_prefix', 'team_name', 'time_zone',
+                       'allow_anonymous_survey_repsonse', 'enable_questionnaires', 'enable_checklists',
+                       'enable_calendar', 'enable_user_profile_editable', 'default_group', 'default_group_role',
+                       'default_group_email_pattern'}
+        },
+        {
+            'name': 'Password Settings',
+            'fields': {'minimum_password_length', 'maximum_password_length', 'number_character_required',
+                       'special_character_required', 'lowercase_character_required', 'uppercase_character_required',
+                       'non_common_password_required'}
+        }
+    ]
+
+    all_fields = {field.name for field in form.visible_fields()}
+    fields_with_section = {field for section in sections for field in section['fields']}
+    fields_without_section = all_fields - fields_with_section
+    sections.append({'name': 'Other', 'fields': fields_without_section})
 
     """
     **** To be Finished JIRA Status info ****
@@ -46,7 +102,7 @@ def system_settings(request):
                                  extra_tags='alert-danger')
 
     """
-    form = SystemSettingsForm(instance=system_settings_obj)
+
     if request.method == 'POST':
         form = SystemSettingsForm(request.POST, instance=system_settings_obj)
         if form.is_valid():
@@ -77,7 +133,7 @@ def system_settings(request):
                                     messages.SUCCESS,
                                     'Settings saved.',
                                     extra_tags='alert-success')
-        return render(request, 'dojo/system_settings.html', {'form': form})
+        return render(request, 'dojo/system_settings.html', {'form': form, 'sections': sections})
 
     else:
         # Celery needs to be set with the setting: CELERY_RESULT_BACKEND = 'db+sqlite:///dojo.celeryresults.sqlite'
@@ -94,11 +150,12 @@ def system_settings(request):
         else:
             celery_bool = False
             celery_msg = "Celery needs to have the setting CELERY_RESULT_BACKEND = 'db+sqlite:///dojo.celeryresults.sqlite' set in settings.py."
-            celery_status = "Unkown"
+            celery_status = "Unknown"
 
     add_breadcrumb(title="Application settings", top_level=False, request=request)
     return render(request, 'dojo/system_settings.html',
                   {'form': form,
+                    'sections': sections,
                    'celery_bool': celery_bool,
                    'celery_msg': celery_msg,
                    'celery_status': celery_status})
