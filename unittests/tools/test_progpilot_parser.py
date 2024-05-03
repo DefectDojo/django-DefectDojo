ab10cfad103c11437f4e8f73cb44d2882510bf03
Manuel Sommer <sommermanuel@gmx.de>
2024-05-04 00:26:01 +0200

    ruff

--- unittests/tools/test_progpilot_parser.py
+++ unittests/tools/test_progpilot_parser.py
@@ -1,6 +1,7 @@
-from ..dojo_test_case import DojoTestCase
 from dojo.models import Test
 from dojo.tools.progpilot.parser import ProgpilotParser
+
+from ..dojo_test_case import DojoTestCase
 
 
 class TestProgpilotParser(DojoTestCase):
