2dff9b142ad9d702cafce5859e8cf1b936a30165
RENAUDIN BRIEUC <brieuc.renaudin@backmarket.com>
2024-05-06 11:38:23 +0200

    feat(trufflehog_v3): add link field and deduplicate issues based on it

--- dojo/tools/trufflehog3/parser.py
+++ dojo/tools/trufflehog3/parser.py
@@ -113,6 +113,7 @@ class TruffleHog3Parser(object):
         # author = json_data.get('author')
         commit = json_data.get("commit")
         date = json_data.get("date")
+        link = json_data.get("link")
 
         title = f"{message} found in {file}"
 
@@ -165,5 +166,6 @@ class TruffleHog3Parser(object):
                 dynamic_finding=False,
                 static_finding=True,
                 nb_occurences=1,
+                url=link
             )
             dupes[dupe_key] = finding
