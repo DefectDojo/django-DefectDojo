1ddba7f330a4c599e4425b843be573d0e2c49929
Manuel Sommer <sommermanuel@gmx.de>
2024-05-14 09:14:39 +0200

    replace okta.py

--- dojo/okta.py
+++ dojo/okta.py
@@ -1,40 +1,53 @@
 """
-Author: selten
-Taken from Pull Request #333 of
-python-social-auth/socail-core
+This script origins from here: https://github.com/python-social-auth/social-core/blob/master/social_core/backends/okta.py
+"""
 
 """
-from jose import jwt
-from jose.jwt import ExpiredSignatureError, JWTError
-from six.moves.urllib.parse import urljoin
-from social_core.backends.oauth import BaseOAuth2
-from social_core.backends.open_id_connect import OpenIdConnectAuth
-from social_core.utils import append_slash
+Okta OAuth2 and OpenIdConnect:
+    https://python-social-auth.readthedocs.io/en/latest/backends/okta.html
+"""
+
+from urllib.parse import urljoin
+
+from ..utils import append_slash
+from .oauth import BaseOAuth2
 
 
 class OktaMixin:
     def api_url(self):
         return append_slash(self.setting("API_URL"))
+        return append_slash(self.setting("API_URL"))
 
     def authorization_url(self):
+        return self._url("v1/authorize")
         return self._url("v1/authorize")
 
     def access_token_url(self):
         return self._url("v1/token")
+        return self._url("v1/token")
 
     def _url(self, path):
         return urljoin(append_slash(self.setting("API_URL")), path)
+        return urljoin(append_slash(self.setting("API_URL")), path)
 
     def oidc_config(self):
-        return self.get_json(self._url("/.well-known/openid-configuration?client_id=" + self.setting("KEY")))
+        return self.get_json(
+            self._url(
+                "/.well-known/openid-configuration?client_id={}".format(
+                    self.setting("KEY")
+                )
+            )
+        )
 
 
 class OktaOAuth2(OktaMixin, BaseOAuth2):
     """Okta OAuth authentication backend"""
+
     name = "okta-oauth2"
     REDIRECT_STATE = False
     ACCESS_TOKEN_METHOD = "POST"
     SCOPE_SEPARATOR = " "
+    ID_KEY = "preferred_username"
 
     DEFAULT_SCOPE = [
         "openid", "profile",
@@ -51,6 +64,12 @@ class OktaOAuth2(OktaMixin, BaseOAuth2):
                 "email": response.get("preferred_username") or "",
                 "first_name": response.get("given_name"),
                 "last_name": response.get("family_name")}
+        return {
+            "username": response.get("preferred_username"),
+            "email": response.get("email") or "",
+            "first_name": response.get("given_name"),
+            "last_name": response.get("family_name"),
+        }
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from Okta"""
