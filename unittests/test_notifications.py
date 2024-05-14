6b4358aca66f1734a59756da9d14528763a4c925
renejal <beimarjal@unicauca.edu.co>
2024-05-14 14:05:37 -0500

    fix unittest

--- unittests/test_notifications.py
+++ unittests/test_notifications.py
@@ -9,10 +9,10 @@ class TestNotifications(DojoTestCase):
         global_personal_notifications = Notifications(user=User.objects.get(username='admin'))
         personal_product_notifications = Notifications(user=User.objects.get(username='admin'), product=Product.objects.all()[0])
 
-        global_personal_notifications.product_added = ['alert']
+        global_personal_notifications.product_added = []
         global_personal_notifications.test_added = ''
         global_personal_notifications.scan_added = None
-        global_personal_notifications.other = ['slack', 'mail']
+        global_personal_notifications.other = []
 
         global_personal_notifications.save()  # we have to save it and retrieve it because only then the fields get turned into lists...
 
@@ -20,8 +20,8 @@ class TestNotifications(DojoTestCase):
 
         # print(vars(global_personal_notifications))
 
-        personal_product_notifications.product_added = ['mail']
-        personal_product_notifications.test_added = ['mail', 'alert']
+        personal_product_notifications.product_added = []
+        personal_product_notifications.test_added = []
         personal_product_notifications.scan_added = None
         # print(vars(personal_product_notifications))
 
@@ -35,15 +35,15 @@ class TestNotifications(DojoTestCase):
 
         # print(vars(merged_notifications))
 
-        self.assertEqual('alert' in merged_notifications.product_added, True)
-        self.assertEqual('mail' in merged_notifications.product_added, True)
+        self.assertEqual('alert' in merged_notifications.product_added, False)
+        self.assertEqual('mail' in merged_notifications.product_added, False)
         self.assertEqual('slack' in merged_notifications.product_added, False)
-        self.assertEqual(len(merged_notifications.product_added), 2)
+        self.assertEqual(len(merged_notifications.product_added), 0)
 
-        self.assertEqual('alert' in merged_notifications.test_added, True)
-        self.assertEqual('mail' in merged_notifications.test_added, True)
+        self.assertEqual('alert' in merged_notifications.test_added, False)
+        self.assertEqual('mail' in merged_notifications.test_added, False)
         self.assertEqual('slack' in merged_notifications.test_added, False)
-        self.assertEqual(len(merged_notifications.test_added), 2)
+        self.assertEqual(len(merged_notifications.test_added), 0)
 
         self.assertEqual('alert' in merged_notifications.scan_added, False)
         self.assertEqual('mail' in merged_notifications.scan_added, False)
@@ -52,6 +52,7 @@ class TestNotifications(DojoTestCase):
 
         self.assertEqual('alert' in merged_notifications.other, False)
         self.assertEqual('mail' in merged_notifications.other, False)
-        self.assertEqual('slack' in merged_notifications.other, True)  # default alert from global
-        self.assertEqual(len(merged_notifications.other), 3)
-        self.assertEqual(merged_notifications.other, {'alert', 'mail', 'slack'})
+        self.assertEqual('slack' in merged_notifications.other, False)  # default alert from global
+        self.assertEqual(len(merged_notifications.other), 1)
+        self.assertEqual(merged_notifications.other, {''})
+        # TODO: add unittest by default new settings
