e3c488dabbf05b9780adb3c73261f33aa1f37c50
AlperenY-cs <alpbeylnx@gmail.com>
2024-05-14 17:56:28 +0300

    Ruff checks were made. Warnings fixed.

--- dojo/tools/rapplex/parser.py
+++ dojo/tools/rapplex/parser.py
@@ -1,20 +1,22 @@
 import json
 from datetime import datetime
-from dojo.models import Finding, Endpoint
 
-class RapplexParser(object):
+from dojo.models import Endpoint, Finding
+
+
+class RapplexParser:
     """
-    Rapplex - Web Application Security Scanner    
+    Rapplex - Web Application Security Scanner
     """
     def get_scan_types(self):
         return ["Rapplex Scan"]
-    
+
     def get_label_for_scan_types(self, scan_type):
         return "Rapplex Scan"
-        
+
     def get_description_for_scan_types(self, scan_type):
         return "Import Rapplex JSON report."
-    
+
     def get_findings(self, file, test):
         raw_data = file.read()
         data = json.loads(raw_data)
@@ -39,7 +41,7 @@ class RapplexParser(object):
                     req = issue.get("HttpRequest", "")
                     res = issue.get("HttpResponse", "")
                     vIndex = issue.get("vIndex", "")
-                    vuln_id = f"{scan_id}_{vIndex}" # scanId and vIndex combined to create unique_id_from_tool
+                    vuln_id = f"{scan_id}_{vIndex}"  # scanId and vIndex combined to create unique_id_from_tool
                     issue_definition = main_issue_group.get("Definition", {})
 
                     cwe_val = None
@@ -52,26 +54,26 @@ class RapplexParser(object):
                     for reference in issue_definition.get("References", []):
                         ref_title = reference.get("Title", "")
                         ref_link = reference.get("Link", "")
-                        reference_texts.append(f"{ref_title}\n{ref_link}") # ref_title and ref_link combined to references section
+                        reference_texts.append(f"{ref_title}\n{ref_link}")  # ref_title and ref_link combined to references section
                     reference_array = "\n".join(reference_texts)
 
                     desc_rem = issue_definition.get("Sections", {}).get("Remediation", "")
                     desc_sum = issue_definition.get("Sections", {}).get("Summary", "")
 
                     if (len(desc_rem) > 0):
-                        desc_text = f"\n{desc_sum}\nRemediation:\n{desc_rem}" # summary and remediation combined to create description
+                        desc_text = f"\n{desc_sum}\nRemediation:\n{desc_rem}"  # summary and remediation combined to create description
                     else:
                         desc_text = desc_sum
 
                     finding = Finding(
-                        title = title,
-                        test = test,
-                        severity = severity_level,
-                        date = formatted_date,
-                        description = desc_text,
-                        cwe = cwe_val,
-                        references = reference_array,
-                        unique_id_from_tool = vuln_id,
+                        title=title,
+                        test=test,
+                        severity=severity_level,
+                        date=formatted_date,
+                        description=desc_text,
+                        cwe=cwe_val,
+                        references=reference_array,
+                        unique_id_from_tool=vuln_id,
                     )
 
                     finding.active = True
@@ -81,5 +83,5 @@ class RapplexParser(object):
                     endpoint = Endpoint.from_uri(url)
                     finding.unsaved_endpoints.append(endpoint)
 
-                    findings.append(finding)                    
+                    findings.append(finding)
         return findings
