576c2a63297ee96666b97bf7f1aaee75c7925dfe
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-05-28 18:06:23 +0200

    :bug: severity mapping, :sparkles: defender performance improvement (#10185)
    * :bug: severity mapping, :sparkles: defender performance improvement
    
    * rotate back uniqueid
    
    * :bug: fix unittests
    
    * undo change

--- dojo/tools/ms_defender/parser.py
+++ dojo/tools/ms_defender/parser.py
@@ -43,7 +43,7 @@ class MSDefenderParser:
                     if "machines/" in content and "machines/" != content:
                         machinefiles.append(content)
                 vulnerabilities = []
-                machines = []
+                machines = {}
                 for vulnerabilityfile in vulnerabilityfiles:
                     output = json.loads(zipdata[vulnerabilityfile].decode('ascii'))['value']
                     for data in output:
@@ -51,12 +51,11 @@ class MSDefenderParser:
                 for machinefile in machinefiles:
                     output = json.loads(zipdata[machinefile].decode('ascii'))['value']
                     for data in output:
-                        machines.append(data)
+                        machines[data.get('id')] = data
                 for vulnerability in vulnerabilities:
                     try:
-                        machine = list(filter(lambda m: m['id'] == vulnerability['machineId'], machines))[0]
-                        self.process_zip(vulnerability, machine)
-                    except IndexError:
+                        self.process_zip(vulnerability, machines[vulnerability['machineId']])
+                    except (IndexError, KeyError):
                         self.process_json(vulnerability)
         else:
             return []
@@ -73,7 +72,7 @@ class MSDefenderParser:
         title = str(vulnerability['cveId'])
         finding = Finding(
             title=title + "_" + vulnerability["machineId"],
-            severity=vulnerability['severity'],
+            severity=self.severity_check(vulnerability['severity']),
             description=description,
             static_finding=False,
             dynamic_finding=True,
@@ -124,7 +123,7 @@ class MSDefenderParser:
             title = title + "_" + str(machine['osPlatform'])
         finding = Finding(
             title=title + "_" + vulnerability["machineId"],
-            severity=vulnerability['severity'],
+            severity=self.severity_check(vulnerability['severity']),
             description=description,
             static_finding=False,
             dynamic_finding=True,
@@ -142,3 +141,9 @@ class MSDefenderParser:
             finding.unsaved_endpoints.append(Endpoint(host=str(machine['lastIpAddress'])))
         if machine['lastExternalIpAddress'] is not None:
             finding.unsaved_endpoints.append(Endpoint(host=str(machine['lastExternalIpAddress'])))
+
+    def severity_check(self, input):
+        if input in ['Informational', 'Low', 'Medium', 'High', 'Critical']:
+            return input
+        else:
+            return "Informational"
