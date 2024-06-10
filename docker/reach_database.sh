cf4350d4b57ac10878d4cf58fdd21cf1d9d3e26c
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-06-10 16:27:57 -0500

    Update to a env var

--- docker/reach_database.sh
+++ docker/reach_database.sh
@@ -3,13 +3,12 @@
 wait_for_database_to_be_reachable() {
     echo -n "Waiting for database to be reachable "
     failure_count=0
-    exit_on_count=30
     until echo "select 1;" | python3 manage.py dbshell > /dev/null
     do 
     echo -n "."
     failure_count=$((failure_count + 1)) 
     sleep 1
-    if [ $exit_on_count = $failure_count ]; then
+    if [ $DD_DATABASE_READINESS_TIMEOUT = $failure_count ]; then
         exit 1
     fi
     done
