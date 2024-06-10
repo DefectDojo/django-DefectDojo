548a2a049c8515072dd43af939c82169d64d48ca
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-06-10 14:36:52 -0500

    Entrypoint Scripts: Add container level breakouts to prevent doom looping

---
+++ docker/reach_database.sh
@@ -0,0 +1,14 @@
+function wait_for_database_to_be_reachable {
+    echo -n "Waiting for database to be reachable "
+    failure_count=0
+    exit_on_count=10
+    until echo "select 1;" | python3 manage.py dbshell > /dev/null
+    do 
+    echo -n "."
+    failure_count=$((failure_count + 1)) 
+    sleep 1
+    if [ $exit_on_count = $failure_count ]; then
+        exit 1
+    fi
+    done
+}
