b18f8e26c937b89aaccd560eba002f94057189d8
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-06-10 15:46:51 -0500

    Increase timeout for helm tests

--- docker/reach_database.sh
+++ docker/reach_database.sh
@@ -3,7 +3,7 @@
 wait_for_database_to_be_reachable() {
     echo -n "Waiting for database to be reachable "
     failure_count=0
-    exit_on_count=10
+    exit_on_count=30
     until echo "select 1;" | python3 manage.py dbshell > /dev/null
     do 
     echo -n "."
