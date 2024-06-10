e56df826ff11c85026e71cfc1b2857a1215d7fa2
kagahd <kay.agahd@idealo.de>
2024-06-10 14:22:40 +0200

    extend prowler v3 parser to parse also prowler v4 reports in oscf-json format

--- unittests/tools/test_aws_prowler_v3_parser.py
+++ unittests/tools/test_aws_prowler_v3_parser.py
@@ -38,3 +38,31 @@ class TestAwsProwlerV3Parser(DojoTestCase):
         with self.subTest(i=3):
             self.assertEqual("prowler-aws-account_maintain_current_contact_details-999999999999-us-east-1-999999999999", findings[2].unique_id_from_tool)
             self.assertIn('Maintain current contact details.', findings[2].description)
+
+    def test_aws_prowler_parser_with_no_vuln_has_no_findings_ocsf_json(self):
+        findings = self.setup(
+            open("unittests/scans/aws_prowler_v3/no_vuln.ocsf.json"))
+        self.assertEqual(0, len(findings))
+
+    def test_aws_prowler_parser_with_critical_vuln_has_one_findings_ocsf_json(self):
+        findings = self.setup(
+            open("unittests/scans/aws_prowler_v3/one_vuln.ocsf.json"))
+        self.assertEqual(1, len(findings))
+        self.assertEqual("prowler-aws-iam_role_administratoraccess_policy_permissive_trust_relationship-123456789012-us-east-1-myAdministratorExecutionRole", findings[0].unique_id_from_tool)
+        self.assertIn('Ensure IAM Roles with attached AdministratorAccess policy have a well defined trust relationship', findings[0].description)
+        self.assertEqual("arn:aws:iam::123456789012:role/myAdministratorExecutionRole", findings[0].component_name)
+        self.assertIn('https://docs.aws.amazon.com/IAM/latest/UserGuide/best-practices.html#grant-least-privilege', findings[0].references)
+
+    def test_aws_prowler_parser_with_many_vuln_has_many_findings_ocsf_json(self):
+        findings = self.setup(
+            open("unittests/scans/aws_prowler_v3/many_vuln.ocsf.json"))
+        self.assertEqual(3, len(findings))
+        with self.subTest(i=0):
+            self.assertEqual("prowler-aws-iam_role_administratoraccess_policy_permissive_trust_relationship-123456789012-us-east-1-myAdministratorExecutionRole", findings[0].unique_id_from_tool)
+            self.assertIn('Ensure IAM Roles with attached AdministratorAccess policy have a well defined trust relationship', findings[0].description)
+        with self.subTest(i=1):
+            self.assertEqual("prowler-aws-iam_role_cross_account_readonlyaccess_policy-123456789012-us-east-1-AuditRole", findings[1].unique_id_from_tool)
+            self.assertIn('Ensure IAM Roles do not have ReadOnlyAccess access for external AWS accounts', findings[1].description)
+        with self.subTest(i=3):
+            self.assertEqual("prowler-aws-iam_role_permissive_trust_relationship-123456789012-us-east-1-CrossAccountResourceAccessRole", findings[2].unique_id_from_tool)
+            self.assertIn('Ensure IAM Roles do not allow assume role from any role of a cross account', findings[2].description)
