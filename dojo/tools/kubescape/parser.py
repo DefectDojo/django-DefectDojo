4f42b18b92d5ba46730bedf318089ccea971374a
Antoine Ruffino <aruffino@cloudbees.com>
2024-06-10 14:47:41 +0200

    Fix typo.

--- dojo/tools/kubescape/parser.py
+++ dojo/tools/kubescape/parser.py
@@ -82,7 +82,7 @@ class KubescapeParser:
                             mitigation = ""
 
                     armoLink = f"https://hub.armosec.io/docs/{controlID.lower()}"
-                    description = "**Summary:** " + f"The ressource '{resourceid}' has failed the control '{control_name}.'  " + "\n"
+                    description = "**Summary:** " + f"The ressource '{resourceid}' has failed the control '{control_name}'." + "\n"
                     if controlSummary is not None and "description" in controlSummary:
                         description += "**Description:** " + controlSummary["description"] + "\n"
 
