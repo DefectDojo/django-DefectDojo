852860f286549d48be914c3188dca183fd040b6e
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-06-20 20:31:50 +0200

    :bug: fix qualys webapp scan request body (#10422)
    * :bug: fix qualys webapp scan request body
    
    * add unittest

--- unittests/tools/test_qualys_webapp_parser.py
+++ unittests/tools/test_qualys_webapp_parser.py
@@ -57,3 +57,14 @@ class TestQualysWebAppParser(DojoTestCase):
         # 21 non-info findings, 21 total
         self.assertEqual(21, len([x for x in findings if x.severity != "Info"]))
         self.assertEqual(21, len(findings))
+
+    def test_discussion_10239(self):
+        testfile = open(
+            get_unit_tests_path() + "/scans/qualys_webapp/discussion_10239.xml"
+        )
+        parser = QualysWebAppParser()
+        findings = parser.get_findings(testfile, Test(), True)
+        testfile.close()
+        self.assertEqual(1, len(findings))
+        finding = findings[0]
+        self.assertEqual(finding.unsaved_req_resp[0].get('req'), "POST: https://example.com/vulnerable/path\nReferer:  https://example.com/\n\nHost:  www.example.com\n\nUser-Agent:  Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15\n\nAccept:  */*\n\nContent-Length:  39\n\nContent-Type:  application/x-www-form-urlencoded REQUEST_ONE\n\nBODY: post_param=malicious_code_here\n")
