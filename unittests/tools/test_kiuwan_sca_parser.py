70bdd191a49c38087eacaefd6ce589af28f9689e
Michael Wager <mail@mwager.de>
2024-06-24 10:04:41 +0200

    fix lint errors

--- unittests/tools/test_kiuwan_sca_parser.py
+++ unittests/tools/test_kiuwan_sca_parser.py
@@ -1,6 +1,8 @@
 from dojo.models import Test
 from dojo.tools.kiuwan_sca.parser import KiuwanSCAParser
+
 from ..dojo_test_case import DojoTestCase
+
 
 # ./dc-unittest.sh --profile postgres-redis --test-case unittests.tools.test_kiuwan_sca_parser.TestKiuwanSCAParser
 class TestKiuwanSCAParser(DojoTestCase):
