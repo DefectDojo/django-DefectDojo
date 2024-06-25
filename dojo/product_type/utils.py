cf56734c63269b8c0ba3bb0bd5e337e65a9cd16b
renejal <beimarjal@unicauca.edu.co>
2024-06-25 15:05:15 -0500

    bug add None Contacts type

--- dojo/product_type/utils.py
+++ dojo/product_type/utils.py
@@ -12,11 +12,12 @@ def get_technical_contacts(product_type: Product_Type):
 def add_technical_contact_whit_member(product_type: Product_Type):
     technical_contacts = get_technical_contacts(product_type)
     for technical_contact in technical_contacts:
-        members = Product_Type_Member.objects.filter(product_type=product_type, user=technical_contact)
-        if members.count() == 0:
-            product_type_member = Product_Type_Member()
-            product_type_member.product_type = product_type
-            product_type_member.user = technical_contact
-            product_type_member.role = Role.objects.get(name="Leader") 
-            product_type_member.save()
+        if technical_contact:
+            members = Product_Type_Member.objects.filter(product_type=product_type, user=technical_contact)
+            if members.count() == 0:
+                product_type_member = Product_Type_Member()
+                product_type_member.product_type = product_type
+                product_type_member.user = technical_contact
+                product_type_member.role = Role.objects.get(name="Leader") 
+                product_type_member.save()
 
