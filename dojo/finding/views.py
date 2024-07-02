6fdd46d35f88975f082316be0385f53c2a7009bc
kiblik <5609770+kiblik@users.noreply.github.com>
2024-07-03 00:35:09 +0200

    Ruff: add and fix Q (except Q000) (#10094)
    * Ruff: add Q001
    
    * Ruff: fix Q001
    
    * Ruff: add Q002
    
    * Ruff: fix Q002
    
    * Ruff: add Q003
    
    * Ruff: fix Q003
    
    * Ruff: add Q004
    
    * Ruff: fix Q004

--- dojo/finding/views.py
+++ dojo/finding/views.py
@@ -1709,7 +1709,7 @@ def request_finding_review(request, fid):
                 finding=finding,
                 reviewers=reviewers,
                 recipients=reviewers_usernames,
-                description=f"User {user.get_full_name()} has requested that user(s) {reviewers_string} review the finding \"{finding.title}\" for accuracy:\n\n{new_note}",
+                description=f'User {user.get_full_name()} has requested that user(s) {reviewers_string} review the finding "{finding.title}" for accuracy:\n\n{new_note}',
                 icon="check",
                 url=reverse("view_finding", args=(finding.id,)),
             )
