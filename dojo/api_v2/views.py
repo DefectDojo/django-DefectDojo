c6e24ddd8f9b14f311168100e34d34bdd4f1f76c
renejal <beimarjal@unicauca.edu.co>
2024-07-10 17:06:28 -0500

    test delete tranfer-finding error

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -3361,12 +3361,12 @@ class TransferFindingViewSet(prefetch.PrefetchListMixin,
     
     def destroy(self, request, pk=None):
         try:
-            transfer_finding_copy = copy.deepcopy(get_object_or_404(TransferFinding, id=pk))
+            # transfer_finding_copy = copy.deepcopy(get_object_or_404(TransferFinding, id=pk))
             obj_transfer_finding_findings = TransferFindingFinding.objects.filter(transfer_findings=int(pk))
             for transfer_finding_finding in obj_transfer_finding_findings:
                 helper_tf.reset_finding_related(transfer_finding_finding.findings)
-            NotificationTransferFinding.transfer_finding_remove(transfer_finding_copy)
-            super().destroy(request, pk)
+            # NotificationTransferFinding.transfer_finding_remove(transfer_finding_copy)
+            # super().destroy(request, pk)
             return http_response.no_content(message="TransferFinding Deleted")
         except Exception as e:
             logger.error(e)
