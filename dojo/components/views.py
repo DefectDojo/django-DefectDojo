9b991c65305ca2c2dedbea4d13d976ce19f9387b
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-07-25 13:40:09 -0500

    Listing Tables: Add toggle switch in system settings (#10617)
    * Listing Tables: Add toggle switch in system settings
    
    * Fixing ruff
    
    * Update help text
    
    * Remove missed italics

--- dojo/components/views.py
+++ dojo/components/views.py
@@ -70,5 +70,6 @@ def components(request):
             "filter": comp_filter,
             "result": result,
             "component_words": sorted(set(component_words)),
+            "enable_table_filtering": get_system_setting("enable_ui_table_based_searching"),
         },
     )
