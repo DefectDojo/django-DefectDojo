855b8bdcd125a1aeeb681681c11b5c87b0b6432e
dogboat <dogboat@users.noreply.github.com>
2024-07-25 10:20:50 -0400

    finding-notes-cascading-deletes remove unused code

--- dojo/notes/helper.py
+++ dojo/notes/helper.py
@@ -1,8 +1,5 @@
 import logging
 
-from django.db.models.signals import pre_delete
-from django.dispatch import receiver
-
 logger = logging.getLogger(__name__)
 
 
@@ -12,17 +9,3 @@ def delete_related_notes(obj):
         return
     logging.debug(f"Deleting {obj.notes.count()} notes for {type(obj).__name__} {obj.id}")
     obj.notes.all().delete()
-
-
-def generate_pre_delete_notes_cleanup(model):
-    if not hasattr(model, 'notes'):
-        logger.warning(
-            f"Attempting to generate notes deleter for model {model.__name__} which has no 'notes' attribute.")
-        return None
-
-    @receiver(pre_delete, sender=model)
-    def model_pre_delete(sender, instance, **kwargs):
-        logging.debug(f"Deleting notes for {model.__name__} {instance.id}")
-        delete_related_notes(instance)
-
-    return model_pre_delete
