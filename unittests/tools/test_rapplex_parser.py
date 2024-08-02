68a57afef8bfeaeb713f9efec3b56be627d30bb1
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2024-08-02 18:26:49 -0500

    Correct ruff errors

--- unittests/tools/test_rapplex_parser.py
+++ unittests/tools/test_rapplex_parser.py
@@ -2,8 +2,7 @@ from os import path
 
 from dojo.models import Test
 from dojo.tools.rapplex.parser import RapplexParser
-
-from ..dojo_test_case import DojoTestCase
+from unittests.dojo_test_case import DojoTestCase
 
 
 class TestRapplexParser(DojoTestCase):
