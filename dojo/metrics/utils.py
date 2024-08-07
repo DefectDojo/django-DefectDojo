4240982d3fa4b4cb6126d98830825ee244aa4813
Manuel Sommer <sommermanuel@gmx.de>
2024-08-07 15:37:03 +0200

    ruff

--- dojo/metrics/utils.py
+++ dojo/metrics/utils.py
@@ -8,7 +8,6 @@ from typing import Any, NamedTuple, TypeVar
 
 from dateutil.relativedelta import relativedelta
 from django.contrib import messages
-from django.db import connection
 from django.db.models import Case, Count, F, IntegerField, Q, Sum, Value, When
 from django.db.models.functions import Coalesce, ExtractDay, Now, TruncMonth, TruncWeek
 from django.db.models.query import QuerySet
