f9776cf2f87f4fc5471630474ad287c43131e876
testaccount90009 <122134756+testaccount90009@users.noreply.github.com>
2024-08-15 16:06:43 -0700

    Update test_asff_parser.py

--- unittests/tools/test_asff_parser.py
+++ unittests/tools/test_asff_parser.py
@@ -51,9 +51,9 @@ class TestAsffParser(DojoTestCase):
             resource_id = data[0]["Resources"][0]["Id"]
             findings = parser.get_findings(file, Test())
             self.assertEqual(len(findings), 5)
-            self.assertEqual(findings.description, f"**AWS resource ARN:** {resource_id}", "\n\n", ["Description"])
             for index, finding in enumerate(findings):
                 self.common_check_finding(finding, data, index)
+                self.assertEqual(finding.description, f"**AWS resource ARN:** {resource_id}", "\n\n", ["Description"])
 
     def test_asff_guardduty(self):
         data = self.load_sample_json("guardduty/Unusual Behaviors-User-Persistence IAMUser-NetworkPermissions.json")
@@ -62,8 +62,8 @@ class TestAsffParser(DojoTestCase):
             resource_id = data[0]["Resources"][0]["Id"]
             findings = parser.get_findings(file, Test())
             self.assertEqual(len(findings), 1)
-            self.assertEqual(findings.description, f"**AWS resource ARN:** {resource_id}", "\n\n", ["Description"])
             for index, finding in enumerate(findings):
                 self.common_check_finding(finding, data, index, guarddutydate=True)
             self.assertEqual(finding.unsaved_endpoints[0], Endpoint(host="10.0.0.1"))
             self.assertTrue(finding.active)
+            self.assertEqual(finding.description, f"**AWS resource ARN:** {resource_id}", "\n\n", ["Description"])
