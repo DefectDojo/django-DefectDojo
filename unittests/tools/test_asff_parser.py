0d1a63b4ef9841dfeb37897dfc67e3f55b092860
testaccount90009 <122134756+testaccount90009@users.noreply.github.com>
2024-08-15 11:57:58 -0700

    Update test_asff_parser.py

--- unittests/tools/test_asff_parser.py
+++ unittests/tools/test_asff_parser.py
@@ -19,7 +19,7 @@ class TestAsffParser(DojoTestCase):
     def common_check_finding(self, finding, data, index, guarddutydate=False):
         parser = AsffParser()
         self.assertEqual(finding.title, data[index]["Title"])
-        self.assertEqual(finding.description, data[index]["Resources"]["Id"]\n\n["Description"])
+        self.assertEqual(finding.description, data[index]"**AWS resource ARN:** "["Resources"]["Id"]\n\n["Description"])
         if guarddutydate:
             self.assertEqual(finding.date.date(),
                 datetime.strptime(data[0]["CreatedAt"], "%Y-%m-%dT%H:%M:%S.%fZ").date())
