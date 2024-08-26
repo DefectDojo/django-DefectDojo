db1920a87715ca1aaa1369fda3e5c6ee325cc938
dogboat <dogboat@users.noreply.github.com>
2024-08-25 21:00:27 -0400

    Finding hash/dedupe changes (#10386)
    * dedupe-help Move logic to set Finding hash code to its own method
    
    * dedupe-help rework set_hash_code method to accept dedupe_option
    
    * Update versions in application files
    
    * Revert "Update versions in application files"
    
    This reverts commit 7ee4bfa0ac60201e38696b6d6cf998b97608c12e.
    
    * dedupe-help reorder method to make linter happy
    
    * dedupe-help Rework finding hash set/dedupe to attempt to load methods based on settings and fall back to existing implementations as defaults
    
    * dedupe-help add helper method to load custom methods and use it
    
    * dedupe-help bug in load custom helper method
    
    * dedupe-help Linter fix (import ordering)
    
    * dedupe-help Update default_importer to handle .values() call on findings set within close old findings method
    
    * dedupe-help extract get_(re)importer methods from engagement/test (re)import views into a separate method
    
    * dedupe-help extract reimport dedupe alg determination into its own method
    
    * dedupe-help refactor where custom methods for hashing/dedupe are called to minimize changes to existing calls
    
    * dedupe-help linter fixes
    
    ---------
    
    Co-authored-by: DefectDojo release bot <dojo-release-bot@users.noreply.github.com>

--- dojo/utils.py
+++ dojo/utils.py
@@ -1,6 +1,7 @@
 import binascii
 import calendar as tcalendar
 import hashlib
+import importlib
 import logging
 import mimetypes
 import os
@@ -8,6 +9,7 @@ import re
 from calendar import monthrange
 from datetime import date, datetime, timedelta
 from math import pi, sqrt
+from typing import Callable, Optional
 
 import bleach
 import crum
@@ -295,6 +297,9 @@ def do_dedupe_finding_task(new_finding, *args, **kwargs):
 
 
 def do_dedupe_finding(new_finding, *args, **kwargs):
+    if dedupe_method := get_custom_method("FINDING_DEDUPE_METHOD"):
+        return dedupe_method(new_finding, *args, **kwargs)
+
     try:
         enabled = System_Settings.objects.get(no_cache=True).enable_deduplication
     except System_Settings.DoesNotExist:
@@ -2592,6 +2597,23 @@ def get_open_findings_burndown(product):
     past_90_days["y_min"] = running_min
 
     return past_90_days
+
+
+def get_custom_method(setting_name: str) -> Optional[Callable]:
+    """
+    Attempts to load and return the method specified by fully-qualified name at the given setting.
+
+    :param setting_name: The name of the setting that holds the fqname of the Python method we want to load
+    :return: The callable if it was able to be loaded, else None
+    """
+    if fq_name := getattr(settings, setting_name, None):
+        try:
+            mn, _, fn = fq_name.rpartition(".")
+            m = importlib.import_module(mn)
+            return getattr(m, fn)
+        except ModuleNotFoundError:
+            pass
+    return None
 
 
 def generate_file_response(file_object: FileUpload) -> FileResponse:
