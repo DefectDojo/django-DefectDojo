5c2ffda87ac5b5d19f60786e3ee3009711ff551c
Hydragyrum <hydragyrum@gmail.com>
2024-09-06 11:26:32 +0200

    Fix the basic structure of the parser

---
+++ dojo/tools/ptart/assessment_parser.py
@@ -0,0 +1,6 @@
+class PTARTAssessmentParser:
+    def get_test_data(self, tree):
+        return []
+
+    def get_findings_for(self, assessment_name, data):
+        return []
---
+++ dojo/tools/ptart/retest_parser.py
@@ -0,0 +1,6 @@
+class PTARTRetestParser:
+    def get_test_data(self, tree):
+        return []
+
+    def get_findings_for(self, retrest_name, data):
+        return []
