e26223b816ce23f311d80512b65b392fe6ecf156
Hydragyrum <hydragyrum@gmail.com>
2024-09-17 17:31:17 +0200

    Add test cases

--- dojo/tools/ptart/assessment_parser.py
+++ dojo/tools/ptart/assessment_parser.py
@@ -1,4 +1,6 @@
 from dojo.models import Finding
+from dojo.templatetags.event_tags import is_file
+from dojo.tools.aqua.parser import severity_of
 from dojo.tools.parser_test import ParserTest
 
 
@@ -12,5 +14,30 @@ class PTARTAssessmentParser:
         return [finding for assessment in assessments for finding in self.parse_assessment(assessment)]
 
     def parse_assessment(self, assessment):
-        return [Finding(title=hit["title"]) for hit in assessment.get("hits", [])]
+        return [Finding(
+            title=hit["title"],
+            description=hit["body"],
+            severity=self.parse_severity(hit["severity"]),
+            mitigation=hit["remediation"],
+            cvssv3=hit["cvss_vector"],
+            unique_id_from_tool=hit["id"],
+            effort_for_fixing=self.parse_fix_effort(hit["fix_complexity"]),
+            component_name=assessment["title"],
+        ) for hit in assessment.get("hits", [])]
 
+    def parse_severity(self, severity):
+        severity_mapping = {
+            1: "Critical",
+            2: "High",
+            3: "Medium",
+            4: "Low"
+        }
+        return severity_mapping.get(severity, "Info")  # Default severity
+
+    def parse_fix_effort(self, effort):
+        effort_mapping = {
+            1: "High",
+            2: "Medium",
+            3: "Low"
+        }
+        return effort_mapping.get(effort, "")
--- unittests/tools/test_ptart_parser.py
+++ unittests/tools/test_ptart_parser.py
@@ -30,6 +30,41 @@ class TestPTARTParser(TestCase):
             with self.subTest(i=0):
                 finding = findings[0]
                 self.assertEqual("Broken Access Control", finding.title)
+                self.assertEqual("High", finding.severity)
+                self.assertEqual("Access control enforces policy such that users cannot act outside of their intended permissions. Failures typically lead to unauthorized information disclosure, modification or destruction of all data, or performing a business function outside of the limits of the user.", finding.description)
+                self.assertEqual("Access control vulnerabilities can generally be prevented by taking a defense-in-depth approach and applying the following principles:\n\n* Never rely on obfuscation alone for access control.\n* Unless a resource is intended to be publicly accessible, deny access by default.\n* Wherever possible, use a single application-wide mechanism for enforcing access controls.\n* At the code level, make it mandatory for developers to declare the access that is allowed for each resource, and deny access by default.\n* Thoroughly audit and test access controls to ensure they are working as designed.", finding.mitigation)
+                self.assertEqual("CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H", finding.cvssv3)
+                self.assertEqual("PTART-2024-00002", finding.unique_id_from_tool)
+                self.assertEqual("Low", finding.effort_for_fixing)
+                self.assertEqual("Test Assessment", finding.component_name)
+
+
+    def test_ptart_parser_with_one_assessment_has_many_findings(self):
+        with open("unittests/scans/ptart/ptart_many_vul.json") as testfile:
+            parser = PTARTParser()
+            findings = parser.get_findings(testfile, self.test)
+            self.assertEqual(2, len(findings))
+            with self.subTest(i=0):
+                finding = findings[0]
+                self.assertEqual("Broken Access Control", finding.title)
+                self.assertEqual("High", finding.severity)
+                self.assertEqual("Access control enforces policy such that users cannot act outside of their intended permissions. Failures typically lead to unauthorized information disclosure, modification or destruction of all data, or performing a business function outside of the limits of the user.", finding.description)
+                self.assertEqual("Access control vulnerabilities can generally be prevented by taking a defense-in-depth approach and applying the following principles:\n\n* Never rely on obfuscation alone for access control.\n* Unless a resource is intended to be publicly accessible, deny access by default.\n* Wherever possible, use a single application-wide mechanism for enforcing access controls.\n* At the code level, make it mandatory for developers to declare the access that is allowed for each resource, and deny access by default.\n* Thoroughly audit and test access controls to ensure they are working as designed.", finding.mitigation)
+                self.assertEqual("CVSS:3.1/AV:N/AC:L/PR:N/UI:N/S:C/C:H/I:H/A:H", finding.cvssv3)
+                self.assertEqual("PTART-2024-00002", finding.unique_id_from_tool)
+                self.assertEqual("Low", finding.effort_for_fixing)
+                self.assertEqual("Test Assessment", finding.component_name)
+            with self.subTest(i=1):
+                finding = findings[1]
+                self.assertEqual("Unrated Hit", finding.title)
+                self.assertEqual("Info", finding.severity)
+                self.assertEqual("Some hits are not rated.", finding.description)
+                self.assertEqual("They can be informational or not related to a direct attack", finding.mitigation)
+                self.assertEqual("", finding.cvssv3)
+                self.assertEqual("PTART-2024-00003", finding.unique_id_from_tool)
+                self.assertEqual("Low", finding.effort_for_fixing)
+                self.assertEqual("Test Assessment", finding.component_name)
+
     #
     #
     # def test_ptart_parser_with_no_vuln_has_no_findings(self):
