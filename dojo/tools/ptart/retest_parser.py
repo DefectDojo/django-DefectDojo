1f3423b7a4cb19dbca5dec20f8cd7c7c1d187089
Hydragyrum <hydragyrum@gmail.com>
2024-10-10 15:12:12 +0200

    Fix formatting to flake8 standards

--- dojo/tools/ptart/retest_parser.py
+++ dojo/tools/ptart/retest_parser.py
@@ -8,7 +8,7 @@ def generate_retest_hit_title(hit, original_hit):
     title = original_hit.get("title", "")
     hit_id = hit.get("id", None)
     if "status" in hit:
-        title = f"{title} ({ptart_tools.parse_retest_fix_status(hit['status'])})"
+        title = f"{title} ({ptart_tools.parse_retest_status(hit['status'])})"
     fake_retest_hit = {
         "title": title,
         "id": hit_id,
@@ -28,7 +28,8 @@ class PTARTRetestParser:
         else:
             return []
 
-        return [finding for retest in retests for finding in self.parse_retest(retest)]
+        return [finding for retest in retests
+                for finding in self.parse_retest(retest)]
 
     def parse_retest(self, retest):
         hits = retest.get("hits", [])
@@ -39,26 +40,35 @@ class PTARTRetestParser:
 
     def get_finding(self, retest, hit):
 
-        # The negatives are a bit confusing, but we want to skip hits that don't have an original hit.
-        # Hit is invalid in a retest if not linked to an original.
+        # The negatives are a bit confusing, but we want to skip hits that
+        # don't have an original hit. Hit is invalid in a retest if not linked
+        # to an original.
         if "original_hit" not in hit or not hit["original_hit"]:
             return None
 
         # Get the original hit from the retest
         original_hit = hit["original_hit"]
 
-        # Set the Finding title to the original hit title with the retest status if available
-        # We don't really have any other places to set this field.
+        # Set the Finding title to the original hit title with the retest
+        # status if available. We don't really have any other places to set
+        # this field.
         finding_title = generate_retest_hit_title(hit, original_hit)
 
-        # As the retest hit doesn't have a date added, use the start of the retest campaign as something that's
-        # close enough.
+        # As the retest hit doesn't have a date added, use the start of the
+        # retest campaign as something that's close enough.
         finding = Finding(
             title=finding_title,
-            severity=ptart_tools.parse_ptart_severity(original_hit.get("severity", 5)),
-            effort_for_fixing=ptart_tools.parse_ptart_fix_effort(original_hit.get("fix_complexity", 3)),
+            severity=ptart_tools.parse_ptart_severity(
+                original_hit.get("severity")
+            ),
+            effort_for_fixing=ptart_tools.parse_ptart_fix_effort(
+                original_hit.get("fix_complexity")
+            ),
             component_name=f"Retest: {retest.get('name', 'Retest')}",
-            date=ptart_tools.parse_date(retest.get("start_date"), "%Y-%m-%d"),
+            date=ptart_tools.parse_date(
+                retest.get("start_date"),
+                "%Y-%m-%d"
+            ),
         )
 
         # Don't add the fields if they are blank.
@@ -71,16 +81,22 @@ class PTARTRetestParser:
         if "id" in hit and hit["id"]:
             finding.unique_id_from_tool = hit.get("id")
 
-        cvss_vector = ptart_tools.parse_cvss_vector(original_hit, self.cvss_type)
+        cvss_vector = ptart_tools.parse_cvss_vector(
+            original_hit,
+            self.cvss_type
+        )
         if cvss_vector:
             finding.cvssv3 = cvss_vector
 
         if "labels" in original_hit:
             finding.unsaved_tags = original_hit["labels"]
 
-        finding.unsaved_endpoints = ptart_tools.parse_endpoints_from_hit(original_hit)
+        finding.unsaved_endpoints = ptart_tools.parse_endpoints_from_hit(
+            original_hit
+        )
 
-        # We only have screenshots in a retest. Refer to the original hit for the attachments.
+        # We only have screenshots in a retest. Refer to the original hit for
+        # the attachments.
         finding.unsaved_files = ptart_tools.parse_screenshots_from_hit(hit)
 
         return finding
