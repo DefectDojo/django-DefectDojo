5d0a056112805a4585b25f2014bbf1871486eb69
Dmitry Maryushkin <dmmaryushkin@ozon.ru>
2024-10-14 23:21:24 +0300

    Refactor mobsf parser for v4 reports

--- unittests/tools/test_mobsf_parser.py
+++ unittests/tools/test_mobsf_parser.py
@@ -5,18 +5,26 @@ from unittests.dojo_test_case import DojoTestCase
 
 class TestMobSFParser(DojoTestCase):
 
-    def test_parse_file(self):
+    def test_parse_android_empty_file(self):
+
         test = Test()
         engagement = Engagement()
         engagement.product = Product()
         test.engagement = engagement
         parser = MobSFParser()
 
-        # Android
         with open("unittests/scans/mobsf/dvba_4_0_7_android_empty.json", encoding="utf-8") as android_empty_file:
 
             android_empty_findings = parser.get_findings(android_empty_file, test)
             self.assertEqual(0, len(android_empty_findings))
+
+    def test_parse_android_one_file(self):
+
+        test = Test()
+        engagement = Engagement()
+        engagement.product = Product()
+        test.engagement = engagement
+        parser = MobSFParser()
 
         with open("unittests/scans/mobsf/dvba_4_0_7_android_one.json", encoding="utf-8") as android_one_file:
 
@@ -26,6 +34,14 @@ class TestMobSFParser(DojoTestCase):
             item = android_one_findings[0]
             self.assertEqual("Base config is insecurely configured to permit clear text traffic to all domains", item.title)
             self.assertEqual("High", item.severity)
+
+    def test_parse_android_full_file(self):
+
+        test = Test()
+        engagement = Engagement()
+        engagement.product = Product()
+        test.engagement = engagement
+        parser = MobSFParser()
 
         with open("unittests/scans/mobsf/dvba_4_0_7_android_full.json", encoding="utf-8") as android_full_file:
 
@@ -40,12 +56,26 @@ class TestMobSFParser(DojoTestCase):
             self.assertEqual("This application has no privacy trackers", item.title)
             self.assertEqual("Info", item.severity)
 
-        # iOS
+    def test_parse_ios_empty_file(self):
+
+        test = Test()
+        engagement = Engagement()
+        engagement.product = Product()
+        test.engagement = engagement
+        parser = MobSFParser()
 
         with open("unittests/scans/mobsf/dvia2_4_0_7_ios_empty.json", encoding="utf-8") as ios_empty_file:
             ios_empty_findings = parser.get_findings(ios_empty_file, test)
 
             self.assertEqual(0, len(ios_empty_findings))
+
+    def test_parse_ios_one_file(self):
+
+        test = Test()
+        engagement = Engagement()
+        engagement.product = Product()
+        test.engagement = engagement
+        parser = MobSFParser()
 
         with open("unittests/scans/mobsf/dvia2_4_0_7_ios_one.json", encoding="utf-8") as ios_one_file:
             ios_one_findings = parser.get_findings(ios_one_file, test)
@@ -56,10 +86,18 @@ class TestMobSFParser(DojoTestCase):
             self.assertEqual("App Transport Security AllowsArbitraryLoads is allowed", item.title)
             self.assertEqual("High", item.severity)
 
+    def test_parse_ios_full_file(self):
+
+        test = Test()
+        engagement = Engagement()
+        engagement.product = Product()
+        test.engagement = engagement
+        parser = MobSFParser()
+
         with open("unittests/scans/mobsf/dvia2_4_0_7_ios_full.json", encoding="utf-8") as ios_full_file:
             ios_full_findings = parser.get_findings(ios_full_file, test)
 
-            self.assertEqual(11, len(android_full_findings))
+            self.assertEqual(11, len(ios_full_findings))
 
             item = ios_full_findings[2]
             self.assertEqual("Binary makes use of the insecure Random function(s)", item.title)
