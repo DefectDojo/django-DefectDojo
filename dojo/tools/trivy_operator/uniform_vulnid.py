072eb27d538dca92759c394e846f79a4cd21ce02
Manuel Sommer <sommermanuel@gmx.de>
2024-11-04 22:44:19 +0100

    fix

--- dojo/tools/trivy_operator/uniform_vulnid.py
+++ dojo/tools/trivy_operator/uniform_vulnid.py
@@ -11,10 +11,10 @@ class UniformTrivyVulnID:
             temp = re.compile("([a-zA-Z-_]+)([0-9]+)")
             number = str(temp.match(vulnid).groups()[1]).zfill(3)
             avd_category = str(temp.match(vulnid.lower()).groups()[0])
-            uniformed_vuln_id = avd_category.upper() + number
-        else:
+            return avd_category.upper() + number
+        if "ksv" in vulnid.lower() or "kcv" in vulnid.lower():
             temp = re.compile("([a-zA-Z-_]+)([0-9]+)")
             number = str(temp.match(vulnid).groups()[1]).zfill(4)
             avd_category = str(temp.match(vulnid.lower().replace("_", "").replace("-", "")).groups()[0].replace("avd", ""))
-            uniformed_vuln_id = "AVD-" + avd_category.upper() + "-" + number
-        return uniformed_vuln_id
+            return "AVD-" + avd_category.upper() + "-" + number
+        return vulnid
