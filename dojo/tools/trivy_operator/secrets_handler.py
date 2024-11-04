dcba7de9e3338cfe87ca49bdcddcdfc47c0aec43
Manuel Sommer <sommermanuel@gmx.de>
2024-11-04 20:59:35 +0100

    fix

--- dojo/tools/trivy_operator/secrets_handler.py
+++ dojo/tools/trivy_operator/secrets_handler.py
@@ -42,6 +42,7 @@ class TrivySecretsHandler:
             secret_description += "\n**resource.kind:** " + resource_kind
             secret_description += "\n**resource.name:** " + resource_name
             secret_description += "\n**resource.namespace:** " + resource_namespace
+            secret_description += "\n**ruleID:** " + secret_rule_id
             finding = Finding(
                 test=test,
                 title=title,
