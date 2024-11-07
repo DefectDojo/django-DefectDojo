b1b030d526a9a86a15e7d9cc7f1543e333e03ac2
Ivan Morgun <ivan.morgun@creatoriq.com>
2024-11-07 22:15:22 +0300

    Add tesst to support prowler version <4.5.0

--- unittests/tools/test_aws_prowler_v3plus_parser.py
+++ unittests/tools/test_aws_prowler_v3plus_parser.py
@@ -43,6 +43,26 @@ class TestAwsProwlerV3plusParser(DojoTestCase):
             open("unittests/scans/aws_prowler_v3plus/no_vuln.ocsf.json", encoding="utf-8"))
         self.assertEqual(0, len(findings))
 
+    def test_aws_prowler_parser_before_4_5_0_with_critical_vuln_has_one_findings_ocsf_json(self):
+        findings = self.setup(
+            open("unittests/scans/aws_prowler_v3plus/one_vuln_before_4_5_0.ocsf.json", encoding="utf-8"))
+        self.assertEqual(1, len(findings))
+        self.assertEqual("prowler-aws-iam_role_administratoraccess_policy_permissive_trust_relationship-123456789012-us-east-1-myAdministratorExecutionRole", findings[0].unique_id_from_tool)
+        self.assertIn("Ensure IAM Roles with attached AdministratorAccess policy have a well defined trust relationship", findings[0].description)
+        self.assertEqual("arn:aws:iam::123456789012:role/myAdministratorExecutionRole", findings[0].component_name)
+        self.assertIn("https://docs.aws.amazon.com/IAM/latest/UserGuide/best-practices.html#grant-least-privilege", findings[0].references)
+
+    def test_aws_prowler_parser_before_4_5_0_with_many_vuln_has_many_findings_ocsf_json(self):
+        findings = self.setup(
+            open("unittests/scans/aws_prowler_v3plus/many_vuln_before_4_5_0.ocsf.json", encoding="utf-8"))
+        self.assertEqual(2, len(findings))
+        with self.subTest(i=0):
+            self.assertEqual("prowler-aws-iam_role_administratoraccess_policy_permissive_trust_relationship-123456789012-us-east-1-myAdministratorExecutionRole", findings[0].unique_id_from_tool)
+            self.assertIn("Ensure IAM Roles with attached AdministratorAccess policy have a well defined trust relationship", findings[0].description)
+        with self.subTest(i=1):
+            self.assertEqual("prowler-aws-iam_role_cross_account_readonlyaccess_policy-123456789012-us-east-1-AuditRole", findings[1].unique_id_from_tool)
+            self.assertIn("Ensure IAM Roles do not have ReadOnlyAccess access for external AWS accounts", findings[1].description)
+
     def test_aws_prowler_parser_with_critical_vuln_has_one_findings_ocsf_json(self):
         findings = self.setup(
             open("unittests/scans/aws_prowler_v3plus/one_vuln.ocsf.json", encoding="utf-8"))
