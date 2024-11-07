91a0b229d55ba91629926227117c6400384f90fb
Manuel Sommer <sommermanuel@gmx.de>
2024-11-07 12:34:04 +0100

    :bug: fix DefendercomputerDNSName is mostly a userinfo

--- dojo/tools/ms_defender/parser.py
+++ dojo/tools/ms_defender/parser.py
@@ -130,12 +130,10 @@ class MSDefenderParser:
             finding.unsaved_vulnerability_ids.append(vulnerability["cveId"])
         self.findings.append(finding)
         finding.unsaved_endpoints = []
-        if machine["computerDnsName"] is not None:
-            finding.unsaved_endpoints.append(Endpoint(host=str(machine["computerDnsName"]).replace(" ","")))
         if machine["lastIpAddress"] is not None:
-            finding.unsaved_endpoints.append(Endpoint(host=str(machine["lastIpAddress"])))
+            finding.unsaved_endpoints.append(Endpoint(host=str(machine["lastIpAddress"]), userinfo=str(machine["computerDnsName"]).replace(" ","")))
         if machine["lastExternalIpAddress"] is not None:
-            finding.unsaved_endpoints.append(Endpoint(host=str(machine["lastExternalIpAddress"])))
+            finding.unsaved_endpoints.append(Endpoint(host=str(machine["lastExternalIpAddress"]), userinfo=str(machine["computerDnsName"]).replace(" ","")))
 
     def severity_check(self, input):
         if input in ["Informational", "Low", "Medium", "High", "Critical"]:
