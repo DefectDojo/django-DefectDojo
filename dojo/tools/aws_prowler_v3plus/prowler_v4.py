ada11bbab19acbf1f75ecfa6f965932000fdc646
Ivan Morhun <114015523+ivan-morhun@users.noreply.github.com>
2024-11-07 13:44:02 +0300

    #11210 prowler_v4.py Replace 'event_time' with 'time' key for finding

--- dojo/tools/aws_prowler_v3plus/prowler_v4.py
+++ dojo/tools/aws_prowler_v3plus/prowler_v4.py
@@ -37,7 +37,7 @@ class AWSProwlerV4Parser:
             documentation = deserialized.get("remediation", {}).get("references", "")
             documentation = str(documentation) + "\n" + str(deserialized.get("unmapped", {}).get("related_url", ""))
             security_domain = deserialized.get("resources", [{}])[0].get("type", "")
-            timestamp = deserialized.get("event_time")
+            timestamp = deserialized.get("time")
             resource_arn = deserialized.get("resources", [{}])[0].get("uid", "")
             resource_id = deserialized.get("resources", [{}])[0].get("name", "")
             unique_id_from_tool = deserialized.get("finding_info", {}).get("uid", "")
