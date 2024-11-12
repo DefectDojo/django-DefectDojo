9b71a3701f460d7ecbb57898913b0b06c28e7ee8
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-11-13 00:43:24 +0100

    Ruff: Add and fix PTH120 (#11201)
    * Ruff: Add and fix PTH120
    
    * fix dedupe_test
    
    * fix dedupe_test
    
    * fix
    
    * sha sum
    
    * ruff
    
    * retrigger unittest
    
    * sha sum

--- unittests/tools/test_burp_enterprise_parser.py
+++ unittests/tools/test_burp_enterprise_parser.py
@@ -1,4 +1,5 @@
 from os import path
+from pathlib import Path
 
 from dojo.models import Test
 from dojo.tools.burp_enterprise.parser import BurpEnterpriseParser
@@ -8,7 +9,7 @@ from unittests.dojo_test_case import DojoTestCase
 class TestBurpEnterpriseParser(DojoTestCase):
 
     def test_burp_enterprise_with_multiple_vulns(self):
-        with open(path.join(path.dirname(__file__), "../scans/burp_enterprise/many_vulns.html"), encoding="utf-8") as test_file:
+        with open(path.join(Path(__file__).parent, "../scans/burp_enterprise/many_vulns.html"), encoding="utf-8") as test_file:
             parser = BurpEnterpriseParser()
             findings = parser.get_findings(test_file, Test())
             for finding in findings:
@@ -37,7 +38,7 @@ class TestBurpEnterpriseParser(DojoTestCase):
                 self.assertIn("**Issue detail**:\nFingerprint Details:\n\nWAF Type : redacted\nWAF tech. details : Cloud-based CDN, WAF & DDoS prevention", finding.description)
 
     def test_burp_enterprise_with_multiple_vulns_newer_format(self):
-        with open(path.join(path.dirname(__file__), "../scans/burp_enterprise/many_vulns_updated_format.html"), encoding="utf-8") as test_file:
+        with open(path.join(Path(__file__).parent, "../scans/burp_enterprise/many_vulns_updated_format.html"), encoding="utf-8") as test_file:
             parser = BurpEnterpriseParser()
             findings = parser.get_findings(test_file, Test())
             for finding in findings:
