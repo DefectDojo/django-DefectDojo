42ebe2b501a84726b4a3270876e995d299798bfe
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-11-12 21:35:10 +0100

    :tada: Add DSA vulnid (#11238)
    * :tada: Add DSA vulnid
    
    * update sha sum
    
    * retrigger unittest
    
    ---------
    
    Co-authored-by: Ross E Esposito <ross@defectdojo.com>

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1749,6 +1749,7 @@ VULNERABILITY_URLS = {
     "CAPEC": "https://capec.mitre.org/data/definitions/&&.html",  # e.g. https://capec.mitre.org/data/definitions/157.html
     "CWE": "https://cwe.mitre.org/data/definitions/&&.html",  # e.g. https://cwe.mitre.org/data/definitions/79.html
     "TEMP": "https://security-tracker.debian.org/tracker/",  # e.g. https://security-tracker.debian.org/tracker/TEMP-0841856-B18BAF
+    "DSA": "https://security-tracker.debian.org/tracker/",  # e.g. https://security-tracker.debian.org/tracker/DSA-5791-1
 }
 # List of acceptable file types that can be uploaded to a given object via arbitrary file upload
 FILE_UPLOAD_TYPES = env("DD_FILE_UPLOAD_TYPES")
