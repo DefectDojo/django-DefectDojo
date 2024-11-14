f1aa0ecd842963609436eb5c6da7508b62e52f9c
testaccount90009 <122134756+testaccount90009@users.noreply.github.com>
2024-11-13 17:57:58 -0800

    refactor

--- unittests/tools/test_mend_platform_api3_parser.py
+++ unittests/tools/test_mend_platform_api3_parser.py
@@ -1,5 +1,5 @@
 from dojo.models import Test
-from dojo.tools.mend_platform_api3.parser import MendPlatformApi3Parser
+from dojo.tools.mend_sca_platform_api3.parser import MendPlatformApi3Parser
 from unittests.dojo_test_case import DojoTestCase, get_unit_tests_path
 
 
