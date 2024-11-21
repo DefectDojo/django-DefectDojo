456e33ca76bdcfe8624d6a3b5f1628453fcc9906
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-11-21 20:20:42 +0100

    Update dojo/api_v2/serializers.py
    Co-authored-by: Charles Neill <1749665+cneill@users.noreply.github.com>

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -284,7 +284,7 @@ class TaggitSerializer(serializers.Serializer):
 class RequestResponseDict(collections.UserList):
     def __init__(self, *args, **kwargs):
         pretty_print = kwargs.pop("pretty_print", True)
-        UserList.__init__(self, *args, **kwargs)
+        collections.UserList.__init__(self, *args, **kwargs)
         self.pretty_print = pretty_print
 
     def __add__(self, rhs):
