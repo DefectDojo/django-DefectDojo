dea6e3d00c0136ce77b11fc7f0b9ab3b72f0ee4e
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2024-12-03 17:02:50 +0100

    Add GLSA gentoo vulnid (#9813)
    * remove psycopg2-binary
    
    * :tada: Add GLSA vulnid
    
    * :tada: Add GLSA vulnid
    
    * update sha sum
    
    * sha sum
    
    * sha sum

--- dojo/templatetags/display_tags.py
+++ dojo/templatetags/display_tags.py
@@ -777,6 +777,8 @@ def vulnerability_url(vulnerability_id):
 
     for key in settings.VULNERABILITY_URLS:
         if vulnerability_id.upper().startswith(key):
+            if key == "GLSA":
+                return settings.VULNERABILITY_URLS[key] + str(vulnerability_id.replace("GLSA-", "glsa/"))
             if key in ["AVD", "KHV", "C-"]:
                 return settings.VULNERABILITY_URLS[key] + str(vulnerability_id.lower())
             if "&&" in settings.VULNERABILITY_URLS[key]:
