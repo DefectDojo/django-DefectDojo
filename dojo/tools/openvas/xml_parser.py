d2212e941dd678c2fe52596f9432b323a2abdcec
Leonardo de Oliveira Maia <101437950+LeoOMaia@users.noreply.github.com>
2024-12-21 21:41:45 -0300

    fixing openvas parser and including script_id for openvas and nmap

--- dojo/tools/openvas/xml_parser.py
+++ dojo/tools/openvas/xml_parser.py
@@ -16,6 +16,7 @@ class OpenVASXMLParser:
         report = root.find("report")
         results = report.find("results")
         for result in results:
+            script_id = None
             for finding in result:
                 if finding.tag == "name":
                     title = finding.text
@@ -27,7 +28,8 @@ class OpenVASXMLParser:
                     title = title + "_" + finding.text
                     description.append(f"**Port**: {finding.text}")
                 if finding.tag == "nvt":
-                    description.append(f"**NVT**: {finding.text}")
+                    script_id = finding.get("oid")
+                    description.append(f"**NVT**: {script_id}")
                 if finding.tag == "severity":
                     severity = self.convert_cvss_score(finding.text)
                     description.append(f"**Severity**: {finding.text}")
@@ -38,10 +40,12 @@ class OpenVASXMLParser:
 
             finding = Finding(
                 title=str(title),
+                test=test,
                 description="\n".join(description),
                 severity=severity,
                 dynamic_finding=True,
                 static_finding=False,
+                vuln_id_from_tool=script_id,
             )
             findings.append(finding)
         return findings
