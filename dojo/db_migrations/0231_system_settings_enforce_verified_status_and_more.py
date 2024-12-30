49095d590f1cb9038a5b96b8cd6725c490b8b9aa
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2024-12-30 11:51:23 -0500

    fix conflicts deps

--- dojo/db_migrations/0231_system_settings_enforce_verified_status_and_more.py
+++ dojo/db_migrations/0231_system_settings_enforce_verified_status_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0217_jira_project_enabled'),
+        ('dojo', '0230_exclusivepermission'),
     ]
 
     operations = [
