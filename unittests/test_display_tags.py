ec21f3ee66b772e24fc1664ce937dfca2cc1e4fa
renejal <bjalvin@bancolombia.com.co>
2025-01-09 17:20:45 -0500

    test develper

--- unittests/test_display_tags.py
+++ unittests/test_display_tags.py
@@ -6,16 +6,17 @@ from dojo.models import ExclusivePermission
 
 class DisplayTags(unittest.TestCase):
     def test_render_exclusive_permission_for_member(self):
-        permissions = [ExclusivePermission("Permission 1", "description 1"),
-                       ExclusivePermission("Permission 2", "description 2"),
-                       ExclusivePermission("Permission 3", "description 3")]
+        permissions = [ExclusivePermission(name="Permission 1",
+                                           short_name="description 1"),
+                       ExclusivePermission(name="Permission 2",
+                                           short_name="description 2"),
+                       ExclusivePermission(name="Permission 3",
+                                           short_name="description 3")]
         
         result = render_exclusive_permission_for_member(permissions)
         
         expected_result = (
-            "<span class='pass_fail Pass'>Permission 1</span><br/>"
-            "<span class='pass_fail Pass'>Permission 2</span><br/>"
-            "<span class='pass_fail Pass'>Permission 3</span><br/>"
-        )
-        
+            "<span class='pass_fail Pass'>description 1</span>"
+            "<span class='pass_fail Pass'>description 2</span>"
+            "<span class='pass_fail Pass'>description 3</span>")
         self.assertEqual(result, expected_result)
