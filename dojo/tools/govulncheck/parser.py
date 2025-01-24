27d79b90d8aa0b312bee3a298eb7d6026237f13d
kiblik <5609770+kiblik@users.noreply.github.com>
2025-01-24 23:17:25 +0100

    Ruff: Add and fix PLW0127

--- dojo/tools/govulncheck/parser.py
+++ dojo/tools/govulncheck/parser.py
@@ -50,7 +50,7 @@ class GovulncheckParser:
                         version = trace.get("version", "Unknown version")
                         package = trace.get("module", "Unknown package")
                         function = trace.get("function", "Unknown function")
-                        filename = filename = trace.get("position", {}).get("filename", "Unknown filename")
+                        filename = trace.get("position", {}).get("filename", "Unknown filename")
                         line = trace.get("position", {}).get("line", "Unknown line")
                         trace_info_str = f"\tModule: {module}, Version: {version}, Package: {package}, Function: {function}, File: {filename}, Line: {line}"
                         trace_info_strs.append(trace_info_str)
