9f7a589f7dcc59a4acbd50df8c05e4726f587cca
ahubert <ahubert@nganalytics.eu>
2025-01-29 14:34:18 +0100

    Update unittest test_osv_scanner_parser script

--- unittests/tools/test_osv_scanner_parser.py
+++ unittests/tools/test_osv_scanner_parser.py
@@ -43,3 +43,4 @@ class TestOSVScannerParser(DojoTestCase):
             finding = findings[17]
             self.assertEqual(finding.references, "https://nvd.nist.gov/vuln/detail/CVE-2021-45115\nhttps://docs.djangoproject.com/en/4.0/releases/security\nhttps://github.com/django/django\nhttps://groups.google.com/forum/#!forum/django-announce\nhttps://lists.fedoraproject.org/archives/list/package-announce@lists.fedoraproject.org/message/B4SQG2EAF4WCI2SLRL6XRDJ3RPK3ZRDV\nhttps://security.netapp.com/advisory/ntap-20220121-0005\nhttps://www.djangoproject.com/weblog/2022/jan/04/security-releases\n")
             self.assertEqual(finding.title, "GHSA-53qw-q765-4fww_django")
+            self.assertEqual(finding.mitigation, "6.5.8")
