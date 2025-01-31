d29bb74b00522576494cdc80039d91f014b9df19
Valentijn Scholten <valentijnscholten@gmail.com>
2025-01-31 23:16:43 +0100

    unit test update

--- unittests/test_dashboard.py
+++ unittests/test_dashboard.py
@@ -122,8 +122,8 @@ class TestDashboard(DojoTestCase):
 
         self.assertEqual(3, response.context["engagement_count"])
         self.assertEqual(11, response.context["finding_count"])
-        self.assertEqual(2, response.context["mitigated_count"])
-        self.assertEqual(2, response.context["accepted_count"])
+        self.assertEqual(3, response.context["mitigated_count"])
+        self.assertEqual(3, response.context["accepted_count"])
 
     def test_counters_as_user(self):
         self._setup_test_counters_findings(product_id=2)
@@ -137,8 +137,8 @@ class TestDashboard(DojoTestCase):
 
         self.assertEqual(3, response.context["engagement_count"])
         self.assertEqual(11, response.context["finding_count"])
-        self.assertEqual(2, response.context["mitigated_count"])
-        self.assertEqual(2, response.context["accepted_count"])
+        self.assertEqual(3, response.context["mitigated_count"])
+        self.assertEqual(3, response.context["accepted_count"])
 
     def _setup_test_charts_findings(self, product_id: int):
         when = self.year_ago
