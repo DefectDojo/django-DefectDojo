c56fed0d03452b6bf4aa795bcc2e3c971e217641
pUrGe12 <helloitsme0218@gmail.com>
2025-02-10 17:28:13 +0530

    fixed

--- dojo/tools/anchore_engine/parser.py
+++ dojo/tools/anchore_engine/parser.py
@@ -1,9 +1,11 @@
 import json
 from dojo.models import Finding
 
+
 class AnchoreEngineParser():
     '''
-    This has been updated to support the latest output types for Anchore engine scans. 
+    This has been updated to support the latest
+    output types for Anchore engine scans.
     '''
     def get_scan_types(self):
         return ['Anchore Engine Scan']
@@ -16,38 +18,52 @@ class AnchoreEngineParser():
 
     def get_findings(self, filename, test):
         dupes = {}
-        
-        try:                                                # If this is a real file
+
+        try:  # If this is a real file
             data = json.load(filename)
         except AttributeError:
-            with open(filename, "r") as file:               # If it's a filename
+            with open(filename, "r") as file:  # If it's a filename
                 data = json.load(file)
 
         metadata = data.get('metadata', {})
 
-        details = f"**Image hash**: {metadata.get('imageDigest', metadata.get('image_digest', 'None'))} \n\n"
-        
+        details = f"""
+        **Image hash**: {metadata.get('imageDigest',
+            metadata.get('image_digest',
+                'None'))} \n\n"""
+
         for item in data.get('securityEvaluation', []):
             vulnerability_id = item.get('vulnerabilityId', 'Unknown')
 
-            title = f"{vulnerability_id} - {item.get('package', 'Unknown')} ({item.get('packageType', 'Unknown')})"
+            title = (
+                vulnerability_id
+                + " - "
+                + item.get("package", "Unknown")
+                + "("
+                + item.get("packageType", "Unknown")
+                + ")"
+            )
 
             details += f"**Package**: {item.get('package', 'Unknown')}\n\n"
             details += f"**Package path**: {item.get('path', 'Unknown')}\n\n"
-            details += f"**Package type**: {item.get('packageType', 'Unknown')}\n\n"
+            details += f"""
+            **Package type**: {item.get('packageType', 'Unknown')}\n\n"""
 
             severity = item.get('severity', 'Unknown')
             if severity.lower() in ['negligible', 'unknown']:
                 severity = 'Info'
 
             mitigation = "No fix available."
-            if item.get('fixAvailable') and item['fixAvailable'] != 'None':
-                mitigation = f"Upgrade to: {' or '.join(item['fixAvailable'].split(','))}\n\n"
+            if item.get('fixAvailable', 'None') != 'None':
+                mitigation = f"""
+                Upgrade to: {' or '.join(item['fixAvailable'].split(','))}
+                \n\n"""
                 mitigation += f"URL: {item.get('link', 'None')}"
 
             cvssv3_base_score = item.get('nvdCvssBaseScore')
 
-            if isinstance(cvssv3_base_score, str) and cvssv3_base_score.replace('.', '', 1).isdigit():
+            if (isinstance(cvssv3_base_score, str)
+                    and cvssv3_base_score.replace('.', '', 1).isdigit()):
                 cvssv3_base_score = float(cvssv3_base_score)
             elif not isinstance(cvssv3_base_score, (int, float)):
                 cvssv3_base_score = None
@@ -65,22 +81,24 @@ class AnchoreEngineParser():
             if dupe_key in dupes:
                 find = dupes[dupe_key]
             else:
-                find = {
-                    "title": title,
-                    "cve": item.get("cves"),
-                    "cvssv3_score": cvssv3_base_score,
-                    "date": item.get("detectedAt"),
-                    "description": details,
-                    "severity": severity,
-                    "mitigation": mitigation,
-                    "references": references,
-                    "file_path": item.get('path'),
-                    "component_name": item.get('package'),
-                    "url": item.get('link'),
-                    "static_finding": True,
-                    "dynamic_finding": False,
-                    "vulnerability_id": vulnerability_id,
-                }
+                find = Finding(
+                    title=title,
+                    cve=item.get("cves"),
+                    cvssv3_score=cvssv3_base_score,
+                    date=item.get("detectedAt"),
+                    description=details,
+                    severity=severity,
+                    mitigation=mitigation,
+                    references=references,
+                    file_path=item.get('path'),
+                    component_name=item.get('package'),
+                    url=item.get('link'),
+                    static_finding=True,
+                    dynamic_finding=False,
+                    vuln_id_from_tool=vulnerability_id,
+                )
+                if vulnerability_id:
+                    find.unsaved_vulnerability_ids = [vulnerability_id]
 
                 dupes[dupe_key] = find
 
