2c9a32f544993b00c11a3eda2c230093a7e6420d
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2025-02-26 20:23:41 +0100

    :tada: implement vulnid logic for suse (#11890)

--- dojo/templatetags/display_tags.py
+++ dojo/templatetags/display_tags.py
@@ -780,6 +780,8 @@ def vulnerability_url(vulnerability_id):
                 return settings.VULNERABILITY_URLS[key] + str(vulnerability_id.replace("GLSA-", "glsa/"))
             if key in ["AVD", "KHV", "C-"]:
                 return settings.VULNERABILITY_URLS[key] + str(vulnerability_id.lower())
+            if key == "SUSE-SU-":
+                return settings.VULNERABILITY_URLS[key] + str(vulnerability_id.lower().removeprefix("suse-su-")[:4]) + "/" + vulnerability_id.replace(":", "")
             if "&&" in settings.VULNERABILITY_URLS[key]:
                 # Process specific keys specially if need
                 if key in ["CAPEC", "CWE"]:
