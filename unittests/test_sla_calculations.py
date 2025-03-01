9f54fd10b34e1c3988c2e53a0415e1985eddbdcc
Valentijn Scholten <valentijnscholten@gmail.com>
2025-03-01 09:50:43 +0100

    sla calc: add unit tests

--- unittests/test_sla_calculations.py
+++ unittests/test_sla_calculations.py
@@ -90,8 +90,9 @@ class TestSLACalculations(DojoTestCase):
             # sla_expiration_date should not change just because a finding is mitigated
             self.assertEqual(initial_sla_expiration_date, finding.sla_expiration_date)
             self.assertEqual((self.now + relativedelta(days=self.sla_config.high)).date(), finding.sla_expiration_date)
-            self.assertEqual(self.sla_config.high - 10, finding.sla_days_remaining())
+            self.assertEqual(20, finding.sla_days_remaining())
             self.assertTrue("within SLA" in finding_sla(finding))
+            self.assertTrue(">20<" in finding_sla(finding))
 
         with patch("django.db.models.fields.timezone.now") as mock_now:
             mock_now.return_value = self.now + relativedelta(days=20)
@@ -102,8 +103,9 @@ class TestSLACalculations(DojoTestCase):
             # sla_expiration_date should not change just because a finding is saved
             self.assertEqual(initial_sla_expiration_date, finding.sla_expiration_date)
             self.assertEqual((self.now + relativedelta(days=self.sla_config.high)).date(), finding.sla_expiration_date)
-            self.assertEqual(self.sla_config.high - 10, finding.sla_days_remaining())
+            self.assertEqual(20, finding.sla_days_remaining())
             self.assertTrue("within SLA" in finding_sla(finding))
+            self.assertTrue(">20<" in finding_sla(finding))
 
     # Finding mitigated outside SLA should have correct sla_exration_date and days_remaining
     def test_mitigated_outside_sla(self):
@@ -115,7 +117,7 @@ class TestSLACalculations(DojoTestCase):
         initial_sla_expiration_date = finding.sla_expiration_date
 
         with patch("django.db.models.fields.timezone.now") as mock_now:
-            mock_now.return_value = self.now + relativedelta(days=40)
+            mock_now.return_value = self.now + relativedelta(days=45)
             finding.mitigated = mock_now.return_value
             finding.is_mitigated = True
             finding.active = False
@@ -126,11 +128,13 @@ class TestSLACalculations(DojoTestCase):
             # sla_expiration_date should not change just because a finding is mitigated
             self.assertEqual(initial_sla_expiration_date, finding.sla_expiration_date)
             self.assertEqual((self.now + relativedelta(days=self.sla_config.high)).date(), finding.sla_expiration_date)
-            self.assertEqual(self.sla_config.high - 40, finding.sla_days_remaining())
+            self.assertEqual(-15, finding.sla_days_remaining())
             self.assertTrue("Out of SLA" in finding_sla(finding))
+            self.assertTrue("15 days past" in finding_sla(finding))
+            self.assertTrue(">15<" in finding_sla(finding))
 
         with patch("django.db.models.fields.timezone.now") as mock_now:
-            mock_now.return_value = self.now + relativedelta(days=50)
+            mock_now.return_value = self.now + relativedelta(days=55)
             finding.save()
             logger.error(f"Finding: {finding.test.id} {finding.id} {finding.date} {finding.mitigated} {finding.sla_expiration_date}")
             logger.error(finding_sla(finding))
@@ -138,5 +142,7 @@ class TestSLACalculations(DojoTestCase):
             # sla_expiration_date should not change just because a finding is mitigated
             self.assertEqual(initial_sla_expiration_date, finding.sla_expiration_date)
             self.assertEqual((self.now + relativedelta(days=self.sla_config.high)).date(), finding.sla_expiration_date)
-            self.assertEqual(self.sla_config.high - 40, finding.sla_days_remaining())
+            self.assertEqual(-15, finding.sla_days_remaining())
             self.assertTrue("Out of SLA" in finding_sla(finding))
+            self.assertTrue("15 days past" in finding_sla(finding))
+            self.assertTrue(">15<" in finding_sla(finding))
