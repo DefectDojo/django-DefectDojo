0b6e55ae40cfbbaaae24bb811a423427651880c8
Jino Tesauro <53376807+Jino-T@users.noreply.github.com>
2025-03-07 15:53:00 -0600

    Async Finding Import: Mark the feature as deprecated (#11915)
    * Added first warning
    
    * Added warnings to async_process_findings
    
    * Added comments to settings to show deprecation
    
    * Moved import statement to top of file
    
    * Update default_importer.py
    
    * Update default_importer.py
    
    * Update default_reimporter.py
    
    ---------
    
    Co-authored-by: Jino Tesauro <jinotesauro@pop-os.localdomain>

--- dojo/importers/base_importer.py
+++ dojo/importers/base_importer.py
@@ -1,5 +1,6 @@
 import base64
 import logging
+from warnings import warn
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
@@ -242,6 +243,7 @@ class BaseImporter(ImporterOptions):
         ASYNC_FINDING_IMPORT_CHUNK_SIZE setting will determine how many
         findings will be processed in a given worker/process/thread
         """
+        warn("This experimental feature has been deprecated as of DefectDojo 2.44.0 (March release). Please exercise caution if using this feature with an older version of DefectDojo, as results may be inconsistent.")
         return self.process_findings(parsed_findings, sync=False, **kwargs)
 
     def determine_process_method(
