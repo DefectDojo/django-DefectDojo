5d8547ae9a3f1bfa0daf6bdb7e8010f726110eef
Manuel Sommer <sommermanuel@gmx.de>
2025-03-15 09:06:32 +0100

    first shot

--- dojo/tools/anchore_engine/parser.py
+++ dojo/tools/anchore_engine/parser.py
@@ -14,7 +14,89 @@ class AnchoreEngineParser:
         return "Anchore-CLI JSON vulnerability report format."
 
     def get_findings(self, filename, test):
-        data = json.load(filename)
+        try:
+            data = json.load(filename)
+        except AttributeError:
+            with open(filename, encoding="utf-8") as file:
+                data = json.load(file)
+        if data.get("metadata"):
+            return self.get_findings_with_metadata(data, test)
+        else:
+            return self.get_findings_without_metadata(data, test)
+    
+    def get_findings_with_metadata(self, data, test):
+        dupes = {}
+        metadata = data.get("metadata", {})
+        details = f"**Image hash**: {metadata.get('imageDigest', metadata.get('image_digest', 'None'))} \n\n"
+
+        for item in data.get("securityEvaluation", []):
+            vulnerability_id = item.get("vulnerabilityId", "Unknown")
+
+            title = (
+                vulnerability_id + "-" + item.get("package", "Unknown") + "(" + item.get("packageType", "Unknown") + ")"
+            )
+
+            details += f"**Package**: {item.get('package', 'Unknown')}\n\n"
+            details += f"**Package path**: {item.get('path', 'Unknown')}\n\n"
+            details += f"**Package type**: {item.get('packageType', 'Unknown')}\n\n"
+
+            severity = item.get("severity", "Unknown")
+
+            if severity.lower() in ["negligible", "unknown"]:
+                severity = "Info"
+
+            mitigation = "No fix available."
+
+            if item.get("fixAvailable") and item["fixAvailable"] != "None":
+                mitigation = f"Upgrade to: {' or '.join(item['fixAvailable'].split(','))}\n\n"
+                mitigation += f"URL: {item.get('link', 'None')}"
+            cvssv3_base_score = item.get("nvdCvssBaseScore")
+
+            if isinstance(cvssv3_base_score, str) and cvssv3_base_score.replace(".", "", 1).isdigit():
+                cvssv3_base_score = float(cvssv3_base_score)
+            elif not isinstance(cvssv3_base_score, int | float):
+                cvssv3_base_score = None
+
+            references = item.get("link")
+
+            dupe_key = "|".join(
+                [
+                    item.get("cves", "None"),
+                    item.get("package", "None"),
+                    item.get("packageType", "None"),
+                    item.get("path", "None"),
+                    item.get("severity", "None"),
+                ],
+            )
+
+            if dupe_key in dupes:
+                find = dupes[dupe_key]
+            else:
+                find = Finding(
+                    title=title,
+                    test=test,
+                    cve=item.get("cves"),
+                    cvssv3_score=cvssv3_base_score,
+                    description=details,
+                    severity=severity,
+                    mitigation=mitigation,
+                    references=references,
+                    file_path=item.get("path"),
+                    component_name=item.get("package"),
+                    url=item.get("link"),
+                    static_finding=True,
+                    dynamic_finding=False,
+                    vuln_id_from_tool=vulnerability_id,
+                )
+
+                if vulnerability_id:
+                    find.unsaved_vulnerability_ids = [vulnerability_id]
+
+                dupes[dupe_key] = find
+
+        return list(dupes.values())
+
+    def get_findings_without_metadata(self, data, test):
         dupes = {}
         for item in data["vulnerabilities"]:
             vulnerability_id = item.get("vuln")
