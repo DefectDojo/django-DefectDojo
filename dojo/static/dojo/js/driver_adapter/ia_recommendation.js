5307682c17026a141f108e457e13a57828a3f80f
renejal <bjalvin@bancolombia.com.co>
2025-03-20 15:10:21 -0500

    fix ia

--- dojo/static/dojo/js/driver_adapter/ia_recommendation.js
+++ dojo/static/dojo/js/driver_adapter/ia_recommendation.js
@@ -1,17 +1,26 @@
 import {MAX_RETRY, RETRY_INTERVAL} from '../settings.js';
-export async function get_ia_recommendaton(finding_id)
-{
-    try
-    {
-        const response = await $.ajax({
-            url: `https://09a7b3c2-0993-47f6-87ef-e7ec9e4c1f0d.mock.pstmn.io/open-assistant/marvin/tools/api/v1/devsecops/full-remediation-process/${finding_id}`,
-            type: "GET",
+export function get_ia_recommendation(finding_id) {
+    $.ajax({
+        url: `https://09a7b3c2-0993-47f6-87ef-e7ec9e4c1f0d.mock.pstmn.io/open-assistant/marvin/tools/api/v1/devsecops/full-remediation-process/${finding_id}`,
+        type: "GET",
+        success: function(response) {
+            return response
+            console.log("print ok")
+        },
+        error: function(error) {
+            console.log("erro or")
+        }
+    });
+}
+
+export function get_ia_recommendation2(finding_id) {
+    return fetch(`https://09a7b3c2-0993-47f6-87ef-e7ec9e4c1f0d.mock.pstmn.io/open-assistant/marvin/tools/api/v1/devsecops/full-remediation-process/${finding_id}`)
+        .then(response => response.json())  // Convierte la respuesta a JSON
+        .then(data => {
+            return data;  // Retorna los datos obtenidos
+        })
+        .catch(error => {
+            console.error("Error al obtener la recomendación:", error);  // Maneja cualquier error
+            throw error;  // Lanza el error nuevamente si quieres manejarlo fuera de la función
         });
-
-    return response;
-
-    }catch(error){
-        console.error(error);
-        throw error;
-    }
 }
--- dojo/static/dojo/js/finding/ia_recomendation.js
+++ dojo/static/dojo/js/finding/ia_recomendation.js
@@ -1,41 +1,101 @@
 
-import { get_ia_recommendaton } from '../driver_adapter/ia_recommendation.js'; 
+// import { get_ia_recommendation, get_ia_recommendation2} from '../driver_adapter/ia_recommendation.js'; 
 
-function writingEffect(text, elementId) {
-    let arrFromStr = text.split('');
-    let i = 0;
-    let element = document.getElementById(elementId);
-    element.innerHTML = '';
-
-    let printStr = setInterval(function() {
-        if (arrFromStr[i] === ' ') {
-            element.innerHTML += ' ';
-            i++;
-        } else {
-            element.innerHTML += arrFromStr[i];
-            i++;
-        }
-
-        if (i === arrFromStr.length) {
-            clearInterval(printStr);
-            element.style.color = "steelblue";
-        }
-    }, 50); 
+ function writingEffect(strings, elementId) {
+    new TypeIt(elementId, {
+        strings: strings,
+        speed: 1,
+        waitUntilVisible: true,
+        cursor: false
+    }).go();
 }
 
 
 
-$(document).ready(function() {
-    $('#id_ia_recommendation').click(async function(event) {
-       let response = await get_ia_recommendaton(1)
-       const panelBody = document.querySelector('#ia_recommendation');
-       panelBody.innerHTML = "";
-       let recommendations = ``; 
-       response.data.recommendations.forEach(element => {
-              recommendations += `${element}`; 
-       });
-       console.log(recommendations);
-       writingEffect(recommendations, "ia_recommendation")
+ function FormaterHtmlRecommendation(recommendations){
+    let html = "";
+    html += `<h5>Recommendation</h5>`
+    html += `<ul>`;
+    recommendations.forEach(element => {
+    html += `<li> ${element} </li>`
+    });
+    html += `</ul>`
+    return html
+}
 
+ function FormaterHtmlMitigations(mitigations){
+    let html = "";
+    html += `<h5>Mitigations</h5>`;
+    mitigations.forEach(element => {
+        element = element.replace(/</g, '<;').replace(/>/g, '>;').replace(/&/g, '&amp;');
+        html += `<pre><code class="language-css">${element}</code></pre>`;
+    });
+    return html
+}
+
+ function formaterHtmlFilesToFix(filesToFix){
+    let html = "";
+    html += `<h5>File To Fix</h5>`;
+    html += `<ul>`;
+    filesToFix.forEach(element => {
+        html += `<li class="file-path" > ${element} </li>`;
+    });
+    html += `</ul>`;
+    return html
+}
+
+function generarHtml(response){
+    console.log(response)
+    let htmlRecommendation =  FormaterHtmlRecommendation(response.data.recommendations)
+    htmlRecommendation +=  FormaterHtmlMitigations(response.data.mitigations)
+    htmlRecommendation +=  formaterHtmlFilesToFix(response.data.files_to_fix)
+    return htmlRecommendation
+}
+
+
+function get_ia_recommendation(finding_id) {
+    $.ajax({
+        url: `https://09a7b3c2-0993-47f6-87ef-e7ec9e4c1f0d.mock.pstmn.io/open-assistant/marvin/tools/api/v1/devsecops/full-remediation-process/${finding_id}`,
+        type: "GET",
+        beforeSend: function() {
+            $('#spinnerLoading').css('display', 'flex');
+        },
+        success: function(response) {
+            let htmlRecomendation = generarHtml(response)
+            writingEffect(htmlRecomendation, "#id_recommendation_content")
+        },
+        error: function(error) {
+            console.log("erro or")
+        },
+        afeterSend: function() {
+            $('#spinnerLoading').css('display', 'none');
+        }
+    });
+}
+
+
+
+
+$(document).ready(function() {
+    $('#id_ia_recommendation').click(function(event)
+    {
+        get_ia_recommendation(1)
+        // try {
+        //     $('#spinnerLoading').css('display', 'flex');
+        //     $('#id_ia_recommendation').prop('disabled', true);
+        //     let response =  get_ia_recommendation(1)
+        //     let htmlRecommendation =  FormaterHtmlRecommendation(response.data.recommendations)
+        //     htmlRecommendation +=  FormaterHtmlMitigations(response.data.mitigations)
+        //     htmlRecommendation +=  formaterHtmlFilesToFix(response.data.files_to_fix)
+        //     writingEffect(htmlRecommendation, "#id_recommendation_content")
+            
+        // } catch (error) {
+        //     console.error(error);
+        // }
+        // finally {
+        //     console.log("Finally");
+        //     $('#spinnerLoading').css('display', 'none');
+        //     $('#id_ia_recommendation').prop('disabled', false);
+        // }
     });
 });
