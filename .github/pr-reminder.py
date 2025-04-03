bccc431794716749c6f13433d3cd3613d07e0ab6
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2025-04-03 13:44:29 -0600

    Correct spacing

--- .github/pr-reminder.py
+++ .github/pr-reminder.py
@@ -19,7 +19,7 @@ GITHUB_USER_NAME_TO_SLACK_EMAIL = {
     "dogboat": "sean@defectdojo.com",
     "cneill": "charles@defectdojo.com",
     "hblankenship": "harold@defectdojo.com",
-    "valentijnscholten ": "valentijn@defectdojo.com",
+    "valentijnscholten": "valentijn@defectdojo.com",
 }
 
 
