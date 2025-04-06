0a6c371c89354c5e1641393bbd7f6a618dce8a2d
Valentijn Scholten <valentijnscholten@gmail.com>
2025-04-06 14:53:19 +0200

    immuniweb json: domains

--- unittests/tools/test_immuniweb_parser.py
+++ unittests/tools/test_immuniweb_parser.py
@@ -28,3 +28,18 @@ class TestImmuniwebParser(DojoTestCase):
                 for endpoint in finding.unsaved_endpoints:
                     endpoint.clean()
             self.assertGreater(len(findings), 2)
+
+    def test_parse_file_with_multiple_vuln_has_multiple_findings_json(self):
+        with open(get_unit_tests_scans_path("immuniweb") / "ImmuniWeb-multiple-vuln.json", encoding="utf-8") as testfile:
+            parser = ImmuniwebParser()
+            findings = parser.get_findings(testfile, Test())
+            for finding in findings:
+                for endpoint in finding.unsaved_endpoints:
+                    endpoint.clean()
+            self.assertEqual(1, len(findings), 1)
+            self.assertEqual("Informational", findings[0].severity)
+            self.assertEqual("domain: some-company.com", findings[0].title)
+            self.assertIn("To prevent spam, email forgery and phishing, configure DKIM, DMARC, SPF DNS records for the domain if it is used to send or receive emails", findings[0].description)
+            self.assertIn("To prevent spam, email forgery and phishing, configure DKIM, DMARC, SPF DNS records for the domain if it is used to send or receive emails", findings[0].mitigation)
+            self.assertEqual(None, findings[0].unsaved_tags)
+            self.assertEqual("some-company.com", findings[0].unsaved_endpoints[0].host)
