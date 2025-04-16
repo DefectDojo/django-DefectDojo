80c452ee0158d5465a6045898e8c7f78b49b02e4
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2025-04-16 15:00:27 -0600

    Tag: Update allowed characters for a unified format (#12194)
    * Implement migration
    
    * Remove lower case requirement
    
    * Create common validator
    
    * UI: Apply validator
    
    * API: Apply validator
    
    * Add release notes
    
    * Add unit tests
    
    * Fixing ruff
    
    * Fix some migration updates
    
    * Applying feedback
    
    * Silly copy/paste

--- dojo/utils.py
+++ dojo/utils.py
@@ -25,6 +25,7 @@ from dateutil.relativedelta import MO, SU, relativedelta
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.db.models import Case, Count, IntegerField, Q, Sum, Value, When
 from django.db.models.query import QuerySet
@@ -2697,3 +2698,21 @@ def generate_file_response_from_file_path(
     response["Content-Disposition"] = f'attachment; filename="{full_file_name}"'
     response["Content-Length"] = file_size
     return response
+
+
+def tag_validator(value: str | list[str], exception_class: Callable = ValidationError) -> None:
+    TAG_PATTERN = re.compile(r'[ ,\'"]')
+    error_messages = []
+
+    if isinstance(value, list):
+        for tag in value:
+            if TAG_PATTERN.search(tag):
+                error_messages.append(f"Invalid tag: '{tag}'. Tags should not contain spaces, commas, or quotes.")
+    elif isinstance(value, str):
+        if TAG_PATTERN.search(value):
+            error_messages.append(f"Invalid tag: '{value}'. Tags should not contain spaces, commas, or quotes.")
+    else:
+        error_messages.append(f"Value must be a string or list of strings: {value} - {type(value)}.")
+
+    if error_messages:
+        raise exception_class(error_messages)
