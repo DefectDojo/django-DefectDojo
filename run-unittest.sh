2fef3874961fdc2f3a6e3f2202115d5d87c42fed
Valentijn Scholten <valentijnscholten@gmail.com>
2025-05-14 08:36:02 +0200

    add migration

--- run-unittest.sh
+++ run-unittest.sh
@@ -54,4 +54,4 @@ echo "Running docker compose unit tests with test case $TEST_CASE ..."
 # Compose V2 integrates compose functions into the Docker platform, continuing to support
 # most of the  previous docker-compose features and flags. You can run Compose V2 by
 # replacing the hyphen (-) with a space, using docker compose, instead of docker-compose.
-docker compose exec uwsgi bash -c "python manage.py test $TEST_CASE -v 3 --keepdb"  --debug-mode
+docker compose exec uwsgi bash -c "python manage.py test $TEST_CASE -v2 --keepdb"
