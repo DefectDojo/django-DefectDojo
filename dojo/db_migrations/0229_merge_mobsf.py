d63f963923de9fd3f42a33079c32f141fe86dfcd
manuelsommer <47991713+manuel-sommer@users.noreply.github.com>
2025-05-26 23:08:02 +0200

    Update 0229_merge_mobsf.py

--- dojo/db_migrations/0229_merge_mobsf.py
+++ dojo/db_migrations/0229_merge_mobsf.py
@@ -17,10 +17,10 @@ def update_parser_test(test, parser_test_type) -> None:
 
 # Update the found_by field to remove Mobsfscan Scan and add MobSF Scan
 def update_parser_finding(finding, newparser_test_type, parser_test_type) -> None:
-    # Check if nessus is in found by list and remove
+    # Check if old parser is in found by list and remove
     if parser_test_type in finding.found_by.all():
         finding.found_by.remove(parser_test_type.id)
-    # Check if tenable is already in list somehow before adding it
+    # Check if new parser is already in list somehow before adding it
     if newparser_test_type not in finding.found_by.all():
         finding.found_by.add(newparser_test_type.id)
     finding.save()
