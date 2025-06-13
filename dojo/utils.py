34b4bb1f33aee1fbfb5581f45461915b9a98796b
Denys Moskalenko <88318209+DenysMoskalenko@users.noreply.github.com>
2025-06-13 22:03:20 +0200

    Apply suggestions from code review
    Fix typo in the "build_count_subquery" docstring
    
    Co-authored-by: valentijnscholten <valentijnscholten@gmail.com>

--- dojo/utils.py
+++ dojo/utils.py
@@ -2674,8 +2674,8 @@ def parse_cvss_data(cvss_vector_string: str) -> dict:
 
 
 def build_count_subquery(model_qs: QuerySet, group_field: str) -> Subquery:
-    """Return a Subquery that yields one aggregated count per test_id."""
+    """Return a Subquery that yields one aggregated count per `group_field`."""
     return Subquery(
-        model_qs.values(group_field).annotate(c=Count("*")).values("c")[:1],  # one row per test_id
+        model_qs.values(group_field).annotate(c=Count("*")).values("c")[:1],  # one row per group_field
         output_field=IntegerField(),
     )
