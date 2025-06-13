e038e117f28867b386596b04d229df7e3ea0fdb5
kiblik <5609770+kiblik@users.noreply.github.com>
2025-06-13 11:49:05 +0200

    update model + migration

--- dojo/metrics/utils.py
+++ dojo/metrics/utils.py
@@ -57,7 +57,7 @@ def finding_queries(
         "test__engagement__product__prod_type",
     ).prefetch_related(
         "risk_acceptance_set",
-        "test__engagement__risk_acceptance",
+        "test__engagement__risk_acceptance_set",
         "test__test_type",
     )
 
@@ -149,7 +149,7 @@ def endpoint_queries(
     ).prefetch_related(
         "finding__test__engagement__product",
         "finding__test__engagement__product__prod_type",
-        "finding__test__engagement__risk_acceptance",
+        "finding__test__engagement__risk_acceptance_set",
         "finding__risk_acceptance_set",
         "finding__reporter",
     )
