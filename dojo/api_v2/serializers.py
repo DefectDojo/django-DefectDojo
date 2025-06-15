2526743ebd239dacfb752f4d4234de3635366355
Valentijn Scholten <valentijnscholten@gmail.com>
2025-06-15 22:47:21 +0200

    adjust vulnerability id code

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -1872,10 +1872,10 @@ class FindingCreateSerializer(serializers.ModelSerializer):
             parsed_vulnerability_ids.extend(vulnerability_id["vulnerability_id"] for vulnerability_id in vulnerability_ids)
             validated_data["cve"] = parsed_vulnerability_ids[0]
 
+        validated_data["unsaved_vulnerability_ids"] = parsed_vulnerability_ids
+
         new_finding = super().create(
             validated_data)
-
-        new_finding.unsaved_vulnerability_ids = parsed_vulnerability_ids
 
         # Deal with all of the many to many things
         if notes:
