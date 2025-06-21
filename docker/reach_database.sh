016e31a0873add59fb15068a414455507ccbc19d
Astound <kirito@disroot.org>
2025-06-20 21:22:08 -0500

    style: normalize sh files (LF + trim)

--- docker/reach_database.sh
+++ docker/reach_database.sh
@@ -5,9 +5,9 @@ wait_for_database_to_be_reachable() {
     failure_count=0
     DD_DATABASE_READINESS_TIMEOUT=${DD_DATABASE_READINESS_TIMEOUT:-30}
     until echo "select 1;" | python3 manage.py dbshell > /dev/null
-    do 
+    do
         echo -n "."
-        failure_count=$((failure_count + 1)) 
+        failure_count=$((failure_count + 1))
         sleep 1
         if [ $DD_DATABASE_READINESS_TIMEOUT = $failure_count ]; then
             exit 1
@@ -23,5 +23,5 @@ EOD
     else
         echo "Simple database test was successful"
     fi
-    return $DB_TEST 
+    return $DB_TEST
 }
