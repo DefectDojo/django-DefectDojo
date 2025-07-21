9a8f4fa775c6928b74cbe9b47a9385027e30e293
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2025-07-21 11:29:53 -0600

    Missed migration

--- dojo/db_migrations/0238_finding_fix_available.py
+++ dojo/db_migrations/0238_finding_fix_available.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0236_clean_tags'),
+        ('dojo', '0237_finding_cvssv4_finding_cvssv4_score'),
     ]
 
     operations = [
