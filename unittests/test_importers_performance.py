a7a3ab66c6235b726e65fdc3a92b9370fdfb3503
Valentijn Scholten <valentijnscholten@gmail.com>
2025-07-27 12:23:36 +0200

    set tags on (re)import

--- unittests/test_importers_performance.py
+++ unittests/test_importers_performance.py
@@ -160,9 +160,9 @@ class TestDojoImporterPerformance(DojoTestCase):
 
     def test_import_reimport_reimport_performance(self):
         self.import_reimport_performance(
-            expected_num_queries1=554,
-            expected_num_async_tasks1=10,
-            expected_num_queries2=469,
+            expected_num_queries1=712,
+            expected_num_async_tasks1=15,
+            expected_num_queries2=656,
             expected_num_async_tasks2=23,
             expected_num_queries3=332,
             expected_num_async_tasks3=20,
@@ -178,9 +178,9 @@ class TestDojoImporterPerformance(DojoTestCase):
         so we patch the we_want_async decorator to always return False.
         """
         self.import_reimport_performance(
-            expected_num_queries1=554,
-            expected_num_async_tasks1=10,
-            expected_num_queries2=469,
+            expected_num_queries1=712,
+            expected_num_async_tasks1=15,
+            expected_num_queries2=656,
             expected_num_async_tasks2=23,
             expected_num_queries3=332,
             expected_num_async_tasks3=20,
@@ -201,9 +201,9 @@ class TestDojoImporterPerformance(DojoTestCase):
         DojoSytemSettingsMiddleware.load()
 
         self.import_reimport_performance(
-            expected_num_queries1=574,
-            expected_num_async_tasks1=15,
-            expected_num_queries2=503,
+            expected_num_queries1=752,
+            expected_num_async_tasks1=25,
+            expected_num_queries2=690,
             expected_num_async_tasks2=30,
             expected_num_queries3=357,
             expected_num_async_tasks3=25,
