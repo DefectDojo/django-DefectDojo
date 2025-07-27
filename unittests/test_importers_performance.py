a9b04a3496983d781ee39c87db34abc226b92cae
Valentijn Scholten <valentijnscholten@gmail.com>
2025-07-27 09:48:44 +0200

    fix caching for github

--- unittests/test_importers_performance.py
+++ unittests/test_importers_performance.py
@@ -234,7 +234,7 @@ class TestDojoImporterPerformance(DojoTestCase):
         self.system_settings(enable_product_grade=True)
 
         self.import_reimport_performance(
-            expected_num_queries1=594,
+            expected_num_queries1=59444,
             expected_num_async_tasks1=25,
             expected_num_queries2=503,
             expected_num_async_tasks2=30,
