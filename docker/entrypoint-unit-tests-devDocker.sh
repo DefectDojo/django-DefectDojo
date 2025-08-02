cb301d019af64004ae2a0f5d938068cd9507eead
Valentijn Scholten <valentijnscholten@gmail.com>
2025-08-02 12:41:56 +0200

    entrypoint revert

--- docker/entrypoint-unit-tests-devDocker.sh
+++ docker/entrypoint-unit-tests-devDocker.sh
@@ -73,7 +73,7 @@ echo "Unit Tests"
 echo "------------------------------------------------------------"
 
 # Removing parallel and shuffle for now to maintain stability
-python3 manage.py test unittests -v 3 --keepdb --no-input --exclude-tag="non-parallel" --parallel --failfast || {
+python3 manage.py test unittests -v 3 --keepdb --no-input --exclude-tag="non-parallel" --parallel || {
     exit 1;
 }
 python3 manage.py test unittests -v 3 --keepdb --no-input --tag="non-parallel"|| {
