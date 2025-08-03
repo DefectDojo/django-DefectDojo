586ddd12809cc2792a446e019e7d410a4279bce2
Valentijn Scholten <valentijnscholten@gmail.com>
2025-08-03 23:17:59 +0200

    fix no async

--- unittests/test_importers_performance.py
+++ unittests/test_importers_performance.py
@@ -217,12 +217,15 @@ class TestDojoImporterPerformance(DojoTestCase):
         so we patch the we_want_async decorator to always return False.
         """
         self.system_settings(enable_product_grade=True)
+        testuser = User.objects.get(username="admin")
+        testuser.usercontactinfo.block_execution = True
+        testuser.usercontactinfo.save()
 
         self.import_reimport_performance(
-            expected_num_queries1=682,
-            expected_num_async_tasks1=10,
-            expected_num_queries2=700,
-            expected_num_async_tasks2=12,
-            expected_num_queries3=292,
-            expected_num_async_tasks3=20,
+            expected_num_queries1=732,
+            expected_num_async_tasks1=15,
+            expected_num_queries2=685,
+            expected_num_async_tasks2=28,
+            expected_num_queries3=357,
+            expected_num_async_tasks3=25,
         )
