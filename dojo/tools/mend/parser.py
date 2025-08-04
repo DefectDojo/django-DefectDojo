8b9a9aa9a6b18afa9119b5e5ed9a2ab0699d569e
testaccount90009 <122134756+testaccount90009@users.noreply.github.com>
2025-08-04 10:33:29 -0700

    fix indent
    fix indent

--- dojo/tools/mend/parser.py
+++ dojo/tools/mend/parser.py
@@ -72,8 +72,8 @@ class MendParser:
                 kev_date = None
                 if kev_date_str:
                     try:
-                    # Parses ISO 8601 format with Zulu time (Z)
-                    kev_date = datetime.strptime(kev_date_str, "%Y-%m-%dT%H:%M:%SZ").date()
+                        # Parses ISO 8601 format with Zulu time (Z)
+                        kev_date = datetime.strptime(kev_date_str, "%Y-%m-%dT%H:%M:%SZ").date()
                 ransomware_used = node.get("malicious", None)
                 known_exploited = node.get("exploitable", None)
                 component_path = node["component"].get("path", None)
