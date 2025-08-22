0f0457d635bbf8d6c3574a19eb7cc6e7d960fbbd
Jino Tesauro <53376807+Jino-T@users.noreply.github.com>
2025-08-21 19:55:38 -0500

    Update Docs Link in Settings.py to Match Current Docs Structure (#13021)
    * Updated Docs to change CLI tool reference
    
    * update-docs-link-in-settings.py
    
    ---------
    
    Co-authored-by: Jino Tesauro <jino@defectdojo.com>

--- dojo/settings/settings.py
+++ dojo/settings/settings.py
@@ -1,7 +1,7 @@
 
 from split_settings.tools import include, optional
 
-# See https://documentation.defectdojo.com/getting_started/configuration/ for options
+# See https://docs.defectdojo.com/en/open_source/installation/configuration/ for options
 # how to tune the configuration to your needs.
 
 include(
