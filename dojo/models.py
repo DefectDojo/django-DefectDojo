544da041af3485cb2777dc9622538c60563cfc85
Manuel Sommer <sommermanuel@gmx.de>
2025-09-11 14:00:05 +0200

    fix

--- dojo/models.py
+++ dojo/models.py
@@ -2054,7 +2054,7 @@ class Endpoint(models.Model):
 
         protocol = url.scheme if url.scheme else None
         userinfo = ":".join(url.userinfo) if url.userinfo not in {(), ("",)} else None
-        host = url.host if ur.host else None
+        host = url.host if url.host else None
         port = url.port
         path = "/".join(url.path)[:500] if url.path not in {None, (), ("",)} else None
         query = query_string[:1000] if query_string is not None and query_string else None
