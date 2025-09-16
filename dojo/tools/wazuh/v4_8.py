3c827c96012c332d376f72522f0808619263f14c
Manuel Sommer <sommermanuel@gmx.de>
2025-09-16 13:24:16 +0200

    :lipstick: Update Wazuh v4.8

--- dojo/tools/wazuh/v4_8.py
+++ dojo/tools/wazuh/v4_8.py
@@ -1,7 +1,4 @@
-import hashlib
-
 from dojo.models import Finding
-
 
 class WazuhV4_8:
     def parse_findings(self, test, data):
@@ -11,28 +8,23 @@ class WazuhV4_8:
             item = item_source.get("_source")
             vuln = item.get("vulnerability")
             cve = vuln.get("id")
+
+            # Construct a unique key for deduplication
+            dupe_key = f"{cve}-{item['agent']['id']}"
+
+            if dupe_key in dupes:
+                continue  # Skip if this finding has already been processed
+
             description = vuln.get("description")
-            description += "\nAgent id:" + item.get("agent").get("id")
-            description += "\nAgent name:" + item.get("agent").get("name")
+            description += f"\nAgent id: {item['agent']['id']}"
+            description += f"\nAgent name: {item['agent']['name']}"
             severity = vuln.get("severity")
             cvssv3_score = vuln.get("score").get("base")
             publish_date = vuln.get("published_at").split("T")[0]
-            agent_id = item.get("agent").get("id")
             detection_time = vuln.get("detected_at").split("T")[0]
-
             references = vuln.get("reference")
 
-            title = (
-                cve + " affects (version: " + item.get("package").get("version") + ")"
-            )
-
-            dupe_key = title + agent_id + description
-            dupe_key = hashlib.sha256(dupe_key.encode("utf-8")).hexdigest()
-
-            if dupe_key in dupes:
-                find = dupes[dupe_key]
-            else:
-                dupes[dupe_key] = True
+            title = f"{cve} affects (version: {item['package']['version']})"
 
             find = Finding(
                 title=title,
@@ -48,6 +40,7 @@ class WazuhV4_8:
                 unique_id_from_tool=dupe_key,
                 date=detection_time,
             )
-            find.unsaved_vulnerability_ids = cve
+            find.unsaved_vulnerability_ids = [cve]
             dupes[dupe_key] = find
+
         return list(dupes.values())
