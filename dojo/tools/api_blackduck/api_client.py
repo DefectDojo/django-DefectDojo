aa262052d5f9e011e1d093a9cec87c6b7e56174d
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2025-09-19 15:52:19 -0600

    Update dojo/tools/api_blackduck/api_client.py

--- dojo/tools/api_blackduck/api_client.py
+++ dojo/tools/api_blackduck/api_client.py
@@ -3,7 +3,7 @@ import warnings
 # Ignore DeprecationWarning from the blackduck library because we cannot do anything about it
 # until they fix it in their library
 with warnings.catch_warnings(action="ignore", category=DeprecationWarning):
-    from blackduck import Client  # noqa: E402
+    from blackduck import Client
 
 
 class BlackduckAPI:
