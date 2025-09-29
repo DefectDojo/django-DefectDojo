57690b9bfd57c718373ad2d8465b0cd314af2562
valentijnscholten <valentijnscholten@gmail.com>
2025-09-29 19:11:40 +0200

    Import history optimize (#13182)
    * import history optimization
    
    * debugtoolbar: fix for requirements-dev.txt
    
    * import history optimization
    
    * fix fallback
    
    * add bulk existence check
    
    * add unit test file
    
    * update query counts
    
    * try without keepdb
    
    * clean database between test runs
    
    * clean database between test runs
    
    * remove keepdb
    
    * fix dojo_testdata fixture
    
    * restore entrypoint
    
    * revert testdata changes, run transactionaltest at the end
    
    * fix entrypoint
    
    * fix excludes
    
    * add comments
    
    * update counts

--- unittests/test_importers_performance.py
+++ unittests/test_importers_performance.py
@@ -176,11 +176,11 @@ class TestDojoImporterPerformance(DojoTestCase):
     # def test_import_reimport_reimport_performance_async(self, mock):
     def test_import_reimport_reimport_performance_async(self):
         self.import_reimport_performance(
-            expected_num_queries1=682,
+            expected_num_queries1=679,
             expected_num_async_tasks1=10,
-            expected_num_queries2=610,
+            expected_num_queries2=606,
             expected_num_async_tasks2=22,
-            expected_num_queries3=292,
+            expected_num_queries3=289,
             expected_num_async_tasks3=20,
         )
 
@@ -198,11 +198,11 @@ class TestDojoImporterPerformance(DojoTestCase):
         testuser.usercontactinfo.block_execution = True
         testuser.usercontactinfo.save()
         self.import_reimport_performance(
-            expected_num_queries1=682,
+            expected_num_queries1=679,
             expected_num_async_tasks1=10,
-            expected_num_queries2=615,
+            expected_num_queries2=611,
             expected_num_async_tasks2=22,
-            expected_num_queries3=297,
+            expected_num_queries3=294,
             expected_num_async_tasks3=20,
         )
 
@@ -222,10 +222,10 @@ class TestDojoImporterPerformance(DojoTestCase):
         self.system_settings(enable_product_grade=True)
 
         self.import_reimport_performance(
-            expected_num_queries1=687,
+            expected_num_queries1=684,
             expected_num_async_tasks1=15,
-            expected_num_queries2=621,
+            expected_num_queries2=617,
             expected_num_async_tasks2=28,
-            expected_num_queries3=302,
+            expected_num_queries3=299,
             expected_num_async_tasks3=25,
         )
