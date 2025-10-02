beae2f7f141aaa48963c7675d5c0b75338e7ed46
RAOUL ELYSEE <raoulelysee@gmail.com>
2025-10-02 10:43:04 -0400

    Modification du csv_format.py known_exploit

--- dojo/tools/tenable/csv_format.py
+++ dojo/tools/tenable/csv_format.py
@@ -135,7 +135,7 @@ class TenableCSVParser:
             )
 
             exploit_info = str(row.get("definition.exploitability_ease", "")).strip().lower()
-            known_exploited = "true" if exploit_info == "available" else "no"
+            known_exploited = "TRUE" if exploit_info == "available" else "FALSE"
             
             unique_id_from_tool = row.get("id")
             vuln_id_from_tool = row.get("definition.cve")            
