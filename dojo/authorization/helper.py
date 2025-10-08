0db96f76a863779fec84836a892a759c0bf17983
renejal <bjalvin@bancolombia.com.co>
2025-10-08 11:26:24 -0500

    fix: delete import not used

--- dojo/authorization/helper.py
+++ dojo/authorization/helper.py
@@ -1,11 +1,7 @@
 import crum
 from dojo.models import GeneralSettings
-from dojo.templatetags.authorization_tags import enable_button
-from dojo.authorization.roles_permissions import Permissions
-from dojo.user.queries import get_role_members
 from dojo.authorization.authorization_api import user_has_permission
 import dojo.risk_acceptance.helper as helper_ra
-import dojo.finding.helper as helper_f
 import dojo.transfer_findings.helper as helper_tf
 
 def get_permissions(obj):
@@ -18,10 +14,6 @@ def get_permissions(obj):
             permissions = [perm.name for perm in permissions]
         return validation_status_permission(obj, permissions) 
     return []
-
-def enable_permission(obj, permission, return_value=False):
-    permissions = get_permissions(obj)
-    return enable_button(permissions, permission)
 
 def validation_status_permission(finding, permissions):
     button_dict = {
