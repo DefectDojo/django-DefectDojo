92296774085f76d59a53e54b202a30c97b0dae98
Jino Tesauro <jinotesauro@gmail.com>
2025-10-29 11:15:42 -0500

    Made file changes to test in dojo pro

--- dojo/jira_link/helper.py
+++ dojo/jira_link/helper.py
@@ -783,6 +783,9 @@ def push_finding_to_jira(finding, *args, **kwargs):
 @app.task
 @dojo_model_from_id(model=Finding_Group)
 def push_finding_group_to_jira(finding_group, *args, **kwargs):
+    # Look for findings that have a single ticket associations
+    for finding in finding_group.findings.filter(jira_issue__isnull=False):
+        update_jira_issue(finding, *args, **kwargs)
     if finding_group.has_jira_issue:
         return update_jira_issue(finding_group, *args, **kwargs)
     return add_jira_issue(finding_group, *args, **kwargs)
