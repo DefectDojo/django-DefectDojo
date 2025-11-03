8819ef4144901350eeb82f5b0cca54cfa76f4e81
Paul Osinski <paul.m.osinski@gmail.com>
2025-11-03 16:13:00 -0500

    revert breaking commit

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -484,7 +484,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    Path(DOJO_ROOT).parent / "docs" / "node_modules",
+    Path(DOJO_ROOT).parent / "components" / "node_modules",
 )
 
 # List of finder classes that know how to find static files in
