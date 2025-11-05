3c41b217d027a762acfc30ef8405e04e176e1ec7
renejal <bjalvin@bancolombia.com.co>
2025-11-05 14:26:22 -0500

    fix: regex

--- dojo/api_v2/validators.py
+++ dojo/api_v2/validators.py
@@ -7,10 +7,10 @@ logger = logging.getLogger(__name__)
 
 class CharacterValidation():
     def __call__(self, value):
-        special_char_regex = GeneralSettings.get_value('REGEX_VALIDATION_NAME', '^.*$') 
+        special_char_regex = GeneralSettings.get_value("REGEX_VALIDATION_NAME", "[<>;&\\(\\)\\{\\};:\\[\\]']") 
         
         if re.search(special_char_regex, value):
-            raise ValidationError("The name cannot contain special characters like < > & ( ) { } ; : [ ] '")
+            raise ValidationError(f"The name : {value} : cannot contain special characters like < > & ( ) ; : [ ] '")
 
         return value  
 
