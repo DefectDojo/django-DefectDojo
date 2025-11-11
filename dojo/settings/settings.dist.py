459c46b92f1da3266423cbaa1dedfd1d9e19dd18
Zeke <zeketierkel@gmail.com>
2025-11-11 15:22:20 -0500

    Remove line and add url to hash code

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1354,7 +1354,7 @@ HASHCODE_FIELDS_PER_SCANNER = {
     "JFrog Xray On Demand Binary Scan": ["title", "component_name", "component_version"],
     "Scout Suite Scan": ["file_path", "vuln_id_from_tool"],  # for now we use file_path as there is no attribute for "service"
     "Meterian Scan": ["cwe", "component_name", "component_version", "description", "severity"],
-    "Github SAST Scan": ["vuln_id_from_tool", "severity", "file_path", "line"],
+    "Github SAST Scan": ["vuln_id_from_tool", "severity", "file_path", "url"], # url is the github alert url
     "Github Vulnerability Scan": ["title", "severity", "component_name", "vulnerability_ids", "file_path"],
     "Github Secrets Detection Report": ["title", "file_path", "line"],
     "Solar Appscreener Scan": ["title", "file_path", "line", "severity"],
