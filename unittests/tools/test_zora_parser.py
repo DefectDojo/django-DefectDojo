306990c7da6a90b36ef1234c290f549ffb6dc0a4
Manuel Sommer <sommermanuel@gmx.de>
2025-11-20 17:30:46 +0100

    fix

--- unittests/tools/test_zora_parser.py
+++ unittests/tools/test_zora_parser.py
@@ -1,6 +1,3 @@
-
-import csv
-
 from dojo.models import Test
 from dojo.tools.zora.parser import ZoraParser
 from unittests.dojo_test_case import DojoTestCase, get_unit_tests_scans_path
@@ -10,16 +7,16 @@ class TestZoraParser(DojoTestCase):
 
     def test_parse_file_with_no_vuln_has_no_finding(self):
         with (get_unit_tests_scans_path("zora") / "scan_empty.csv").open(encoding="utf-8") as testfile:
-            reader = csv.DictReader(testfile)
+            content = testfile.read()  # Read raw content
             parser = ZoraParser()
-            findings = parser.get_findings(Test(), reader)
+            findings = parser.get_findings(content, Test())
             self.assertEqual(0, len(findings))
 
     def test_parse_file_with_many_vuln_has_many_findings(self):
         with (get_unit_tests_scans_path("zora") / "scan_many.csv").open(encoding="utf-8") as testfile:
-            reader = csv.DictReader(testfile)
+            content = testfile.read()  # Read raw content
             parser = ZoraParser()
-            findings = parser.get_findings(Test(), reader)
+            findings = parser.get_findings(content, Test())
             self.assertEqual(198, len(findings))  # Adjust based on your test file
             # Check a specific finding for correctness
             finding = findings[10]
