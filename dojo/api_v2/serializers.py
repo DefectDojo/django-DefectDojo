5048b9d7852bf3c076ff31a5464165117f4d1251
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2025-11-20 14:27:02 -0700

    feat(serializers): allow null values for choice fields in ProductSerializer

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -2103,10 +2103,10 @@ class ProductSerializer(serializers.ModelSerializer):
     findings_count = serializers.SerializerMethodField()
     findings_list = serializers.SerializerMethodField()
 
-    business_criticality = serializers.ChoiceField(choices=Product.BUSINESS_CRITICALITY_CHOICES, allow_blank=True)
-    platform = serializers.ChoiceField(choices=Product.PLATFORM_CHOICES, allow_blank=True)
-    lifecycle = serializers.ChoiceField(choices=Product.LIFECYCLE_CHOICES, allow_blank=True)
-    origin = serializers.ChoiceField(choices=Product.ORIGIN_CHOICES, allow_blank=True)
+    business_criticality = serializers.ChoiceField(choices=Product.BUSINESS_CRITICALITY_CHOICES, allow_blank=True, allow_null=True)
+    platform = serializers.ChoiceField(choices=Product.PLATFORM_CHOICES, allow_blank=True, allow_null=True)
+    lifecycle = serializers.ChoiceField(choices=Product.LIFECYCLE_CHOICES, allow_blank=True, allow_null=True)
+    origin = serializers.ChoiceField(choices=Product.ORIGIN_CHOICES, allow_blank=True, allow_null=True)
 
     tags = TagListSerializerField(required=False)
     product_meta = ProductMetaSerializer(read_only=True, many=True)
