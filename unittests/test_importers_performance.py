dfd3c8e08b60cc72ac16c8944a79cb2ceda7fe00
Valentijn Scholten <valentijnscholten@gmail.com>
2025-12-12 23:23:51 +0100

    dedupe: optimize found_by

--- unittests/test_importers_performance.py
+++ unittests/test_importers_performance.py
@@ -288,11 +288,11 @@ class TestDojoImporterPerformance(DojoTestCase):
         self.system_settings(enable_product_grade=True)
 
         self._import_reimport_performance(
-            expected_num_queries1=313,
+            expected_num_queries1=314,
             expected_num_async_tasks1=8,
-            expected_num_queries2=288,
+            expected_num_queries2=289,
             expected_num_async_tasks2=19,
-            expected_num_queries3=177,
+            expected_num_queries3=178,
             expected_num_async_tasks3=18,
         )
 
@@ -449,9 +449,9 @@ class TestDojoImporterPerformance(DojoTestCase):
         testuser.usercontactinfo.save()
 
         self._deduplication_performance(
-            expected_num_queries1=316,
+            expected_num_queries1=317,
             expected_num_async_tasks1=7,
-            expected_num_queries2=287,
+            expected_num_queries2=282,
             expected_num_async_tasks2=7,
         )
 
@@ -469,8 +469,8 @@ class TestDojoImporterPerformance(DojoTestCase):
         testuser.usercontactinfo.save()
 
         self._deduplication_performance(
-            expected_num_queries1=280,
+            expected_num_queries1=281,
             expected_num_async_tasks1=7,
-            expected_num_queries2=250,
+            expected_num_queries2=245,
             expected_num_async_tasks2=7,
         )
