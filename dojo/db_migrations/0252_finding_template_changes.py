963876f1dd84b7070994c95542f4d0162f78576f
Valentijn Scholten <valentijnscholten@gmail.com>
2025-12-21 09:28:05 +0100

    squash migrations

--- dojo/db_migrations/0252_finding_template_changes.py
+++ dojo/db_migrations/0252_finding_template_changes.py
@@ -13,18 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='system_settings',
-            name='enable_template_match',
-        ),
-        migrations.RemoveField(
-            model_name='finding_template',
-            name='template_match',
-        ),
-        migrations.RemoveField(
-            model_name='finding_template',
-            name='template_match_title',
-        ),
         pgtrigger.migrations.RemoveTrigger(
             model_name='finding_template',
             name='insert_insert',
@@ -36,6 +24,18 @@ class Migration(migrations.Migration):
         pgtrigger.migrations.RemoveTrigger(
             model_name='finding_template',
             name='delete_delete',
+        ),
+        migrations.RemoveField(
+            model_name='system_settings',
+            name='enable_template_match',
+        ),
+        migrations.RemoveField(
+            model_name='finding_template',
+            name='template_match',
+        ),
+        migrations.RemoveField(
+            model_name='finding_template',
+            name='template_match_title',
         ),
         migrations.RemoveField(
             model_name='finding_templateevent',
@@ -196,5 +196,5 @@ class Migration(migrations.Migration):
         pgtrigger.migrations.AddTrigger(
             model_name='finding_template',
             trigger=pgtrigger.compiler.Trigger(name='delete_delete', sql=pgtrigger.compiler.UpsertTriggerSql(func='INSERT INTO "dojo_finding_templateevent" ("component_name", "component_version", "cve", "cvssv3", "cvssv3_score", "cvssv4", "cvssv4_score", "cwe", "description", "effort_for_fixing", "endpoints_text", "fix_available", "fix_version", "id", "impact", "last_used", "mitigation", "notes", "numerical_severity", "pgh_context_id", "pgh_created_at", "pgh_label", "pgh_obj_id", "planned_remediation_version", "refs", "severity", "severity_justification", "steps_to_reproduce", "title", "vulnerability_ids_text") VALUES (OLD."component_name", OLD."component_version", OLD."cve", OLD."cvssv3", OLD."cvssv3_score", OLD."cvssv4", OLD."cvssv4_score", OLD."cwe", OLD."description", OLD."effort_for_fixing", OLD."endpoints_text", OLD."fix_available", OLD."fix_version", OLD."id", OLD."impact", OLD."last_used", OLD."mitigation", OLD."notes", OLD."numerical_severity", _pgh_attach_context(), NOW(), \'delete\', OLD."id", OLD."planned_remediation_version", OLD."refs", OLD."severity", OLD."severity_justification", OLD."steps_to_reproduce", OLD."title", OLD."vulnerability_ids_text"); RETURN NULL;', hash='925844fc74c390b9d4fc446e0d3f44f556817fd4', operation='DELETE', pgid='pgtrigger_delete_delete_3f3a6', table='dojo_finding_template', when='AFTER')),
-        ),
+        )
     ]
