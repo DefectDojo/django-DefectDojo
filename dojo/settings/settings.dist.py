b331d34954bf4446542ae81732624e24f18b5710
kiblik <5609770+kiblik@users.noreply.github.com>
2026-01-26 20:05:37 +0100

    feat(async): Drop args from async_dupe_delete

--- dojo/settings/settings.dist.py
+++ dojo/settings/settings.dist.py
@@ -1241,7 +1241,6 @@ CELERY_BEAT_SCHEDULE = {
     "dedupe-delete": {
         "task": "dojo.tasks.async_dupe_delete",
         "schedule": timedelta(minutes=1),
-        "args": [timedelta(minutes=1)],
     },
     "flush_auditlog": {
         "task": "dojo.tasks.flush_auditlog",
