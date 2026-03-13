b101f03dacaf815ebbc263d24b7df1625a6df3c9
valentijnscholten <valentijnscholten@gmail.com>
2026-03-13 19:57:17 +0100

    feat: allow sorting endpoints by active findings count (#14462)
    * feat: sort endpoints by active findings count
    
    Add ability to sort the endpoint list (and host list) by the number of
    active findings, for both the UI and API, including the V3 Locations
    code path.
    
    * fix: use subquery for active_finding_count annotation to preserve queryset ordering
    
    * fix: make dojo_sort field param configurable; add active_findings to URL API filter

--- dojo/url/api/views.py
+++ dojo/url/api/views.py
@@ -1,9 +1,13 @@
-from django.db.models import QuerySet
+from django.db.models import OuterRef, QuerySet, Value
+from django.db.models.functions import Coalesce
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import DjangoModelPermissions
 
 from dojo.api_v2.permissions import IsSuperUser
 from dojo.api_v2.views import PrefetchDojoModelViewSet
+from dojo.location.models import LocationFindingReference
+from dojo.location.status import FindingLocationStatus
+from dojo.query_utils import build_count_subquery
 from dojo.url.api.filters import URLFilter
 from dojo.url.api.serializer import URLSerializer
 from dojo.url.models import URL
@@ -22,5 +26,13 @@ class URLViewSet(PrefetchDojoModelViewSet):
     lookup_url_kwarg = "pk"
 
     def get_queryset(self) -> QuerySet[URL]:
-        """Return the queryset of Vulnerabilities."""
-        return URL.objects.all()
+        active_finding_subquery = build_count_subquery(
+            LocationFindingReference.objects.filter(
+                location__url=OuterRef("pk"),
+                status=FindingLocationStatus.Active,
+            ),
+            group_field="location__url",
+        )
+        return URL.objects.annotate(
+            active_findings=Coalesce(active_finding_subquery, Value(0)),
+        )
