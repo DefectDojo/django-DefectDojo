46608e6cc236ca94f7a03430cdf2604e7990dc71
dogboat <dogboat@users.noreply.github.com>
2026-06-11 11:25:33 -0400

    comment

--- dojo/cicd_infrastructure/ui/forms.py
+++ dojo/cicd_infrastructure/ui/forms.py
@@ -11,4 +11,7 @@ class CICDInfrastructureForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if self.instance.pk:
+            # Disallow editing of the infra type on an instance; engagement CICD FKs are scoped by infrastructure_type
+            # via limit_choices_to (build_server/scm_server/orchestration), so changing the type would create a
+            # semantic conflict between an engagement and this object.
             self.fields["infrastructure_type"].disabled = True
