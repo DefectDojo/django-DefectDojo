7f177c98b5dddd565c2d9e6869bdcd9e68569db2
Cody Maffucci <46459665+Maffooch@users.noreply.github.com>
2026-07-13 09:23:06 -0600

    Update migration dependency to 0278_global_search_fts_trigram_indexes

--- dojo/db_migrations/0279_jira_project_transition_fields.py
+++ dojo/db_migrations/0279_jira_project_transition_fields.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dojo', '0277_seed_deduplication_execution_mode'),
+        ('dojo', '0278_global_search_fts_trigram_indexes'),
     ]
 
     operations = [
